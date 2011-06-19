058f2a775b7019ae362661429c17e627f878d38a
Igor Tkachev <igor.tkachev@gmail.com>
2011-06-19 01:10:26 -0400

    Working on DataTypes.

---
+++ Source/DataType.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace LinqToDB
+{
+	public enum DataType
+	{
+		/// <summary>
+		/// A fixed-length stream of non-Unicode characters ranging between 1 and 8,000 characters.
+		/// </summary>
+		Char,
+
+		/// <summary>
+		/// A variable-length stream of non-Unicode characters ranging between 1 and 8,000 characters.
+		/// Use VarChar when the database column is varchar(max).
+		/// </summary>
+		VarChar,
+
+		/// <summary>
+		/// A variable-length stream of non-Unicode data with a maximum length of 2 31 -1 (or 2,147,483,647) characters.
+		/// </summary>
+		Text,
+
+		/// <summary>
+		/// A fixed-length stream of Unicode characters ranging between 1 and 4,000 characters.
+		/// </summary>
+		NChar,
+
+		/// <summary>
+		/// A variable-length stream of Unicode characters ranging between 1 and 4,000 characters.
+		/// Implicit conversion fails if the string is greater than 4,000 characters.
+		/// </summary>
+		NVarChar,
+
+		/// <summary>
+		/// A variable-length stream of Unicode data with a maximum length of 2 30 - 1 (or 1,073,741,823) characters.
+		/// </summary>
+		NText,
+
+		/// <summary>
+		/// A fixed-length stream of binary data ranging between 1 and 8,000 bytes.
+		/// </summary>
+		Binary,
+
+		/// <summary>
+		/// A variable-length stream of binary data ranging between 1 and 8,000 bytes.
+		/// Implicit conversion fails if the byte array is greater than 8,000 bytes.
+		/// </summary>
+		VarBinary,
+
+		/// <summary>
+		/// A variable-length stream of binary data ranging from 0 to 2 31 -1 (or 2,147,483,647) bytes.
+		/// </summary>
+		Image,
+
+		/// <summary>
+		/// A simple type representing Boolean values of true or false.
+		/// </summary>
+		Boolean,
+
+		/// <summary>
+		/// A globally unique identifier (or GUID).
+		/// </summary>
+		Guid,
+
+		/// <summary>
+		/// An integral type representing signed 8-bit integers with values between -128 and 127.
+		/// </summary>
+		SByte,
+
+		/// <summary>
+		/// An integral type representing signed 16-bit integers with values between -32768 and 32767.
+		/// </summary>
+		Int16,
+
+		/// <summary>
+		/// An integral type representing signed 32-bit integers with values between -2147483648 and 2147483647.
+		/// </summary>
+		Int32,
+
+		/// <summary>
+		/// An integral type representing signed 64-bit integers with values between -9223372036854775808 and 9223372036854775807.
+		/// </summary>
+		Int64,
+
+		/// <summary>
+		/// An 8-bit unsigned integer ranging in value from 0 to 255.
+		/// </summary>
+		Byte,
+
+		/// <summary>
+		/// An integral type representing unsigned 16-bit integers with values between 0 and 65535.
+		/// </summary>
+		UInt16,
+
+		/// <summary>
+		/// An integral type representing unsigned 32-bit integers with values between 0 and 4294967295.
+		/// </summary>
+		UInt32,
+
+		/// <summary>
+		/// An integral type representing unsigned 64-bit integers with values between 0 and 18446744073709551615.
+		/// </summary>
+		UInt64,
+
+		/// <summary>
+		/// A floating point number within the range of -3.40E +38 through 3.40E +38.
+		/// </summary>
+		Single,
+
+		/// <summary>
+		/// A floating point number within the range of -1.79E +308 through 1.79E +308.
+		/// </summary>
+		Double,
+
+		/// <summary>
+		/// A simple type representing values ranging from 1.0 x 10 -28 to approximately 7.9 x 10 28 with 28-29 significant digits.
+		/// </summary>
+		Decimal,
+
+		/// <summary>
+		/// A currency value ranging from -2 63 (or -9,223,372,036,854,775,808) to 2 63 -1 (or +9,223,372,036,854,775,807)
+		/// with an accuracy to a ten-thousandth of a currency unit.
+		/// </summary>
+		Money,
+
+		/// <summary>
+		/// A currency value ranging from -214,748.3648 to +214,748.3647 with an accuracy to a ten-thousandth of a currency unit.
+		/// </summary>
+		SmallMoney,
+
+
+	/*
+	DateTime	 DateTime . Date and time data ranging in value from January 1, 1753 to December 31, 9999 to an accuracy of 3.33 milliseconds.
+	SmallDateTime	 DateTime . Date and time data ranging in value from January 1, 1900 to June 6, 2079 to an accuracy of one minute.
+	Timestamp	 Array of type Byte. Automatically generated binary numbers, which are guaranteed to be unique within a database. timestamp is used typically as a mechanism for version-stamping table rows. The storage size is 8 bytes.
+	Variant	 Object . A special data type that can contain numeric, string, binary, or date data as well as the SQL Server values Empty and Null, which is assumed if no other type is declared.
+	Xml	An XML value. Obtain the XML as a string using the GetValue method or Value property, or as an XmlReader by calling the CreateReader method.
+	Udt	A SQL Server 2005 user-defined type (UDT).
+	Structured	A special data type for specifying structured data contained in table-valued parameters.
+	Date	Date data ranging in value from January 1,1 AD through December 31, 9999 AD.
+	Time	Time data based on a 24-hour clock. Time value range is 00:00:00 through 23:59:59.9999999 with an accuracy of 100 nanoseconds. Corresponds to a SQL Server time value.
+	DateTime2	Date and time data. Date value range is from January 1,1 AD through December 31, 9999 AD. Time value range is 00:00:00 through 23:59:59.9999999 with an accuracy of 100 nanoseconds.
+	DateTimeOffset	Date and time data with time zone awareness. Date value range is from January 1,1 AD through December 31, 9999 AD. Time value range is 00:00:00 through 23:59:59.9999999 with an accuracy of 100 nanoseconds. Time zone value range is -14:00 through +14:00.
+
+	Date	A type representing a date value.
+	DateTime	A type representing a date and time value.
+	Object	A general type representing any reference or value type not explicitly represented by another DbType value.
+	Time	A type representing a SQL Server DateTime value. If you want to use a SQL Server time value, use Time.
+	VarNumeric	A variable-length numeric value.
+	Xml	A parsed representation of an XML document or fragment.
+	DateTime2	Date and time data. Date value range is from January 1,1 AD through December 31, 9999 AD. Time value range is 00:00:00 through 23:59:59.9999999 with an accuracy of 100 nanoseconds.
+	DateTimeOffset	Date and time data with time zone awareness. Date value range is from January 1,1 AD through December 31, 9999 AD. Time value range is 00:00:00 through 23:59:59.9999999 with an accuracy of 100 nanoseconds. Time zone value range is -14:00 through +14:00.		 */
+	}
+}
---
+++ Source/DataTypeExtensions.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Data;
+using System.Data.Linq;
+
+namespace LinqToDB
+{
+	public static class SqlDataType
+	{
+		public static Type ToSystemType(this DataType type)
+		{
+			switch (type)
+			{
+				case DataType.Char       :
+				case DataType.VarChar    :
+				case DataType.NChar      :
+				case DataType.NVarChar   :
+				case DataType.Text       :
+				case DataType.NText      : return typeof(string);
+
+				case DataType.Binary     :
+				case DataType.VarBinary  :
+				case DataType.Image      : return typeof(byte[]);
+
+				case DataType.Boolean    : return typeof(bool);
+				case DataType.Guid       : return typeof(Guid);
+
+				case DataType.SByte      : return typeof(sbyte);
+				case DataType.Int16      : return typeof(short);
+				case DataType.Int32      : return typeof(int);
+				case DataType.Int64      : return typeof(long);
+				case DataType.Byte       : return typeof(byte);
+				case DataType.UInt16     : return typeof(ushort);
+				case DataType.UInt32     : return typeof(uint);
+				case DataType.UInt64     : return typeof(ulong);
+
+				case DataType.Single     : return typeof(float);
+				case DataType.Double     : return typeof(double);
+				case DataType.Decimal    :
+				case DataType.Money      :
+				case DataType.SmallMoney : return typeof(decimal);
+			}
+
+			throw new InvalidOperationException();
+		}
+
+		public static DataType ToDataType(this Type type)
+		{
+			if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+				type = type.GetGenericArguments()[0];
+
+			if (type.IsEnum)
+				type = Enum.GetUnderlyingType(type);
+
+			switch (Type.GetTypeCode(type))
+			{
+				case TypeCode.String  : return DataType.NVarChar;
+				case TypeCode.Boolean : return DataType.Boolean;
+				case TypeCode.SByte   : return DataType.SByte;
+				case TypeCode.Int16   : return DataType.Int16;
+				case TypeCode.Int32   : return DataType.Int32;
+				case TypeCode.Int64   : return DataType.Int64;
+				case TypeCode.Byte    : return DataType.Byte;
+				case TypeCode.UInt16  : return DataType.UInt16;
+				case TypeCode.UInt32  : return DataType.UInt32;
+				case TypeCode.UInt64  : return DataType.UInt64;
+				case TypeCode.Single  : return DataType.Single;
+				case TypeCode.Double  : return DataType.Double;
+				case TypeCode.Decimal : return DataType.Decimal;
+			}
+
+			if (type == typeof(Guid))   return DataType.Guid;
+			if (type == typeof(byte[])) return DataType.VarBinary;
+			if (type == typeof(Binary)) return DataType.VarBinary;
+
+			throw new InvalidOperationException();
+		}
+
+		public static SqlDbType ToSqlDbType(this DataType type)
+		{
+			switch (type)
+			{
+				case DataType.Char       : return SqlDbType.Char;
+				case DataType.VarChar    : return SqlDbType.VarChar;
+				case DataType.NChar      : return SqlDbType.NChar;
+				case DataType.NVarChar   : return SqlDbType.NVarChar;
+				case DataType.Text       : return SqlDbType.Text;
+				case DataType.NText      : return SqlDbType.NText;
+
+				case DataType.Binary     : return SqlDbType.Binary;
+				case DataType.VarBinary  : return SqlDbType.VarBinary;
+				case DataType.Image      : return SqlDbType.Image;
+
+				case DataType.Boolean    : return SqlDbType.Bit;
+				case DataType.Guid       : return SqlDbType.UniqueIdentifier;
+
+				case DataType.SByte      : return SqlDbType.SmallInt;
+				case DataType.Int16      : return SqlDbType.SmallInt;
+				case DataType.Int32      : return SqlDbType.Int;
+				case DataType.Int64      : return SqlDbType.BigInt;
+				case DataType.Byte       : return SqlDbType.TinyInt;
+				case DataType.UInt16     : return SqlDbType.Int;
+				case DataType.UInt32     : return SqlDbType.BigInt;
+				case DataType.UInt64     : return SqlDbType.Decimal;
+
+				case DataType.Single     : return SqlDbType.Real;
+				case DataType.Double     : return SqlDbType.Float;
+				case DataType.Decimal    : return SqlDbType.Decimal;
+				case DataType.Money      : return SqlDbType.Money;
+				case DataType.SmallMoney : return SqlDbType.SmallMoney;
+			}
+
+			throw new InvalidOperationException();
+		}
+
+		public static DataType ToDataType(this SqlDbType type)
+		{
+			switch (type)
+			{
+				case SqlDbType.Char             : return DataType.Char;
+				case SqlDbType.VarChar          : return DataType.VarChar;
+				case SqlDbType.NChar            : return DataType.NChar;
+				case SqlDbType.NVarChar         : return DataType.NVarChar;
+				case SqlDbType.Text             : return DataType.Text;
+				case SqlDbType.NText            : return DataType.NText;
+
+				case SqlDbType.Binary           : return DataType.Binary;
+				case SqlDbType.VarBinary        : return DataType.VarBinary;
+				case SqlDbType.Image            : return DataType.Image;
+
+				case SqlDbType.Bit              : return DataType.Boolean;
+				case SqlDbType.UniqueIdentifier : return DataType.Guid;
+
+				case SqlDbType.TinyInt          : return DataType.Byte;
+				case SqlDbType.SmallInt         : return DataType.Int16;
+				case SqlDbType.Int              : return DataType.Int32;
+				case SqlDbType.BigInt           : return DataType.Int64;
+
+				case SqlDbType.Real             : return DataType.Single;
+				case SqlDbType.Float            : return DataType.Double;
+				case SqlDbType.Decimal          : return DataType.Decimal;
+				case SqlDbType.Money            : return DataType.Money;
+				case SqlDbType.SmallMoney       : return DataType.SmallMoney;
+			}
+
+			throw new InvalidOperationException();
+		}
+
+		public static DbType ToDbType(this DataType type)
+		{
+			switch (type)
+			{
+				case DataType.Char       : return DbType.AnsiStringFixedLength;
+				case DataType.VarChar    :
+				case DataType.Text       : return DbType.AnsiString;
+				case DataType.NChar      : return DbType.StringFixedLength;
+				case DataType.NVarChar   :
+				case DataType.NText      : return DbType.String;
+
+				case DataType.Binary     :
+				case DataType.VarBinary  :
+				case DataType.Image      : return DbType.Binary;
+
+				case DataType.Boolean    : return DbType.Boolean;
+				case DataType.Guid       : return DbType.Guid;
+
+				case DataType.SByte      : return DbType.SByte;
+				case DataType.Int16      : return DbType.Int16;
+				case DataType.Int32      : return DbType.Int32;
+				case DataType.Int64      : return DbType.Int64;
+				case DataType.Byte       : return DbType.Byte;
+				case DataType.UInt16     : return DbType.UInt16;
+				case DataType.UInt32     : return DbType.UInt32;
+				case DataType.UInt64     : return DbType.UInt64;
+
+				case DataType.Single     : return DbType.Single;
+				case DataType.Double     : return DbType.Double;
+				case DataType.Decimal    : return DbType.Decimal;
+				case DataType.Money      : return DbType.Currency;
+				case DataType.SmallMoney : return DbType.Currency;
+			}
+
+			throw new InvalidOperationException();
+		}
+
+		public static DataType ToDataType(this DbType type)
+		{
+			switch (type)
+			{
+				case DbType.AnsiStringFixedLength : return DataType.Char;
+				case DbType.AnsiString            : return DataType.VarChar;
+				case DbType.StringFixedLength     : return DataType.NChar;
+				case DbType.String                : return DataType.NVarChar;
+
+				case DbType.Binary                : return DataType.VarBinary;
+
+				case DbType.Boolean               : return DataType.Boolean;
+				case DbType.Guid                  : return DataType.Guid;
+
+				case DbType.SByte                 : return DataType.SByte;
+				case DbType.Int16                 : return DataType.Int16;
+				case DbType.Int32                 : return DataType.Int32;
+				case DbType.Int64                 : return DataType.Int64;
+				case DbType.Byte                  : return DataType.Byte;
+				case DbType.UInt16                : return DataType.UInt16;
+				case DbType.UInt32                : return DataType.UInt32;
+				case DbType.UInt64                : return DataType.UInt64;
+
+				case DbType.Single                : return DataType.Single;
+				case DbType.Double                : return DataType.Double;
+				case DbType.Decimal               : return DataType.Decimal;
+				case DbType.Currency              : return DataType.Money;
+			}
+
+			throw new InvalidOperationException();
+		}
+	}
+}
