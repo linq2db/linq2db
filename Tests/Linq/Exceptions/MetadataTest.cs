08e56a3d95e274cce4fba6956392ca9abb1b58df
Igor Tkachev <igor.tkachev@gmail.com>
2011-12-22 00:03:24 -0500

    Included .n project into solution.

---
+++ Tests/Linq/Exceptions/MetadataTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace Tests.Exceptions
+{
+	using LinqToDB_Temp;
+
+	[TestFixture]
+	public class MetadataTest
+	{
+		[Test, ExpectedException(typeof(LinqToDBException), ExpectedMessage = "There is no corresponding TypeCode for 'Tests.Exceptions.MetadataTest'.")]
+		public void TypeCodeTest()
+		{
+			typeof(MetadataTest).ToCode();
+		}
+	}
+}
