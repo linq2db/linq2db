8fa4d1d8f875695746d06b769b4caba225b165ac
Igor Tkachev <igor.tkachev@gmail.com>
2011-12-23 01:24:51 -0500

    Working on SqlExpr.

---
+++ Tests/Linq/Data/DataConnectionTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Data;
+
+using NUnit.Framework;
+
+using LinqToDB_Temp;
+using LinqToDB_Temp.Data;
+using LinqToDB_Temp.DataProvider;
+
+namespace Tests.Data
+{
+	[TestFixture]
+	public class DataConnectionTest
+	{
+		[Test]
+		public void Test1()
+		{
+			using (var conn = new DataConnection(new SqlServerDataProvider(), "Server=.;Database=BLToolkitData;Integrated Security=SSPI"))
+			{
+				Assert.That(conn.Connection.State,    Is.EqualTo(ConnectionState.Open));
+				Assert.That(conn.ConfigurationString, Is.Null);
+			}
+		}
+
+		[Test]
+		public void Test2()
+		{
+			using (var conn = new DataConnection())
+			{
+				Assert.That(conn.Connection.State,    Is.EqualTo(ConnectionState.Open));
+				Assert.That(conn.ConfigurationString, Is.EqualTo(DataConnection.DefaultConfiguration));
+			}
+		}
+
+		[Test]
+		public void Test3()
+		{
+			using (var conn = new DataConnection(ProviderName.SqlServer))
+			{
+				Assert.That(conn.Connection.State,    Is.EqualTo(ConnectionState.Open));
+				Assert.That(conn.ConfigurationString, Is.EqualTo(ProviderName.SqlServer));
+			}
+		}
+
+		[Test]
+		public void Test4()
+		{
+			using (var conn = new DataConnection(ProviderName.Access))
+			{
+				Assert.That(conn.Connection.State,    Is.EqualTo(ConnectionState.Open));
+				Assert.That(conn.ConfigurationString, Is.EqualTo(ProviderName.Access));
+			}
+		}
+	}
+}
