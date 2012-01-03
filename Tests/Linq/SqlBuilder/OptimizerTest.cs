a4ea8cfd0ebf13d299728376297c7d45a0fba7c0
Igor Tkachev <igor.tkachev@gmail.com>
2012-01-03 13:22:30 -0500

    Working on SqlPredicate.

--- Tests/Linq/SqlBuilder/OptimizerTest.cs
+++ Tests/Linq/SqlBuilder/OptimizerTest.cs
@@ -7,7 +7,14 @@ namespace Tests.SqlBuilder
 	[TestFixture]
 	public class OptimizerTest
 	{
+		static void TTT()
+		{
+			var n = (LinqToDB_Temp.SqlBuilder.SqlDataType)null;
+			n.Equals(null);
+		}
+
 		[Test] public void ConvertString() { Nemerle.OptimizerTest.ConvertString(); }
 		[Test] public void ConvertNumber() { Nemerle.OptimizerTest.ConvertNumber(); }
+		[Test] public void ConvertCase  () { Nemerle.OptimizerTest.ConvertCase  (); }
 	}
 }
