cc5fac916e4c0f079c7434cd1e700139a737310f
Igor Tkachev <igor.tkachev@gmail.com>
2012-01-06 13:26:55 -0500

    Working on DefaultValue.

--- Tests/Linq/Common/DefaultValueTest.cs
+++ Tests/Linq/Common/DefaultValueTest.cs
@@ -12,8 +12,6 @@ namespace Tests.Common
 		[Test]
 		public void BaseTypes()
 		{
-			//LinqToDB_Temp.SqlBuilder.SqlDataType
-
 			Assert.AreEqual(default(int),            DefaultValue<int>.           Value);
 			Assert.AreEqual(default(uint),           DefaultValue<uint>.          Value);
 			Assert.AreEqual(default(byte),           DefaultValue<byte>.          Value);
