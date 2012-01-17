6a26440aa20407c8e0ff2b3d3dfad2331c9c3655
Igor Tkachev <igor.tkachev@gmail.com>
2012-01-17 00:00:18 -0500

    Working on MappingSchema.

---
+++ Tests/Linq/SqlBuilder/SqlTableTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using LinqToDB_Temp;
+using LinqToDB_Temp.Mapping;
+using LinqToDB_Temp.SqlBuilder;
+
+using NUnit.Framework;
+
+namespace Tests.SqlBuilder
+{
+	[TestFixture]
+	public class SqlTableTest
+	{
+		[Table]
+		class Table1
+		{
+			[Column(DbType = "varchar(max)")] public string Field1 = "";
+		}
+
+		[Test]
+		public void Test1()
+		{
+			var t = new SqlTable(MappingSchema.Default, typeof(Table1));
+
+			Assert.AreEqual("VarChar(max)", t["Field1"].Type.ToString());
+		}
+	}
+}
