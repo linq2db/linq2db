2c8da74b66cd03f0c2997f8d343ba2af0adc9afe
Igor Tkachev <igor.tkachev@gmail.com>
2012-01-17 23:44:56 -0500

    Working on MappingSchema.

---
+++ Tests/Linq/SqlBuilder/TypesTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Data.SqlTypes;
+
+using LinqToDB_Temp.SqlBuilder;
+
+using NUnit.Framework;
+
+namespace Tests.SqlBuilder
+{
+	public class TypesTest
+	{
+		[Test]
+		public void SqlNullableTest()
+		{
+			var p = new SqlExpr.Parameter(new SqlDataType.Int32(typeof(SqlInt32)), "p", new SqlValue.Object(new SqlInt32(1)));
+			Assert.IsTrue(p.CanBeNull);
+
+			p = new SqlExpr.Parameter(new SqlDataType.Int32(typeof(Int32)), "p", new SqlValue.Int32(1));
+			Assert.IsFalse(p.CanBeNull);
+		}
+	}
+}
