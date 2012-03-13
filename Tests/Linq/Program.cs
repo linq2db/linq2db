fa680c2ea8fbeea7cbcacf418b0fe328d5a732da
VladD2 <vc@rsdn.ru>
2012-03-13 17:41:07 +0400

    Convert Tests project to exe.

---
+++ Tests/Linq/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+  class Program
+  {
+    static void Main()
+    {
+      var path = typeof(Tests.Create.CreateData).Assembly.Location;
+    
+      Console.WriteLine(path);
+    
+      var res = NUnit.ConsoleRunner.Runner.Main(new []{ path });
+      
+      if (res != 0)
+      {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.WriteLine("Error!");
+        Console.ReadLine();
+      }
+    }
+  }
+}
