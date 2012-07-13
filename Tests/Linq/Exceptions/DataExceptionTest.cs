5d2b3f76e092306715d9e04433b879ac57c6c821
Igor Tkachev <igor.tkachev@gmail.com>
2012-07-12 21:34:44 -0400

    New test.

---
+++ Tests/Linq/Exceptions/DataExceptionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+using LinqToDB.Data;
+using LinqToDB.DataProvider;
+
+using NUnit.Framework;
+
+namespace Tests.Exceptions
+{
+	using Model;
+
+	[TestFixture]
+	public class DataExceptionTest
+	{
+		[Test]
+		public void ParameterPrefixTest()
+		{
+			try
+			{
+				using (var db = new DbManager(new MySqlDataProvider(), "Server=DBHost;Port=3306;Database=nodatabase;Uid=bltoolkit;Pwd=TestPassword;"))
+				{
+					db.GetTable<Person>().ToList();
+				}
+			}
+			catch (DataException ex)
+			{
+				var number = ex.Number;
+			}
+		}
+	}
+}
