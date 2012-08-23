a871adeac0d3cef57583c0b01c86d8e77036d353
Igor Tkachev <igor.tkachev@gmail.com>
2012-08-23 16:33:40 -0400

    Move providers to separated folders.

---
+++ DataProviders/Firebird/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+using LinqToDB;
+
+[assembly: AssemblyTitle           (LinqToDBConstants.ProductName        + " Data Provider")]
+[assembly: AssemblyDescription     (LinqToDBConstants.ProductDescription + " Data Provider")]
+[assembly: AssemblyProduct         (LinqToDBConstants.ProductName        + " Data Provider")]
+[assembly: AssemblyCopyright       (LinqToDBConstants.Copyright)]
+[assembly: AssemblyCulture         ("")]
+[assembly: ComVisible              (false)]
+[assembly: Guid                    ("6c3fb028-daf7-4134-8697-eedfe34d779e")]
+[assembly: AssemblyVersion         ("1.0.0.0")]
+[assembly: AssemblyFileVersion     ("1.0.0.0")]
+[assembly: CLSCompliant            (true)]
+[assembly: NeutralResourcesLanguage("en-US")]
+//[assembly: AllowPartiallyTrustedCallers]
