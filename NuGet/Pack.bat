348825efeb4f393450bac7997a51a88572b4b0fe
Igor Tkachev <igor.tkachev@gmail.com>
2013-02-27 21:06:36 -0500

    Working on LinqToDB template.

---
+++ NuGet/Pack.bat
@@ -0,0 +1,13 @@
+cd ..\..\linq2db\Source
+call Compile.bat
+
+cd ..\..\t4Models\NuGet
+
+del *.nupkg
+
+copy /b AddLinq2dbTool.txt + ..\Templates\LinqToDB.ttinclude LinqToDB.ttinclude
+
+NuGet Pack linq2db.t4models.nuspec
+rem rename linq2db.t4models.*.nupkg linq2db.t4models.nupkg
+
+del LinqToDB.ttinclude
