732899905d9ddfa10f476e73f5c23fdd9f15e87b
Igor Tkachev <igor.tkachev@gmail.com>
2013-04-07 17:20:11 -0400

    Working on NuGet.

--- NuGet/Pack.bat
+++ NuGet/Pack.bat
@@ -10,4 +10,6 @@ copy /b AddLinq2dbTool.txt + ..\Templates\LinqToDB.ttinclude LinqToDB.ttinclude
 NuGet Pack linq2db.t4models.nuspec
 rem rename linq2db.t4models.*.nupkg linq2db.t4models.nupkg
 
+NuGet Pack linq2db.SqlServer.nuspec
+
 del LinqToDB.ttinclude
