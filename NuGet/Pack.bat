bbda8512ba98cc8f92cbd58d7082c37e04394507
Igor Tkachev <igor.tkachev@gmail.com>
2013-04-17 19:05:41 -0400

    Working on Firebird data provider.

--- NuGet/Pack.bat
+++ NuGet/Pack.bat
@@ -11,6 +11,7 @@ copy /b AddLinq2dbTool.txt + ..\Templates\LinqToDB.ttinclude LinqToDB.ttinclude
 rem rename linq2db.t4models.*.nupkg linq2db.t4models.nupkg
 
 ..\Redist\NuGet Pack linq2db.Access.nuspec
+rem ..\Redist\NuGet Pack linq2db.Firebird.nuspec
 ..\Redist\NuGet Pack linq2db.SqlCe.nuspec
 ..\Redist\NuGet Pack linq2db.SqlServer.nuspec
 
