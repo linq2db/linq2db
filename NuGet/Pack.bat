11e7e6eca140fd1f9f87f4ee59b8c4d5385ce40f
Igor Tkachev <igor.tkachev@gmail.com>
2013-04-19 00:19:07 -0400

    Working on SQLite nuget.

--- NuGet/Pack.bat
+++ NuGet/Pack.bat
@@ -9,6 +9,7 @@ copy /b AddTool.Linq2db.txt   + ..\Templates\LinqToDB.ttinclude           LinqTo
 copy /b AddTool.SQLite.txt    + ..\Templates\LinqToDB.SQLite.ttinclude    LinqToDB.SQLite.ttinclude
 copy /b AddTool.MySql.txt     + ..\Templates\LinqToDB.MySql.ttinclude     LinqToDB.MySql.ttinclude
 copy /b AddTool.SqlServer.txt + ..\Templates\LinqToDB.SqlServer.ttinclude LinqToDB.SqlServer.ttinclude
+copy /b AddTool.SqlCe.txt     + ..\Templates\LinqToDB.SqlCe.ttinclude     LinqToDB.SqlCe.ttinclude
 
 ..\Redist\NuGet Pack linq2db.t4models.nuspec
 rem rename linq2db.t4models.*.nupkg linq2db.t4models.nupkg
