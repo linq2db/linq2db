e76319642deee75c45552b5080025e1511d7333d
Igor Tkachev <igor.tkachev@gmail.com>
2013-05-13 10:41:51 -0400

    Code cleanup.

--- Source/DataProvider/Firebird/FirebirdSchemaProvider.cs
+++ Source/DataProvider/Firebird/FirebirdSchemaProvider.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data.Common;
 using System.Linq;
 using System.Data;
-using System.Text.RegularExpressions;
 
 namespace LinqToDB.DataProvider.Firebird
 {
