cb0340de7cec3302af623c59b48c5a82f476ba7d
Igor Tkachev <igor.tkachev@gmail.com>
2013-09-12 19:58:37 -0400

    Working on DB2 z/OS schema provider.

--- Source/DataProvider/DB2/DB2SchemaProvider.cs
+++ Source/DataProvider/DB2/DB2SchemaProvider.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
@@ -47,6 +47,8 @@ namespace LinqToDB.DataProvider.DB2
 					new DataTypeInfo { TypeName = "BIGINT",                    CreateParameters = null,               DataType = "System.Int64"    },
 					new DataTypeInfo { TypeName = "LONG VARCHAR FOR BIT DATA", CreateParameters = null,               DataType = "System.Byte[]"   },
 					new DataTypeInfo { TypeName = "VARCHAR () FOR BIT DATA",   CreateParameters = "LENGTH",           DataType = "System.Byte[]"   },
+					new DataTypeInfo { TypeName = "VARBIN",                    CreateParameters = "LENGTH",           DataType = "System.Byte[]"   },
+					new DataTypeInfo { TypeName = "BINARY",                    CreateParameters = "LENGTH",           DataType = "System.Byte[]"   },
 					new DataTypeInfo { TypeName = "CHAR () FOR BIT DATA",      CreateParameters = "LENGTH",           DataType = "System.Byte[]"   },
 					new DataTypeInfo { TypeName = "LONG VARCHAR",              CreateParameters = "LENGTH",           DataType = "System.String"   },
 					new DataTypeInfo { TypeName = "CHAR",                      CreateParameters = "LENGTH",           DataType = "System.String"   },
@@ -59,6 +61,8 @@ namespace LinqToDB.DataProvider.DB2
 					new DataTypeInfo { TypeName = "DATE",                      CreateParameters = null,               DataType = "System.DateTime" },
 					new DataTypeInfo { TypeName = "TIME",                      CreateParameters = null,               DataType = "System.TimeSpan" },
 					new DataTypeInfo { TypeName = "TIMESTAMP",                 CreateParameters = null,               DataType = "System.DateTime" },
+					new DataTypeInfo { TypeName = "TIMESTMP",                  CreateParameters = null,               DataType = "System.DateTime" },
+					new DataTypeInfo { TypeName = "ROWID",                     CreateParameters = "LENGTH",           DataType = "System.Byte[]"   },
 				};
 			}
 
@@ -87,7 +91,7 @@ namespace LinqToDB.DataProvider.DB2
 				from t in tables.AsEnumerable()
 				where
 					new[] { "TABLE", "VIEW" }.Contains(t.Field<string>("TABLE_TYPE"))
-				let catalog = t.Field<string>("TABLE_CATALOG")
+				let catalog = dataConnection.Connection.Database
 				let schema  = t.Field<string>("TABLE_SCHEMA")
 				let name    = t.Field<string>("TABLE_NAME")
 				where IncludedSchemas.Length != 0 || ExcludedSchemas.Length != 0 || schema == _currenSchema
@@ -104,16 +108,18 @@ namespace LinqToDB.DataProvider.DB2
 			).ToList();
 		}
 
+		List<PrimaryKeyInfo> _primaryKeys;
+
 		protected override List<PrimaryKeyInfo> GetPrimaryKeys(DataConnection dataConnection)
 		{
 			if (_iszOS)
 			{
-				return dataConnection.Query(
+				return _primaryKeys = dataConnection.Query(
 					rd => new PrimaryKeyInfo
 					{
-						TableID        = dataConnection.Connection.Database + "." + rd[0] + "." + rd[1],
-						PrimaryKeyName = rd.GetString(2),
-						ColumnName     = rd.GetString(3),
+						TableID        = dataConnection.Connection.Database + "." + rd.ToString(0) + "." + rd.ToString(1),
+						PrimaryKeyName = rd.ToString(2),
+						ColumnName     = rd.ToString(3),
 						Ordinal        = Converter.ChangeTypeTo<int>(rd[4])
 					},@"
 					SELECT
@@ -139,9 +145,9 @@ namespace LinqToDB.DataProvider.DB2
 				from pk in dataConnection.Query(
 					rd => new
 					{
-						id   = dataConnection.Connection.Database + "." + rd[0] + "." + rd[1],
-						name = rd.GetString(2),
-						cols = rd.GetString(3).Split('+').Skip(1).ToArray(),
+						id   = dataConnection.Connection.Database + "." + rd.ToString(0) + "." + rd.ToString(1),
+						name = rd.ToString(2),
+						cols = rd.ToString(3).Split('+').Skip(1).ToArray(),
 					},@"
 					SELECT
 						TABSCHEMA,
@@ -206,28 +212,74 @@ namespace LinqToDB.DataProvider.DB2
 				rd => new ColumnInfo
 				{
 					TableID     = dataConnection.Connection.Database + "." + rd.GetString(0) + "." + rd.GetString(1),
-					Name        = Converter.ChangeTypeTo<string>(rd[2]),
-					Length      = Converter.ChangeTypeTo<int>(   rd[3]),
-					Scale       = Converter.ChangeTypeTo<int>(   rd[4]),
-					IsNullable  = Converter.ChangeTypeTo<string>(rd[5]) == "Y",
-					IsIdentity  = Converter.ChangeTypeTo<string>(rd[6]) == "Y",
-					Ordinal     = Converter.ChangeTypeTo<int>(   rd[7]),
-					DataType    = Converter.ChangeTypeTo<string>(rd[8]),
-					Description = Converter.ChangeTypeTo<string>(rd[9]),
+					Name        = rd.ToString(2),
+					Length      = Converter.ChangeTypeTo<int>(rd[3]),
+					Scale       = Converter.ChangeTypeTo<int>(rd[4]),
+					IsNullable  = rd.ToString(5) == "Y",
+					IsIdentity  = rd.ToString(6) == "Y",
+					Ordinal     = Converter.ChangeTypeTo<int>(rd[7]),
+					DataType    = rd.ToString(8),
+					Description = rd.ToString(9),
 				},
 				sql).ToList();
 		}
 
 		protected override List<ForeingKeyInfo> GetForeignKeys(DataConnection dataConnection)
 		{
+			if (_iszOS)
+			{
+				return
+				(
+					from fk in dataConnection.Query(rd => new
+						{
+							name        = rd.ToString(0),
+							thisTable   = dataConnection.Connection.Database + "." + rd.ToString(1)  + "." + rd.ToString(2),
+							thisColumn  = rd.ToString(3),
+							ordinal     = Converter.ChangeTypeTo<int>(rd[4]),
+							otherTable  = dataConnection.Connection.Database + "." + rd.ToString(5)  + "." + rd.ToString(6),
+						},@"
+							SELECT
+								A.RELNAME,
+								A.CREATOR,
+								A.TBNAME,
+								B.COLNAME,
+								B.COLSEQ,
+								A.REFTBCREATOR,
+								A.REFTBNAME
+							FROM
+								SYSIBM.SYSRELS A
+									JOIN SYSIBM.SYSFOREIGNKEYS B ON
+										A.CREATOR = B.CREATOR AND
+										A.TBNAME  = B.TBNAME  AND
+										A.RELNAME = B.RELNAME
+							WHERE
+								" + GetSchemaFilter("A.CREATOR") + @"
+							ORDER BY
+								A.CREATOR,
+								A.RELNAME,
+								B.COLSEQ")
+					let   otherColumn = _primaryKeys.Where(pk => pk.TableID == fk.otherTable).ElementAtOrDefault(fk.ordinal - 1)
+					where otherColumn != null
+					select new ForeingKeyInfo
+					{
+						Name = fk.name,
+						ThisTableID  = fk.thisTable,
+						ThisColumn   = fk.thisColumn,
+						Ordinal      = fk.ordinal,
+						OtherTableID = fk.otherTable,
+						OtherColumn  = otherColumn.ColumnName
+					}
+				).ToList();
+			}
+
 			return dataConnection
 				.Query(rd => new
 				{
-					name         = rd.GetString(0),
-					thisTable    = dataConnection.Connection.Database + "." + rd.GetString(1)  + "." + rd.GetString(2),
-					thisColumns  = rd.GetString(3),
-					otherTable   = dataConnection.Connection.Database + "." + rd.GetString(4)  + "." + rd.GetString(5),
-					otherColumns = rd.GetString(6),
+					name         = rd.ToString(0),
+					thisTable    = dataConnection.Connection.Database + "." + rd.ToString(1)  + "." + rd.ToString(2),
+					thisColumns  = rd.ToString(3),
+					otherTable   = dataConnection.Connection.Database + "." + rd.ToString(4)  + "." + rd.ToString(5),
+					otherColumns = rd.ToString(6),
 				},@"
 					SELECT
 						CONSTNAME,
@@ -237,7 +289,10 @@ namespace LinqToDB.DataProvider.DB2
 						REFTABSCHEMA,
 						REFTABNAME,
 						PK_COLNAMES
-					FROM SYSCAT.REFERENCES")
+					FROM
+						SYSCAT.REFERENCES
+					WHERE
+						" + GetSchemaFilter("TABSCHEMA"))
 				.SelectMany(fk =>
 				{
 					var thisTable    = _columns.Where(c => c.TableID == fk.thisTable). OrderByDescending(c => c.Length).ToList();
@@ -313,29 +368,33 @@ namespace LinqToDB.DataProvider.DB2
 		{
 			switch (dataType)
 			{
-				case "XML"                       : return DataType.Xml;      // Xml             System.String
-				case "DECFLOAT"                  : return DataType.Decimal;  // DecimalFloat    System.Decimal
-				case "DBCLOB"                    : return DataType.Text;     // DbClob          System.String
-				case "CLOB"                      : return DataType.Text;     // Clob            System.String
-				case "BLOB"                      : return DataType.Blob;     // Blob            System.Byte[]
-				case "LONG VARGRAPHIC"           : return DataType.Text;     // LongVarGraphic  System.String
-				case "VARGRAPHIC"                : return DataType.Text;     // VarGraphic      System.String
-				case "GRAPHIC"                   : return DataType.Text;     // Graphic         System.String
-				case "BIGINT"                    : return DataType.Int64;    // BigInt          System.Int64
-				case "LONG VARCHAR FOR BIT DATA" : return DataType.VarBinary;// LongVarBinary   System.Byte[]
-				case "VARCHAR () FOR BIT DATA"   : return DataType.VarBinary;// VarBinary       System.Byte[]
-				case "CHAR () FOR BIT DATA"      : return DataType.Binary;   // Binary          System.Byte[]
-				case "LONG VARCHAR"              : return DataType.VarChar;  // LongVarChar     System.String
-				case "CHAR"                      : return DataType.Char;     // Char            System.String
-				case "DECIMAL"                   : return DataType.Decimal;  // Decimal         System.Decimal
-				case "INTEGER"                   : return DataType.Int32;    // Integer         System.Int32
-				case "SMALLINT"                  : return DataType.Int16;    // SmallInt        System.Int16
-				case "REAL"                      : return DataType.Single;   // Real            System.Single
-				case "DOUBLE"                    : return DataType.Double;   // Double          System.Double
-				case "VARCHAR"                   : return DataType.VarChar;  // VarChar         System.String
-				case "DATE"                      : return DataType.Date;     // Date            System.DateTime
-				case "TIME"                      : return DataType.Time;     // Time            System.TimeSpan
-				case "TIMESTAMP"                 : return DataType.Timestamp;// Timestamp       System.DateTime
+				case "XML"                       : return DataType.Xml;       // Xml             System.String
+				case "DECFLOAT"                  : return DataType.Decimal;   // DecimalFloat    System.Decimal
+				case "DBCLOB"                    : return DataType.Text;      // DbClob          System.String
+				case "CLOB"                      : return DataType.Text;      // Clob            System.String
+				case "BLOB"                      : return DataType.Blob;      // Blob            System.Byte[]
+				case "LONG VARGRAPHIC"           : return DataType.Text;      // LongVarGraphic  System.String
+				case "VARGRAPHIC"                : return DataType.Text;      // VarGraphic      System.String
+				case "GRAPHIC"                   : return DataType.Text;      // Graphic         System.String
+				case "BIGINT"                    : return DataType.Int64;     // BigInt          System.Int64
+				case "LONG VARCHAR FOR BIT DATA" : return DataType.VarBinary; // LongVarBinary   System.Byte[]
+				case "VARCHAR () FOR BIT DATA"   : return DataType.VarBinary; // VarBinary       System.Byte[]
+				case "VARBIN"                    : return DataType.VarBinary; // VarBinary       System.Byte[]
+				case "BINARY"                    : return DataType.Binary;    // Binary          System.Byte[]
+				case "CHAR () FOR BIT DATA"      : return DataType.Binary;    // Binary          System.Byte[]
+				case "LONG VARCHAR"              : return DataType.VarChar;   // LongVarChar     System.String
+				case "CHAR"                      : return DataType.Char;      // Char            System.String
+				case "DECIMAL"                   : return DataType.Decimal;   // Decimal         System.Decimal
+				case "INTEGER"                   : return DataType.Int32;     // Integer         System.Int32
+				case "SMALLINT"                  : return DataType.Int16;     // SmallInt        System.Int16
+				case "REAL"                      : return DataType.Single;    // Real            System.Single
+				case "DOUBLE"                    : return DataType.Double;    // Double          System.Double
+				case "VARCHAR"                   : return DataType.VarChar;   // VarChar         System.String
+				case "DATE"                      : return DataType.Date;      // Date            System.DateTime
+				case "TIME"                      : return DataType.Time;      // Time            System.TimeSpan
+				case "TIMESTAMP"                 : return DataType.Timestamp; // Timestamp       System.DateTime
+				case "TIMESTMP"                  : return DataType.Timestamp; // Timestamp       System.DateTime
+				case "ROWID"                     : return DataType.Undefined; // RowID           System.Byte[]
 			}
 
 			return DataType.Undefined;
@@ -369,8 +428,9 @@ namespace LinqToDB.DataProvider.DB2
 			return dataConnection
 				.Query(rd =>
 				{
-					var schema = rd.GetString(0).Trim();
-					var name   = rd.GetString(1).Trim();
+					var schema     = rd.ToString(0);
+					var name       = rd.ToString(1);
+					var isFunction = _iszOS && rd.ToString(2) == "F";
 
 					return new ProcedureInfo
 					{
@@ -378,9 +438,28 @@ namespace LinqToDB.DataProvider.DB2
 						CatalogName   = dataConnection.Connection.Database,
 						SchemaName    = schema,
 						ProcedureName = name,
+						IsFunction    = isFunction,
 					};
 				},
-				@"SELECT PROCSCHEMA, PROCNAME FROM SYSCAT.PROCEDURES")
+				_iszOS ?
+					@"
+					SELECT
+						SCHEMA,
+						NAME,
+						ROUTINETYPE
+					FROM
+						SYSIBM.SYSROUTINES
+					WHERE
+						" + GetSchemaFilter("SCHEMA")
+					:
+					@"
+					SELECT
+						PROCSCHEMA,
+						PROCNAME
+					FROM
+						SYSCAT.PROCEDURES
+					WHERE
+						" + GetSchemaFilter("PROCSCHEMA"))
 				.Where(p => IncludedSchemas.Length != 0 || ExcludedSchemas.Length != 0 || p.SchemaName == _currenSchema)
 				.ToList();
 		}
@@ -390,16 +469,16 @@ namespace LinqToDB.DataProvider.DB2
 			return dataConnection
 				.Query(rd =>
 				{
-					var schema   = rd.GetString(0).Trim();
-					var procname = rd.GetString(1).Trim();
-					var length   = ConvertTo<int>.From(rd["LENGTH"]);
-					var mode     = rd.GetString(4);
+					var schema   = rd.ToString(0);
+					var procname = rd.ToString(1);
+					var length   = ConvertTo<int>.   From(rd["LENGTH"]);
+					var mode     = ConvertTo<string>.From(rd[4]);
 
 					return new ProcedureParameterInfo
 					{
 						ProcedureID   = dataConnection.Connection.Database + "." + schema + "." + procname,
-						ParameterName = rd.GetString(2),
-						DataType      = rd.GetString(3),
+						ParameterName = rd.ToString(2),
+						DataType      = rd.ToString(3),
 						Ordinal       = ConvertTo<int>.From(rd["ORDINAL"]),
 						Length        = length,
 						Precision     = length,
@@ -408,18 +487,44 @@ namespace LinqToDB.DataProvider.DB2
 						IsOut         = mode.Contains("OUT"),
 						IsResult      = false
 					};
-				}, @"
-					SELECT
-						PROCSCHEMA,
-						PROCNAME,
-						PARMNAME,
-						TYPENAME,
-						PARM_MODE,
+				},
+					_iszOS ?
+						@"
+						SELECT
+							SCHEMA,
+							NAME,
+							PARMNAME,
+							TYPENAME,
+							CASE ROWTYPE
+								WHEN 'P' THEN 'IN'
+								WHEN 'O' THEN 'OUT'
+								WHEN 'B' THEN 'INOUT'
+								WHEN 'S' THEN 'IN'
+							END,
 
-						ORDINAL,
-						LENGTH,
-						SCALE
-					FROM SYSCAT.PROCPARMS")
+							ORDINAL,
+							LENGTH,
+							SCALE
+						FROM
+							SYSIBM.SYSPARMS
+						WHERE
+							" + GetSchemaFilter("SCHEMA")
+						:
+						@"
+						SELECT
+							PROCSCHEMA,
+							PROCNAME,
+							PARMNAME,
+							TYPENAME,
+							PARM_MODE,
+
+							ORDINAL,
+							LENGTH,
+							SCALE
+						FROM
+							SYSCAT.PROCPARMS
+						WHERE
+							" + GetSchemaFilter("PROCSCHEMA"))
 				.ToList();
 		}
 
@@ -442,10 +547,17 @@ namespace LinqToDB.DataProvider.DB2
 
 				return sql;
 			}
-			else
-			{
-				return string.Format("{0} = '{1}'", schemaNameField, _currenSchema);
-			}
+
+			return string.Format("{0} = '{1}'", schemaNameField, _currenSchema);
+		}
+	}
+
+	static class DB2Extensions
+	{
+		public static string ToString(this IDataReader reader, int i)
+		{
+			var value = Converter.ChangeTypeTo<string>(reader[i]);
+			return value == null ? null : value.TrimEnd();
 		}
 	}
 }
