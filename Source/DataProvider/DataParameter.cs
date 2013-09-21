276d1a09dce9bf80fb7611a81951655c71f23bc1
Igor Tkachev <igor.tkachev@gmail.com>
2013-09-21 19:30:46 -0400

    Working on BulkCopy.

---
+++ Source/DataProvider/DataParameter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinqToDB.DataProvider
+{
+	using System.Data;
+
+	class DataParameter : IDbDataParameter
+	{
+		public DbType             DbType        { get; set; }
+		public ParameterDirection Direction     { get; set; }
+		public bool               IsNullable    { get; private set; }
+		public string             ParameterName { get; set; }
+		public string             SourceColumn  { get; set; }
+		public DataRowVersion     SourceVersion { get; set; }
+		public object             Value         { get; set; }
+		public byte               Precision     { get; set; }
+		public byte               Scale         { get; set; }
+		public int                Size          { get; set; }
+	}
+}
