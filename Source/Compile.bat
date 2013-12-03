4d24b04cf3e6923d680f786a9c4bb10f98ca39a9
Igor Tkachev <igor.tkachev@gmail.com>
2013-12-02 21:25:07 -0500

    Removed unnecessary proj and sln files.

--- Source/Compile.bat
+++ Source/Compile.bat
@@ -1,12 +1,12 @@
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /target:Clean LinqToDB.csproj /property:Configuration=Debug
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /target:Clean LinqToDB.csproj /property:Configuration=Release
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LinqToDB.csproj /property:Configuration=Debug
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LinqToDB.csproj /property:Configuration=Release
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LinqToDB.csproj /property:Configuration=Debug   /property:OutputPath=bin\Debug.4.0\   /property:TargetFrameworkVersion=v4.0
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LinqToDB.csproj /property:Configuration=Release /property:OutputPath=bin\Release.4.0\ /property:TargetFrameworkVersion=v4.0
 
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /target:Clean LinqToDB.4.0.csproj /property:Configuration=Debug
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /target:Clean LinqToDB.4.0.csproj /property:Configuration=Release
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LinqToDB.4.0.csproj /property:Configuration=Debug
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LinqToDB.4.0.csproj /property:Configuration=Release
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /target:Clean LinqToDB.csproj /property:Configuration=Debug
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /target:Clean LinqToDB.csproj /property:Configuration=Release
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LinqToDB.csproj /property:Configuration=Debug   
+%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe LinqToDB.csproj /property:Configuration=Release 
 
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /target:Clean LinqToDB.Silverlight.4.csproj /property:Configuration=Debug
 %windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /target:Clean LinqToDB.Silverlight.4.csproj /property:Configuration=Release
