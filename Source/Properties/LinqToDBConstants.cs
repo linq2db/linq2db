8d7e9d916af21b2da8a63c673029d46b166aef77
Igor Tkachev <igor.tkachev@gmail.com>
2014-01-27 23:54:23 -0500

    Updated version.

--- Source/Properties/LinqToDBConstants.cs
+++ Source/Properties/LinqToDBConstants.cs
@@ -4,10 +4,10 @@ namespace LinqToDB
 {
 	public static class LinqToDBConstants
 	{
-		public const string FullVersionString  = "1.0.0.0";
+		public const string FullVersionString  = "1.0.2.0";
 
 		public const string ProductName        = "Linq to DB";
 		public const string ProductDescription = "Linq to DB";
-		public const string Copyright          = "\xA9 2011-2013 www.linq2db.net";
+		public const string Copyright          = "\xA9 2011-2014 www.linq2db.net";
 	}
 }
