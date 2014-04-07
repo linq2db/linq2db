68dbbbfffba7dae45bdf1c5d5326467cc90cfb07
Igor Tkachev <igor.tkachev@gmail.com>
2014-04-06 23:16:56 -0400

    Closes #42.

--- Tests/Linq/Linq/IssueTests.cs
+++ Tests/Linq/Linq/IssueTests.cs
@@ -2,6 +2,8 @@
 using System.Diagnostics;
 using System.Linq;
 
+using LinqToDB;
+
 using NUnit.Framework;
 
 using Tests.Model;
@@ -11,6 +13,8 @@ namespace Tests.Linq
 	[TestFixture]
 	public class IssueTests : TestBase
 	{
+		// https://github.com/linq2db/linq2db/issues/38
+		//
 		[Test, DataContextSource(false)]
 		public void Issue38Test(string context)
 		{
@@ -27,6 +31,29 @@ namespace Tests.Linq
 				Assert.That(sql, Is.Not.Contains("INNER JOIN"));
 
 				Debug.WriteLine(sql);
+			}
+		}
+
+		// https://github.com/linq2db/linq2db/issues/42
+		//
+		[Test, IncludeDataContextSource(ProviderName.Access)]
+		public void Issue42Test(string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var t1 = db.Types2.First();
+
+				t1.BoolValue = !t1.BoolValue;
+
+				db.Update(t1);
+
+				var t2 = db.Types2.First();
+
+				Assert.That(t2.BoolValue, Is.EqualTo(t1.BoolValue));
+
+				t1.BoolValue = !t1.BoolValue;
+
+				db.Update(t1);
 			}
 		}
 	}
