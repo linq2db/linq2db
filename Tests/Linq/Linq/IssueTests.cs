f2b33c5e28efed3394b5948affee49af10133d21
Igor Tkachev <igor.tkachev@gmail.com>
2014-04-07 21:46:10 -0400

    Fixed #60.

--- Tests/Linq/Linq/IssueTests.cs
+++ Tests/Linq/Linq/IssueTests.cs
@@ -3,6 +3,7 @@ using System.Diagnostics;
 using System.Linq;
 
 using LinqToDB;
+using LinqToDB.Data;
 
 using NUnit.Framework;
 
@@ -54,6 +55,33 @@ namespace Tests.Linq
 				t1.BoolValue = !t1.BoolValue;
 
 				db.Update(t1);
+			}
+		}
+
+		// https://github.com/linq2db/linq2db/issues/60
+		//
+		[Test, IncludeDataContextSource(
+			ProviderName.SqlServer2000,
+			ProviderName.SqlServer2005,
+			ProviderName.SqlServer2008,
+			ProviderName.SqlServer2012,
+			ProviderName.SqlCe)]
+		public void Issue60Test(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var sp       = db.DataProvider.GetSchemaProvider();
+				var dbSchema = sp.GetSchema(db);
+
+				var q =
+					from t in dbSchema.Tables
+					from c in t.Columns
+					where c.ColumnType.StartsWith("tinyint") && c.MemberType.StartsWith("sbyte")
+					select c;
+
+				var column = q.FirstOrDefault();
+
+				Assert.That(column, Is.Null);
 			}
 		}
 	}
