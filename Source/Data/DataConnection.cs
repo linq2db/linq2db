d255c4fbf8773f743b40b24a0822eaa5c91eb10c
Igor Tkachev <igor.tkachev@gmail.com>
2015-02-15 18:27:08 -0500

    Formatting.

--- Source/Data/DataConnection.cs
+++ Source/Data/DataConnection.cs
@@ -3,7 +3,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Data.Common;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
