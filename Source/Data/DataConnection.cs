739ccf0187fd450a2e128f56d058a7a5fd858c94
alexey.ermishkin <alexey.ermishkin@engine6.com>
2015-07-31 14:08:18 +0500

    Added event when query text changes

--- Source/Data/DataConnection.cs
+++ Source/Data/DataConnection.cs
@@ -523,6 +523,7 @@ namespace LinqToDB.Data
 		#region Command
 
 		public string LastQuery;
+		public event EventHandler<string> LastQueryChanged;
 
 		internal void InitCommand(CommandType commandType, string sql, DataParameter[] parameters, List<string> queryHints)
 		{
@@ -535,6 +536,9 @@ namespace LinqToDB.Data
 
 			DataProvider.InitCommand(this, commandType, sql, parameters);
 			LastQuery = Command.CommandText;
+			if (LastQueryChanged != null)
+				LastQueryChanged(this, LastQuery);
+			
 		}
 
 		private int? _commandTimeout;
