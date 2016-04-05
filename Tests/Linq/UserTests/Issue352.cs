06ade78e9f3eb45f47e0f123e6f1dcf3902ff58e
Igor Tkachev <igor.tkachev@gmail.com>
2016-04-05 13:51:06 -0400

    Test for #352.

---
+++ Tests/Linq/UserTests/Issue352.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+using NUnit.Framework;
+
+using Tests.Model;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue352 : TestBase
+	{
+
+		[Test, NorthwindDataContext]
+		public void Test(string context)
+		{
+			using (var db = new NorthwindDB())
+			{
+				var zz =
+					from e in db.Employee
+					from et in db.EmployeeTerritory
+					where et.EmployeeID == e.EmployeeID
+					group e by new { e.EmployeeID }
+					into g
+					select new
+					{
+						g.Key.EmployeeID,
+						//g.FirstOrDefault().FirstName,
+						db.Employee.FirstOrDefault(em => em.EmployeeID == g.Key.EmployeeID).FirstName,
+					};
+
+				//    zz = zz.OrderBy(a => a.FirstName);
+
+				var res = zz.ToList();
+			}
+		}
+	}
+}
