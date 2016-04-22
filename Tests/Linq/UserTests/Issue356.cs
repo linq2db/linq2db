110fbf854f014b65aaf656ec266fa01301eecc20
a.rechkalov <a.rechkalov@2gis.ru>
2016-04-22 17:37:18 +0600

    Added a test for #356

---
+++ Tests/Linq/UserTests/Issue356.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+using LinqToDB;
+
+using NUnit.Framework;
+
+namespace Tests.UserTests
+{
+    using LinqToDB.Data;
+
+    [TestFixture]
+    public class Issue356 : TestBase
+    {
+        public class Foo
+        {
+            public long Id { get; set; }
+            public string Value { get; set; }
+        }
+
+        public class Bar
+        {
+            public long Key { get; set; }
+        }
+
+        public void SetUp(string context)
+        {
+            using (var db = new DataConnection(context))
+            {
+                db.CreateTable<Foo>();
+                db.CreateTable<Bar>();
+            }
+        }
+
+        public void TearDown(string context)
+        {
+            using (var db = new DataConnection(context))
+            {
+                db.DropTable<Foo>();
+                db.DropTable<Bar>();
+            }
+        }
+
+        [Test, IncludeDataContextSource(ProviderName.SqlServer2014, ProviderName.SQLite, ProviderName.SqlCe)]
+        public void Test(string context)
+        {
+            SetUp(context);
+            try
+            {
+                using (var db = new DataConnection(context))
+                {
+                    var union = db.GetTable<Foo>()
+                        .Union(db.GetTable<Foo>())
+                        .Distinct();
+
+                    var result = db.GetTable<Bar>()
+                        .SelectMany(x => union.Where(date => date.Id == x.Key).Select(z => new {x.Key, z.Value}));
+
+                    Assert.That(() => result.Take(10).ToArray(), Throws.Nothing);
+                }
+            }
+            finally
+            {
+                TearDown(context);
+            }
+        }
+    }
+}
