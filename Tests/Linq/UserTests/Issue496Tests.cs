b51609cbd098dd1e041abf00ed5b482be65b7a07
ili <chudinia@gmail.com>
2016-12-11 03:16:48 +0500

    test fix

--- Tests/Linq/UserTests/Issue496Tests.cs
+++ Tests/Linq/UserTests/Issue496Tests.cs
@@ -14,31 +14,31 @@ namespace Tests.UserTests
 		[Table("Parent", IsColumnAttributeRequired = false)]
 		class Parent1
 		{
-			public int ParentId;
-			[Association(ThisKey = "ParentId", OtherKey = "ParentId", CanBeNull = true, IsBackReference = true)]
+			public int ParentID;
+			[Association(ThisKey = "ParentID", OtherKey = "ParentID", CanBeNull = true, IsBackReference = true)]
 			public ICollection<Child1> Children;
 		}
 
 		[Table("Child", IsColumnAttributeRequired = false)]
 		class Child1
 		{
-			public int  ChildId;
-			public int? ParentId;
+			public int  ChildID;
+			public int? ParentID;
 		}
 
 		[Table("Parent", IsColumnAttributeRequired = false)]
 		class Parent2
 		{
-			public int ParentId;
-			[Association(ThisKey = "ParentId", OtherKey = "ParentId", CanBeNull = true, IsBackReference = true)]
+			public int ParentID;
+			[Association(ThisKey = "ParentID", OtherKey = "ParentID", CanBeNull = true, IsBackReference = true)]
 			public ICollection<Child2> Children;
 		}
 
 		[Table("Child", IsColumnAttributeRequired = false)]
 		class Child2
 		{
-			public int  ChildId;
-			public long ParentId;
+			public int  ChildID;
+			public long ParentID;
 		}
 
 		public class MyInt
@@ -49,48 +49,48 @@ namespace Tests.UserTests
 		[Table("Parent", IsColumnAttributeRequired = false)]
 		class Parent3
 		{
-			public int ParentId;
-			[Association(ThisKey = "ParentId", OtherKey = "ParentId", CanBeNull = true, IsBackReference = true)]
+			public int ParentID;
+			[Association(ThisKey = "ParentID", OtherKey = "ParentID", CanBeNull = true, IsBackReference = true)]
 			public ICollection<Child3> Children;
 		}
 
 		[Table("Child", IsColumnAttributeRequired = false)]
 		class Child3
 		{
-			        public int   ChildId;
-			[Column]public MyInt ParentId;
+			        public int   ChildID;
+			[Column]public MyInt ParentID;
 		}
 
 		[Table("Parent", IsColumnAttributeRequired = false)]
 		class Parent4
 		{
 			[Column]
-			public MyInt ParentId;
-			[Association(ThisKey = "ParentId", OtherKey = "ParentId", CanBeNull = true, IsBackReference = true)]
+			public MyInt ParentID;
+			[Association(ThisKey = "ParentID", OtherKey = "ParentID", CanBeNull = true, IsBackReference = true)]
 			public ICollection<Child4> Children;
 		}
 
 		[Table("Child", IsColumnAttributeRequired = false)]
 		class Child4
 		{
-			public int ChildId;
-			public int ParentId;
+			public int ChildID;
+			public int ParentID;
 		}
 
 		[Table("Parent", IsColumnAttributeRequired = false)]
 		class Parent5
 		{
 			[Column]
-			public MyInt ParentId;
-			[Association(ThisKey = "ParentId", OtherKey = "ParentId", CanBeNull = true, IsBackReference = true)]
+			public MyInt ParentID;
+			[Association(ThisKey = "ParentID", OtherKey = "ParentID", CanBeNull = true, IsBackReference = true)]
 			public ICollection<Child5> Children;
 		}
 
 		[Table("Child", IsColumnAttributeRequired = false)]
 		class Child5
 		{
-			        public int   ChildId;
-			[Column]public MyInt ParentId;
+			        public int   ChildID;
+			[Column]public MyInt ParentID;
 		}
 
 		[Test, DataContextSource]
@@ -99,14 +99,14 @@ namespace Tests.UserTests
 			using (var db = GetDataContext(context))
 			{
 				var children = db.GetTable<Parent1>()
-					.Where(_ => _.ParentId == 1)
+					.Where(_ => _.ParentID == 1)
 					.SelectMany(_ => _.Children)
 					.ToList();
 
 				Assert.IsNotEmpty(children);
 
 				var expected = Child.Where(_ => _.ParentID == 1);
-				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildId, ParentID = _.ParentId.Value });
+				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildID, ParentID = _.ParentID.Value });
 
 				AreEqual(expected, result);
 			}
@@ -140,14 +140,14 @@ namespace Tests.UserTests
 			using (var db = GetDataContext(context))
 			{
 				var children = db.GetTable<Parent2>()
-					.Where(_ => _.ParentId == 1)
+					.Where(_ => _.ParentID == 1)
 					.SelectMany(_ => _.Children)
 					.ToList();
 
 				Assert.IsNotEmpty(children);
 
 				var expected = Child.Where(_ => _.ParentID == 1);
-				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildId, ParentID = (int)_.ParentId });
+				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildID, ParentID = (int)_.ParentID });
 
 				AreEqual(expected, result);
 			}
@@ -198,14 +198,14 @@ namespace Tests.UserTests
 			using (var db = GetDataContext(context, GetMyIntSchema()))
 			{
 				var children = db.GetTable<Parent3>()
-					.Where(_ => _.ParentId == 1)
+					.Where(_ => _.ParentID == 1)
 					.SelectMany(_ => _.Children)
 					.ToList();
 
 				Assert.IsNotEmpty(children);
 
 				var expected = Child.Where(_ => _.ParentID == 1);
-				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildId, ParentID = _.ParentId.RealValue });
+				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildID, ParentID = _.ParentID.RealValue });
 
 				AreEqual(expected, result);
 			}
@@ -217,14 +217,14 @@ namespace Tests.UserTests
 			using (var db = GetDataContext(context, GetMyIntSchema()))
 			{
 				var children = db.GetTable<Parent4>()
-					.Where(_ => _.ParentId.RealValue == 1)
+					.Where(_ => _.ParentID.RealValue == 1)
 					.SelectMany(_ => _.Children)
 					.ToList();
 
 				Assert.IsNotEmpty(children);
 
 				var expected = Child.Where(_ => _.ParentID == 1);
-				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildId, ParentID = _.ParentId });
+				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildID, ParentID = _.ParentID });
 
 				AreEqual(expected, result);
 			}
@@ -236,14 +236,14 @@ namespace Tests.UserTests
 			using (var db = GetDataContext(context, GetMyIntSchema()))
 			{
 				var children = db.GetTable<Parent5>()
-					.Where(_ => _.ParentId.RealValue == 1)
+					.Where(_ => _.ParentID.RealValue == 1)
 					.SelectMany(_ => _.Children)
 					.ToList();
 
 				Assert.IsNotEmpty(children);
 
 				var expected = Child.Where(_ => _.ParentID == 1);
-				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildId, ParentID = _.ParentId.RealValue });
+				var result = children.Select(_ => new Model.Child() { ChildID = _.ChildID, ParentID = _.ParentID.RealValue });
 
 				AreEqual(expected, result);
 			}
