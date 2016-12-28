c0a20ab5c56c396c3a960dcfe95541209abc70d6
ili <chudinia@gmail.com>
2016-12-28 14:12:40 +0500

    working on #504

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#exit if any command fails
+set -e
+
+artifactsFolder="./artifacts"
+
+if [ -d $artifactsFolder ]; then  
+  rm -R $artifactsFolder
+fi
+
+dotnet restore
+
+xbuild /p:Configuration=ReleaseMono linq2db.Mono.sln
+
+# Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
+# but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
+
+dotnet test ./Tests/Linq -c Release -f netcoreapp1.0
+
+# Instead, run directly with mono for the full .net version 
+# dotnet build ./test/TEST_PROJECT_NAME -c Release -f net451
+
+mono ./testrunner/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe Tests/Linq/bin/ReleaseMono/linq2db.Tests.dll --where "cat != WindowsOnly"
+
