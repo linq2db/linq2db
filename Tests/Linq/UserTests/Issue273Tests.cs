005793f62baa451797ce0d948a6aa20a8ea594df
jogibear9988 <jochen.kuehner@gmx.de>
2017-01-02 09:09:49 +0100

    Test for issue #273

---
+++ Tests/Linq/UserTests/Issue273Tests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using LinqToDB;
+using LinqToDB.Mapping;
+using NUnit.Framework;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue273Tests : TestBase
+	{
+		[Table("LinqDataTypes")]
+		class ContainEnumTest
+		{
+			public enum TestFieldEnum
+			{
+				Value1,
+				Value2
+			}
+
+			[PrimaryKey, Column("ID")]
+			public int Id;
+			[Column("BigIntValue")]
+			public TestFieldEnum TestField;
+		}
+
+		[Test, IncludeDataContextSource(false, ProviderName.SQLite)]
+		public void EnumInTest(string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var data = new[] { new { TestField = ContainEnumTest.TestFieldEnum.Value1 } };
+				db.GetTable<ContainEnumTest>().Where(x => data.Contains(new { TestField = x.TestField })).ToList();
+			}
+		}
+	}
+
+	
+	
+}
