c03ce060f4845fac9f0cdd583ba14975abff3b7c
Svyatoslav Danyliv <sdanyliv@gmail.com>
2017-02-03 14:56:23 +0200

    Fix for issue #461

--- Tests/Linq/Linq/IssueTests.cs
+++ Tests/Linq/Linq/IssueTests.cs
@@ -107,5 +107,38 @@ namespace Tests.Linq
 					select t);
 			}
 		}
+
+		// https://github.com/linq2db/linq2db/issues/461
+		//
+		[Test, DataContextSource()]
+		public void Issue461Test(string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var q1 = from sep in db.Parent
+					select new
+					{
+						Child =
+						(from l in db.Child
+							select new
+							{
+								Id = l.ParentID + 1
+							}).FirstOrDefault()
+					};
+				
+				var q2 = from sep in Parent
+					select new
+					{
+						Child =
+						(from l in Child
+							select new
+							{
+								Id = l.ParentID + 1
+							}).FirstOrDefault()
+					};
+
+				AreEqual(q1, q2);
+			}
+		}
 	}
 }
