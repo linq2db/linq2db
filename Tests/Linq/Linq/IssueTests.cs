5642c4e35c7329564dd5db17ca178f4999cf986d
Svyatoslav Danyliv <sdanyliv@gmail.com>
2017-02-07 16:02:28 +0200

    Possible fix for #498

--- Tests/Linq/Linq/IssueTests.cs
+++ Tests/Linq/Linq/IssueTests.cs
@@ -107,5 +107,34 @@ namespace Tests.Linq
 					select t);
 			}
 		}
+
+		// https://github.com/linq2db/linq2db/issues/498
+		//
+		[Test, DataContextSource()]
+		public void Issue498Test(string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var q = from x in db.Child
+					from y in x.GrandChildren1
+					select x.ParentID;
+
+				var r = from x in q
+					group x by x
+					into g
+					select new { g.Key, Cghildren = g.Count() };
+
+				var qq = from x in Child
+					from y in x.GrandChildren
+					select x.ParentID;
+
+				var rr = from x in qq
+					group x by x
+					into g
+					select new { g.Key, Cghildren = g.Count() };
+
+				AreEqual(rr, r);
+			}
+		}
 	}
 }
