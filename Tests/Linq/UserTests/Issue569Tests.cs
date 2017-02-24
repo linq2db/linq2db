507648856cd59cf19ea68bcc4c63cdfb55fb8358
Svyatoslav Danyliv <sdanyliv@gmail.com>
2017-02-24 13:08:43 +0200

    Added CROSS JOIN support

---
+++ Tests/Linq/UserTests/Issue569Tests.cs
@@ -0,0 +1,45 @@
+﻿namespace Tests.UserTests
+{
+	using System.Linq;
+
+	using NUnit.Framework;
+
+	[TestFixture]
+	public class Issue569Tests : TestBase
+	{
+		[Test, DataContextSource]
+		public void Test1(string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var result = from territory in db.Patient
+					from employee in db.Person
+					from eter in db.Doctor
+						.Where(x => x.PersonID == employee.ID && x.PersonID == territory.PersonID)
+						.DefaultIfEmpty()
+					where employee.FirstName.StartsWith("J")
+					select new
+					{
+						PersonId = territory.PersonID,
+						FirstName = employee.FirstName,
+						Taxonomy = eter.Taxonomy
+					};
+
+				var expected = from territory in Patient
+					from employee in Person
+					from eter in Doctor
+						.Where(x => x.PersonID == employee.ID && x.PersonID == territory.PersonID)
+						.DefaultIfEmpty()
+					where employee.FirstName.StartsWith("J")
+					select new
+					{
+						PersonId = territory.PersonID,
+						FirstName = employee.FirstName,
+						Taxonomy = eter != null ? eter.Taxonomy : null
+					};
+
+				AreEqual(expected, result);
+			}
+		}
+	}
+}
