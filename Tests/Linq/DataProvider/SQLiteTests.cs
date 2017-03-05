2630412f046669567d9cea6c7de32cd56d3af791
ili <chudinia@gmail.com>
2017-03-06 00:19:39 +0500

    core buildfix

--- Tests/Linq/DataProvider/SQLiteTests.cs
+++ Tests/Linq/DataProvider/SQLiteTests.cs
@@ -452,44 +452,5 @@ namespace Tests.DataProvider
 				}
 			}
 		}
-
-		[Test, IncludeDataContextSource(ProviderName.SQLite, TestProvName.SQLiteMs)]
-		public void SchemaProviderNormalizeName(string context)
-		{
-			using (var db = new DataConnection(ProviderName.SQLite, "Data Source=:memory:;"))
-			{
-				db.Execute(
-					@"create table Customer
-					(
-						ID int not null primary key,
-						Name nvarchar(30) not null
-					)");
-
-				db.Execute(
-					@"create table Purchase
-					(
-						ID int not null primary key,
-						CustomerID int null references Customer (ID),
-						Date datetime not null,
-						Description varchar(30) not null,
-						Price decimal not null
-					)");
-
-				db.Execute(
-					@"create table PurchaseItem
-					(
-						ID int not null primary key,
-						PurchaseID int not null references Purchase (ID),
-						Detail varchar(30) not null,
-						Price decimal not null
-					)");
-
-				var sp = db.DataProvider.GetSchemaProvider();
-				var sc = sp.GetSchema(db);
-
-				Assert.IsNotNull(sc);
-				Assert.IsEmpty(sc.Tables.SelectMany(_ => _.ForeignKeys).Where(_ => _.MemberName.Any(char.IsDigit)));
-			}
-		}
 	}
 }
