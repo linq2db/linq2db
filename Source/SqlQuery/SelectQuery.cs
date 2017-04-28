87fb4b7af3d477d38fc7d0e7969769bf9c695723
MaceWindu <dmitry.github@lukashenko.com>
2017-04-28 22:30:14 +0200

    fix issue with duplicate parameters over WCF

--- Source/SqlQuery/SelectQuery.cs
+++ Source/SqlQuery/SelectQuery.cs
@@ -3695,9 +3695,8 @@ namespace LinqToDB.SqlQuery
 								{
 									objs.Add(expr, expr);
 									p.Name = GetAlias(p.Name, "p");
+									Parameters.Add(p);
 								}
-
-								Parameters.Add(p);
 							}
 							else
 								IsParameterDependent = true;
