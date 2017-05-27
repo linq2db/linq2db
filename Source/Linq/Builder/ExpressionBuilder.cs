5eb07ff9fc976b5e6bdc0b9d9e588eb5551160e3
MaceWindu <dmitry.github@lukashenko.com>
2017-05-27 15:20:48 +0200

    store project build fix

--- Source/Linq/Builder/ExpressionBuilder.cs
+++ Source/Linq/Builder/ExpressionBuilder.cs
@@ -60,7 +60,7 @@ namespace LinqToDB.Linq.Builder
 			new ChangeTypeExpressionBuilder(),
 			new WithTableExpressionBuilder (),
 			new ContextParser              (),
-#if !SILVERLIGHT
+#if !SILVERLIGHT && !NETFX_CORE
 			new MergeContextParser         (),
 #endif
 		};
