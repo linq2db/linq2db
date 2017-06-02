b297da9d5583f7591f2a99631a3ade8b94b3eb48
ili <chudinia@gmail.com>
2017-06-02 07:26:30 +0500

    fix for 695

--- Tests/Linq/DataProvider/SqlCeTests.cs
+++ Tests/Linq/DataProvider/SqlCeTests.cs
@@ -454,5 +454,21 @@ namespace Tests.DataProvider
 				}
 			}
 		}
+
+		[Test, IncludeDataContextSource(ProviderName.SqlCe)]
+		public void Issue695Test(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var sp = db.DataProvider.GetSchemaProvider();
+				var sh = sp.GetSchema(db);
+				var t  = sh.Tables.Single(_ => _.TableName.Equals("Issue695", StringComparison.OrdinalIgnoreCase));
+
+				Assert.AreEqual(2, t.Columns.Count);
+				Assert.AreEqual(1, t.Columns.Count(_ => _.IsPrimaryKey));
+				Assert.AreEqual(1, t.ForeignKeys.Count);
+			}
+
+		}
 	}
 }
