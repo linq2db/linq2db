3ff75f09bf66e5fd08ae1e8231e907e360f5ac7a
Svyatoslav Danyliv <sdanyliv@gmail.com>
2017-06-06 16:17:31 +0300

    Corrected non working test.

--- Tests/Linq/UserTests/Issue708Tests.cs
+++ Tests/Linq/UserTests/Issue708Tests.cs
@@ -19,8 +19,8 @@ namespace Tests.UserTests
 				var parents1 =    Parent.Where(_ => _.ParentID == id).Select(_ => _.ParentID);
 				var parents2 = db.Parent.Where(_ => _.ParentID == id).Select(_ => _.ParentID);
 
-				var query1 =    Child.Where(_ => parents1.Contains(_.ChildID) && _.ChildID >= 0 && _.ChildID <= 100);
-				var query2 = db.Child.Where(_ => parents2.Contains(_.ChildID) && _.ChildID >= 0 && _.ChildID <= 100);
+				var query1 =    Child.Where(_ => parents1.Contains(_.ParentID) && _.ChildID >= 0 && _.ChildID <= 100);
+				var query2 = db.Child.Where(_ => parents2.Contains(_.ParentID) && _.ChildID >= 0 && _.ChildID <= 100);
 
 				AreEqual(query1, query2);
 			}
