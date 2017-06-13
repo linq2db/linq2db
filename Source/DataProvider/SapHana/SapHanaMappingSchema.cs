c345a4f86f5928f0fa1cb9123469c44d44118afb
MaceWindu <dmitry.github@lukashenko.com>
2017-06-14 00:24:25 +0200

    formatting

--- Source/DataProvider/SapHana/SapHanaMappingSchema.cs
+++ Source/DataProvider/SapHana/SapHanaMappingSchema.cs
@@ -16,8 +16,8 @@ namespace LinqToDB.DataProvider.SapHana
 		{
 			SetDataType(typeof(string), new SqlDataType(DataType.NVarChar, typeof(string), 255));
 
-			SetValueToSqlConverter(typeof(String), (sb, dt, v) => ConvertStringToSql(sb, v.ToString()));
-			SetValueToSqlConverter(typeof(Char), (sb, dt, v) => ConvertCharToSql(sb, (char)v));
+			SetValueToSqlConverter(typeof(string), (sb, dt, v) => ConvertStringToSql(sb, v.ToString()));
+			SetValueToSqlConverter(typeof(char),   (sb, dt, v) => ConvertCharToSql  (sb, (char)v));
 		}
 
 		static void AppendConversion(StringBuilder stringBuilder, int value)
