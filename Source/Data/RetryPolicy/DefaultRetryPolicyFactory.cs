4c975acd8511a09cd8fe081e893da0d6b1fef07a
Andrew Koryavchenko <avk@rsdn.org>
2017-06-22 14:43:01 +0300

    Revert "Fix old stuff 2"
    This reverts commit f9b83c6b4a8d7c1cb571673c151b641b35856aa5.

--- Source/Data/RetryPolicy/DefaultRetryPolicyFactory.cs
+++ Source/Data/RetryPolicy/DefaultRetryPolicyFactory.cs
@@ -2,18 +2,14 @@
 
 namespace LinqToDB.Data.RetryPolicy
 {
-#if !SILVERLIGHT
 	using DataProvider.SqlServer;
-#endif
 
 	static class DefaultRetryPolicyFactory
 	{
 		public static IRetryPolicy GetRetryPolicy(DataConnection dataContext)
 		{
-#if !SILVERLIGHT
 			if (dataContext.DataProvider is SqlServerDataProvider)
 				return new SqlServerRetryPolicy();
-#endif
 
 			return null;
 		}
