f9b83c6b4a8d7c1cb571673c151b641b35856aa5
Andrew Koryavchenko <avk@rsdn.org>
2017-06-22 14:31:49 +0300

    Fix old stuff 2

--- Source/Data/RetryPolicy/DefaultRetryPolicyFactory.cs
+++ Source/Data/RetryPolicy/DefaultRetryPolicyFactory.cs
@@ -2,14 +2,18 @@
 
 namespace LinqToDB.Data.RetryPolicy
 {
+#if !SILVERLIGHT
 	using DataProvider.SqlServer;
+#endif
 
 	static class DefaultRetryPolicyFactory
 	{
 		public static IRetryPolicy GetRetryPolicy(DataConnection dataContext)
 		{
+#if !SILVERLIGHT
 			if (dataContext.DataProvider is SqlServerDataProvider)
 				return new SqlServerRetryPolicy();
+#endif
 
 			return null;
 		}
