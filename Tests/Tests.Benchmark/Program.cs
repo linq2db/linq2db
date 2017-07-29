cbe93a4b6508251ac2f38f97adb869ec893e8c7d
Svyatoslav Danyliv <sdanyliv@gmail.com>
2017-07-29 15:27:38 +0300

    Added Benchmark project (#739)

---
+++ Tests/Tests.Benchmark/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Running;
+
+namespace Tests.Benchmark
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (Debugger.IsAttached)
+			{
+				QueryTests.VisitAllBigPredicateNonRecursive();
+				Console.WriteLine("Finished...");
+				Console.ReadLine();
+			}
+			else
+			{
+				var summary = BenchmarkRunner.Run<QueryTests>();
+			}
+		}
+	}
+}
