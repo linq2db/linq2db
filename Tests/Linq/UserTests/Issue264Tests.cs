1d0ff9362c485b2cf29e9cc2e72f94931cb8efd3
MaceWindu <dmitry.github@lukashenko.com>
2017-08-01 18:17:30 +0200

    [ci skip] test with workaround for #264

--- Tests/Linq/UserTests/Issue264Tests.cs
+++ Tests/Linq/UserTests/Issue264Tests.cs
@@ -34,6 +34,27 @@ namespace Tests.UserTests
 			}
 		}
 
+		[IncludeDataContextSource(false, ProviderName.SqlServer2005, ProviderName.SqlServer2008, ProviderName.SqlServer2012, ProviderName.SqlServer2014)]
+		public void TestWorkaround(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<LinqDataTypes>()
+					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
+					// select required due to #781, remove when fixed
+					.Select(_ => _.Key)
+					.Count();
+
+				Assert.AreEqual(4, cnt);
+
+				var values = db.GetTable<LinqDataTypes>()
+					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
+					.Select(_ => new { _.Key.month, _.Key.year }).ToList();
+
+				Assert.AreEqual(4, values.Count);
+			}
+		}
+
 		[Sql.Expression("MONTH({0})", ServerSideOnly = true)]
 		public static int ByMonth(DateTime date)
 		{
