156afbab33a920b80508a91b7422bc93f9fc1260
MaceWindu <dmitry.github@lukashenko.com>
2017-08-01 18:10:29 +0200

    Issue 781 and 264 tests

---
+++ Tests/Linq/UserTests/Issue264Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+using LinqToDB;
+using LinqToDB.Data;
+
+using NUnit.Framework;
+
+using Tests.Model;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue264Tests : TestBase
+	{
+		[IncludeDataContextSource(false, ProviderName.SqlServer2005, ProviderName.SqlServer2008, ProviderName.SqlServer2012, ProviderName.SqlServer2014)]
+		public void Test(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<LinqDataTypes>()
+					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
+					// select required due to #781, remove when fixed
+					.Select(_ => _.Key)
+					.Count();
+
+				Assert.AreEqual(4, cnt);
+
+				var values = db.GetTable<LinqDataTypes>()
+					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
+					.Select(_ => _.Key).ToList();
+
+				Assert.AreEqual(4, values.Count);
+			}
+		}
+
+		[Sql.Expression("MONTH({0})", ServerSideOnly = true)]
+		public static int ByMonth(DateTime date)
+		{
+			return date.Month;
+		}
+
+		[Sql.Expression("YEAR({0})", ServerSideOnly = true)]
+		public static int ByYear(DateTime date)
+		{
+			return date.Year;
+		}
+	}
+}
---
+++ Tests/Linq/UserTests/Issue781Tests.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Linq;
+
+using LinqToDB;
+using LinqToDB.Data;
+
+using NUnit.Framework;
+
+using Tests.Model;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue781Tests : TestBase
+	{
+		[DataContextSource(false)]
+		public void TestCount(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<Person>()
+					.GroupBy(_ => Sql.Concat("test", _.Patient.Diagnosis))
+					.Count();
+
+				Assert.AreEqual(2, cnt);
+				Assert.True(db.LastQuery.IndexOf("COUNT", StringComparison.InvariantCultureIgnoreCase) != -1);
+			}
+		}
+
+		[DataContextSource(false)]
+		public void TestLongCount(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<Person>()
+					.GroupBy(_ => "test" + _.Patient.Diagnosis)
+					.LongCount();
+
+				Assert.AreEqual(2, cnt);
+				Assert.True(db.LastQuery.IndexOf("COUNT", StringComparison.InvariantCultureIgnoreCase) != -1);
+			}
+		}
+
+		[DataContextSource(false)]
+		public void TestHavingCount(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<Person>()
+					.GroupBy(_ => "test" + _.Patient.Diagnosis)
+					.Having(_ => _.Key != null)
+					.Count();
+
+				Assert.AreEqual(1, cnt);
+				Assert.True(db.LastQuery.IndexOf("COUNT", StringComparison.InvariantCultureIgnoreCase) != -1);
+			}
+		}
+
+		[DataContextSource(false)]
+		public void TestHavingLongCount(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<Person>()
+					.GroupBy(_ => "test" + _.Patient.Diagnosis)
+					.Having(_ => _.Key != null)
+					.LongCount();
+
+				Assert.AreEqual(1, cnt);
+				Assert.True(db.LastQuery.IndexOf("COUNT", StringComparison.InvariantCultureIgnoreCase) != -1);
+			}
+		}
+
+		[DataContextSource(false)]
+		public void TestCountWithSelect(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<Person>().GroupBy(_ => "test" + _.Patient.Diagnosis).Select(_ => _.Key).Count();
+
+				Assert.AreEqual(2, cnt);
+				Assert.True(db.LastQuery.IndexOf("COUNT", StringComparison.InvariantCultureIgnoreCase) != -1);
+			}
+		}
+
+		[DataContextSource(false)]
+		public void TestLongCountWithSelect(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<Person>().GroupBy(_ => "test" + _.Patient.Diagnosis).Select(_ => _.Key).LongCount();
+
+				Assert.AreEqual(2, cnt);
+				Assert.True(db.LastQuery.IndexOf("COUNT", StringComparison.InvariantCultureIgnoreCase) != -1);
+			}
+		}
+
+		[DataContextSource(false)]
+		public void TestHavingCountWithSelect(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<Person>()
+					.GroupBy(_ => "test" + _.Patient.Diagnosis)
+					.Having(_ => _.Key != null)
+					.Select(_ => _.Key)
+					.Count();
+
+				Assert.AreEqual(1, cnt);
+				Assert.True(db.LastQuery.IndexOf("COUNT", StringComparison.InvariantCultureIgnoreCase) != -1);
+			}
+		}
+
+		[DataContextSource(false)]
+		public void TestHavingLongCountWithSelect(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var cnt = db.GetTable<Person>()
+					.GroupBy(_ => "test" + _.Patient.Diagnosis)
+					.Having(_ => _.Key != null)
+					.Select(_ => _.Key)
+					.LongCount();
+
+				Assert.AreEqual(1, cnt);
+				Assert.True(db.LastQuery.IndexOf("COUNT", StringComparison.InvariantCultureIgnoreCase) != -1);
+			}
+		}
+	}
+}
