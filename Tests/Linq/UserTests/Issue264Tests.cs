86376223b627048ff34b57856a70cd448140b4fa
Svyatoslav Danyliv <sdanyliv@gmail.com>
2017-08-02 14:06:39 +0300

    Corrected tests for #264

--- Tests/Linq/UserTests/Issue264Tests.cs
+++ Tests/Linq/UserTests/Issue264Tests.cs
@@ -13,45 +13,57 @@ namespace Tests.UserTests
 	[TestFixture]
 	public class Issue264Tests : TestBase
 	{
-		[IncludeDataContextSource(false, ProviderName.SqlServer2005, ProviderName.SqlServer2008, ProviderName.SqlServer2012, ProviderName.SqlServer2014)]
+		[Test, IncludeDataContextSource(false, ProviderName.SqlServer2005, ProviderName.SqlServer2008, ProviderName.SqlServer2012, ProviderName.SqlServer2014)]
 		public void Test(string context)
 		{
 			using (var db = new DataConnection(context))
 			{
-				var cnt = db.GetTable<LinqDataTypes>()
+				var actualCount = db.GetTable<LinqDataTypes>()
 					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
-					// select required due to #781, remove when fixed
-					.Select(_ => _.Key)
 					.Count();
 
-				Assert.AreEqual(4, cnt);
+				var expectedCount = Types
+					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
+					.Count();
 
-				var values = db.GetTable<LinqDataTypes>()
+				Assert.AreEqual(expectedCount, actualCount);
+
+				var actual = db.GetTable<LinqDataTypes>()
 					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
 					.Select(_ => _.Key).ToList();
 
-				Assert.AreEqual(4, values.Count);
+				var expected = Types
+					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
+					.Select(_ => _.Key).ToList();
+
+				AreEqual(expected, actual);
 			}
 		}
 
-		[IncludeDataContextSource(false, ProviderName.SqlServer2005, ProviderName.SqlServer2008, ProviderName.SqlServer2012, ProviderName.SqlServer2014)]
+		[Test, IncludeDataContextSource(false, ProviderName.SqlServer2005, ProviderName.SqlServer2008, ProviderName.SqlServer2012, ProviderName.SqlServer2014)]
 		public void TestWorkaround(string context)
 		{
 			using (var db = new DataConnection(context))
 			{
-				var cnt = db.GetTable<LinqDataTypes>()
+				var actualCount = db.GetTable<LinqDataTypes>()
 					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
-					// select required due to #781, remove when fixed
-					.Select(_ => _.Key)
 					.Count();
 
-				Assert.AreEqual(4, cnt);
+				var expectedCount = db.GetTable<LinqDataTypes>()
+					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
+					.Count();
 
-				var values = db.GetTable<LinqDataTypes>()
+				Assert.AreEqual(expectedCount, actualCount);
+
+				var actual = db.GetTable<LinqDataTypes>()
 					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
 					.Select(_ => new { _.Key.month, _.Key.year }).ToList();
 
-				Assert.AreEqual(4, values.Count);
+				var expected = Types
+					.GroupBy(_ => new { month = ByMonth(_.DateTimeValue), year = ByYear(_.DateTimeValue) })
+					.Select(_ => new { _.Key.month, _.Key.year }).ToList();
+
+				AreEqual(expected, actual);
 			}
 		}
 
