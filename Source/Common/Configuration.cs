362612a97c9f81a615a6efa493ff49bd03633407
MaceWindu <dmitry.github@lukashenko.com>
2017-08-05 17:44:43 +0200

    Configuration docs

--- Source/Common/Configuration.cs
+++ Source/Common/Configuration.cs
@@ -9,24 +9,92 @@ namespace LinqToDB.Common
 	using Data.RetryPolicy;
 #endif
 
+	/// <summary>
+	/// Contains global linq2db settings.
+	/// </summary>
 	[PublicAPI]
 	public static class Configuration
 	{
+		/// <summary>
+		/// If <c>true</c> - non-primitive and non-enum value types (structures) will be treated as scalar types (e.g. <see cref="DateTime"/>) during mapping;
+		/// otherwise they will be treated the same way as classes.
+		/// Default value: <c>true</c>.
+		/// </summary>
 		public static bool IsStructIsScalarType = true;
+
+		/// <summary>
+		/// If <c>true</c> - data providers will try to use standard ADO.NET interfaces instead of provider-specific functionality when possible. This option could be usefull if you need to intercept
+		/// database calls using tools like <see cref="https://github.com/MiniProfiler/dotnet">MiniProfiler</see>.
+		/// Default value: <c>false</c>.
+		/// </summary>
 		public static bool AvoidSpecificDataProviderAPI;
 
+		/// <summary>
+		/// LINQ queries settings.
+		/// </summary>
+		[PublicAPI]
 		public static class Linq
 		{
+			/// <summary>
+			/// Controls when group data for LINQ queries, that end with GroupBy will be loaded:
+			/// - if <c>true</c> - group data will be loaded together with main query, resulting in 1 + N queries, where N - number of groups;
+			/// - if <c>false</c> - group data will be loaded when you call enumerator for specific group <see cref="System.Linq.IGrouping{TKey, TElement}"/>.
+			/// Default value: <c>false</c>.
+			/// </summary>
 			public static bool PreloadGroups;
+
+			/// <summary>
+			/// Controls behavior of linq2db when there is no updateable fields in Update query:
+			/// - if <c>true</c> - query not executed and Update operation returns 0 as number of affected records;
+			/// - if <c>false</c> - <see cref="LinqToDB.Linq.LinqException"/> will be thrown.
+			/// Default value: <c>false</c>.
+			/// </summary>
 			public static bool IgnoreEmptyUpdate;
+
+			/// <summary>
+			/// Controls behavior of linq2db when multiple queries required to load requested data:
+			/// - if <c>true</c> - multiple queries allowed;
+			/// - if <c>false</c> - <see cref="LinqToDB.Linq.LinqException"/> will be thrown.
+			/// This option required, if you want to select related collections, e.g. using <see cref="LinqExtensions.LoadWith{T}(ITable{T}, System.Linq.Expressions.Expression{Func{T, object}})"/> method.
+			/// Default value: <c>false</c>.
+			/// </summary>
 			public static bool AllowMultipleQuery;
+
+			/// <summary>
+			/// Enables generation of test class for each LINQ query, executed while this option is enabled.
+			/// This option could be usefull for issue reporting, when you need to provide reproduceable case.
+			/// Test file will be placed to <c>linq2db</c> subfolder of temp folder and exact file path will be logged
+			/// to data connection tracing infrastructure.
+			/// See <see cref="DataConnection.TraceSwitch"/> for more details.
+			/// Default value: <c>false</c>.
+			/// </summary>
 			public static bool GenerateExpressionTest;
+
+			/// <summary>
+			/// Enables logging of generated mapping expression to data connection tracing infrastructure.
+			/// See <see cref="DataConnection.TraceSwitch"/> for more details.
+			/// Default value: <c>false</c>.
+			/// </summary>
 			public static bool TraceMapperExpression;
+
+			/// <summary>
+			/// Controls behavior, when LINQ query chain contains multiple <see cref="System.Linq.Queryable.OrderBy()"/> or <see cref="System.Linq.Queryable.OrderByDescending()"/> calls:
+			/// - if <c>true</c> - non-first OrderBy* call will be treated as ThenBy* call;
+			/// - if <c>false</c> - OrdredBy* call will discard sort specifications, added by previous OrderBy* and ThenBy* calls.
+			/// Default value: <c>false</c>.
+			/// </summary>
 			public static bool DoNotClearOrderBys;
+
+			/// <summary>
+			/// If enabled, linq2db will try to reduce number of generated SQL JOINs for LINQ query.
+			/// TODO: add more details on what kind of optimizations performed.
+			/// Default value: <c>true</c>.
+			/// </summary>
 			public static bool OptimizeJoins = true;
 
 			/// <summary>
-			/// If set to true nullable fields would be checked for IS NULL when comparasion type is NotEqual 
+			/// If set to true nullable fields would be checked for IS NULL when comparasion type is NotEqual.
+			/// Default value: <c>true</c>.
 			/// <example>
 			/// public class MyEntity
 			/// {
@@ -43,70 +111,108 @@ namespace LinqToDB.Common
 			public static bool CheckNullForNotEquals = true;
 
 			/// <summary>
-			/// Prevents to use constructions like q.GroupBy(_ => _.SomeValue) which leads to unexpected behaviour.
+			/// Controls behavior of LINQ query, which ends with GroupBy call.
+			/// - if <c>true</c> - <seealso cref="LinqToDBException"/> will be thrown for such queries;
+			/// - if <c>false</c> - behavior is controlled by <see cref="PreloadGroups"/> option.
+			/// Default value: <c>false</c>.
 			/// </summary>
 			/// <remarks>
 			/// https://github.com/linq2db/linq2db/issues/365
 			/// </remarks>
-			public static bool GuardGrouping = false;
+			public static bool GuardGrouping;
 
 			/// <summary>
-			/// Experimental
-			/// Used to optimize big logical operations with great number of operands like expr1.and.axpr2...and.exprN into to one <see cref="LinqToDB.Expressions.BinaryAggregateExpression"/>.
-			/// This saves from deep recursion in visitors.
+			/// Experimental feature.
+			/// Used to optimize huge logical operations with large number of operands like expr1.and.axpr2...and.exprN into to one <see cref="LinqToDB.Expressions.BinaryAggregateExpression"/>.
+			/// Without this option, such conditions could lead to <seealso cref="StackOverflowException"/>.
+			/// Default value: <c>false</c>.
 			/// <remarks>
-			/// Default: <value>false</value>
-			/// Switched off in 1.8.2 as unstable
 			/// </remarks>
 			/// </summary>
 			/// <remarks>
-			/// See: 
+			/// Switched off in 1.8.2 due to instability (<seealso cref="https://github.com/linq2db/linq2db/issues/708"/>).
+			/// See:
 			/// https://github.com/linq2db/linq2db/issues/447
 			/// https://github.com/linq2db/linq2db/pull/563
 			/// </remarks>
-			public static bool UseBinaryAggregateExpression = false;
+			public static bool UseBinaryAggregateExpression;
 		}
 
+		/// <summary>
+		/// Linq over WCF global settings.
+		/// </summary>
 		[PublicAPI]
 		public static class LinqService
 		{
+			/// <summary>
+			/// Controls format of type name, sent over WCF:
+			/// - if <c>true</c> - name from <see cref="Type.AssemblyQualifiedName"/> used;
+			/// - if <c>false</c> - name from <see cref="Type.FullName"/> used.
+			/// Default value: <c>false</c>.
+			/// </summary>
 			public static bool SerializeAssemblyQualifiedName;
+
+			/// <summary>
+			/// Controls behavior of linq2db, when it cannot load <see cref="Type"/> by type name on query deserialization:
+			/// - if <c>true</c> - <see cref="LinqToDBException"/> will be thrown;
+			/// - if <c>false</c> - type load error will be ignored.
+			/// Default value: <c>false</c>.
+			/// </summary>
 			public static bool ThrowUnresolvedTypeException;
 		}
 
 #if !SILVERLIGHT && !WINSTORE
+		/// <summary>
+		/// Retry policy global settings.
+		/// </summary>
+		[PublicAPI]
 		public static class RetryPolicy
 		{
+			/// <summary>
+			/// Retry policy factory method, used to create retry policy for new <see cref="DataConnection"/> instance.
+			/// If factory method is not set, retry policy is not used.
+			/// Not set by default.
+			/// </summary>
 			public static Func<DataConnection,IRetryPolicy> Factory;
 
+			/// <summary>
+			/// Status of use of default retry policy.
+			/// Getter returns <c>true</c> if default retry policy used, and false if custom retry policy used or retry policy is not set.
+			/// Setter sets <see cref="Factory"/> to default retry policy factory if <paramref name="value"/> is <c>true</c>, otherwise removes retry policy.
+			/// </summary>
 			public static bool UseDefaultPolicy
 			{
-				get { return Factory != null; }
+				get { return Factory == DefaultRetryPolicyFactory.GetRetryPolicy; }
 				set { Factory = value ? DefaultRetryPolicyFactory.GetRetryPolicy : (Func<DataConnection,IRetryPolicy>)null; }
 			}
 
 			/// <summary>
 			/// The default number of retry attempts.
+			/// Default value: <c>5</c>.
 			/// </summary>
 			public static int DefaultMaxRetryCount = 5;
 
 			/// <summary>
 			/// The default maximum time delay between retries, must be nonnegative.
+			/// /// Default value: 30 seconds.
 			/// </summary>
 			public static TimeSpan DefaultMaxDelay = TimeSpan.FromSeconds(30);
 
 			/// <summary>
 			/// The default maximum random factor, must not be lesser than 1.
+			/// Default value: <c>1.1</c>.
 			/// </summary>
 			public static double DefaultRandomFactor = 1.1;
 
 			/// <summary>
 			/// The default base for the exponential function used to compute the delay between retries, must be positive.
+			/// Default value: <c>2</c>.
 			/// </summary>
 			public static double DefaultExponentialBase = 2;
 
 			/// <summary>
 			/// The default coefficient for the exponential function used to compute the delay between retries, must be nonnegative.
+			/// Default value: 1 second.
 			/// </summary>
 			public static TimeSpan DefaultCoefficient = TimeSpan.FromSeconds(1);
 		}
