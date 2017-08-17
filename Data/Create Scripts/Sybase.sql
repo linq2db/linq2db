98cb04a646b25dc4662d8771d00525f167575066
MaceWindu <dmitry.github@lukashenko.com>
2017-08-17 22:32:48 +0200

    test

--- Data/Create Scripts/Sybase.sql
+++ Data/Create Scripts/Sybase.sql
@@ -281,3 +281,10 @@ CREATE TABLE TestMerge2
 
 	CONSTRAINT PK_TestMerge2 PRIMARY KEY CLUSTERED (Id)
 )
+
+GO
+
+CREATE OR REPLACE PROCEDURE AddIssue792Record AS
+	INSERT INTO dbo.AllTypes(char20DataType, bitDataType) VALUES('issue792', 1)
+	SELECT * FROM dbo.AllTypes
+RETURN
---
+++ Tests/Linq/UserTests/Issue792Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+using LinqToDB;
+using LinqToDB.Data;
+
+using NUnit.Framework;
+
+using Tests.Model;
+using LinqToDB.SchemaProvider;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue792Tests : TestBase
+	{
+		public class AllTypes
+		{
+			public string char20DataType;
+		}
+
+		[Test, IncludeDataContextSource(false, ProviderName.Sybase)]
+		[Explicit("https://github.com/linq2db/linq2db/issues/792")]
+		public void Test(string context)
+		{
+			using (var db = new DataConnection(context))
+			{
+				var recordsBefore = db.GetTable<AllTypes>().Count();
+
+				var sp = db.DataProvider.GetSchemaProvider();
+
+				// on fail procedure AddIssue792Record will add 1 record to AllTypes
+				var schema = sp.GetSchema(db, new GetSchemaOptions()
+				{
+					GetTables = false
+				});
+
+				var recordsAfter = db.GetTable<AllTypes>().Count();
+
+				// cleanup
+				db.GetTable<AllTypes>().Delete(_ => _.char20DataType == "issue792");
+
+				Assert.AreEqual(recordsBefore, recordsAfter);
+			}
+		}
+	}
+}
