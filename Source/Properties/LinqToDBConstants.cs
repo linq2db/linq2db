1c9bbb646c6534b9c53ff77fd4144ce428ac6b55
Ilya Chudin <chudinia@gmail.com>
2017-08-17 18:54:04 +0500

    #803 versions

--- Source/Properties/LinqToDBConstants.cs
+++ Source/Properties/LinqToDBConstants.cs
@@ -4,10 +4,10 @@ namespace LinqToDB
 {
 	public static class LinqToDBConstants
 	{
-		public const string FullVersionString  = "1.8.4.0";
+		public const string FullVersionString  = "1.9.0.0";
 
 		public const string ProductName        = "Linq to DB";
 		public const string ProductDescription = "Linq to DB";
-		public const string Copyright          = "\xA9 2011-2016 blog.linq2db.com";
+		public const string Copyright          = "\xA9 2011-2017 blog.linq2db.com";
 	}
 }
