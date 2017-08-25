8e6b5ea36f91b6715787bccce5d7a3fc323feb12
MaceWindu <dmitry.github@lukashenko.com>
2017-08-25 09:56:48 +0200

    regression test

---
+++ Tests/Linq/UserTests/Issue825Tests.cs
@@ -0,0 +1,37 @@
+﻿using LinqToDB;
+using LinqToDB.Common;
+using LinqToDB.Data;
+using LinqToDB.Linq;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using Tests.Model;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue825Tests : TestBase
+	{
+		[Test, DataContextSource]
+		public void TestCount(string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var grandChildId = 322;
+				var childId = 32;
+
+				var query = db.Parent
+					.Where(p => p.GrandChildren.Any(g => g.GrandChildID == grandChildId))
+					.SelectMany(parent => parent.Children)
+					.Where(child => child.ChildID == childId)
+					.Select(child => child.Parent);
+
+				var result = query.ToList();
+
+				Assert.AreEqual(1, result.Count);
+				Assert.AreEqual(3, result[0].ParentID);
+
+			}
+		}
+	}
+}
