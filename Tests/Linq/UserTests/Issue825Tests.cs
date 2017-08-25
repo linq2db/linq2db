02769198256a0e780a934e988dc870cafe0602d4
MaceWindu <dmitry.github@lukashenko.com>
2017-08-25 12:08:07 +0200

    update test

--- Tests/Linq/UserTests/Issue825Tests.cs
+++ Tests/Linq/UserTests/Issue825Tests.cs
@@ -2,8 +2,10 @@
 using LinqToDB.Common;
 using LinqToDB.Data;
 using LinqToDB.Linq;
+using LinqToDB.Mapping;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Tests.Model;
 
@@ -12,25 +14,74 @@ namespace Tests.UserTests
 	[TestFixture]
 	public class Issue825Tests : TestBase
 	{
+		[Table(Name = "Child")]
+		public class Child825
+		{
+			[PrimaryKey, Identity, Column("ChildID")]
+			public int Id { get; set; }
+
+			[Column("ParentID"), NotNull]
+			public int ParentId { get; set; }
+
+			[Association(ThisKey = "ParentId", OtherKey = "Id", CanBeNull = false)]
+			public Parent825 Parent { get; set; }
+		}
+
+		[Table(Name = "Parent")]
+		public class Parent825
+		{
+			[PrimaryKey, Identity, Column("ParentID")]
+			public int Id { get; set; }
+
+			[Association(ThisKey = "Id", OtherKey = "ParentId", CanBeNull = true, IsBackReference = true)]
+			public IList<ParentPermission> ParentPermissions { get; set; }
+
+			[Association(ThisKey = "Id", OtherKey = "ParentId", CanBeNull = true, IsBackReference = true)]
+			public IList<Child825> Childs { get; set; }
+		}
+
+		[Table(Name = "GrandChild")]
+		public class ParentPermission
+		{
+			[PrimaryKey, Identity, Column("GrandChildID")]
+			public int Id { get; set; }
+
+			[Column("ParentID"), NotNull]
+			public int ParentId { get; set; }
+
+			[Column("ChildID"), NotNull]
+			public int UserId { get; set; }
+		}
+
 		[Test, DataContextSource]
-		public void TestCount(string context)
+		public void Test(string context)
 		{
 			using (var db = GetDataContext(context))
 			{
-				var grandChildId = 322;
+				var userId = 32;
 				var childId = 32;
 
-				var query = db.Parent
-					.Where(p => p.GrandChildren.Any(g => g.GrandChildID == grandChildId))
-					.SelectMany(parent => parent.Children)
-					.Where(child => child.ChildID == childId)
-					.Select(child => child.Parent);
+				var old = Configuration.Linq.AllowMultipleQuery;
 
-				var result = query.ToList();
+				try
+				{
+					Configuration.Linq.AllowMultipleQuery = true;
 
-				Assert.AreEqual(1, result.Count);
-				Assert.AreEqual(3, result[0].ParentID);
+					var query = db.GetTable<Parent825>()
+						.Where(p => p.ParentPermissions.Any(permission => permission.UserId == userId))
+						.SelectMany(parent => parent.Childs)
+						.Where(child => child.Id == childId)
+						.Select(child => child.Parent);
 
+					var result = query.ToList();
+
+					Assert.AreEqual(1, result.Count);
+					Assert.AreEqual(3, result[0].Id);
+				}
+				finally
+				{
+					Configuration.Linq.AllowMultipleQuery = old;
+				}
 			}
 		}
 	}
