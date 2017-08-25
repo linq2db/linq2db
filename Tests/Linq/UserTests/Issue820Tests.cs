9dc941fb7132496f6ad61a4c3790994dfedbe0cf
MaceWindu <dmitry.github@lukashenko.com>
2017-08-25 09:27:27 +0200

    more tests

--- Tests/Linq/UserTests/Issue820Tests.cs
+++ Tests/Linq/UserTests/Issue820Tests.cs
@@ -13,6 +13,72 @@ namespace Tests.UserTests
 	[TestFixture]
 	public class Issue820Tests : TestBase
 	{
+		[Sql.Expression("{0}", ServerSideOnly = true)]
+		public static short Nope2(short? value)
+		{
+			return value.Value;
+		}
+
+		[Sql.Expression("{0}", ServerSideOnly = true)]
+		public static short Nope(short value)
+		{
+			return value;
+		}
+
+		[Test, DataContextSource]
+		public void TestAndWithFunction(string context)
+		{
+			short? param = 1;
+			using (var db = GetDataContext(context))
+			{
+				var query = db.GetTable<LinqDataTypes2>()
+					.Where(_ => param != null && Nope2(param) == _.SmallIntValue);
+
+				var result1 = query.ToList();
+				param = null;
+				var result2 = query.ToList();
+
+				Assert.AreEqual(1, result1.Count);
+				Assert.AreEqual(0, result2.Count);
+			}
+		}
+
+		[Test, DataContextSource]
+		public void TestAndWithCastAndFunction(string context)
+		{
+			short? param = 1;
+			using (var db = GetDataContext(context))
+			{
+				var query = db.GetTable<LinqDataTypes2>()
+					.Where(_ => param != null && Nope((short)param) == _.SmallIntValue);
+
+				var result1 = query.ToList();
+				param = null;
+				var result2 = query.ToList();
+
+				Assert.AreEqual(1, result1.Count);
+				Assert.AreEqual(0, result2.Count);
+			}
+		}
+
+		[Test, DataContextSource]
+		public void TestAndWithCast(string context)
+		{
+			short? param = 1;
+			using (var db = GetDataContext(context))
+			{
+				var query = db.GetTable<LinqDataTypes2>()
+					.Where(_ => param != null && (short)param == _.SmallIntValue);
+
+				var result1 = query.ToList();
+				param = null;
+				var result2 = query.ToList();
+
+				Assert.AreEqual(1, result1.Count);
+				Assert.AreEqual(0, result2.Count);
+			}
+		}
+
 		[Test, DataContextSource]
 		public void TestAndWithValue(string context)
 		{
