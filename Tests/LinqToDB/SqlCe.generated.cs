977ebd33f3c006b4a8358a08bc972bf4a4c35817
Ilya Chudin <chudinia@gmail.com>
2017-08-29 20:32:52 +0500

    [ci skip]
    #48 add Humanizer sample

--- Tests/LinqToDB/SqlCe.generated.cs
+++ Tests/LinqToDB/SqlCe.generated.cs
@@ -14,8 +14,8 @@ namespace SqlCeDataContext
 {
 	/// <summary>
 	/// Database       : TestData
-	/// Data Source    : I:\linq2db.t4models\\Data\TestData.sdf
-	/// Server Version : 4.0.8876.1
+	/// Data Source    : D:\job\t4models\\Data\TestData.sdf
+	/// Server Version : 4.0.8482.1
 	/// </summary>
 	public partial class TestDataDB : LinqToDB.Data.DataConnection
 	{
@@ -47,26 +47,26 @@ namespace SqlCeDataContext
 	[Table("AllTypes")]
 	public partial class AllType
 	{
-		[PrimaryKey, NotNull    ] public int       ID                       { get; set; } // int
-		[Column,        Nullable] public long?     bigintDataType           { get; set; } // bigint
-		[Column,        Nullable] public decimal?  numericDataType          { get; set; } // numeric(18, 0)
-		[Column,        Nullable] public bool?     bitDataType              { get; set; } // bit
-		[Column,        Nullable] public short?    smallintDataType         { get; set; } // smallint
-		[Column,        Nullable] public decimal?  decimalDataType          { get; set; } // numeric(18, 0)
-		[Column,        Nullable] public int?      intDataType              { get; set; } // int
-		[Column,        Nullable] public byte?     tinyintDataType          { get; set; } // tinyint
-		[Column,        Nullable] public decimal?  moneyDataType            { get; set; } // money
-		[Column,        Nullable] public double?   floatDataType            { get; set; } // float
-		[Column,        Nullable] public float?    realDataType             { get; set; } // real
-		[Column,        Nullable] public DateTime? datetimeDataType         { get; set; } // datetime
-		[Column,        Nullable] public string    ncharDataType            { get; set; } // nchar(20)
-		[Column,        Nullable] public string    nvarcharDataType         { get; set; } // nvarchar(20)
-		[Column,        Nullable] public string    ntextDataType            { get; set; } // ntext
-		[Column,        Nullable] public byte[]    binaryDataType           { get; set; } // binary(1)
-		[Column,        Nullable] public byte[]    varbinaryDataType        { get; set; } // varbinary(1)
-		[Column,        Nullable] public byte[]    imageDataType            { get; set; } // image
-		[Column,        Nullable] public byte[]    timestampDataType        { get; set; } // rowversion
-		[Column,        Nullable] public Guid?     uniqueidentifierDataType { get; set; } // uniqueidentifier
+		[Column(),                            PrimaryKey,  NotNull] public int       ID                       { get; set; } // int
+		[Column(@"bigintDataType"),              Nullable         ] public long?     BigintDataType           { get; set; } // bigint
+		[Column(@"numericDataType"),             Nullable         ] public decimal?  NumericDataType          { get; set; } // numeric(18, 0)
+		[Column(@"bitDataType"),                 Nullable         ] public bool?     BitDataType              { get; set; } // bit
+		[Column(@"smallintDataType"),            Nullable         ] public short?    SmallintDataType         { get; set; } // smallint
+		[Column(@"decimalDataType"),             Nullable         ] public decimal?  DecimalDataType          { get; set; } // numeric(18, 0)
+		[Column(@"intDataType"),                 Nullable         ] public int?      IntDataType              { get; set; } // int
+		[Column(@"tinyintDataType"),             Nullable         ] public byte?     TinyintDataType          { get; set; } // tinyint
+		[Column(@"moneyDataType"),               Nullable         ] public decimal?  MoneyDataType            { get; set; } // money
+		[Column(@"floatDataType"),               Nullable         ] public double?   FloatDataType            { get; set; } // float
+		[Column(@"realDataType"),                Nullable         ] public float?    RealDataType             { get; set; } // real
+		[Column(@"datetimeDataType"),            Nullable         ] public DateTime? DatetimeDataType         { get; set; } // datetime
+		[Column(@"ncharDataType"),               Nullable         ] public string    NcharDataType            { get; set; } // nchar(20)
+		[Column(@"nvarcharDataType"),            Nullable         ] public string    NvarcharDataType         { get; set; } // nvarchar(20)
+		[Column(@"ntextDataType"),               Nullable         ] public string    NtextDataType            { get; set; } // ntext
+		[Column(@"binaryDataType"),              Nullable         ] public byte[]    BinaryDataType           { get; set; } // binary(1)
+		[Column(@"varbinaryDataType"),           Nullable         ] public byte[]    VarbinaryDataType        { get; set; } // varbinary(1)
+		[Column(@"imageDataType"),               Nullable         ] public byte[]    ImageDataType            { get; set; } // image
+		[Column(@"timestampDataType"),           Nullable         ] public byte[]    TimestampDataType        { get; set; } // rowversion
+		[Column(@"uniqueidentifierDataType"),    Nullable         ] public Guid?     UniqueidentifierDataType { get; set; } // uniqueidentifier
 	}
 
 	[Table("Child")]
@@ -88,6 +88,16 @@ namespace SqlCeDataContext
 	{
 		[PrimaryKey, NotNull] public int    PersonID { get; set; } // int
 		[Column,     NotNull] public string Taxonomy { get; set; } // nvarchar(50)
+
+		#region Associations
+
+		/// <summary>
+		/// FK_Doctor_Person
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=false, Relationship=Relationship.OneToOne, KeyName="FK_Doctor_Person", BackReferenceName="Doctor")]
+		public Person Person { get; set; }
+
+		#endregion
 	}
 
 	[Table("GrandChild")]
@@ -125,6 +135,16 @@ namespace SqlCeDataContext
 	{
 		[PrimaryKey, NotNull] public int    PersonID  { get; set; } // int
 		[Column,     NotNull] public string Diagnosis { get; set; } // nvarchar(256)
+
+		#region Associations
+
+		/// <summary>
+		/// FK_Patient_Person
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=false, Relationship=Relationship.OneToOne, KeyName="FK_Patient_Person", BackReferenceName="Patient")]
+		public Person Person { get; set; }
+
+		#endregion
 	}
 
 	[Table("Person")]
@@ -135,6 +155,22 @@ namespace SqlCeDataContext
 		[Column,     NotNull    ] public string LastName   { get; set; } // nvarchar(50)
 		[Column,        Nullable] public string MiddleName { get; set; } // nvarchar(50)
 		[Column,     NotNull    ] public char   Gender     { get; set; } // nchar(1)
+
+		#region Associations
+
+		/// <summary>
+		/// FK_Doctor_Person_BackReference
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=true, Relationship=Relationship.OneToOne, IsBackReference=true)]
+		public Doctor Doctor { get; set; }
+
+		/// <summary>
+		/// FK_Patient_Person_BackReference
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=true, Relationship=Relationship.OneToOne, IsBackReference=true)]
+		public Patient Patient { get; set; }
+
+		#endregion
 	}
 
 	[Table("TestIdentity")]
@@ -174,5 +210,93 @@ namespace SqlCeDataContext
 			return table.FirstOrDefault(t =>
 				t.ID == ID);
 		}
+
+		#region Associations
+
+		#region Doctor Associations
+
+		/// <summary>
+		/// FK_Doctor_Person
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=false, Relationship=Relationship.OneToOne, KeyName="FK_Doctor_Person", BackReferenceName="Doctor")]
+		public static IQueryable<Person> People(this Doctor obj, IDataContext db)
+		{
+			return db.GetTable<Person>().Where(c => c.PersonID == obj.PersonID);
+		}
+
+		/// <summary>
+		/// FK_Doctor_Person
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=false, Relationship=Relationship.OneToOne, KeyName="FK_Doctor_Person", BackReferenceName="Doctor")]
+		public static Person Person(this Doctor obj, IDataContext db)
+		{
+			return db.GetTable<Person>().Where(c => c.PersonID == obj.PersonID).First();
+		}
+
+		#endregion
+
+		#region Patient Associations
+
+		/// <summary>
+		/// FK_Patient_Person
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=false, Relationship=Relationship.OneToOne, KeyName="FK_Patient_Person", BackReferenceName="Patient")]
+		public static IQueryable<Person> People(this Patient obj, IDataContext db)
+		{
+			return db.GetTable<Person>().Where(c => c.PersonID == obj.PersonID);
+		}
+
+		/// <summary>
+		/// FK_Patient_Person
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=false, Relationship=Relationship.OneToOne, KeyName="FK_Patient_Person", BackReferenceName="Patient")]
+		public static Person Person(this Patient obj, IDataContext db)
+		{
+			return db.GetTable<Person>().Where(c => c.PersonID == obj.PersonID).First();
+		}
+
+		#endregion
+
+		#region Person Associations
+
+		/// <summary>
+		/// FK_Doctor_Person_BackReference
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=true, Relationship=Relationship.OneToOne, IsBackReference=true)]
+		public static IQueryable<Doctor> Doctors(this Person obj, IDataContext db)
+		{
+			return db.GetTable<Doctor>().Where(c => c.PersonID == obj.PersonID);
+		}
+
+		/// <summary>
+		/// FK_Doctor_Person_BackReference
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=true, Relationship=Relationship.OneToOne, IsBackReference=true)]
+		public static Doctor Doctor(this Person obj, IDataContext db)
+		{
+			return db.GetTable<Doctor>().Where(c => c.PersonID == obj.PersonID).FirstOrDefault();
+		}
+
+		/// <summary>
+		/// FK_Patient_Person_BackReference
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=true, Relationship=Relationship.OneToOne, IsBackReference=true)]
+		public static IQueryable<Patient> Patients(this Person obj, IDataContext db)
+		{
+			return db.GetTable<Patient>().Where(c => c.PersonID == obj.PersonID);
+		}
+
+		/// <summary>
+		/// FK_Patient_Person_BackReference
+		/// </summary>
+		[Association(ThisKey="PersonID", OtherKey="PersonID", CanBeNull=true, Relationship=Relationship.OneToOne, IsBackReference=true)]
+		public static Patient Patient(this Person obj, IDataContext db)
+		{
+			return db.GetTable<Patient>().Where(c => c.PersonID == obj.PersonID).FirstOrDefault();
+		}
+
+		#endregion
+
+		#endregion
 	}
 }
