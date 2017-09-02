6a7d240ea91e36cb6a6fbc3141fcc5c243494355
MaceWindu <dmitry.github@lukashenko.com>
2017-09-02 18:21:33 +0200

    - mapping descriptors
    - fluent mapper

--- Source/Mapping/ColumnDescriptor.cs
+++ Source/Mapping/ColumnDescriptor.cs
@@ -12,8 +12,17 @@ namespace LinqToDB.Mapping
 
 	using Reflection;
 
+	/// <summary>
+	/// Stores mapping entity column descriptor.
+	/// </summary>
 	public class ColumnDescriptor
 	{
+		/// <summary>
+		/// Creates descriptor instance.
+		/// </summary>
+		/// <param name="mappingSchema">Mapping schema, associated with descriptor.</param>
+		/// <param name="columnAttribute">Column attribute, from which descriptor data should be extracted.</param>
+		/// <param name="memberAccessor">Column mapping member accessor.</param>
 		public ColumnDescriptor(MappingSchema mappingSchema, ColumnAttribute columnAttribute, MemberAccessor memberAccessor)
 		{
 			MemberAccessor = memberAccessor;
@@ -117,30 +126,162 @@ namespace LinqToDB.Mapping
 			}
 		}
 
+		/// <summary>
+		/// Gets column mapping member accessor.
+		/// </summary>
 		public MemberAccessor MemberAccessor  { get; private set; }
+
+		/// <summary>
+		/// Gets column mapping member (field or property).
+		/// </summary>
 		public MemberInfo     MemberInfo      { get; private set; }
+
+		/// <summary>
+		/// Gets value storage member (field or property).
+		/// </summary>
 		public MemberInfo     StorageInfo     { get; private set; }
+
+		/// <summary>
+		/// Gets type of column mapping member (field or property).
+		/// </summary>
 		public Type           MemberType      { get; private set; }
+
+		/// <summary>
+		/// Gets type of column value storage member (field or property).
+		/// </summary>
 		public Type           StorageType     { get; private set; }
+
+		/// <summary>
+		/// Gets the name of mapped member.
+		/// When applied to class or interface, should contain name of property of field.
+		/// 
+		/// If column is mapped to a property or field of composite object, <see cref="MemberName"/> should contain a path to that
+		/// member using dot as separator.
+		/// <example>
+		/// public class Address
+		/// {
+		///     public string City     { get; set; }
+		///     public string Street   { get; set; }
+		///     public int    Building { get; set; }
+		/// }
+		/// 
+		/// [Column("city", "Residence.Street")]
+		/// [Column("user_name", "Name")]
+		/// public class User
+		/// {
+		///     public string Name;
+		///     
+		///     [Column("street", ".Street")]
+		///     [Column("building_number", MemberName = ".Building")]
+		///     public Address Residence { get; set; }
+		/// }
+		/// </example>
+		/// </summary>
 		public string         MemberName      { get; private set; }
+
+		/// <summary>
+		/// Gets the name of a column in database.
+		/// If not specified, <see cref="MemberName"/> value will be used.
+		/// </summary>
 		public string         ColumnName      { get; private set; }
+
+		/// <summary>
+		/// Gets storage property or field to hold the value from a column.
+		/// Could be usefull e.g. in combination of private storage field and getter-only mapping property.
+		/// </summary>
 		public string         Storage         { get; private set; }
+
+		/// <summary>
+		/// Gets whether a column contains a discriminator value for a LINQ to DB inheritance hierarchy.
+		/// <see cref="InheritanceMappingAttribute"/> for more details.
+		/// Default value: <c>false</c>.
+		/// </summary>
 		public bool           IsDiscriminator { get; private set; }
+
+		/// <summary>
+		/// Gets LINQ to DB type for column.
+		/// </summary>
 		public DataType       DataType        { get; private set; }
+
+		/// <summary>
+		/// Gets the name of the database column type.
+		/// </summary>
 		public string         DbType          { get; private set; }
+
+		/// <summary>
+		/// Gets whether a column contains values that the database auto-generates.
+		/// </summary>
 		public bool           IsIdentity      { get; private set; }
+
+		/// <summary>
+		/// Gets whether a column is insertable.
+		/// This flag will affect only insert operations with implicit columns specification like
+		/// <see cref="DataExtensions.Insert{T}(IDataContext, T, string, string, string)"/>
+		/// method and will be ignored when user explicitly specifies value for this column.
+		/// </summary>
 		public bool           SkipOnInsert    { get; private set; }
+
+		/// <summary>
+		/// Gets whether a column is updatable.
+		/// This flag will affect only update operations with implicit columns specification like
+		/// <see cref="DataExtensions.Update{T}(IDataContext, T)"/>
+		/// method and will be ignored when user explicitly specifies value for this column.
+		/// </summary>
 		public bool           SkipOnUpdate    { get; private set; }
+
+		/// <summary>
+		/// Gets whether this member represents a column that is part or all of the primary key of the table.
+		/// Also see <see cref="PrimaryKeyAttribute"/>.
+		/// </summary>
 		public bool           IsPrimaryKey    { get; private set; }
+
+		/// <summary>
+		/// Gets order of current column in composite primary key.
+		/// Order is used for query generation to define in which order primary key columns must be mentioned in query
+		/// from columns with smallest order value to greatest.
+		/// </summary>
 		public int            PrimaryKeyOrder { get; private set; }
+
+		/// <summary>
+		/// Gets whether a column can contain null values.
+		/// </summary>
 		public bool           CanBeNull       { get; private set; }
+
+		/// <summary>
+		/// Gets the length of the database column.
+		/// </summary>
 		public int?           Length          { get; private set; }
+
+		/// <summary>
+		/// Gets the precision of the database column.
+		/// </summary>
 		public int?           Precision       { get; private set; }
+
+		/// <summary>
+		/// Gets the Scale of the database column.
+		/// </summary>
 		public int?           Scale           { get; private set; }
+
+		/// <summary>
+		/// Custom template for column definition in create table SQL expression, generated using
+		/// <see cref="DataExtensions.CreateTable{T}(IDataContext, string, string, string, string, string, SqlQuery.DefaulNullable)"/> methods.
+		/// Template accepts following string parameters:
+		/// - {0} - column name;
+		/// - {1} - column type;
+		/// - {2} - NULL specifier;
+		/// - {3} - identity specification.
+		/// </summary>
 		public string         CreateFormat    { get; private set; }
 
 		Func<object,object> _getter;
 
+		// TODO: passing mapping schema to generate converter in combination with converter caching looks wrong
+		/// <summary>
+		/// Extracts column value, converted to database type, from entity object.
+		/// </summary>
+		/// <param name="mappingSchema">Mapping schema with conversion information.</param>
+		/// <param name="obj">Enity object to extract column value from.</param>
+		/// <returns>Returns column value, converted to database type.</returns>
 		public virtual object GetValue(MappingSchema mappingSchema, object obj)
 		{
 			if (_getter == null)
