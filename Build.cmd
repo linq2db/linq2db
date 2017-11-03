5f59815d53345e8e1149cbf68c96867d225906be
Igor Tkachev <igor.tkachev@gmail.com>
2017-11-03 10:34:16 -0400

    Test appveyor.

--- Build.cmd
+++ Build.cmd
@@ -1,1 +1,1 @@
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" linq2db.sln /p:Configuration=Release /t:Restore;Clean;Rebuild /v:m
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" linq2db.sln /p:Configuration=Release /t:Restore;Rebuild /v:m
