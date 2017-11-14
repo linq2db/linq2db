919930808fa67277d9ce11f5fa57166be9b550e9
n9 <n9>
2017-11-14 14:46:42 +0100

    #911 test

---
+++ Tests/Linq/UserTests/Issue911Tests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Tests.Model;
+using LinqToDB;
+using System.Linq.Expressions;
+
+namespace Tests.UserTests
+{
+	public class Issue911Tests : TestBase
+	{
+		[Test, DataContextSource]
+		public void Test(string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var query = db.Child
+					.GroupBy(p => p.ParentID % 2 == 0)
+					.Select(g => new
+					{
+						g.Key,
+						X = g.OrderBy(_ => _.ParentID).First()
+					});
+
+				var array = query.ToArray();
+			}
+		}
+	}
+}
