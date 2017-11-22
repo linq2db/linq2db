a99cb6384bd7120d583f5aaf3b22806998badb8e
Ilya Chudin <chudinia@gmail.com>
2017-11-22 19:53:12 +0500

    ci builds

--- travisbuild.sh
+++ travisbuild.sh
@@ -16,9 +16,9 @@ dotnet restore
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
 
-dotnet build ./Source/LinqToDb/ -c Release -f netstandard1.6
+dotnet build ./Source/LinqToDB/LinqToDB.csproj -c Release -f netstandard1.6
 
-dotnet test ./Tests/Linq/ -c Release -f netcoreapp1.0 --where "cat != WindowsOnly"
+dotnet test ./Tests/Linq/Tests.csproj -c Release -f netcoreapp1.0 --where "cat != WindowsOnly"
 
 # Instead, run directly with mono for the full .net version 
 # dotnet build ./test/TEST_PROJECT_NAME -c Release -f net451
