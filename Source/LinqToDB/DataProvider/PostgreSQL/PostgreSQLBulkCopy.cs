a3aa724b511454400c2ea8b1477f0cd153877058
Ilya Chudin <chudinia@gmail.com>
2017-11-26 02:04:10 +0500

    formatting

--- Source/LinqToDB/DataProvider/PostgreSQL/PostgreSQLBulkCopy.cs
+++ Source/LinqToDB/DataProvider/PostgreSQL/PostgreSQLBulkCopy.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using LinqToDB.Extensions;
@@ -11,23 +11,22 @@ namespace LinqToDB.DataProvider.PostgreSQL
 	class PostgreSQLBulkCopy : BasicBulkCopy
 	{
 		private readonly PostgreSQLDataProvider _dataProvider;
-		private readonly Type _connectionType;
+		private readonly Type                   _connectionType;
 
 		public PostgreSQLBulkCopy(PostgreSQLDataProvider dataProvider, Type connectionType)
 		{
-			_dataProvider = dataProvider;
+			_dataProvider   = dataProvider;
 			_connectionType = connectionType;
 		}
 
-		protected override BulkCopyRowsCopied MultipleRowsCopy<T>(
-			DataConnection dataConnection, BulkCopyOptions options, IEnumerable<T> source)
+		protected override BulkCopyRowsCopied MultipleRowsCopy<T>(DataConnection dataConnection, BulkCopyOptions options, IEnumerable<T> source)
 		{
 			return MultipleRowsCopy1(dataConnection, options, false, source);
 		}
 
 		protected override BulkCopyRowsCopied ProviderSpecificCopy<T>(DataConnection dataConnection, BulkCopyOptions options, IEnumerable<T> source)
 		{
-			if (dataConnection == null) throw new ArgumentNullException("dataConnection");
+			if (dataConnection == null) throw new ArgumentNullException(nameof(dataConnection));
 
 			var connection = dataConnection.Connection;
 
@@ -37,38 +36,34 @@ namespace LinqToDB.DataProvider.PostgreSQL
 			if (!(connection.GetType() == _connectionType || connection.GetType().IsSubclassOfEx(_connectionType)))
 				return MultipleRowsCopy(dataConnection, options, source);
 
-			var sqlBuilder = _dataProvider.CreateSqlBuilder();
-			var ed = dataConnection.MappingSchema.GetEntityDescriptor(typeof(T));
-			var tableName = GetTableName(sqlBuilder, options, ed);
-			var columns = ed.Columns.Where(c => !c.SkipOnInsert || options.KeepIdentity == true && c.IsIdentity).ToList();
+			var sqlBuilder   = _dataProvider.CreateSqlBuilder();
+			var ed           = dataConnection.MappingSchema.GetEntityDescriptor(typeof(T));
+			var tableName    = GetTableName(sqlBuilder, options, ed);
+			var columns      = ed.Columns.Where(c => !c.SkipOnInsert || options.KeepIdentity == true && c.IsIdentity).ToArray();
 
-			var fields = string.Join(", ", columns.Select(column => sqlBuilder.Convert(column.ColumnName, ConvertType.NameToQueryField)));
-			var copyCommand = $"COPY {tableName} ({fields}) FROM STDIN (FORMAT BINARY)";
+			var fields       = string.Join(", ", columns.Select(column => sqlBuilder.Convert(column.ColumnName, ConvertType.NameToQueryField)));
+			var copyCommand  = $"COPY {tableName} ({fields}) FROM STDIN (FORMAT BINARY)";
 
-			var rowsCopied = new BulkCopyRowsCopied();
-			var batchSize = Math.Max(10, options.MaxBatchSize ?? 10000);
+			var rowsCopied   = new BulkCopyRowsCopied();
+			var batchSize    = Math.Max(10, options.MaxBatchSize ?? 10000);
 			var currentCount = 0;
 
-			var dc = (dynamic)connection;
-			var writer = dc.BeginBinaryImport(copyCommand);
+			var dc           = (dynamic)connection;
+			var writer       = dc.BeginBinaryImport(copyCommand);
 
 			foreach (var item in source)
 			{
 				writer.StartRow();
 
-				for (var i = 0; i < columns.Count; i++)
+				for (var i = 0; i < columns.Length; i++)
 				{
 					var column = columns[i];
-					var value = column.GetValue(dataConnection.MappingSchema, item);
+					var value  = column.GetValue(dataConnection.MappingSchema, item);
 
 					if (value == null)
-					{
 						writer.WriteNull();
-					}
 					else
-					{
 						writer.Write(value);
-					}
 				}
 
 				currentCount++;
