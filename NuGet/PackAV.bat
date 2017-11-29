fdfa77e78f7275abba3a3c2e1b7ff65c07723d66
Igor Tkachev <igor.tkachev@gmail.com>
2017-11-29 15:23:16 -0500

    Updated appveyor.yml

--- NuGet/PackAV.bat
+++ NuGet/PackAV.bat
@@ -1,27 +1,19 @@
 @rem Building packages for appveyor
-@rem
-@rem first  parameter - target package version
-@rem second parameter - target linq2db version
-
-set version=%1%
-
-echo packages version: %version% for linq2db version %2%
-
 del *.nupkg
 
-..\Redist\NuGet Pack linq2db.nuspec                  -Version %version%
-..\Redist\NuGet Pack linq2db.t4models.nuspec         -Version %version%
+..\Redist\NuGet Pack linq2db.nuspec
+..\Redist\NuGet Pack linq2db.t4models.nuspec
 
-..\Redist\NuGet Pack linq2db.Access.nuspec           -Version %version%
-..\Redist\NuGet Pack linq2db.DB2.nuspec              -Version %version%
-..\Redist\NuGet Pack linq2db.Firebird.nuspec         -Version %version%
-..\Redist\NuGet Pack linq2db.Informix.nuspec         -Version %version%
-..\Redist\NuGet Pack linq2db.MySql.nuspec            -Version %version%
-..\Redist\NuGet Pack linq2db.Oracle.Managed.nuspec   -Version %version%
-..\Redist\NuGet Pack linq2db.Oracle.Unmanaged.nuspec -Version %version%
-..\Redist\NuGet Pack linq2db.PostgreSQL.nuspec       -Version %version%
-..\Redist\NuGet Pack linq2db.SapHana.nuspec          -Version %version%
-..\Redist\NuGet Pack linq2db.SqlCe.nuspec            -Version %version%
-..\Redist\NuGet Pack linq2db.SQLite.nuspec           -Version %version%
-..\Redist\NuGet Pack linq2db.SqlServer.nuspec        -Version %version%
-..\Redist\NuGet Pack linq2db.Sybase.nuspec           -Version %version%
+..\Redist\NuGet Pack linq2db.Access.nuspec
+..\Redist\NuGet Pack linq2db.DB2.nuspec
+..\Redist\NuGet Pack linq2db.Firebird.nuspec
+..\Redist\NuGet Pack linq2db.Informix.nuspec
+..\Redist\NuGet Pack linq2db.MySql.nuspec
+..\Redist\NuGet Pack linq2db.Oracle.Managed.nuspec
+..\Redist\NuGet Pack linq2db.Oracle.Unmanaged.nuspec
+..\Redist\NuGet Pack linq2db.PostgreSQL.nuspec
+..\Redist\NuGet Pack linq2db.SapHana.nuspec
+..\Redist\NuGet Pack linq2db.SqlCe.nuspec
+..\Redist\NuGet Pack linq2db.SQLite.nuspec
+..\Redist\NuGet Pack linq2db.SqlServer.nuspec
+..\Redist\NuGet Pack linq2db.Sybase.nuspec
