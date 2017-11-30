490b935d60fa24554cb518e954ec03299d4ae336
Igor Tkachev <igor.tkachev@gmail.com>
2017-11-29 19:27:26 -0500

    Working on appveyor tests.

--- Build/AppVeyor.NUnit.Tests.ps1
+++ Build/AppVeyor.NUnit.Tests.ps1
@@ -1,19 +1,19 @@
 $wc = New-Object System.Net.WebClient
 
-$logFileName = "nunit_net452_results.xml"
+$logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_net452_results.xml"
 
 nunit3-console Tests\Linq\bin\AppVeyor\net452\linq2db.Tests.dll --result=$logFileName --where "cat != Ignored"
 
 if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 echo "UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $logFileName"
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID", (Resolve-Path $logFileName))
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID", "$logFileName")
 if ($LastExitCode -ne 0) {
 	echo "FAIL: UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $logFileName"
 	$host.SetShouldExit($LastExitCode)
 }
 
 
-$logFileName = "nunit_core2_results.xml"
+$logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_core2_results.xml"
 
 dotnet test Tests\Linq\bin\AppVeyor\netcoreapp2.0\linq2db.Tests.Core2.dll --logger:"trx;LogFileName=$logFileName" --filter TestCategory!=Ignored
 
@@ -23,13 +23,13 @@ if ($LastExitCode -ne 0) {
 }
 
 echo "UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $logFileName"
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID", (Resolve-Path $logFileName))
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID", "$logFileName")
 if ($LastExitCode -ne 0) {
 	echo "FAIL: UploadFile: https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID from $logFileName"
 	$host.SetShouldExit($LastExitCode)
 }
 
-$logFileName = "nunit_core1_results.xml"
+$logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_core1_results.xml"
 
 dotnet test Tests\Linq\bin\AppVeyor\netcoreapp1.0\linq2db.Tests.Core1.dll --logger:"trx;LogFileName=$logFileName" --filter TestCategory!=Ignored
 
@@ -39,7 +39,7 @@ if ($LastExitCode -ne 0) {
 }
 
 echo "UploadFile: https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID from $logFileName"
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID", (Resolve-Path $logFileName))
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID", "$logFileName")
 if ($LastExitCode -ne 0) {
 	echo "FAIL: UploadFile: https://ci.appveyor.com/api/testresults/mstest/$env:APPVEYOR_JOB_ID from $logFileName"
 	$host.SetShouldExit($LastExitCode)
