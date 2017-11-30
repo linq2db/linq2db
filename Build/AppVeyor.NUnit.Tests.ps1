4cc54f5c5b0770db8107a4edd67560342f2dba55
Igor Tkachev <igor.tkachev@gmail.com>
2017-11-30 01:23:20 -0500

    Working on appveyor tests.

--- Build/AppVeyor.NUnit.Tests.ps1
+++ Build/AppVeyor.NUnit.Tests.ps1
@@ -16,7 +16,7 @@ if ($LastExitCode -ne 0) {
 
 $logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_core2_results.xml"
 
-dotnet test Tests\Linq\ -f netcoreapp2.0 --logger:"trx;LogFileName=$logFileName" --filter:"TestCategory!=Ignored"
+dotnet test Tests\Linq\ -f netcoreapp2.0 --logger:"trx;LogFileName=$logFileName" --filter:"TestCategory!=Ignored" -c AppVeyor
 
 if ($LastExitCode -ne 0) {
 	echo "FAIL: dotnet vstest $a --logger:'trx;LogFileName=$logFileName'"
@@ -33,7 +33,7 @@ if ($LastExitCode -ne 0) {
 
 $logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_core1_results.xml"
 
-dotnet test Tests\Linq\ -f netcoreapp1.0 --logger:"trx;LogFileName=$logFileName" --filter:"TestCategory!=Ignored"
+dotnet test Tests\Linq\ -f netcoreapp1.0 --logger:"trx;LogFileName=$logFileName" --filter:"TestCategory!=Ignored" -c AppVeyor
 
 if ($LastExitCode -ne 0) {
 	echo "FAIL: dotnet vstest $a --logger:'trx;LogFileName=$logFileName'"
