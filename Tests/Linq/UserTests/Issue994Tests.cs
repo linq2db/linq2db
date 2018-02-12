fd31becf1bbcbc7a1bb947e14cb11946de93a479
jkuehner <jochen.kuehner@kardex.com>
2018-02-12 20:35:57 +0100

    increase test to component mapping, seems not to work! (don't know why atm)

--- Tests/Linq/UserTests/Issue994Tests.cs
+++ Tests/Linq/UserTests/Issue994Tests.cs
@@ -20,6 +20,12 @@ public class Issue994Tests : TestBase
 		public string Xy { get; set; }
 	}
 
+	public class Name
+	{
+		public string First { get; set; }
+		public string Second { get; set; }
+	}
+
 	public class Animal
 	{
 		public int Id { get; set; }
@@ -55,6 +61,8 @@ public class Issue994Tests : TestBase
 		public Eye Bla { get; set; }
 
 		public int? EyeId { get; set; }
+
+		public Name DogName { get; set; }
 	}
 
 	public class Test
@@ -83,6 +91,8 @@ public class Issue994Tests : TestBase
 
 		Assert.Null(listTest3.First().TestAnimal);
 		Assert.NotNull(((Dog)listTest3.Skip(1).First().TestAnimal).Bla);
+
+		var listTest4 = LoadTest4();
 	}
 
 	private void InsertData()
@@ -98,7 +108,8 @@ public class Issue994Tests : TestBase
 			Id = 1,
 			Discriminator = "Dog",
 			EyeId = 1,
-			Name = "FirstDog"
+			Name = "FirstDog",
+			DogName = new Name {First = "a", Second = "b"}
 		};
 
 		var test = new Test
@@ -118,7 +129,7 @@ public class Issue994Tests : TestBase
 			db.SetCommand("DROP TABLE IF EXISTS `Animals`").Execute();
 			db.SetCommand("DROP TABLE IF EXISTS `Eyes`").Execute();
 			db.SetCommand("DROP TABLE IF EXISTS `Test`").Execute();
-			db.SetCommand("CREATE TABLE `Animals` ( `Id` INTEGER NOT NULL PRIMARY KEY, `Name` TEXT,`Discriminator` TEXT, `EyeId` INTEGER )").Execute();
+			db.SetCommand("CREATE TABLE `Animals` ( `Id` INTEGER NOT NULL PRIMARY KEY, `Name` TEXT,`Discriminator` TEXT, `EyeId` INTEGER, `First` TEXT, `Second` TEXT )").Execute();
 			db.SetCommand("CREATE TABLE `Eyes` ( `Id` INTEGER NOT NULL PRIMARY KEY, `Xy` TEXT )").Execute();
 			db.SetCommand("CREATE TABLE `Test` ( `Id` INTEGER NOT NULL PRIMARY KEY, `TestAnimalId` INTEGER NULL )").Execute();
 		}
@@ -159,6 +170,16 @@ public class Issue994Tests : TestBase
 		}
 	}
 
+	private List<Dog> LoadTest4()
+	{
+		using (var db = new TestDataConnection())
+		{
+			var a = db.GetTable<Dog>().ToList();
+			var sql = db.LastQuery;
+			return a;
+		}
+	}
+
 	private void SetMappings()
 	{
 		var mappingBuilder = MappingSchema.Default.GetFluentMappingBuilder();
@@ -175,6 +196,8 @@ public class Issue994Tests : TestBase
 			.HasTableName("Animals")
 			.Property(x => x.Bla).IsNotColumn()
 			.Property(x => x.EyeId).IsColumn().IsNullable().HasColumnName("EyeId")
+			.Property(x => x.DogName.Second).HasColumnName("Second")
+			.Property(x => x.DogName.First).HasColumnName("First")
 			.Association(x => x.Bla, x => x.EyeId, x => x.Id);
 
 		mappingBuilder.Entity<WildAnimal>()
