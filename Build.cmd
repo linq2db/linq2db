8ad970ae25bd69bf07f37a6c989c07538658f165
OneDeveloper <beercsaba@gmail.com>
2018-03-17 11:43:52 +0100

    Update Build.cmd

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,3 @@
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" linq2db.sln /p:Configuration=Release  /t:Restore;Rebuild /v:m
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" linq2db.sln /p:Configuration=Debug    /t:Restore;Rebuild /v:m
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" linq2db.sln /p:Configuration=AppVeyor /t:Restore;Rebuild /v:m
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" linq2db.sln /p:Configuration=Release  /t:Restore;Rebuild /v:m
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" linq2db.sln /p:Configuration=Debug    /t:Restore;Rebuild /v:m
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" linq2db.sln /p:Configuration=AppVeyor /t:Restore;Rebuild /v:m
