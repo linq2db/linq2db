11bcfe6c1d5b0dafd074ffc341480bf2dfe8fb34
Milan Figuric <figuric@gmail.com>
2018-03-25 20:34:58 +0200

    Fix calling Compile.cmd out of root folder

--- Source/LinqToDB/Compile.cmd
+++ Source/LinqToDB/Compile.cmd
@@ -1,4 +1,6 @@
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /v:m /target:Clean LinqToDB.csproj /property:Configuration=Debug
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /v:m /target:Clean LinqToDB.csproj /property:Configuration=Release
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /v:m LinqToDB.csproj /property:Configuration=Debug   
-"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /v:m LinqToDB.csproj /property:Configuration=Release 
+SET scriptpath=%~dp0
+::echo %scriptpath%
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /v:m /target:Clean %scriptpath%LinqToDB.csproj /property:Configuration=Debug
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /v:m /target:Clean %scriptpath%LinqToDB.csproj /property:Configuration=Release
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /v:m %scriptpath%LinqToDB.csproj /property:Configuration=Debug   
+"%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" /v:m %scriptpath%LinqToDB.csproj /property:Configuration=Release 
