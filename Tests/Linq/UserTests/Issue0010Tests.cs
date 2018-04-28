1395eec6e270b5b234ab0da2dede744cf9e53d1b
MaceWindu <dmitry.github@lukashenko.com>
2018-04-28 15:53:07 +0200

    test and fix

---
+++ Tests/Linq/UserTests/Issue0010Tests.cs
@@ -0,0 +1,31 @@
+﻿#if !NETSTANDARD1_6 && !NETSTANDARD2_0
+using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.DataProvider.Access;
+using NUnit.Framework;
+using System.Linq;
+
+namespace Tests.UserTests
+{
+	// https://github.com/linq2db/linq2db.LINQPad/issues/10
+	[TestFixture]
+	public class Issue0010Tests : TestBase
+	{
+		[Test, IncludeDataContextSource(false, ProviderName.Access)]
+		public void Test(string context)
+		{
+			using (var db = new DataConnection(new AccessDataProvider(), "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Database\\issue_10_linqpad.accdb;"))
+			{
+				var schemaProvider = db.DataProvider.GetSchemaProvider();
+
+				// call twice to ensure connection is still in good shape after call
+				var schema = schemaProvider.GetSchema(db);
+				schema     = schemaProvider.GetSchema(db);
+
+				// all returned primary keys are defined on system/access tables
+				Assert.True(schema.Tables.Any(t => t.Columns.Any(c => c.IsPrimaryKey)));
+			}
+		}
+	}
+}
+#endif
