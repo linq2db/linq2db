550e9f1025ae21884189d264c2b80bd3e4bef2bd
Ilya Chudin <chudinia@gmail.com>
2018-05-23 13:15:36 +0500

    working on docfx

--- Build/AppVeyor.DocFx.ps1
+++ Build/AppVeyor.DocFx.ps1
@@ -1,11 +1,19 @@
 Param(
 	[Parameter(Mandatory=$true)][string]$gitHubUser,
     [Parameter(Mandatory=$true)][string]$gitHubAccessToken,
-    [Parameter(Mandatory=$true)][string]$gitHubUserEmail
+    [Parameter(Mandatory=$true)][string]$gitHubUserEmail,
+    [Parameter(Mandatory=$true)][bool]$gitDeploy
 )
 
 
-Write-Host Building docfx with config
+Write-Host Building docfx
+
+rm .\Doc\_site\ -Recurse -Force
+rm linq2db.github.io -Recurse -Force
+rm .\Doc\api\*.yml
+rm .\Doc\api\.manifest
+
+.\Redist\NuGet.exe install msdn.4.5.2 -ExcludeVersion -OutputDirectory ./tools -Prerelease
 
 choco install docfx
 
@@ -29,10 +37,9 @@ git config user.email $gitHubUserEmail
 git add -A 2>&1
 git commit -m "CI docfx update" -q
 
-
-git push "https://$($gitHubUser):$($gitHubAccessToken)@github.com/linq2db/linq2db.github.io.git" master -q
+if ($gitDeploy)
+{
+    git push "https://$($gitHubUser):$($gitHubAccessToken)@github.com/linq2db/linq2db.github.io.git" master -q
+}
 
 cd ..\..
-
-rm .\Doc\_site\.git -Recurse -Force
-rm linq2db.github.io -Recurse -Force
