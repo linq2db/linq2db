6ef755e5c5cc3811e04dc483435225d215a38b39
ili <chudinia@gmail.com>
2018-05-23 10:28:56 +0500

    build will fail on docfx build error
    missed file

--- Build/AppVeyor.DocFx.ps1
+++ Build/AppVeyor.DocFx.ps1
@@ -13,6 +13,11 @@ docfx .\Doc\docfx.json
 
 git clone https://github.com/linq2db/linq2db.github.io.git -b master linq2db.github.io -q
 
+if ($LASTEXITCODE -ne 0)
+{
+    throw "DocFx build failed";
+}
+
 Copy-Item linq2db.github.io/.git ./doc/_site -recurse
 
 cd .\doc\_site
