a964d611f595b7251024387d8e50b9168dbdd867
Svyatoslav Danyliv <sdanyliv@gmail.com>
2018-06-20 16:26:14 +0300

    Activated tests for issue #975.

--- Tests/Linq/UserTests/Issue975Tests.cs
+++ Tests/Linq/UserTests/Issue975Tests.cs
@@ -10,7 +10,7 @@ using NUnit.Framework;
 
 namespace Tests.UserTests
 {
-	[ActiveIssue(975)]
+	[TestFixture]
 	public class Issue975Tests : TestBase
 	{
 		public static class SqlServer
@@ -67,7 +67,7 @@ namespace Tests.UserTests
 			[Column,     NotNull    ] public int       TargetId    { get; set; } // Int
 		}
 
-		[Test, IncludeDataContextSource(ProviderName.SqlServer)]
+		[Test, IncludeDataContextSource(ProviderName.SqlServer2012, ProviderName.SqlServer2014, ProviderName.SqlServer)]
 		public void Test(string context)
 		{
 			using (var db = GetDataContext(context))
