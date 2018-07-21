6aec6bcdcf5366e8cc9c3abc83434677c463df93
ili <chudinia@gmail.com>
2018-07-21 23:46:07 +0500

    TypeScript files generation for T4 models

---
+++ Tests/T4.Model/include.ts
@@ -0,0 +1,31 @@
+﻿export function format(formatString: string) {
+	return function (target, propertyKey: string) {
+		console.log("format(): called");
+	}
+}
+
+export function getFormat(target: any, propertyKey: string) {
+	//return Reflect.getMetadata(formatMetadataKey, target, propertyKey);
+}
+
+export interface ISomething {
+	something(val?: number);
+}
+
+export function g() {
+	console.log("g(): evaluated");
+	return function (target, propertyKey: string, descriptor: PropertyDescriptor) {
+		console.log("g(): called");
+	}
+}
+
+export function sealed(constructor?: Function) {
+	Object.seal(constructor);
+	Object.seal(constructor.prototype);
+}
+
+export function configurable(value: boolean) {
+	return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+		descriptor.configurable = value;
+	};
+}
