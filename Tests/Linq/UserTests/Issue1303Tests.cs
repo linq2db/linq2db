253e36db9e43b29cd8418bde600c94b96573196b
MaceWindu <dmitry.github@lukashenko.com>
2018-09-30 14:02:35 +0200

    test and fixes for binary table creation and literals support for all providers

--- Tests/Linq/UserTests/Issue1303Tests.cs
+++ Tests/Linq/UserTests/Issue1303Tests.cs
@@ -1,8 +1,6 @@
 ﻿using LinqToDB;
-using LinqToDB.Data;
 using LinqToDB.Mapping;
 using NUnit.Framework;
-using System;
 using System.Data.Linq;
 using System.Linq;
 
@@ -16,40 +14,47 @@ namespace Tests.UserTests
 		{
 			[PrimaryKey]
 			public int    ID     { get; set; }
-			[Column]
+			[Column(Length = 10)]
 			public byte[] Array  { get; set; }
-			[Column]
+			[Column(Length = 10)]
 			public Binary Binary { get; set; }
 		}
 
-		[Test, DataContextSource]
-		public void TestBinaryLiterals(string context)
+		[Test, Combinatorial]
+		public void TestBinary([DataSources] string context, [Values] bool inlineParameters)
 		{
-			using (var db = new DataConnection(context))
+			using (var db = GetDataContext(context))
 			using (var tbl = db.CreateLocalTable<Issue1303>())
 			{
 				tbl.Insert(() => new Issue1303()
 				{
-					ID = 1,
-					Array = new byte[] { 1, 2,3 },
+					ID     = 1,
+					Array  = new byte[] { 1, 2,3 },
 					Binary = new Binary(new byte[] { 4, 5})
 				});
 
-				var byId     = tbl.Where(_ => _.ID == 1)                                  .Single();
-				var byArray  = tbl.Where(_ => _.Array == new byte[] { 1, 2, 3 })          .Single();
-				var byBinary = tbl.Where(_ => _.Binary == new Binary(new byte[] { 4, 5 })).Single();
+				db.InlineParameters = inlineParameters;
+
+				var byId     = tbl.Where(_ => _.ID == 1).Single();
 
 				Assert.AreEqual(1, byId.ID);
 				Assert.True(new byte[] { 1, 2, 3 }.SequenceEqual(byId.Array));
-				Assert.True(new byte[] { 4, 5 }.SequenceEqual(byId.Binary.ToArray()));
+				Assert.True(new byte[] { 4, 5 }   .SequenceEqual(byId.Binary.ToArray()));
 
-				Assert.AreEqual(1, byArray.ID);
-				Assert.True(new byte[] { 1, 2, 3 }.SequenceEqual(byArray.Array));
-				Assert.True(new byte[] { 4, 5 }.SequenceEqual(byArray.Binary.ToArray()));
+				// Informix: doesn't support blobs in conditions
+				if (!context.StartsWith("Informix"))
+				{
+					var byArray  = tbl.Where(_ => _.Array  == new byte[] { 1, 2, 3 })         .Single();
+					var byBinary = tbl.Where(_ => _.Binary == new Binary(new byte[] { 4, 5 })).Single();
 
-				Assert.AreEqual(1, byBinary.ID);
-				Assert.True(new byte[] { 1, 2, 3 }.SequenceEqual(byBinary.Array));
-				Assert.True(new byte[] { 4, 5 }.SequenceEqual(byBinary.Binary.ToArray()));
+					Assert.AreEqual(1, byArray.ID);
+					Assert.True(new byte[] { 1, 2, 3 }.SequenceEqual(byArray.Array));
+					Assert.True(new byte[] { 4, 5 }   .SequenceEqual(byArray.Binary.ToArray()));
+
+					Assert.AreEqual(1, byBinary.ID);
+					Assert.True(new byte[] { 1, 2, 3 }.SequenceEqual(byBinary.Array));
+					Assert.True(new byte[] { 4, 5 }   .SequenceEqual(byBinary.Binary.ToArray()));
+				}
 			}
 		}
 	}
