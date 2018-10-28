dd53c1051aa210488abbd7c3961669d066016ccf
MaceWindu <dmitry.github@lukashenko.com>
2018-10-28 15:35:39 +0100

    revert sqlite changes. will be added by separate pr

--- Source/LinqToDB/DataProvider/SQLite/SQLiteTools.cs
+++ Source/LinqToDB/DataProvider/SQLite/SQLiteTools.cs
@@ -163,58 +163,5 @@ namespace LinqToDB.DataProvider.SQLite
 		}
 
 		#endregion
-
-
-		/// <summary>
-		/// Executes "PRAGMA key = &lt;KEY&gt;" query to set database encryption key.
-		/// <seealso href="https://www.bricelam.net/2016/06/13/sqlite-encryption.html"/>.
-		/// </summary>
-		/// <param name="connection">Database connection to configure.</param>
-		/// <param name="key">Database encryption key.</param>
-		public static void PragmaKey(IDbConnection connection, string key)
-		{
-			string quotedKey = Quote(connection, key);
-
-			using (var command = connection.CreateCommand())
-			{
-				command.CommandText = "PRAGMA key = " + quotedKey;
-				command.ExecuteNonQuery();
-			}
-		}
-
-		private static string Quote(IDbConnection connection, string value)
-		{
-			using (var command = connection.CreateCommand())
-			{
-				command.CommandText = "SELECT quote($value);";
-
-				var parameter = command.CreateParameter();
-				parameter.ParameterName = "$value";
-				parameter.Value = value;
-				command.Parameters.Add(parameter);
-
-				return (string)command.ExecuteScalar();
-			}
-		}
-
-		/// <summary>
-		/// Executes "PRAGMA rekey = &lt;KEY&gt;" query to change database encryption key.
-		/// <seealso href="https://www.bricelam.net/2016/06/13/sqlite-encryption.html"/>.
-		/// </summary>
-		/// <param name="connection">Database connection to configure.</param>
-		/// <param name="key">New database encryption key or <c>null</c> to decrypt database.</param>
-		public static void PragmaRekey(IDbConnection connection, string key)
-		{
-			var quotedKey = "NULL";
-
-			if (key != null)
-				quotedKey = Quote(connection, key);
-
-			using (var command = connection.CreateCommand())
-			{
-				command.CommandText = "PRAGMA rekey = " + quotedKey;
-				command.ExecuteNonQuery();
-			}
-		}
 	}
 }
