16f7f48621e509bcb5740be0c418c8ff295122df
MaceWindu <MaceWindu@users.noreply.github.com>
2018-11-24 16:57:30 +0300

    Better DbProviderFactories support (#1410)
    * - add DbProviderFactories support to DynamicDataProviderBase
    
    - remove DbProviderFactories code from oracle.native/sap providers and use DbProviderFactories functionality
    
    * - add factory support to db2, informix, sql ce, Sybase
    
    - fix nugets/t4 templates to use installed providers
    
    * readd copylocal
    
    * fix DB2/Informix tests to work with installed provider assembly

--- Tests/Linq/TestsInitialization.cs
+++ Tests/Linq/TestsInitialization.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Data.Common;
+using System.Reflection;
 using NUnit.Framework;
 
 using Tests;
@@ -14,6 +15,22 @@ public class TestsInitialization
 	[OneTimeSetUp]
 	public void TestAssemblySetup()
 	{
+#if !NETSTANDARD1_6 && !NETSTANDARD2_0
+		// configure assembly redirect for referenced assemblies to use version from GAC
+		// this solves exception from provider-specific tests, when it tries to load version from redist folder
+		// but loaded from GAC assembly has other version
+		AppDomain.CurrentDomain.AssemblyResolve += (sender, args) =>
+		{
+			var requestedAssembly = new AssemblyName(args.Name);
+			if (requestedAssembly.Name == "IBM.Data.DB2")
+				return DbProviderFactories.GetFactory("IBM.Data.DB2").GetType().Assembly;
+			if (requestedAssembly.Name == "IBM.Data.Informix")
+				return DbProviderFactories.GetFactory("IBM.Data.Informix").GetType().Assembly;
+
+			return null;
+		};
+#endif
+
 		// register test provider
 		TestNoopProvider.Init();
 	}
