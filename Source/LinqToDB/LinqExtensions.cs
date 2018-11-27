17843112686f82f8b33dcb87b5eb7eac36199f55
Svyatoslav Danyliv <sdanyliv@gmail.com>
2018-11-27 18:23:00 +0200

    Fixed formatting.

--- Source/LinqToDB/LinqExtensions.cs
+++ Source/LinqToDB/LinqExtensions.cs
@@ -3035,13 +3035,13 @@ namespace LinqToDB
 
 		#region AsQueryable
 
-	    /// <summary>Converts a generic <see cref="T:System.Collections.Generic.IEnumerable`1" /> to Linq To DB query.</summary>
-	    /// <param name="source">A sequence to convert.</param>
+		/// <summary>Converts a generic <see cref="T:System.Collections.Generic.IEnumerable`1" /> to Linq To DB query.</summary>
+		/// <param name="source">A sequence to convert.</param>
 		/// <param name="dataContext">Database connection context.</param>
-	    /// <typeparam name="TElement">The type of the elements of <paramref name="source" />.</typeparam>
-	    /// <returns>An <see cref="T:System.Linq.IQueryable`1" /> that represents the input sequence.</returns>
-	    /// <exception cref="T:System.ArgumentNullException">
-	    /// <paramref name="source" /> is <see langword="null" />.</exception>
+		/// <typeparam name="TElement">The type of the elements of <paramref name="source" />.</typeparam>
+		/// <returns>An <see cref="T:System.Linq.IQueryable`1" /> that represents the input sequence.</returns>
+		/// <exception cref="T:System.ArgumentNullException">
+		/// <paramref name="source" /> is <see langword="null" />.</exception>
 		public static IQueryable<TElement> AsQueryable<TElement>(
 			[SqlQueryDependent] [NotNull] this IEnumerable<TElement> source, 
 			[NotNull]                          IDataContext          dataContext)
