22467e35b25a93cfe99e68e9cdc30134d2314401
Svyatoslav Danyliv <sdanyliv@gmail.com>
2018-12-07 15:34:38 +0200

    Redesigned parameter parsing for FromSql method.

--- Tests/Linq/Linq/FromSqlTests.cs
+++ Tests/Linq/Linq/FromSqlTests.cs
@@ -53,6 +53,58 @@ namespace Tests.Linq
 		}
 
 		[Test]
+		public void TestFormattable2([DataSources] string context)
+		{
+			using (var db = GetDataContext(context))
+			using (var table = db.CreateLocalTable(GenerateTestData()))
+			{
+				int startId = 5;
+				int endId   = 15;
+
+				var query = db.FromSql<SampleClass>($"SELECT * FROM sample_class where id >= {new DataParameter("startId", startId, DataType.Int64)} and id < {endId}");
+				var projection = query
+					.Where(c => c.Id > 10)
+					.Select(c => new { c.Value, c.Id })
+					.ToArray();
+
+				var expected = table
+					.Where(t => t.Id >= startId && t.Id < endId)
+					.Where(c => c.Id > 10)
+					.Select(c => new { c.Value, c.Id })
+					.ToArray();
+
+				Assert.AreEqual(expected, projection);
+			}
+		}
+
+		[Test]
+		public void TestFormattableSameParam([DataSources] string context)
+		{
+			using (var db = GetDataContext(context))
+			using (var table = db.CreateLocalTable(GenerateTestData()))
+			{
+				int startId = 5;
+				int endId   = 15;
+
+				var startIdParam = new DataParameter("startId", startId, DataType.Int64);
+				var query = db.FromSql<SampleClass>($"SELECT * FROM sample_class where id >= {startIdParam} and id < {endId}");
+				var queryWithProjection = query
+					.Where(c => c.Id > 10)
+					.Select(c => new { c.Value, c.Id });
+
+				var projection = queryWithProjection.ToArray();
+
+				var expected = table
+					.Where(t => t.Id >= startId && t.Id < endId)
+					.Where(c => c.Id > 10)
+					.Select(c => new { c.Value, c.Id })
+					.ToArray();
+
+				Assert.AreEqual(expected, projection);
+			}
+		}
+
+		[Test]
 		public void TestFormattableInExpr([DataSources] string context)
 		{
 			using (var db = GetDataContext(context))
@@ -64,6 +116,35 @@ namespace Tests.Linq
 				var query = 
 					from t in table 
 					from s in db.FromSql<SampleClass>($"SELECT * FROM sample_class where id >= {startId} and id < {endId}").InnerJoin(s => s.Id == t.Id)
+					select s;
+
+				var projection = query
+					.Where(c => c.Id > 10)
+					.Select(c => new { c.Value, c.Id })
+					.ToArray();
+
+				var expected = table
+					.Where(t => t.Id >= startId && t.Id < endId)
+					.Where(c => c.Id > 10)
+					.Select(c => new { c.Value, c.Id })
+					.ToArray();
+
+				Assert.AreEqual(expected, projection);
+			}
+		}	
+		
+		[Test]
+		public void TestFormattableInExpr2([DataSources] string context)
+		{
+			using (var db = GetDataContext(context))
+			using (var table = db.CreateLocalTable(GenerateTestData()))
+			{
+				int startId = 5;
+				int endId   = 15;
+
+				var query = 
+					from t in table 
+					from s in db.FromSql<SampleClass>($"SELECT * FROM sample_class where id >= {new DataParameter("startId", startId, DataType.Int64)} and id < {endId}").InnerJoin(s => s.Id == t.Id)
 					select s;
 
 				var projection = query
@@ -114,26 +195,31 @@ namespace Tests.Linq
 			using (var db = GetDataContext(context))
 			using (var table = db.CreateLocalTable(GenerateTestData()))
 			{
-				int startId = 5;
-				int endId   = 15;
+				void LocalTest(int startId, int endId)
+				{
+					var query =
+						from t in table
+						from s in db.FromSql<SampleClass>("SELECT * FROM sample_class where id >= {0} and id < {1}",
+							new DataParameter("startId", startId, DataType.Int64), endId).InnerJoin(s => s.Id == t.Id)
+						select s;
 
-				var query =
-					from t in table 
-					from s in db.FromSql<SampleClass>("SELECT * FROM sample_class where id >= {0} and id < {1}", new DataParameter("startId", startId, DataType.Int64), endId).InnerJoin(s => s.Id == t.Id)
-					select s;
+					var projection = query
+						.Where(c => c.Id > 10)
+						.Select(c => new { c.Value, c.Id })
+						.ToArray();
 
-				var projection = query
-					.Where(c => c.Id > 10)
-					.Select(c => new { c.Value, c.Id })
-					.ToArray();
+					var expected = table
+						.Where(t => t.Id >= startId && t.Id < endId)
+						.Where(c => c.Id > 10)
+						.Select(c => new { c.Value, c.Id })
+						.ToArray();
 
-				var expected = table
-					.Where(t => t.Id >= startId && t.Id < endId)
-					.Where(c => c.Id > 10)
-					.Select(c => new { c.Value, c.Id })
-					.ToArray();
+					Assert.AreEqual(expected, projection);
+				}
 
-				Assert.AreEqual(expected, projection);
+				LocalTest(5, 15);
+				LocalTest(1, 6);
+
 			}
 		}
 
