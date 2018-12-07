8ec7b5d2aa97b35816d72283c71136fff951f76f
MaceWindu <MaceWindu@users.noreply.github.com>
2018-12-07 11:32:07 +0100

    remove remaining bad type name

--- Source/LinqToDB/DataProvider/PostgreSQL/PostgreSQLDataProvider.cs
+++ Source/LinqToDB/DataProvider/PostgreSQL/PostgreSQLDataProvider.cs
@@ -566,7 +566,7 @@ namespace LinqToDB.DataProvider.PostgreSQL
 			if (dbType.StartsWith("numeric(") || dbType.StartsWith("decimal"))
 				dbType = "numeric";
 
-			if (dbType.StartsWith("varchar varying(") || dbType.StartsWith("varchar(") || dbType.StartsWith("character varying("))
+			if (dbType.StartsWith("varchar(") || dbType.StartsWith("character varying("))
 				dbType = "character varying";
 
 			if (dbType.StartsWith("char(") || dbType.StartsWith("character("))
