7f290eb5a82df003b698c346bbcb01811f9abd0f
Svyatoslav Danyliv <sdanyliv@gmail.com>
2019-01-16 14:53:02 +0200

    Fix for #1543. Bad type has been passed for discovering ExpressionMethodAttribute.
    (cherry picked from commit 3de1f7bbfed78b426d0d7fdbfa05bc9ec3b5fc32)

--- Source/LinqToDB/Linq/Builder/ExpressionBuilder.cs
+++ Source/LinqToDB/Linq/Builder/ExpressionBuilder.cs
@@ -481,7 +481,7 @@ namespace LinqToDB.Linq.Builder
 					case ExpressionType.MemberAccess:
 						{
 							var me = (MemberExpression)expr;
-							var l  = ConvertMethodExpression(me.Member.ReflectedTypeEx(), me.Member);
+							var l  = ConvertMethodExpression(me.Expression?.Type ?? me.Member.ReflectedTypeEx(), me.Member);
 
 							if (l != null)
 							{
@@ -677,7 +677,7 @@ namespace LinqToDB.Linq.Builder
 						}
 						else
 						{
-							var l = ConvertMethodExpression(call.Method.ReflectedTypeEx(), call.Method);
+							var l = ConvertMethodExpression(call.Object?.Type ?? call.Method.ReflectedTypeEx(), call.Method);
 
 							if (l != null)
 								return new TransformInfo(OptimizeExpression(ConvertMethod(call, l)));
