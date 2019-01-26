4bc2aa53a1e180c4c4c3b2fde9836d292209978c
Igor Tkachev <igor.tkachev@gmail.com>
2019-01-26 13:27:19 -0500

    Added test to reproduce the issue.

---
+++ Tests/Linq/UserTests/Issue1556Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+using LinqToDB;
+
+using NUnit.Framework;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue1556Tests : TestBase
+	{
+		[Test]
+		public void Issue1556Test([DataSources(ProviderName.Sybase, ProviderName.OracleNative)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var records = db.Parent
+					.InnerJoin(db.Child,
+						(p,c) => p.ParentID == c.ParentID || c.GrandChildren.Any(y => y.ParentID == p.ParentID),
+						(p,c) => new { p, c })
+					.ToList();
+			}
+		}
+	}
+}
