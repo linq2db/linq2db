3f55475c261c44cc0acf379ec5ff3b63b57cdf63
GonzoSpire <41677129+GonzoSpire@users.noreply.github.com>
2019-03-03 17:56:16 +0300

    fix#1513  Add constr for DataContext(providerName, connectionString) (#1612)
    * fix#1513  Add constructor for DataContex(providerName, connectionString) and Tests
    
    fix #1513
    
    * fix
    
    * test fix
    
    * formatting fix
    
    * 1

--- Tests/Linq/Linq/DataContextTests.cs
+++ Tests/Linq/Linq/DataContextTests.cs
@@ -71,5 +71,20 @@ namespace Tests.Linq
 				ctx.KeepConnectionAlive = false;
 			}
 		}
+
+		[Test]
+		public void issue1513Test([DataSources(false)]string context)
+		{
+			using (var db = (TestDataConnection)GetDataContext(context))
+			{
+				var _db  = new DataContext(db.DataProvider.Name, db.ConnectionString);
+				var _db1 = new DataContext(db.DataProvider.Name, "FakeConnectionString");
+
+				Assert.AreEqual(db.DataProvider.Name, _db.DataProvider.Name);
+				Assert.AreEqual(db.ConnectionString, _db.ConnectionString);
+				Assert.Throws(typeof(LinqToDBException), () => new DataContext("FakeAdapter", db.ConnectionString));
+				Assert.Throws(typeof(System.ArgumentException), () => _db1.GetTable<Child>().FirstOrDefault());
+			}
+		}
 	}
 }
