1ad1dcaa6b66ef4fa12acb32dbb300a8ba6ad847
Svyatoslav Danyliv <sdanyliv@gmail.com>
2019-03-10 18:33:42 +0200

    Fix for #1546. Fixed problem with projection of nullable values. (#1618)
    * Fixed problem with projection of nullable values.
    
    * More strict logic to detect ToNullable analogue.
    
    * add more failing tests
    
    * Improved Nullable result handling.
    
    * Disabled tests for MySql, which do not support Having with non aggregate expression.
    
    * fix test filters
    
    * fix build

--- Source/LinqToDB/Expressions/ConvertFromDataReaderExpression.cs
+++ Source/LinqToDB/Expressions/ConvertFromDataReaderExpression.cs
@@ -24,7 +24,7 @@ namespace LinqToDB.Expressions
 		readonly int          _idx;
 		readonly Expression   _dataReaderParam;
 		readonly IDataContext _dataContext;
-		readonly Type         _type;
+		         Type         _type;
 
 		public override Type           Type      => _type;
 		public override ExpressionType NodeType  => ExpressionType.Extension;
@@ -212,5 +212,17 @@ namespace LinqToDB.Expressions
 		{
 			return $"ConvertFromDataReaderExpression<{_type.Name}>({_idx})";
 		}
+
+		public ConvertFromDataReaderExpression MakeNullable()
+		{
+			if (Type.IsValueTypeEx())
+			{
+				var type = typeof(Nullable<>).MakeGenericType(Type);
+				return new ConvertFromDataReaderExpression(type, _idx, _dataReaderParam, _dataContext);
+			}
+
+			return this;
+		}
+
 	}
 }
