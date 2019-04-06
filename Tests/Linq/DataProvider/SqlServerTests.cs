ad4b2ca839240080d01482c1005228e8aa24c3f3
GonzoSpire <41677129+GonzoSpire@users.noreply.github.com>
2019-04-06 16:55:25 +0300

    issue_1613  Test (#1623)
    * #1613  Test
    
    * using CrateLocalTable
    
    * format
    
    * add 2 tests
    
    * fix test
    
    * Update active issue reference

--- Tests/Linq/DataProvider/SqlServerTests.cs
+++ Tests/Linq/DataProvider/SqlServerTests.cs
@@ -1326,5 +1326,72 @@ namespace Tests.DataProvider
 			}
 		}
 #endif
+		[Table("Issue1613")]
+		private class Issue1613Table
+		{
+			[Column("dt"), Nullable] 
+			public DateTimeOffset? DateTimeOffset { get; set; }
+		}
+
+		private static Issue1613Table[] GenerateData()
+		{
+			var sampleData = new[]
+			{
+				new Issue1613Table { DateTimeOffset = null },
+				new Issue1613Table { DateTimeOffset = DateTimeOffset.Now.AddDays(1) },
+				new Issue1613Table { DateTimeOffset = DateTimeOffset.Now.AddDays(2) },
+				new Issue1613Table { DateTimeOffset = DateTimeOffset.Now.AddDays(3) },
+				new Issue1613Table { DateTimeOffset = DateTimeOffset.Now.AddDays(4) }
+			};
+			return sampleData;
+		}
+
+		[Test]
+		public void Issue1613Test1([AllSqlServerDataContext] string context)
+		{
+			using (var db = GetDataContext(context))
+			using (var table = db.CreateLocalTable(GenerateData()))
+			{ 
+
+				var query1 = table.GroupBy(x => x.DateTimeOffset).Select(g => g.Key).ToList();
+				var query2 = table.Select(r => r.DateTimeOffset).ToList();
+
+				Assert.AreEqual(5, query1.Count);
+				Assert.AreEqual(5, query2.Count);
+				Assert.AreEqual(query1, query2);
+			}
+		}
+		
+		[Test, ActiveIssue(1666)]
+		public void Issue1613Test2([AllSqlServerDataContext] string context)
+		{
+			using (var db = GetDataContext(context))
+			using (var table = db.CreateLocalTable(GenerateData()))
+			{ 
+
+				var query1 = table.GroupBy(x => x.DateTimeOffset.Value.Date).Select(g => g.Key).ToList();
+				var query2 = table.Select(r => r.DateTimeOffset.Value.Date).ToList();
+
+				Assert.AreEqual(5, query1.Count);
+				Assert.AreEqual(5, query2.Count);
+				Assert.AreEqual(query1, query2);
+			}
+		}
+
+		[Test, ActiveIssue(1666)]
+		public void Issue1613Test3([AllSqlServerDataContext] string context)
+		{
+			using (var db = GetDataContext(context))
+			using (var table = db.CreateLocalTable(GenerateData()))
+			{ 
+
+				var query1 = table.GroupBy(x => x.DateTimeOffset.Value.TimeOfDay).Select(g => g.Key).ToList();
+				var query2 = table.Select(r => r.DateTimeOffset.Value.TimeOfDay).ToList();
+
+				Assert.AreEqual(5, query1.Count);
+				Assert.AreEqual(5, query2.Count);
+				Assert.AreEqual(query1, query2);
+			}
+		}
 	}
 }
