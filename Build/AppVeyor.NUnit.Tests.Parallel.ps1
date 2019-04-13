33412689bde2a514e2063930ad8fc615696dd306
MaceWindu <MaceWindu@users.noreply.github.com>
2019-04-13 17:56:02 +0200

    another attempt to fix

--- Build/AppVeyor.NUnit.Tests.Parallel.ps1
+++ Build/AppVeyor.NUnit.Tests.Parallel.ps1
@@ -1,42 +1,40 @@
 $wc = New-Object System.Net.WebClient
 
 $net46Tests = {
-	param($url)
-	$logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_net46_results.xml"
+	param($logFileName)
 	$null = nunit3-console Tests\Linq\bin\AppVeyor\net46\linq2db.Tests.dll --result=$logFileName --where "cat != Ignored & cat != SkipCI"
 	return $LastExitCode
 }
 
 $netcore2Tests = {
-	param($url)
-	$logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_core2_results.xml"
+	param($logFileName)
 	$null = dotnet test Tests\Linq\ -f netcoreapp2.0 --logger "trx;LogFileName=$logFileName" --filter "TestCategory != Ignored & TestCategory != ActiveIssue & TestCategory != SkipCI" -c AppVeyor
 	return $LastExitCode
 }
 
 $netcore1Tests = {
-	param($url)
-	$logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_core1_results.xml"
+	param($logFileName)
 	$null = dotnet test Tests\Linq\ -f netcoreapp1.0 --logger "trx;LogFileName=$logFileName" --filter "TestCategory != Ignored & TestCategory != ActiveIssue & TestCategory != SkipCI" -c AppVeyor
 	return $LastExitCode
 }
 
-$url = "https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID"
-Start-Job $net46Tests -ArgumentList $url
-Start-Job $netcore2Tests -ArgumentList $url
-Start-Job $netcore1Tests -ArgumentList $url
+$logFileNameNet45 = "$env:APPVEYOR_BUILD_FOLDER\nunit_net46_results.xml"
+$logFileNameCore2 = "$env:APPVEYOR_BUILD_FOLDER\nunit_core2_results.xml"
+$logFileNameCore1 = "$env:APPVEYOR_BUILD_FOLDER\nunit_core1_results.xml"
+
+Start-Job $net46Tests  -ArgumentList $logFileNameNet45
+Start-Job $netcore2Tests  -ArgumentList $logFileNameCore2
+Start-Job $netcore1Tests  -ArgumentList $logFileNameCore1
 
 While (Get-Job -State "Running")
 {
   Start-Sleep 1
 }
 
-$logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_net46_results.xml"
-#$wc.UploadFile("$url", "$logFileName")
-$logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_core2_results.xml"
-$wc.UploadFile("$url", "$logFileName")
-$logFileName = "$env:APPVEYOR_BUILD_FOLDER\nunit_core1_results.xml"
-$wc.UploadFile("$url", "$logFileName")
+$url = "https://ci.appveyor.com/api/testresults/nunit3/$env:APPVEYOR_JOB_ID"
+#$wc.UploadFile($url, $logFileNameNet45)
+$wc.UploadFile($url, $logFileNameCore2)
+$wc.UploadFile($url, $logFileNameCore1)
 
 $exit = (Get-Job | Receive-Job | Measure-Object -Sum).Sum
 
