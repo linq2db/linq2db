569332f324c5034ab64ccb974219fcb8cc30676d
MaceWindu <MaceWindu@users.noreply.github.com>
2019-04-16 09:16:43 +0200

    try to locate evil addin

--- Build/AppVeyor.NUnit.Tests.ps1
+++ Build/AppVeyor.NUnit.Tests.ps1
@@ -35,9 +35,21 @@ $logFileNameNet45 = "$env:APPVEYOR_BUILD_FOLDER\net46_test_results.xml"
 $logFileNameCore2 = "$env:APPVEYOR_BUILD_FOLDER\core2_test_results.trx"
 $logFileNameCore1 = "$env:APPVEYOR_BUILD_FOLDER\core1_test_results.trx"
 
+# disable slow (and so useless) integration with tests tab
+Write-Host "Search for appveyor.addins file...
+$matches = $env:PATH.Split(';') | %{ join-path $_ "appveyor.addins"} | ?{ test-path $_ }
+
+if ($matches.Length -eq 0){
+	"Nothing found"
+} else {
+	$matches
+}
+
+#Remove-Item "$env:SYSTEMDRIVE\Tools\NUnit3\bin\appveyor.addins"
+
 $dir = Get-Location
-#Start-Job -Name "netfx_tests" $net46Tests -ArgumentList $dir,$logFileNameNet45
-Start-Job -Name "netcore_2_tests" $netcore2Tests -ArgumentList $dir,$logFileNameCore2
+Start-Job -Name "netfx_tests" $net46Tests -ArgumentList $dir,$logFileNameNet45
+#Start-Job -Name "netcore_2_tests" $netcore2Tests -ArgumentList $dir,$logFileNameCore2
 #Start-Job -Name "netcore_1_tests" $netcore1Tests -ArgumentList $dir,$logFileNameCore1
 
 While (Get-Job -State "Running")
