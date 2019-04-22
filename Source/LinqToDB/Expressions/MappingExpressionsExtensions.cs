a74a09ac93b11a5274c9e77c17df4a062ab68efe
MaceWindu <MaceWindu@users.noreply.github.com>
2019-04-22 11:22:01 +0200

    refactor if to switch

--- Source/LinqToDB/Expressions/MappingExpressionsExtensions.cs
+++ Source/LinqToDB/Expressions/MappingExpressionsExtensions.cs
@@ -18,36 +18,37 @@ namespace LinqToDB.Extensions
 			if (members.Length > 1)
 				throw new LinqToDBException($"Ambiguous members '{memberName}' for type '{type.Name}' has been found");
 
-			if (members[0] is PropertyInfo propInfo)
+			switch (members[0])
 			{
-				var value = propInfo.GetValue(null, null);
-				if (value == null)
-					return null;
+				case PropertyInfo propInfo:
+					{
+						var value = propInfo.GetValue(null, null);
+						if (value == null)
+							return null;
 
-				if (value is TExpression expression)
-					return expression;
+						if (value is TExpression expression)
+							return expression;
 
-				throw new LinqToDBException($"Property '{memberName}' for type '{type.Name}' should return expression");
+						throw new LinqToDBException($"Property '{memberName}' for type '{type.Name}' should return expression");
+					}
+				case MethodInfo method:
+					{
+						if (method.GetParameters().Length > 0)
+							throw new LinqToDBException($"Method '{memberName}' for type '{type.Name}' should have no parameters");
+
+						var value = method.Invoke(null, Array<object>.Empty);
+						if (value == null)
+							return null;
+
+						if (value is TExpression expression)
+							return expression;
+
+						throw new LinqToDBException($"Method '{memberName}' for type '{type.Name}' should return expression");
+					}
+				default:
+					throw new LinqToDBException(
+						$"Member '{memberName}' for type '{type.Name}' should be static property or method");
 			}
-			else
-			{
-				if (members[0] is MethodInfo method)
-				{
-					if (method.GetParameters().Length > 0)
-						throw new LinqToDBException($"Method '{memberName}' for type '{type.Name}' should have no parameters");
-					var value = method.Invoke(null, Array<object>.Empty);
-					if (value == null)
-						return null;
-
-					if (value is TExpression expression)
-						return expression;
-
-					throw new LinqToDBException($"Method '{memberName}' for type '{type.Name}' should return expression");
-				}
-			}
-
-			throw new LinqToDBException(
-				$"Member '{memberName}' for type '{type.Name}' should be static property or method");
 		}
 	}
 }
