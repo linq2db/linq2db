c630947640b61f806e58c2943de182be8298e41a
Svyatoslav Danyliv <sdanyliv@gmail.com>
2019-05-24 15:11:14 +0300

    Added additional Conditions for Union projection generation.

---
+++ Tests/Linq/UserTests/IndexOutOfRangeInUnions.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using LinqToDB;
+using NUnit.Framework;
+
+namespace Tests.UserTests
+{
+	public class IndexOutOfRangeInUnions : TestBase
+	{
+		[Test]
+		public void ErrorInUinon([DataSources] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				IQueryable<ClassTypeOfResult> query = null;
+
+				foreach (var tipoDeDocumento in Enumerable.Range(1, 2))
+				{
+					var innerQuery =
+						from doSap in db.GetTable<ClassTypeOne>()
+							.TableName($"O{tipoDeDocumento}")
+						select new ClassTypeOfResult
+						{
+							NumeroInterno   = doSap.DocEntry,
+							StatusValor     = doSap.DocStatus == "O" ? "Aberto" : "Fechado",
+							DescricaoStatus = "Manual/Externo",
+						};
+
+					query = query?.Union(innerQuery) ?? innerQuery;
+				}
+
+				var resultado = query?.ToString();
+				Console.WriteLine(resultado);
+			
+				Assert.IsNotEmpty(resultado);
+			}
+
+		}
+
+		public class ClassTypeOne
+		{
+			public int DocEntry { get; set; }
+			public int BplId { get; set; }
+			public string ChaveAcesso { get; set; }
+			public string DocStatus { get; set; }
+		}
+
+		public class ClassTypeOfResult
+		{
+			/// <summary>
+			/// DocEntry
+			/// </summary>
+			public int NumeroInterno { get; set; }
+			public string ChaveDeAcesso { get; set; }
+
+			public string StatusValor { get; set; }
+			public string DescricaoStatus { get; set; }
+		}
+	}
+}
