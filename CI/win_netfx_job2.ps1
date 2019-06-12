7ccb5abdee888da3645d769fb706f6420cc20ab6
siarhei.hanich <siarhei.hanich@soft-fx.lv>
2019-06-12 12:41:32 +0300

    1

--- CI/win_netfx_job2.ps1
+++ CI/win_netfx_job2.ps1
@@ -1,1 +1,9 @@
 Move-Item -Path "win_netfx_job2.json" -Destination "DataProviders.json" -Force
+
+
+$sqlInstance = "(local)\SQL2014"
+
+# create databases
+$dbName = "TestDatanet45"
+sqlcmd -S "$sqlInstance" -Q "Use [master]; CREATE DATABASE [$dbName]"
+sqlcmd -U sa -P Password12! -S "$sqlInstance" -i ".\Data\Create Scripts\Northwind.sql" > nul
