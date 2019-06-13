add13b3b922e1e13fde2f4da4f480f4d24ae4e1b
siarhei.hanich <siarhei.hanich@soft-fx.lv>
2019-06-13 17:53:21 +0300

    1

--- CI/win_netfx_job1.ps1
+++ CI/win_netfx_job1.ps1
@@ -1,1 +1,5 @@
 Move-Item -Path "win_netfx_job1.json" -Destination "DataProviders.json" -Force
+
+#install postgresql
+choco install postgresql10 --force --params '/Password:Password12!'
+set PATH=%PATH%;"C:\Program Files\PostgreSQL\10\bin;C:\Program Files\PostgreSQL\10\lib"
