b55cffb75948c54e49094153a06b054b3f8b3a05
siarhei.hanich <siarhei.hanich@soft-fx.lv>
2019-06-19 13:43:23 +0300

    1

--- CI/lin_core2_job1.ps1
+++ CI/lin_core2_job1.ps1
@@ -1,1 +1,8 @@
 Move-Item -Path "lin_core2_job1.json" -Destination "DataProviders.json" -Force
+
+docker run -d --name postgres10 -e POSTGRES_PASSWORD=Password12! -p 5432:5432 -v /var/run/postgresql:/var/run/postgresql postgres:10
+until docker exec postgres10 psql -U postgres -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+docker exec postgres10 psql -U postgres -c "create database testdata"
