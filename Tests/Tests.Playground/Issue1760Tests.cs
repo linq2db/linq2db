823822529be1b298f5799b0bcb214da0d6c01ec2
Igor Tkachev <igor.tkachev@gmail.com>
2019-06-27 19:17:08 -0400

    Simplifying tests.Tests.
    LeftJoin method issue detected.

--- Tests/Tests.Playground/Issue1760Tests.cs
+++ Tests/Tests.Playground/Issue1760Tests.cs
@@ -1,8 +1,9 @@
-﻿using System.Linq;
-using System.Linq.Expressions;
+﻿using System;
+using System.Linq;
+
 using LinqToDB;
-using LinqToDB.Common;
 using LinqToDB.Mapping;
+
 using NUnit.Framework;
 
 namespace Tests.Playground
@@ -11,7 +12,7 @@ namespace Tests.Playground
 	public class Issue1760Tests : TestBase
 	{
 		[Table(Schema="dbo", Name="b_table2")]
-		public partial class BTable2
+		public class BTable2
 		{
 			[Column("id"),   PrimaryKey, Identity] public int    Id   { get; set; } // int
 			[Column("col1"), Nullable            ] public string Col1 { get; set; } // varchar(50)
@@ -20,13 +21,13 @@ namespace Tests.Playground
 		}
 
 		[Table(Schema="dbo", Name="commonTable")]
-		public partial class CommonTable
+		public class CommonTable
 		{
 			[Column("id"), PrimaryKey, Identity] public int Id { get; set; } // int
 		}
 
 		[Table(Schema="dbo", Name="c_table2")]
-		public partial class CTable2
+		public class CTable2
 		{
 			[Column("id"),          PrimaryKey, Identity] public int    Id        { get; set; } // int
 			[Column("col1"),        Nullable            ] public string Col1      { get; set; } // varchar(50)
@@ -34,17 +35,17 @@ namespace Tests.Playground
 		}
 
 		[Table(Schema="dbo", Name="table1")]
-		public partial class Table1
+		public class Table1
 		{
 			[Column("id"),            PrimaryKey,  Identity] public int  Id            { get; set; } // int
 			[Column("id_tbl2"),          Nullable          ] public int? IdTbl2        { get; set; } // int
 			[Column("id_tbl3"),          Nullable          ] public int? IdTbl3        { get; set; } // int
 			[Column("commonTableId"), NotNull              ] public int  CommonTableId { get; set; } // int
-			[Column("c_tb1l_Id"),        Nullable          ] public int? CTb1lId       { get; set; } // int
+			[Column("c_tb1l_Id"),        Nullable          ] public int? CTb1LId       { get; set; } // int
 		}
 
 		[Table(Schema="dbo", Name="table2")]
-		public partial class Table2
+		public class Table2
 		{
 			[Column("id"),      PrimaryKey, Identity] public int    Id      { get; set; } // int
 			[Column("textCol"), Nullable            ] public string TextCol { get; set; } // varchar(50)
@@ -52,43 +53,27 @@ namespace Tests.Playground
 		}
 
 		[Table(Schema="dbo", Name="table3")]
-		public partial class Table3
+		public class Table3
 		{
 			[Column("id"),  PrimaryKey, Identity] public int  Id  { get; set; } // int
 			[Column("col"), Nullable            ] public int? Col { get; set; } // int
-		}
-
-
-		class SomeClass
-		{
-			[PrimaryKey]
-			public int Id { get; set; }
-		}
-
-		class SomeOtherClass
-		{
-			[PrimaryKey]
-			public int Id { get; set; }
 		}
 
 		[Test]
 		public void OriginalTestSimplified([IncludeDataSources(TestProvName.AllSQLite)] string context)
 		{
 			using (var db = GetDataContext(context))
-			using (db.CreateLocalTable<BTable2>())
-			using (db.CreateLocalTable<CommonTable>())
-			using (db.CreateLocalTable<CTable2>())
 			using (db.CreateLocalTable<Table1>())
 			using (db.CreateLocalTable<Table2>())
 			using (db.CreateLocalTable<Table3>())
 			{
 				int id = 0;
 				var part1 = db.GetTable<Table1>()
-					.GroupBy(w => w.CTb1lId)
+					.GroupBy(w => w.CTb1LId)
 					.Select(w => new
 					{
 						Col3 = w.Key,
-						maxCol = w.Max(s => s.CTb1lId)
+						maxCol = w.Max(s => s.CTb1LId)
 					})
 					.LeftJoin(db.GetTable<Table3>(),
 						(allE, tbl3) => allE.maxCol == tbl3.Id,
@@ -97,10 +82,11 @@ namespace Tests.Playground
 						(allM, btbl) => allM.Col == btbl.Id,
 						(allF, btbl) => new {all = allF, btbl})
 					;
+
 				var general = db.GetTable<Table1>()
 					.Where(w => w.CommonTableId == id)
 					.LeftJoin(db.GetTable<Table2>(),
-						(t1, bt1) => t1.CTb1lId == bt1.Id,
+						(t1, bt1) => t1.CTb1LId == bt1.Id,
 						(t1, bt1) => new {t1, bt1})
 					.LeftJoin(part1,
 						(allA, ctb) => allA.bt1.Col3 == ctb.btbl.Id,
@@ -113,19 +99,15 @@ namespace Tests.Playground
 						s.ctb,
 					})
 					.Select(s => new {s.Key});
-				var ss = general.ToList();
 
+				_ = general.ToList();
 			}
 		}
-
 
 		[Test]
 		public void OriginalTest([IncludeDataSources(TestProvName.AllSQLite)] string context)
 		{
 			using (var db = GetDataContext(context))
-			using (db.CreateLocalTable<BTable2>())
-			using (db.CreateLocalTable<CommonTable>())
-			using (db.CreateLocalTable<CTable2>())
 			using (db.CreateLocalTable<Table1>())
 			using (db.CreateLocalTable<Table2>())
 			using (db.CreateLocalTable<Table3>())
@@ -159,10 +141,11 @@ namespace Tests.Playground
 						b2 = w.btbl.Col2 ?? string.Empty,
 						b3 = w.btbl.Col3 ?? string.Empty,
 					});
+
 				var general = db.GetTable<Table1>()
 					.Where(w => w.CommonTableId == id)
 					.LeftJoin(db.GetTable<Table2>(),
-						(t1, bt1) => t1.CTb1lId == bt1.Id,
+						(t1, bt1) => t1.CTb1LId == bt1.Id,
 						(t1, bt1) => new {t1, bt1})
 					.LeftJoin(part1,
 						(allA, ctb) => allA.bt1.Col3 == ctb.c1,
@@ -178,8 +161,74 @@ namespace Tests.Playground
 						s.bt1.TextCol
 					})
 					.Select(s => new { s.Key.TextCol, s.Key.b1, s.Key.b2, s.Key.b3});
-				var ss = general.ToList();
 
+				_ = general.ToList();
+			}
+		}
+
+		[Test]
+		public void OriginalTest1([IncludeDataSources(TestProvName.AllSQLite)] string context)
+		{
+			using (var db = GetDataContext(context))
+			using (db.CreateLocalTable<Table1>())
+			using (db.CreateLocalTable<Table2>())
+			using (db.CreateLocalTable<Table3>())
+			{
+				var part1 = db.GetTable<Table1>()
+					.InnerJoin(db.GetTable<Table2>(),
+						(tbl1, tbl2) => tbl1.IdTbl2 == tbl2.Id,
+						(tbl1, tbl2) => new {tbl1, tbl2})
+					.GroupBy(w => w.tbl2.Col3)
+					.Select(w => new
+					{
+						Col3 = w.Key,
+						maxCol = w.Max(s => s.tbl1.Id)
+					})
+					.LeftJoin(db.GetTable<Table3>(),
+						(allE, tbl3) => allE.maxCol == tbl3.Id,
+						(allG, tbl3) => new {c1=allG.Col3, b1=tbl3.Col});
+
+				var general = db.GetTable<Table1>()
+					.LeftJoin(part1,
+						(allA, ctb) => allA.Id == ctb.c1,
+						(allB, ctb) => allB.Id);
+
+				_ = general.ToList();
+			}
+		}
+
+		[Test]
+		public void OriginalTest2([IncludeDataSources(TestProvName.AllSQLite)] string context)
+		{
+			using (var db = GetDataContext(context))
+			using (db.CreateLocalTable<Table1>())
+			using (db.CreateLocalTable<Table2>())
+			using (db.CreateLocalTable<Table3>())
+			{
+				var part1 = db.GetTable<Table1>()
+					.InnerJoin(db.GetTable<Table2>(),
+						(tbl1, tbl2) => tbl1.IdTbl2 == tbl2.Id,
+						(tbl1, tbl2) => new {tbl1, tbl2})
+					.InnerJoin(db.GetTable<Table3>(),
+						(all, tbl3) => all.tbl1.IdTbl3 == tbl3.Id,
+						(all, tbl3) => new {all.tbl2, tbl3})
+					.GroupBy(w => w.tbl2.Col3)
+					.Select(w => new
+					{
+						Col3 = w.Key,
+						maxCol = w.Max(s => s.tbl3.Id)
+					})
+					.LeftJoin(db.GetTable<Table3>(),
+						(allE, tbl3) => allE.maxCol == tbl3.Id,
+						(allG, tbl3) => new {c1=allG.Col3, b1=tbl3.Col});
+
+				var general =
+					from allA in db.GetTable<Table1>()
+					join ctb in part1 on allA.Id equals ctb.c1 into g
+					from ctb in g.DefaultIfEmpty()
+					select allA.Id;
+
+				_ = general.ToList();
 			}
 		}
 	}
