3c6d3073bc43cc73ffb96d7acd8189b8a0a0d3a2
GonzoSpire <41677129+GonzoSpire@users.noreply.github.com>
2019-07-07 10:31:15 +0300

    Fix weak joins optimization (#1793)
    * Add test for issue 1614
    
    #1614
    
    * format
    
    * fix

--- Source/LinqToDB/SqlQuery/SelectQueryOptimizer.cs
+++ Source/LinqToDB/SqlQuery/SelectQueryOptimizer.cs
@@ -635,9 +635,17 @@ namespace LinqToDB.SqlQuery
 
 			var areTablesCollected = false;
 
+			var visitor = new QueryVisitor();
+
+			void TableCollector(IQueryElement expr)
+			{
+				if (expr is SqlField field && !tables.Contains(field.Table))
+					tables.Add(field.Table);
+			}
+
 			_selectQuery.ForEachTable(table =>
 			{
-				for (var i = 0; i < table.Joins.Count; i++)
+				for (var i = table.Joins.Count - 1; i >= 0; i--)
 				{
 					var join = table.Joins[i];
 
@@ -646,14 +654,6 @@ namespace LinqToDB.SqlQuery
 						if (!areTablesCollected)
 						{
 							areTablesCollected = true;
-
-							void TableCollector(IQueryElement expr)
-							{
-								if (expr is SqlField field && !tables.Contains(field.Table))
-									tables.Add(field.Table);
-							}
-
-							var visitor = new QueryVisitor();
 
 							visitor.VisitAll(_selectQuery.Select,  TableCollector);
 							visitor.VisitAll(_selectQuery.Where,   TableCollector);
@@ -688,9 +688,11 @@ namespace LinqToDB.SqlQuery
 						else
 						{
 							table.Joins.RemoveAt(i);
-							i--;
+							continue;
 						}
 					}
+
+					visitor.VisitAll(join.Condition, TableCollector);
 				}
 			}, new HashSet<SelectQuery>());
 		}
