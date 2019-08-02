1fee11d70487db75e5e945d49567b98642c2b879
Igor Tkachev <igor.tkachev@gmail.com>
2019-08-01 22:03:32 -0400

    Unit tests for #1840.

--- Tests/Tests.Playground/TestTemplate.cs
+++ Tests/Tests.Playground/TestTemplate.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Linq;
+
+using LinqToDB;
 using LinqToDB.Mapping;
+
 using NUnit.Framework;
 
 namespace Tests.Playground
@@ -22,6 +25,24 @@ namespace Tests.Playground
 			using (var table = db.CreateLocalTable<SampleClass>())
 			{
 				var result = table.ToArray();
+			}
+		}
+
+		[Test]
+		public void Test([IncludeDataSources(
+			ProviderName.SqlCe, ProviderName.SqlServer2012, ProviderName.SqlServer2014)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var query =
+					from p in db.Parent
+					from c in db.Child.Take(1).DefaultIfEmpty()
+					select new
+					{
+						a = p.ParentID == 1 ? c != null ? "1" : "2" : "3"
+					};
+
+				_ = query.ToList();
 			}
 		}
 	}
