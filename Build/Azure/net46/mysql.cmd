86735b6fe79dd5ab33fd53277a21ce359c79d343
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-03 12:18:12 +0200

    mysql docker fixes

--- Build/Azure/net46/mysql.cmd
+++ Build/Azure/net46/mysql.cmd
@@ -1,3 +1,3 @@
 ﻿REM windows agent has only mysql command-line tools installed, no db itself
-docker run -d --name mysql57 --net host -p 3357:3306 mysql:5.7 --health-cmd='mysqladmin ping --silent' -e MYSQL_ROOT_PASSWORD=root
+docker run -d --name mysql57 --net host mysql:5.7 --health-cmd='mysqladmin ping --silent' -e MYSQL_ROOT_PASSWORD=root
 docker exec mysql57 mysql -e 'CREATE DATABASE testdata DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;' -uroot -proot
