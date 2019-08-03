3c666251500af4bf61b25f2e80054b4d5bb495d7
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-03 13:05:59 +0200

    add pgsql

--- Build/Azure/netcoreapp20/mysql.sh
+++
@@ -1,3 +0,0 @@
-﻿#!/bin/bash
-docker run -d --name mysql57 --net host -p 3357:3306 mysql:5.7 --health-cmd='mysqladmin ping --silent' -e MYSQL_ROOT_PASSWORD=root
-docker exec mysql57 mysql -e 'CREATE DATABASE testdata DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;' -uroot -proot
---
+++ Build/Azure/scripts/pgsql.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/bash
+set -x
+docker run -d --name pgsql --net host -p 5432:5432 postgres:11 -e POSTGRES_PASSWORD=Password12!
+until docker exec pgsql psql -U postgres -c '\l'; do
+    >&2 echo "waiting for pgsql..."
+    sleep 1
+done
+docker exec pgsql pgsql -U postgres -c 'create database testdata'
