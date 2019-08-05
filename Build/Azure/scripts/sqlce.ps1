47a692511c00e0ac0363497c51d56e131e7ba1c3
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-05 16:26:22 +0200

    cmon...

--- Build/Azure/scripts/sqlce.ps1
+++ Build/Azure/scripts/sqlce.ps1
@@ -1,2 +1,2 @@
 ﻿Invoke-WebRequest -Uri https://download.microsoft.com/download/F/F/D/FFDF76E3-9E55-41DA-A750-1798B971936C/ENU/SSCERuntime_x86-ENU.exe -OutFile SSCERuntime_x86-ENU.exe
-SSCERuntime_x86-ENU.exe /quiet /qn
+.\SSCERuntime_x86-ENU.exe /quiet /qn
