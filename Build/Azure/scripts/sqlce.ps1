1e776acd708fc91fb2d034249632419a2dfa5052
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-05 17:59:33 +0200

    maybe this?

--- Build/Azure/scripts/sqlce.ps1
+++ Build/Azure/scripts/sqlce.ps1
@@ -1,2 +1,3 @@
 ﻿Invoke-WebRequest -Uri https://download.microsoft.com/download/F/F/D/FFDF76E3-9E55-41DA-A750-1798B971936C/ENU/SSCERuntime_x64-ENU.exe -OutFile SSCERuntime_x64-ENU.exe
 .\SSCERuntime_x64-ENU.exe /quiet /qn
+Copy-Item "c:\Program Files (x86)\Microsoft SQL Server Compact Edition\v4.0\Private\*" .\ -Recurse -Include *
