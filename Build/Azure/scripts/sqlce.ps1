e6a4bc4520d897902c7b210b902358c3bb009c08
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-06 09:54:32 +0200

    try another install

--- Build/Azure/scripts/sqlce.ps1
+++ Build/Azure/scripts/sqlce.ps1
@@ -1,6 +1,9 @@
-﻿Invoke-WebRequest -Uri https://download.microsoft.com/download/F/F/D/FFDF76E3-9E55-41DA-A750-1798B971936C/ENU/SSCERuntime_x64-ENU.exe -OutFile SSCERuntime_x64-ENU.exe
-.\SSCERuntime_x64-ENU.exe \x:sqlce
-$process = Start-Process -FilePath msiexec.exe -ArgumentList ('/i', '.\sqlce\SSCERuntime_x64-ENU.msi', '/qn', '/lxv', 'install.log') -Wait -PassThru
+﻿#Invoke-WebRequest -Uri https://download.microsoft.com/download/F/F/D/FFDF76E3-9E55-41DA-A750-1798B971936C/ENU/SSCERuntime_x64-ENU.exe -OutFile SSCERuntime_x64-ENU.exe
+#.\SSCERuntime_x64-ENU.exe \x:sqlce
+
+Invoke-WebRequest -Uri https://download.microsoft.com/download/6/9/5/695ED23C-F40F-420C-98D1-0F752B8D4E2B/1033/x64/SSCERuntime_x64-ENU.msi -OutFile SSCERuntime_x64-ENU.msi
+
+$process = Start-Process -FilePath msiexec.exe -ArgumentList ('/i', 'SSCERuntime_x64-ENU.msi', '/qn', '/lxv', 'install.log') -Wait -PassThru
 $exitCode = $process.ExitCode
 
 Get-Content 'install.log'
