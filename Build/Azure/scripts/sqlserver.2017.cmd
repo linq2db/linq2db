3f9575e1451953e0779d21a400cb9a182a4395d0
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-10 14:17:05 +0200

    fix sql path

--- Build/Azure/scripts/sqlserver.2017.cmd
+++ Build/Azure/scripts/sqlserver.2017.cmd
@@ -3,5 +3,5 @@ docker run -d -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h ms
 docker ps -a
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData;'
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData2017;'
-docker cp scripts/sql/northwind.sql mssql:/northwind.sql
+docker cp scripts/northwind.sql mssql:/northwind.sql
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -i /northwind.sql
--- Build/Azure/scripts/sqlserver.2017.sh
+++ Build/Azure/scripts/sqlserver.2017.sh
@@ -4,5 +4,5 @@ docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql
 docker ps -a
 docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData;'
 docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData2017;'
-docker cp scripts/sql/northwind.sql mssql:/northwind.sql
+docker cp scripts/northwind.sql mssql:/northwind.sql
 docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -i /northwind.sql
