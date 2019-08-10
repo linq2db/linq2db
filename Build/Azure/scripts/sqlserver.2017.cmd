6d7bf9129bb228ed1e34b8efe847d7a007b1a9e9
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-10 13:22:54 +0200

    fix shell files

--- Build/Azure/scripts/sqlserver.2017.cmd
+++ Build/Azure/scripts/sqlserver.2017.cmd
@@ -1,10 +1,10 @@
 ﻿docker pull microsoft/mssql-server-windows-express:2017-latest
-docker run -d -it -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql microsoft/mssql-server-windows-express:2017-latest
+docker run -d -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql microsoft/mssql-server-windows-express:2017-latest
 docker ps -a
 sleep 5
-docker exec -it mssql sqlcmd -S localhost -U sa -P Password12! -Q 'select @@Version'
-docker exec -it mssql sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData;'
-docker exec -it mssql sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData2017;'
-docker exec -it mssql sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE NorthwindDB;'
+docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q 'select @@Version'
+docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData;'
+docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData2017;'
+docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE NorthwindDB;'
 docker cp scripts/sql/northwind.sql mssql:/northwind.sql
-docker exec -it mssql sqlcmd -S localhost -U sa -P Password12! -i /northwind.sql
+docker exec mssql sqlcmd -S localhost -U sa -P Password12! -i /northwind.sql
--- Build/Azure/scripts/sqlserver.2017.sh
+++ Build/Azure/scripts/sqlserver.2017.sh
@@ -1,11 +1,11 @@
-﻿#!/bin/bash
+#!/bin/bash
 docker pull mcr.microsoft.com/mssql/server:2017-latest
-docker run -it -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql -d mcr.microsoft.com/mssql/server:2017-latest
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql -d mcr.microsoft.com/mssql/server:2017-latest
 docker ps -a
 sleep 5
-docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'select @@Version'
-docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData;'
-docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData2017;'
-docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE NorthwindDB;'
+docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'select @@Version'
+docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData;'
+docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData2017;'
+docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE NorthwindDB;'
 docker cp scripts/sql/northwind.sql mssql:/northwind.sql
-docker exec -it mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -i /northwind.sql
+docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -i /northwind.sql
