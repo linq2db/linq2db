30396cf0715ad738b52a97ba447793ba627f9a40
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-11 17:58:13 +0200

    wat

--- Build/Azure/scripts/pgsql10.sh
+++ Build/Azure/scripts/pgsql10.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker run -d --name postgres10 --net host -e POSTGRES_PASSWORD=Password12! -p 5432:5432 -v /var/run/postgresql:/var/run/postgresql postgres:10
-until docker exec postgres10 psql -U postgres -c '\l'; do
+docker run -d --name pgsql --net host -e POSTGRES_PASSWORD=Password12! -p 5432:5432 -v /var/run/postgresql:/var/run/postgresql postgres:10
+until docker exec pgsql psql -U postgres -c '\l'; do
 >&2 echo "Postgres is unavailable - sleeping"
 sleep 5
 done
-docker exec postgres10 psql -U postgres -c 'create database testdata'
-docker exec postgres10 psql -U postgres -c '\l'
+docker exec pgsql psql -U postgres -c 'create database testdata'
+docker exec pgsql psql -U postgres -c '\l'
