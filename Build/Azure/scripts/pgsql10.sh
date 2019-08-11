b082c11b4d7fd31dda20b2c71c65251b96e0cb9a
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-11 17:46:47 +0200

    1

--- Build/Azure/scripts/pgsql10.sh
+++ Build/Azure/scripts/pgsql10.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-docker pull postgres:10
-docker run -d --name pgsql --net host postgres:10 -e POSTGRES_PASSWORD=Password12! -e POSTGRES_DB=testdata -p 5432:5432 -v /var/run/postgresql:/var/run/postgresql
-docker ps -a
-
-# Wait for start
-echo "Waiting for PostgreSQL started"
-docker exec pgsql psql -U postgres -c 'select 1'
-is_up=$?
-while [ $is_up -ne 0 ] ; do
-    docker exec pgsql psql -U postgres -c 'select 1'
-    is_up=$?
+docker run -d --name postgres10 --net host -e POSTGRES_PASSWORD=Password12! -p 5432:5432 -v /var/run/postgresql:/var/run/postgresql postgres:10
+until docker exec postgres10 psql -U postgres -c '\l'; do
+>&2 echo "Postgres is unavailable - sleeping"
+sleep 5
 done
-echo "PostgreSQL is operational"
+docker exec postgres10 psql -U postgres -c 'create database testdata'
+docker exec postgres10 psql -U postgres -c '\l'
