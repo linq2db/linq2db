9717c0a1f53ff9c4bd6f6ed3acfcd4afe99ae5d1
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-12 09:13:40 +0200

    1

--- Build/Azure/scripts/mac.pgsql.sh
+++ Build/Azure/scripts/mac.pgsql.sh
@@ -15,7 +15,7 @@ echo "Waiting for docker service to be in the running state"
 done
 
 #docker pull postgres:12
-docker run -d --name pgsql --net host -e POSTGRES_PASSWORD=Password12! -p 5432:5432 -v /var/run/postgresql:/var/run/postgresql postgres:12
+docker run -d --name pgsql -h pgsql -e POSTGRES_PASSWORD=Password12! -p 5432:5432 -v pgdb:/var/run/postgresql postgres:12
 until docker exec pgsql psql -U postgres -c '\l'; do
 >&2 echo "Postgres is unavailable - sleeping"
 sleep 1
