6df1437b92f35d646e512da133d12a367b7293a0
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-12 17:49:47 +0200

    fix asserts

--- Tests/Linq/Update/MergeTests.Types.cs
+++ Tests/Linq/Update/MergeTests.Types.cs
@@ -661,7 +661,7 @@ namespace Tests.xUpdate
 						var msecs = expected.Value.Milliseconds;
 						if (msecs > 500)
 						{
-							expected.Value.Add(TimeSpan.FromSeconds(1));
+							expected = expected.Value.Add(TimeSpan.FromSeconds(1));
 						}
 
 						expected = TimeSpan.FromTicks((expected.Value.Ticks / 10000000) * 10000000);
