24696afa6947dc38ea2b8a53c70bc4967bec125f
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-12 10:27:29 +0200

    1

--- Build/Azure/scripts/mysql.sh
+++ Build/Azure/scripts/mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #docker pull mysql:latest
-docker run -d --name mysql mysql:latest -e MYSQL_ROOT_PASSWORD=root -p 33060:3306 -v /var/lib/mysql:/var/lib/mysql
+docker run -d --name mysql mysql:latest -e MYSQL_ROOT_PASSWORD=root -p 33060:3306 -v /var/lib/mysql:/var/lib/mysql --net host
 docker ps -a
 
 # Wait for start
