09c913112e24dc2cc66e537cb1179a7c9b0afeec
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-13 11:32:50 +0200

    test pgsql windows containers

---
+++ Build/Azure/scripts/test5.cmd
@@ -0,0 +1,21 @@
+docker run -d --name pgsql -e POSTGRES_PASSWORD=Password12! -p 5432:5432 madee/postgres:latest
+docker ps -a
+
+echo "Waiting"
+set max = 100
+:repeat
+echo pinging sql server
+sleep 1
+docker exec docker exec pgsql psql -U postgres -c "\l"
+set /a max=max-1
+if %max% EQU 0 goto fail
+if %errorlevel% NEQ 0 goto repeat
+echo "Container is UP"
+
+docker exec pgsql psql -U postgres -c "create database testdata"
+docker exec pgsql psql -U postgres -c "\l"
+docker exec pgsql psql -U postgres -c "SELECT version();"
+goto:eof
+
+:fail
+echo "Fail"
