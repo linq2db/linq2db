5411c8b57bf3a24321d1cd1121af9bf0420f96a5
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-13 14:29:26 +0200

    1

--- Build/Azure/scripts/pgsql10.cmd
+++ Build/Azure/scripts/pgsql10.cmd
@@ -1,4 +1,4 @@
-rem this is the only image for windows on dockerhub that worked
+rem this is the only image for windows on dockerhub that worked (mostly due to azure agents issues)
 rem we will use it for now while we don't have our images
 rem runs pgsql 10.5
 docker run -d --name pgsql -h pgsql -e POSTGRES_PASSWORD=Password12! -p 5432:5432 dominiquesavoie/postgres:windows-latest
@@ -7,13 +7,13 @@ docker ps -a
 echo "Waiting"
 set max = 100
 :repeat
-echo pinging sql server
+echo pinging postgres
 sleep 1
 docker exec pgsql psql -U postgres -c "\l"
 set /a max=max-1
 if %max% EQU 0 goto fail
 if %errorlevel% NEQ 0 goto repeat
-echo "Container is UP"
+echo "postgres is UP"
 
 docker exec pgsql psql -U postgres -c "create database testdata"
 docker exec pgsql psql -U postgres -c "\l"
---
+++ Build/Azure/scripts/test8.cmd
@@ -0,0 +1,23 @@
+docker run -d --name mysql -h mysql -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 danblan/mysql5.6:nanoserver
+docker ps -a
+
+echo "Waiting"
+set max = 100
+:repeat
+echo pinging
+sleep 1
+docker exec mysql mysql --protocol TCP -uroot -proot -e "show databases;"
+set /a max=max-1
+if %max% EQU 0 goto fail
+if %errorlevel% NEQ 0 goto repeat
+echo "Container is UP"
+
+docker exec mysql mysql -e "CREATE DATABASE testdata DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" -uroot -proot
+docker exec mysql mysql -e "CREATE DATABASE testdata2 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" -uroot -proot
+docker exec mysql mysql -e "SELECT VERSION();" -uroot -proot
+docker logs mysql
+goto:eof
+
+:fail
+echo "Fail"
+docker logs mysql
