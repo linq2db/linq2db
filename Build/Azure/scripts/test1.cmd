3e0a12ce73e2ef3f3a9c9120da5658991d80f246
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-13 16:42:28 +0200

    1

--- Build/Azure/scripts/test1.cmd
+++ Build/Azure/scripts/test1.cmd
@@ -13,7 +13,7 @@ if %max% EQU 0 goto fail
 if %errorlevel% NEQ 0 goto repeat
 echo "Container is UP"
 
-docker exec mysql mysql --protocol TCP -uroot -p54321 -e "ALTER USER 'root' IDENTIFIED BY 'root';"
+docker exec mysql mysql --protocol TCP -uroot -p54321 -e "ALTER USER USER() IDENTIFIED BY 'root';"
 docker exec mysql mysql -e "CREATE DATABASE testdata DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" -uroot -proot
 docker exec mysql mysql -e "CREATE DATABASE testdata2 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" -uroot -proot
 docker exec mysql mysql -e "SELECT VERSION();" -uroot -proot
