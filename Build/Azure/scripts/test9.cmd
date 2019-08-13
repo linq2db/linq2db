3d75176823c702ff14532dea792fef5af9230672
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-13 12:04:41 +0200

    1

--- Build/Azure/scripts/test9.cmd
+++ Build/Azure/scripts/test9.cmd
@@ -4,9 +4,9 @@ docker ps -a
 echo "Waiting"
 set max = 100
 :repeat
-echo pinging sql server
+echo pinging server
 sleep 1
-docker exec docker exec pgsql psql -U postgres -c "\l"
+docker exec pgsql psql -U postgres -c "\l"
 set /a max=max-1
 if %max% EQU 0 goto fail
 if %errorlevel% NEQ 0 goto repeat
@@ -19,3 +19,4 @@ goto:eof
 
 :fail
 echo "Fail"
+docker logs pgsql
