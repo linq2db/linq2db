38aca04bb75d2df3fa9ee55c72c156082be8f311
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-18 17:42:05 +0200

    1

--- Build/Azure/scripts/firebird3.sh
+++ Build/Azure/scripts/firebird3.sh
@@ -2,4 +2,6 @@
 docker run -d --name firebird -eFIREBIRD_USER=test -e ISC_PASSWORD=test -e FIREBIRD_DATABASE=testdb.fdb -e EnableWireCrypt=true -p 3050:3050 jacobalberty/firebird:3.0
 docker ps -a
 sleep 15
+docker exec firebird ls -a /firebird/data
 docker logs firebird
+
