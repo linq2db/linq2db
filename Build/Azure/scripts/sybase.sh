38a0c4d7c713d40503f9a78408b5cf94804c9b0a
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-18 11:21:26 +0200

    wrong image

--- Build/Azure/scripts/sybase.sh
+++ Build/Azure/scripts/sybase.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d --name sybase -e SYBASE_DB=TestDataCore -p 5000:5000 datagrip/sybase162
+docker run -d --name sybase -e SYBASE_DB=TestDataCore -p 5000:5000 datagrip/sybase:16.0
 docker ps -a
 
 #retries=0
