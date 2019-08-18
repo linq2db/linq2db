e5ebbc035505860fff1cdbb747df82f1834608ea
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-18 17:38:04 +0200

    1

--- Build/Azure/scripts/oracle.sh
+++ Build/Azure/scripts/oracle.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker run -d --name oracle --net host -p 1521:1521 datagrip/oracle:11.2
+docker run -d --name oracle -p 1521:1521 datagrip/oracle:11.2
 sleep 80
 docker ps -a
 docker logs oracle
-docker run oracle cat /u01/app/oracle/product/11.2.0/xe/config/log
+docker exec oracle cat /u01/app/oracle/product/11.2.0/xe/config/log
