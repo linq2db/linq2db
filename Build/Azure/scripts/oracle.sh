b51eadd02d1bfbf4536b2cce18527d7ef84a98b1
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-19 12:39:32 +0200

    1

--- Build/Azure/scripts/oracle.sh
+++ Build/Azure/scripts/oracle.sh
@@ -6,7 +6,7 @@ docker run -d --name oracle -p 1521:1521 -p 8080:8080 datagrip/oracle:11.2
 #chmod +x scripts/wait-for-it.sh
 #scripts/wait-for-it.sh localhost:8080 --timeout=600
 
-docker cp scripts/ping-oracle oracle:test.sql
+docker cp scripts/ping-oracle oracle:/test.sql
 
 retries=0
 status="1"
@@ -14,7 +14,7 @@ while [ "$status" != "0" ]
 do
     sleep 5
     retries=`expr $retries + 1`
-    docker exec oracle sqlplus /nolog @test.sql
+    docker exec oracle sqlplus /nolog @/test.sql
     status=$?
     if [ $retries -gt 100 ]; then
         >&2 echo 'Failed to start oracle'
