a080c3b8016f3713910079de9aef95c440b47c3d
MaceWindu <MaceWindu@users.noreply.github.com>
2019-08-25 14:38:45 +0200

    start poking hana2

---
+++ Build/Azure/scripts/mac.hana2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+docker run -d --name hana2 -p 39013:39013 store/saplabs/hanaexpress:2.00.040.00.20190729.1 --agree-to-sap-license --passwords-url file:///password.json
+
+echo Generate password file
+cat <<-EOJSON > hana_password.json
+{"master_password": "password"}
+EOJSON
+
+docker cp hana_password.json hana2:/password.json
+
+docker ps -a
+
+retries=0
+status="1"
+until docker logs hana2 | grep -q 'Startup finished'; do
+    sleep 5
+    retries=`expr $retries + 1`
+    echo waiting for hana2 to start
+    if [ $retries -gt 100 ]; then
+        echo hana2 not started or takes too long to start
+        exit 1
+    fi;
+done
+
+docker logs hana2
