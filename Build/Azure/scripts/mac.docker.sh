98758b3162b2fcd685571eaa43c15e8771c32c53
MaceWindu <MaceWindu@users.noreply.github.com>
2019-10-01 11:36:53 +0200

    1

--- Build/Azure/scripts/mac.docker.sh
+++ Build/Azure/scripts/mac.docker.sh
@@ -19,9 +19,9 @@ while ! docker info 2>/dev/null ; do
         echo 'docker not running, restart'
         /Applications/Docker.app/Contents/MacOS/Docker --unattended &
         echo 'searching for logs start'
+        tree '/Users/vsts/Library/Containers/com.docker.docker/'
         ls '/Users/vsts/Library/Containers/com.docker.docker/Data/vms/0/data'
-        tree '/Users/vsts/Library/Containers/com.docker.docker/Data/vms/data'
-        cat '/Users/vsts/Library/Containers/com.docker.docker/Data/vms/data'
+        cat '/Users/vsts/Library/Containers/com.docker.docker/Data/vms/0/data'
         echo 'searching for logs end'
     fi
     if [ $retries -gt 30 ]; then
