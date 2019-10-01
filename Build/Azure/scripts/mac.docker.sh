dbc64cdfb6643471cb114f85be67e9e033d20138
MaceWindu <MaceWindu@users.noreply.github.com>
2019-10-01 11:51:41 +0200

    1

--- Build/Azure/scripts/mac.docker.sh
+++ Build/Azure/scripts/mac.docker.sh
@@ -5,6 +5,7 @@
 #https://github.com/microsoft/azure-pipelines-image-generation/issues/738
 retries=0
 # recent 37199 version of docker fails to start, so we need to specify most recent working one
+brew install tree
 brew cask install docker
 # install working 2.0.0.3-ce-mac81,31259
 #brew cask install https://raw.githubusercontent.com/Homebrew/homebrew-cask/8ce4e89d10716666743b28c5a46cd54af59a9cc2/Casks/docker.rb
@@ -19,9 +20,12 @@ while ! docker info 2>/dev/null ; do
         echo 'docker not running, restart'
         /Applications/Docker.app/Contents/MacOS/Docker --unattended &
         echo 'searching for logs start'
+        echo 'tree1'
         tree '/Users/vsts/Library/Containers/com.docker.docker/'
-        ls '/Users/vsts/Library/Containers/com.docker.docker/Data/vms/0/data'
-        cat '/Users/vsts/Library/Containers/com.docker.docker/Data/vms/0/data'
+        echo 'tree2'
+        tree '/Users/vsts/Library/Group Containers/group.com.docker/'
+        #ls '/Users/vsts/Library/Containers/com.docker.docker/Data/vms/0/data'
+        #cat '/Users/vsts/Library/Containers/com.docker.docker/Data/vms/0/data'
         echo 'searching for logs end'
     fi
     if [ $retries -gt 30 ]; then
