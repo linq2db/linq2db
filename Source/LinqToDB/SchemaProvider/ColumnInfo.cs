a07922dc6b60d941132a6febc3644904d948f1b9
ili <chudinia@gmail.com>
2019-10-13 12:16:21 +0500

    Trimming ColumnInfo.DataType & ColumnType
    Schema provider can read type with spaces

--- Source/LinqToDB/SchemaProvider/ColumnInfo.cs
+++ Source/LinqToDB/SchemaProvider/ColumnInfo.cs
@@ -6,12 +6,15 @@ namespace LinqToDB.SchemaProvider
 	[DebuggerDisplay("TableID = {TableID}, Name = {Name}, DataType = {DataType}, Length = {Length}, Precision = {Precision}, Scale = {Scale}")]
 	public class ColumnInfo
 	{
+		private string _dataType;
+		private string _columnType;
+
 		public string TableID;
 		public string Name;
 		public bool   IsNullable;
 		public int    Ordinal;
-		public string DataType;
-		public string ColumnType;
+		public string DataType      { get => _dataType;   set => _dataType   = value.Trim(); }
+		public string ColumnType    { get => _columnType; set => _columnType = value.Trim(); }
 		public long?  Length;
 		public int?   Precision;
 		public int?   Scale;
