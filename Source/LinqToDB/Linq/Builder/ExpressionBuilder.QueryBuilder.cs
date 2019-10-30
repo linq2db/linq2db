58d3987749ef3ef832473f9e8830a38d7e843e27
MaceWindu <MaceWindu@users.noreply.github.com>
2019-10-30 15:05:00 +0100

    fix build after merge

--- Source/LinqToDB/Linq/Builder/ExpressionBuilder.QueryBuilder.cs
+++ Source/LinqToDB/Linq/Builder/ExpressionBuilder.QueryBuilder.cs
@@ -91,7 +91,7 @@ namespace LinqToDB.Linq.Builder
 			if (resultExpr.NodeType == ExpressionType.Call)
 			{
 				var mc = (MethodCallExpression)resultExpr;
-				var attr = MappingSchema.GetAttribute<Sql.ExpressionAttribute>(mc.Method.ReflectedTypeEx(), mc.Method);
+				var attr = MappingSchema.GetAttribute<Sql.ExpressionAttribute>(mc.Method.ReflectedType, mc.Method);
 
 				if (attr != null
 					&& attr.IsNullable == Sql.IsNullableType.IfAnyParameterNullable
