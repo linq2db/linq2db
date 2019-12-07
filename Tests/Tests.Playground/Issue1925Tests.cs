c1b65f23d4b4ec1ead9de52e0b366cf62cded521
to11mtm <laingas@gmail.com>
2019-12-07 18:01:59 -0500

    Repro Test

---
+++ Tests/Tests.Playground/Issue1925Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToDB;
+using LinqToDB.Mapping;
+using NUnit.Framework;
+
+namespace Tests.Playground
+{
+	[TestFixture]
+	public class TestAccess1925 : TestBase
+	{
+
+		[Table]
+		class SampleClass
+		{
+			[Column] public int Id { get; set; }
+			[Column] public string Value { get; set; }
+		}
+
+		[Test]
+
+		[ActiveIssue(1925)]
+		public void Issue1925Test([IncludeDataSources(ProviderName.Access)]  string context)
+		{
+			using (var db = GetDataContext(context))
+			using (var table = db.CreateLocalTable<SampleClass>())
+			{
+
+				table.Insert(() => new SampleClass() { Id = 1, Value = "6" });
+				Assert.AreEqual(1, table.ToList().Count());
+				var asParam = "[0-9]";
+				Assert.AreEqual(1, table.Where(r => AccessLike(r.Value, asParam)).ToList().Count());
+				Assert.AreEqual(1, table.Where(r => AccessLike(r.Value, "[0-9]")).ToList().Count());
+
+				Assert.AreEqual(1, table.Where(r => Sql.Like(r.Value, "[0-9]")).ToList().Count());
+
+			}
+		}
+
+		[Sql.Expression("{0} LIKE {1}", ServerSideOnly = true, IsPredicate = true)]
+		public static bool AccessLike(string col, string expr)
+		{
+			return Sql.Like(col, expr);
+		}
+	}
+}
