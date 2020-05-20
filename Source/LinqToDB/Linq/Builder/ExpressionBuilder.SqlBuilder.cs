60047b01d2b48bbbd8827e6d967f96d16775096c
Svyatoslav Danyliv <sdanyliv@gmail.com>
2020-05-21 01:44:59 +0300

    Fixed Having handling.

--- Source/LinqToDB/Linq/Builder/ExpressionBuilder.SqlBuilder.cs
+++ Source/LinqToDB/Linq/Builder/ExpressionBuilder.SqlBuilder.cs
@@ -170,7 +170,7 @@ namespace LinqToDB.Linq.Builder
 					if (mce.Object != null && typeof(IGrouping<,>).IsSameOrParentOf(mce.Object.Type))
 						return true;
 
-					if (mce.IsAggregate(mappingSchema))
+					if (mce.IsSameGenericMethod(Methods.LinqToDB.GroupBy.Grouping))
 						return true;
 
 					return mce.Arguments.Any(a => typeof(IGrouping<,>).IsSameOrParentOf(a.Type));
