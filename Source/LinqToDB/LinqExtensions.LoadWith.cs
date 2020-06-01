aaeff5bab0dbe838444790def8a9ca47374b6c61
Svyatoslav Danyliv <sdanyliv@gmail.com>
2020-06-01 16:30:54 +0300

    Moved ILoadWithQueryable out of LinqExtensions class.

--- Source/LinqToDB/LinqExtensions.LoadWith.cs
+++ Source/LinqToDB/LinqExtensions.LoadWith.cs
@@ -57,16 +57,6 @@ namespace LinqToDB
 			return table;
 		}
 
-		/// <summary>
-		/// Provides support for queryable LoadWith/ThenLoad chaining operators.
-		/// </summary>
-		/// <typeparam name="TEntity">The entity type.</typeparam>
-		/// <typeparam name="TProperty">The property type.</typeparam>
-		// ReSharper disable once UnusedTypeParameter
-		public interface ILoadWithQueryable<out TEntity, out TProperty> : IQueryable<TEntity>, IAsyncEnumerable<TEntity>
-		{
-		}
-
 		class LoadWithQueryable<TEntity, TProperty> : ILoadWithQueryable<TEntity, TProperty>
 		{
 			private readonly IQueryable<TEntity> _query;
