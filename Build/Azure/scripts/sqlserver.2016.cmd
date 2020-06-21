2eef6c0d754cb6178b4a5f049984b8a06d2fed83
MaceWindu <MaceWindu@users.noreply.github.com>
2020-06-21 15:37:01 +0200

    add v2016 and v2019

--- Build/Azure/scripts/sqlserver.2016.cmd
+++ Build/Azure/scripts/sqlserver.2016.cmd
@@ -1,5 +1,5 @@
 rem docker pull microsoft/mssql-server-windows-developer:2017-latest
-docker run -d -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password12!" -p 1433:1433 -h mssql --name=mssql microsoft/mssql-server-windows-express:2017-latest
+docker run -d -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password12!" -p 1433:1433 -h mssql --name=mssql microsoft/mssql-server-windows-express:2016-sp1
 docker ps -a
 
 echo "Waiting for SQL Server to accept connections"
@@ -12,7 +12,7 @@ echo "SQL Server is operational"
 echo "create TestData"
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE TestData;"
 echo "create TestData2017"
-docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE TestData2017;"
+docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE TestData2016;"
 echo "copy Northwind"
 docker cp scripts/northwind.sql mssql:northwind.sql
 echo "create Northwind"
