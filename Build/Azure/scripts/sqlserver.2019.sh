d4af98c3d841f2faf9c3674bc054e8f5f38848bc
MaceWindu <MaceWindu@users.noreply.github.com>
2020-06-22 18:08:47 +0200

    1

--- Build/Azure/scripts/sqlserver.2019.sh
+++ Build/Azure/scripts/sqlserver.2019.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql -d mcr.microsoft.com/mssql/server:2019-latest
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql -d microservicesharinghub/sqlserver-with-fullsearch:latest
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql -d huyttq/sqlserver-2019-with-fts:2019
 docker ps -a
 
 # Wait for start
