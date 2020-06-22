deed1a052476757f2521108092e63dc805cecfe0
MaceWindu <MaceWindu@users.noreply.github.com>
2020-06-22 17:55:35 +0200

    1

--- Build/Azure/scripts/sqlserver.2019.sh
+++ Build/Azure/scripts/sqlserver.2019.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql -d mcr.microsoft.com/mssql/server:2019-latest
+#docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql -d mcr.microsoft.com/mssql/server:2019-latest
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Password12!' -p 1433:1433 -h mssql --name=mssql -d microservicesharinghub/sqlserver-with-fullsearch:latest
 docker ps -a
 
 # Wait for start
@@ -12,6 +13,7 @@ while [ $is_up -ne 0 ] ; do
 done
 echo "SQL Server is operational"
 
+docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'SELECT @@Version'
 docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData;'
 docker exec mssql /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Password12! -Q 'CREATE DATABASE TestData2019;'
 docker cp scripts/northwind.sql mssql:/northwind.sql
