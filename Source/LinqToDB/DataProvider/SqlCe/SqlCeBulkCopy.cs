cedd5a714c4b021b768315063e7e2bd21aaaa844
Shane Krueger <Shane@acdmail.com>
2020-07-03 19:18:47 -0400

    SqlCe support

--- Source/LinqToDB/DataProvider/SqlCe/SqlCeBulkCopy.cs
+++ Source/LinqToDB/DataProvider/SqlCe/SqlCeBulkCopy.cs
@@ -3,6 +3,7 @@
 namespace LinqToDB.DataProvider.SqlCe
 {
 	using Data;
+	using System.Threading.Tasks;
 
 	class SqlCeBulkCopy : BasicBulkCopy
 	{
@@ -21,5 +22,43 @@ namespace LinqToDB.DataProvider.SqlCe
 
 			return ret;
 		}
+
+		protected override async Task<BulkCopyRowsCopied> MultipleRowsCopyAsync<T>(ITable<T> table, BulkCopyOptions options, IEnumerable<T> source)
+		{
+			var helper = new MultipleRowsHelper<T>(table, options);
+
+			if (options.KeepIdentity == true)
+				await helper.DataConnection.ExecuteAsync("SET IDENTITY_INSERT " + helper.TableName + " ON")
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+
+			var ret = await MultipleRowsCopy2Async(helper, source, "")
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+
+			if (options.KeepIdentity == true)
+				await helper.DataConnection.ExecuteAsync("SET IDENTITY_INSERT " + helper.TableName + " OFF")
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+
+			return ret;
+		}
+
+#if !NET45 && !NET46
+		protected override async Task<BulkCopyRowsCopied> MultipleRowsCopyAsync<T>(ITable<T> table, BulkCopyOptions options, IAsyncEnumerable<T> source)
+		{
+			var helper = new MultipleRowsHelper<T>(table, options);
+
+			if (options.KeepIdentity == true)
+				await helper.DataConnection.ExecuteAsync("SET IDENTITY_INSERT " + helper.TableName + " ON")
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+
+			var ret = await MultipleRowsCopy2Async(helper, source, "")
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+
+			if (options.KeepIdentity == true)
+				await helper.DataConnection.ExecuteAsync("SET IDENTITY_INSERT " + helper.TableName + " OFF")
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+
+			return ret;
+		}
+#endif
 	}
 }
