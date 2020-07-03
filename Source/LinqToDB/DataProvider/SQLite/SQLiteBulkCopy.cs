4e918ffdcea510d61b3c6e7f20dce7a86f388fd5
Shane Krueger <Shane@acdmail.com>
2020-07-03 19:18:52 -0400

    SQLite support

--- Source/LinqToDB/DataProvider/SQLite/SQLiteBulkCopy.cs
+++ Source/LinqToDB/DataProvider/SQLite/SQLiteBulkCopy.cs
@@ -3,6 +3,7 @@
 namespace LinqToDB.DataProvider.SQLite
 {
 	using Data;
+	using System.Threading.Tasks;
 
 	class SQLiteBulkCopy : BasicBulkCopy
 	{
@@ -11,5 +12,19 @@ namespace LinqToDB.DataProvider.SQLite
 		{
 			return MultipleRowsCopy1(table, options, source);
 		}
+
+		protected override Task<BulkCopyRowsCopied> MultipleRowsCopyAsync<T>(
+			ITable<T> table, BulkCopyOptions options, IEnumerable<T> source)
+		{
+			return MultipleRowsCopy1Async(table, options, source);
+		}
+
+#if !NET45 && !NET46
+		protected override Task<BulkCopyRowsCopied> MultipleRowsCopyAsync<T>(
+			ITable<T> table, BulkCopyOptions options, IAsyncEnumerable<T> source)
+		{
+			return MultipleRowsCopy1Async(table, options, source);
+		}
+#endif
 	}
 }
