70a6287fb753143f69deb7280e1ec668d615d3de
jkuehner <jochen.kuehner@kardex.com>
2020-07-29 14:00:26 +0200

    minimal test showing error

--- Tests/Linq/UserTests/Issue2375Tests.cs
+++ Tests/Linq/UserTests/Issue2375Tests.cs
@@ -18,78 +18,23 @@ namespace Tests.UserTests
 			Finished = 88
 		}
 
-		public enum InfeedStatus
-		{
-			Undefined = 0,
-			Finished = 88
-		}
-
+		[Table]
 		public class InventoryResourceDTO
 		{
+			[Column]
 			public Guid Id { get; set; }
 
 			[Column(DataType = DataType.Int32)]
 			public InventoryResourceStatus Status { get; set; }
 
+			[Column]
 			public Guid ResourceID { get; set; }
 
-			public Guid MaterialID { get; set; }
-
-			public string? Unit { get; set; }
-
+			[Column]
 			public Guid? InfeedAdviceID { get; set; }
 
+			[Column]
 			public DateTime? ModifiedTimeStamp { get; set; }
-
-			public DateTime? ProductionDate { get; set; }
-
-			public long CustomLong { get; set; }
-
-			public DateTime? ExpiryDate { get; set; }
-
-			public string? BatchNumber { get; set; }
-
-			public decimal? QuantityPerBundle { get; set; }
-
-			public string? BundleUnit { get; set; }
-
-			public int ProductStatus { get; set; }
-
-			public string? ReasonForRestriction { get; set; }
-
-			public string? RestrictionLabel { get; set; }
-
-			public string? RestrictionText { get; set; }
-
-			public int ToHostStatus { get; set; }
-
-			public string? CustomField1 { get; set; }
-			public string? CustomField2 { get; set; }
-			public string? CustomField3 { get; set; }
-			public string? CustomField4 { get; set; }
-			public string? CustomField5 { get; set; }
-			public string? CustomField6 { get; set; }
-			public string? CustomField7 { get; set; }
-			public string? CustomField8 { get; set; }
-			public string? CustomField9 { get; set; }
-			public DateTime? CustomDate1 { get; set; }
-			public DateTime? CustomDate2 { get; set; }
-			public DateTime? CustomDate3 { get; set; }
-			public DateTime? CustomDate4 { get; set; }
-			public DateTime? CustomDate5 { get; set; }
-			public DateTime? CustomDate6 { get; set; }
-			public DateTime? CustomDate7 { get; set; }
-			public DateTime? CustomDate8 { get; set; }
-			public DateTime? CustomDate9 { get; set; }
-			public long? CustomLong1 { get; set; }
-			public long? CustomLong2 { get; set; }
-			public long? CustomLong3 { get; set; }
-			public long? CustomLong4 { get; set; }
-			public long? CustomLong5 { get; set; }
-			public long? CustomLong6 { get; set; }
-			public long? CustomLong7 { get; set; }
-			public long? CustomLong8 { get; set; }
-			public long? CustomLong9 { get; set; }
 		}
 
 		[Table]
@@ -100,30 +45,6 @@ namespace Tests.UserTests
 
 			[Column]
 			public string? ResourceLabel { get; set; }
-
-			[Column]
-			public string? Name { get; set; }
-		}
-
-		[Table]
-		public class InfeedAdviceDTO
-		{
-			public Guid Id { get; set; }
-
-			[Column(DataType = DataType.Int32)]
-			public InfeedStatus Status { get; set; }
-
-			public DateTime? ModifiedTimeStamp { get; set; }
-		}
-
-		[Table]
-		public class MaterialDTO
-		{
-			[Column]
-			public Guid Id { get; set; }
-
-			[Column]
-			public string? Name { get; set; }
 		}
 
 		[Test]
@@ -134,84 +55,31 @@ namespace Tests.UserTests
 			{
 				using (var itb = db.CreateLocalTable<InventoryResourceDTO>())
 				using (var lctb = db.CreateLocalTable<WmsLoadCarrierDTO>())
-				using (var intb = db.CreateLocalTable<InfeedAdviceDTO>())
-				using (var mtb = db.CreateLocalTable<MaterialDTO>())
 				{
-					var res = new WmsLoadCarrierDTO { Id = Guid.NewGuid(), Name = "a" };
-					lctb.Insert(() => res);
+					var res = new WmsLoadCarrierDTO { Id = Guid.NewGuid(), ResourceLabel = "b" };
+					db.Insert(res);
 					var dto1 = new InventoryResourceDTO
 					{
 						Status = InventoryResourceStatus.Used,
-						MaterialID = Guid.NewGuid(),
-						ResourceID = res.Id,
-						ModifiedTimeStamp = DateTime.UtcNow,
+						ModifiedTimeStamp = DateTime.UtcNow - TimeSpan.FromHours(2),
 						Id = Guid.NewGuid(),
 						InfeedAdviceID = Guid.NewGuid()
 					};
-					itb.Insert(() => dto1);
+					db.Insert(dto1);
 					var dto2 = new InventoryResourceDTO
 					{
 						Status = InventoryResourceStatus.Used,
-						MaterialID = Guid.NewGuid(),
-						ResourceID = res.Id,
-						ModifiedTimeStamp = DateTime.UtcNow,
+						ModifiedTimeStamp = DateTime.UtcNow- TimeSpan.FromHours(2),
 						Id = Guid.NewGuid(),
 						InfeedAdviceID = Guid.NewGuid()
 					};
-					itb.Insert(() => dto2);
+					db.Insert(dto2);
 
 					var qry = from inventory in itb
 							   join lc in lctb on inventory.ResourceID equals lc.Id
-							   join material in mtb on inventory.MaterialID equals material.Id
-							   join infeeds in intb on inventory.InfeedAdviceID equals infeeds.Id into infeedList
-							   from infeed in infeedList.DefaultIfEmpty()
-							   where inventory.Status < InventoryResourceStatus.Finished
-										  && inventory.ModifiedTimeStamp < DateTime.UtcNow - TimeSpan.FromHours(1)
-										  && inventory.ProductionDate == null
-										  && inventory.Unit != "m" && inventory.Unit != "M"
-										  && (infeed == null || (infeed.Status >= InfeedStatus.Undefined && infeed.ModifiedTimeStamp < DateTime.UtcNow - TimeSpan.FromHours(1)))
 							   group inventory by new
 							   {
 								   inventory.Status,
-								   inventory.MaterialID,
-								   inventory.ResourceID,
-								   inventory.ExpiryDate,
-								   inventory.BatchNumber,
-								   inventory.Unit,
-								   inventory.QuantityPerBundle,
-								   inventory.BundleUnit,
-								   inventory.ProductStatus,
-								   inventory.ReasonForRestriction,
-								   inventory.RestrictionLabel,
-								   inventory.RestrictionText,
-								   inventory.ToHostStatus,
-								   inventory.CustomField1,
-								   inventory.CustomField2,
-								   inventory.CustomField3,
-								   inventory.CustomField4,
-								   inventory.CustomField5,
-								   inventory.CustomField6,
-								   inventory.CustomField7,
-								   inventory.CustomField8,
-								   inventory.CustomField9,
-								   inventory.CustomDate1,
-								   inventory.CustomDate2,
-								   inventory.CustomDate3,
-								   inventory.CustomDate4,
-								   inventory.CustomDate5,
-								   inventory.CustomDate6,
-								   inventory.CustomDate7,
-								   inventory.CustomDate8,
-								   inventory.CustomDate9,
-								   inventory.CustomLong1,
-								   inventory.CustomLong2,
-								   inventory.CustomLong3,
-								   inventory.CustomLong4,
-								   inventory.CustomLong5,
-								   inventory.CustomLong6,
-								   inventory.CustomLong7,
-								   inventory.CustomLong8,
-								   inventory.CustomLong9,
 								   lc.ResourceLabel
 							   }
 							into grp
