7be6dce0e00c278b84a24cae17d6fb24ea58d380
Igor Tkachev <igor.tkachev@gmail.com>
2020-09-11 10:42:23 -0400

    #2368. Added IsTemporary overload.

--- Source/LinqToDB/LinqExtensions.cs
+++ Source/LinqToDB/LinqExtensions.cs
@@ -119,6 +119,21 @@ namespace LinqToDB
 		}
 
 		/// <summary>
+		/// Overrides IsTemporary flag for the current table. This call will have effect only for databases that support
+		/// temporary tables.
+		/// <para>Supported by: DB2, Oracle, PostgreSQL, Informix, SQL Server, Sybase ASE.</para>
+		/// </summary>
+		/// <typeparam name="T">Table record mapping class.</typeparam>
+		/// <param name="table">Table-like query source.</param>
+		/// <returns>Table-like query source with new owner/schema name.</returns>
+		[LinqTunnel]
+		[Pure]
+		public static ITable<T> IsTemporary<T>(this ITable<T> table)
+		{
+			return ((ITableMutable<T>)table).ChangeTableOptions(table.TableOptions | LinqToDB.TableOptions.IsTemporary);
+		}
+
+		/// <summary>
 		/// Overrides TableOptions value for the current table. This call will have effect only for databases that support
 		/// the options.
 		/// </summary>
