42ea550fb2946bf73377340ae8ea2db079ab7f25
MaceWindu <MaceWindu@users.noreply.github.com>
2020-10-14 21:00:55 +0300

    Update Source/LinqToDB/DataProvider/Access/AccessSqlBuilderBase.cs
    Co-authored-by: Stuart Turner <stuart@turner-isageek.com>

--- Source/LinqToDB/DataProvider/Access/AccessSqlBuilderBase.cs
+++ Source/LinqToDB/DataProvider/Access/AccessSqlBuilderBase.cs
@@ -385,7 +385,7 @@ namespace LinqToDB.DataProvider.Access
 		{
 			AppendIndent();
 			StringBuilder.Append("CONSTRAINT ").Append(pkName).Append(" PRIMARY KEY CLUSTERED (");
-			StringBuilder.Append(fieldNames.Aggregate((f1,f2) => f1 + InlineComma + f2));
+			StringBuilder.Append(string.Join(InlineComma, fieldNames));
 			StringBuilder.Append(")");
 		}
 
