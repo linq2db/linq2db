7d4e8bb8bb18aac6cf869802fcb0a0852eb96c66
Svyatoslav Danyliv <sdanyliv@gmail.com>
2020-10-15 12:29:29 +0300

    Fix for #2546. Do not convert properties if they should be parameters.

---
+++ Tests/Linq/UserTests/Issue2546Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using LinqToDB;
+using LinqToDB.Mapping;
+using NUnit.Framework;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue2546Tests : TestBase
+	{
+		class Issue2546Class
+		{
+			[Column] [PrimaryKey] public int Id { get; set; }
+			[Column] public string? Value { get; set; }
+			public string? Value2 { get; set; }
+		}
+
+		[Test]
+		public void TestContainerParameter([IncludeDataSources(TestProvName.AllSQLite)] string context)
+		{
+			var ms = new MappingSchema();
+
+			ms.GetFluentMappingBuilder()
+				.Entity<Issue2546Class>()
+				.Property(m => m.Value2)
+				.HasSkipOnInsert()
+				.HasSkipOnUpdate()
+				.IsExpression(x => Sql.Property<object>(x, "Value"));
+
+			var data = new[] { new Issue2546Class { Id = 1, Value = "Hello World" } };
+
+			using (var db = GetDataContext(context, ms))
+			using (db.CreateLocalTable(data))
+			{
+				var container = new Issue2546Class();
+				container.Value2 = "Hello World";
+
+				var something = (from x in db.GetTable<Issue2546Class>()
+					where x.Value2 == container.Value2
+					select x).ToList();
+
+				Assert.That(something.Count, Is.EqualTo(1));
+			}
+		}
+	}
+}
