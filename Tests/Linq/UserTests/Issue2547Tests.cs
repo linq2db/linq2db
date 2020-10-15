6f1383957dfb908bfa474850b305043ae310aa80
jkuehner <jochen.kuehner@kardex.com>
2020-10-15 19:41:58 +0200

    fix test for datatype

--- Tests/Linq/UserTests/Issue2547Tests.cs
+++ Tests/Linq/UserTests/Issue2547Tests.cs
@@ -25,6 +25,15 @@ namespace Tests.UserTests
 			[Column(DataType = DataType.VarChar), NotNull] public AA TestEnum { get; set; }
 		}
 
+		[Table(Name = "Issue2547Item")]
+		public partial class Item2
+		{
+			[PrimaryKey, NotNull] public int Id { get; set; } // int
+			[Column, NotNull] public string Name { get; set; } = null!; // nvarchar(50)
+			[Column, NotNull] public TimeSpan TestSpan { get; set; }
+			[Column, NotNull] public AA TestEnum { get; set; }
+		}
+
 		[Test]
 		public void Issue2547Test([IncludeDataSources(TestProvName.AllSqlServer, TestProvName.AllOracle, TestProvName.AllPostgreSQL)] string context)
 		{
@@ -56,13 +65,13 @@ namespace Tests.UserTests
 				return TimeSpan.FromTicks(val);
 			});
 
-			var items = new Item[]
+			var items = new Item2[]
 			{
-				new Item { Id = 1, Name = "Item 1", TestSpan = TimeSpan.FromSeconds(450), TestEnum = AA.A },
+				new Item2 { Id = 1, Name = "Item 1", TestSpan = TimeSpan.FromSeconds(450), TestEnum = AA.A },
 			};
 
 			using (var db = GetDataContext(context, mappingSchema))
-			using (var lt = db.CreateLocalTable(items))
+			using (var lt = db.CreateLocalTable<Item>())
 			{
 				lt.Delete();
 				db.Insert(items[0]);
