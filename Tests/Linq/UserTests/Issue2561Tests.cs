7f586bac0f52823ed71f26a3823471c01174100e
jkuehner <jochen.kuehner@kardex.com>
2020-10-21 12:19:53 +0200

    bugfix issue test

--- Tests/Linq/UserTests/Issue2561Tests.cs
+++ Tests/Linq/UserTests/Issue2561Tests.cs
@@ -20,13 +20,12 @@ namespace Tests.UserTests
 			public Guid Id { get; set; }
 			public string? Name { get; set; }
 			public long Archive { get; set; }
-			public string? ServiceName { get; set; }
 			public Issue2561ScriptType ScriptType { get; set; }
 			public string? Script { get; set; }
 		}
 
 		[Test]
-		public void TestContainerParameter([IncludeDataSources(TestProvName.AllOracle, TestProvName.AllPostgreSQL)] string context)
+		public void TestOracle([IncludeDataSources(TestProvName.AllOracle)] string context)
 		{
 			var createTable = "CREATE TABLE Common_Scripts (Id RAW(16), \"Archive\" NUMBER(20,0) NOT NULL, Name NVARCHAR2(255) NOT NULL, ScriptType NUMBER(10,0) NOT NULL, Script NCLOB NULL)";
 			var ms = new MappingSchema();
@@ -35,8 +34,7 @@ namespace Tests.UserTests
 				.Entity<Issue2561Class>()
 				.HasTableName("Common_Scripts")
 				.Property(e => e.Id).IsPrimaryKey()
-				.Property(e => e.Archive).IsPrimaryKey()
-				.Property(e => e.ServiceName).IsPrimaryKey()
+				.Property(e => e.Archive).IsPrimaryKey()				
 				.Property(e => e.Name)
 				.Property(e => e.ScriptType)
 				.Property(e => e.Script).HasDataType(DataType.NText);
@@ -52,13 +50,11 @@ namespace Tests.UserTests
 				var dto = new Issue2561Class() {Id=Guid.NewGuid(), Script="aa".PadLeft(2500,'a') };
 				db.Insert(new[] { dto });
 
-				var b = "b";
 				var c = "aaa";
 				var q = db.GetTable<Issue2561Class>()
 						  .Where(x =>
 									x.Archive == 0 &&
 									x.ScriptType == Issue2561ScriptType.Type1 &&
-									x.ServiceName == b &&
 									x.Script == c)
 						  .FirstOrDefault();
 
@@ -67,7 +63,50 @@ namespace Tests.UserTests
 						  .Where(x =>
 									x.Archive == 0 &&
 									x.ScriptType == Issue2561ScriptType.Type1 &&
-									x.ServiceName == b &&
+									x.Script == c)
+						  .FirstOrDefault();
+			}
+		}
+
+		[Test]
+		public void TestPostgres([IncludeDataSources(TestProvName.AllPostgreSQL)] string context)
+		{
+			var ms = new MappingSchema();
+
+			ms.GetFluentMappingBuilder()
+				.Entity<Issue2561Class>()
+				.HasTableName("Common_Scripts")
+				.Property(e => e.Id).IsPrimaryKey()
+				.Property(e => e.Archive).IsPrimaryKey()
+				.Property(e => e.Name)
+				.Property(e => e.ScriptType)
+				.Property(e => e.Script).HasDataType(DataType.NText);
+
+
+			using (var db = GetDataContext(context, ms))
+			{
+				db.DropTable<Issue2561Class>(throwExceptionIfNotExists: false);
+				db.CreateTable<Issue2561Class>();
+
+				var l = db.GetTable<Issue2561Class>().ToList();
+
+				var dto = new Issue2561Class() {Id=Guid.NewGuid(), Script="aa".PadLeft(2500,'a') };
+				db.Insert(new[] { dto });
+
+				var b = "b";
+				var c = "aaa";
+				var q = db.GetTable<Issue2561Class>()
+						  .Where(x =>
+									x.Archive == 0 &&
+									x.ScriptType == Issue2561ScriptType.Type1 &&
+									x.Script == c)
+						  .FirstOrDefault();
+
+				c = "aaa".PadLeft(2000, 'b');
+				var q2 = db.GetTable<Issue2561Class>()
+						  .Where(x =>
+									x.Archive == 0 &&
+									x.ScriptType == Issue2561ScriptType.Type1 &&
 									x.Script == c)
 						  .FirstOrDefault();
 			}
