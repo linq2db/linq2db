52bb9f75c36e2bf6f33c339b512a88d172a86fa4
MaceWindu <MaceWindu@users.noreply.github.com>
2020-11-12 21:13:41 +0100

    fix grouping set regression

--- Source/LinqToDB/Linq/Builder/GroupByBuilder.cs
+++ Source/LinqToDB/Linq/Builder/GroupByBuilder.cs
@@ -131,12 +131,7 @@ namespace LinqToDB.Linq.Builder
 			{
 				var groupSql = builder.ConvertExpressions(key, keySelector.Body.Unwrap(), ConvertFlags.Key, null);
 
-				var allowed = groupSql.Where(s =>
-				{
-					var underlying = QueryHelper.GetUnderlyingExpression(s.Sql);
-					return underlying != null &&
-						   underlying.ElementType.NotIn(QueryElementType.SqlValue, QueryElementType.SqlParameter);
-				});
+				var allowed = groupSql.Where(s => !QueryHelper.IsConstant(s.Sql));
 
 				foreach (var sql in allowed)
 					sequence.SelectQuery.GroupBy.Expr(sql.Sql);
@@ -728,15 +723,13 @@ namespace LinqToDB.Linq.Builder
 			{
 				var expr = SelectQuery.Select.Columns[index].Expression;
 
-				if (!SelectQuery.GroupBy.EnumItems().Any(_ => expr.Equals(_)))
+				if (!QueryHelper.IsConstant(expr)
+					&& !SelectQuery.GroupBy.EnumItems().Any(_ => ReferenceEquals(_, expr) || (expr is SqlColumn column && ReferenceEquals(_, column.Expression))))
 				{
-					if (expr.ElementType.In(QueryElementType.SqlValue, QueryElementType.SqlParameter) == false)
-					{
-						if (SelectQuery.GroupBy.GroupingType == GroupingType.GroupBySets)
-							SelectQuery.GroupBy.Items.Add(new SqlGroupingSet(new[] { expr }));
-						else
-							SelectQuery.GroupBy.Items.Add(expr);
-					}
+					if (SelectQuery.GroupBy.GroupingType == GroupingType.GroupBySets)
+						SelectQuery.GroupBy.Items.Add(new SqlGroupingSet(new[] { expr }));
+					else
+						SelectQuery.GroupBy.Items.Add(expr);
 				}
 
 				return base.ConvertToParentIndex(index, this);
