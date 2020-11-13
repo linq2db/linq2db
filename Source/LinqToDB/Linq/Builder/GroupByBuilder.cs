204000c25545a1a2f8812349850b8498e1f6a226
MaceWindu <MaceWindu@users.noreply.github.com>
2020-11-13 20:24:05 +0100

    remove informix fix from wrong place

--- Source/LinqToDB/Linq/Builder/GroupByBuilder.cs
+++ Source/LinqToDB/Linq/Builder/GroupByBuilder.cs
@@ -131,8 +131,7 @@ namespace LinqToDB.Linq.Builder
 			{
 				var groupSql = builder.ConvertExpressions(key, keySelector.Body.Unwrap(), ConvertFlags.Key, null);
 
-				var allowed = groupSql.Where(s => (builder.DataContext.SqlProviderFlags.IsGroupByColumnRequred && s.Sql is SqlColumn)
-					|| !QueryHelper.IsConstant(s.Sql));
+				var allowed = groupSql.Where(s => !QueryHelper.IsConstant(s.Sql));
 
 				foreach (var sql in allowed)
 					sequence.SelectQuery.GroupBy.Expr(sql.Sql);
