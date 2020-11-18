42399be36fb4a486c4ce05d61dd0cf3888da8d25
nsarris <nsarris@gmail.com>
2020-11-18 10:06:03 +0200

    Fixed comments and added a helper method on CustomizationSupportInterceptor.

--- Tests/Base/CustomizationSupportInterceptor.cs
+++ Tests/Base/CustomizationSupportInterceptor.cs
@@ -4,36 +4,46 @@ using System.Collections.Generic;
 namespace Tests
 {
 	/// <summary>
-	/// Providers an API for intercepting test filtering operations.
+	/// Provides an API for intercepting test filtering operations.
 	/// For use in running the tests with 3rd party DataProviders.
 	/// </summary>
 	public class CustomizationSupportInterceptor
 	{
 		/// <summary>
-		/// Intercept and mutate the set of unsupported providers for tests annotated with InsertOrUpdateDataSourcesAttribute
-		/// (tests that include InsertOrUpdate/InserOrReplace calls)
+		/// Intercept and mutate the set of providers available globally by the testing framework
 		/// </summary>
-		/// <param name="providers">The default unsupported providers from InsertOrUpdateDataSourcesAttribute</param>
-		/// <returns></returns>
+		/// <param name="providers">The default providers supported by the testing framework.</param>
+		/// <returns>The actual providers supported at runtime.</returns>
 		public virtual IEnumerable<string> GetSupportedProviders(IEnumerable<string> providers)
 			=> providers;
 
 		/// <summary>
-		/// Intercept and mutate the set of unsupported providers for tests annotated with MergeDataSourcesAttribute
-		/// (tests that include Merge API calls)
+		/// Intercept and mutate the set of supported datasources/contexts when scanning for tests.
+		/// Use this method to replace aliases with actual providers.
 		/// </summary>
-		/// <param name="providers">The default unsupported providers from MergeDataSourcesAttribute</param>
-		/// <returns></returns>
+		/// <param name="dataSourcesAttribute">The DataSourcesAttribute instance associated with the test.</param>
+		/// <param name="contexts">The default datasources passed to a test by DataSourcesAttribute.</param>
+		/// <returns>The actual datasources that test should run with.</returns>
 		public virtual IEnumerable<string> InterceptDataSources(DataSourcesBaseAttribute dataSourcesAttribute, IEnumerable<string> contexts)
 			=> contexts;
 
 		/// <summary>
-		/// Intercept and mutate the set of unsupported providers for tests annotated with IdentityInsertMergeDataSourcesAttribute
-		/// (tests that include Merge API with Identity Insert calls)
+		/// Intercept and mutate the set of datasources/contexts passed to a specific test.
+		/// Use this method to mutate providers or switch off specific tess.
 		/// </summary>
-		/// <param name="providers">The default unsupported providers from IdentityInsertMergeDataSourcesAttribute</param>
+		/// <param name="dataSourcesAttribute">The DataSourcesAttribute instance associated with the test.</param>
+		/// <param name="testMethod">The reflected test method.</param>
+		/// <param name="contexts">The default datasources/contexts for given tests</param>
 		/// <returns></returns>
 		public virtual IEnumerable<string> InterceptTestDataSources(DataSourcesBaseAttribute dataSourcesAttribute, IMethodInfo testMethod, IEnumerable<string> contexts)
 			=> contexts;
+
+		/// <summary>
+		/// Helper method to extract the class name and method name of a test method.
+		/// </summary>
+		/// <param name="testMethod"></param>
+		/// <returns></returns>
+		protected static (string className, string methodName) ExtractMethod(IMethodInfo testMethod)
+			=> (testMethod.TypeInfo.Name, testMethod.Name);
 	}
 }
