33871fe96505626e70bb6423236576fca93d1883
MaceWindu <MaceWindu@users.noreply.github.com>
2020-11-25 10:37:50 +0100

    migrate to `dotnet test` where possible

--- Test.cmd
+++ Test.cmd
@@ -20,7 +20,7 @@ ECHO Configuration=%CONFIG%, net472 enabled:%NET472%, netcoreapp2.1 enabled:%NET
 
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\MSBuild.exe" linq2db.sln /p:Configuration=%CONFIG% /t:Restore;Build /v:m
 
-IF %NET472%       NEQ 0 (dotnet vstest ./Tests/Linq/bin/%CONFIG%/net472/linq2db.Tests.dll        /Framework:.NETFramework,Version=v4.7.2 /logger:%FORMAT%;LogFileName=net472.%FORMAT%)
-IF %NETCOREAPP21% NEQ 0 (dotnet vstest ./Tests/Linq/bin/%CONFIG%/netcoreapp2.1/linq2db.Tests.dll /Framework:.NETCoreApp,Version=v2.1     /logger:%FORMAT%;LogFileName=netcoreapp21.%FORMAT%)
-IF %NETCOREAPP31% NEQ 0 (dotnet vstest ./Tests/Linq/bin/%CONFIG%/netcoreapp3.1/linq2db.Tests.dll /Framework:.NETCoreApp,Version=v3.1     /logger:%FORMAT%;LogFileName=netcoreapp31.%FORMAT%)
-IF %NET50%        NEQ 0 (dotnet vstest ./Tests/Linq/bin/%CONFIG%/net5.0/linq2db.Tests.dll        /Framework:net5.0                       /logger:%FORMAT%;LogFileName=net50.%FORMAT%)
+IF %NET472%       NEQ 0 (dotnet test ./Tests/Linq/bin/%CONFIG%/net472/linq2db.Tests.dll        /Framework:net472        /logger:%FORMAT%;LogFileName=net472.%FORMAT%)
+IF %NETCOREAPP21% NEQ 0 (dotnet test ./Tests/Linq/bin/%CONFIG%/netcoreapp2.1/linq2db.Tests.dll /Framework:netcoreapp2.1 /logger:%FORMAT%;LogFileName=netcoreapp21.%FORMAT%)
+IF %NETCOREAPP31% NEQ 0 (dotnet test ./Tests/Linq/bin/%CONFIG%/netcoreapp3.1/linq2db.Tests.dll /Framework:netcoreapp3.1 /logger:%FORMAT%;LogFileName=netcoreapp31.%FORMAT%)
+IF %NET50%        NEQ 0 (dotnet test ./Tests/Linq/bin/%CONFIG%/net5.0/linq2db.Tests.dll        /Framework:net5.0        /logger:%FORMAT%;LogFileName=net50.%FORMAT%)
