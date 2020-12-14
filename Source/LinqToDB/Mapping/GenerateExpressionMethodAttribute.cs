8d326cfcb4782e2295c8b8e29ab34000fd8171de
Stuart Turner <stuart@turner-isageek.com>
2020-12-14 11:43:49 -0600

    Add GenerateExpressionMethodAttribute

---
+++ Source/LinqToDB/Mapping/GenerateExpressionMethodAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace LinqToDB.Mapping
+{
+	/// <summary>
+	/// <para>
+	/// When applied to method, tells linq2db to create an <see cref="ExpressionMethodAttribute"/> method
+	/// version of the function. This is only supported in C# 9 or later, using Source Generators.
+	/// </para>
+	/// 
+	/// <para>
+	/// Requirements:
+	/// </para>
+	/// 
+	/// <list type="bullet">
+	/// 
+	/// <item>
+	/// <term><c>class</c></term>
+	/// <description>The containing class must be marked <c>partial</c>, so that the generator can add new methods to it.</description>
+	/// </item> 
+	/// <item>
+	/// <term><c>method</c></term>
+	/// <description>
+	/// <list type="bullet">
+	/// <item>Must have exactly one parameter.</item>
+	/// <item>Must not have <c>void</c> return type.</item>
+	/// <item>Must have a single statement returning a mapped object.</item>
+	/// </list>
+	/// </description>
+	/// </item>
+	/// </list>
+	/// </summary>
+	/// <remarks>
+	/// See <see href="https://devblogs.microsoft.com/dotnet/introducing-c-source-generators/"/> for additional information.
+	/// </remarks>
+	[PublicAPI]
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+	public class GenerateExpressionMethodAttribute : Attribute { }
+}
