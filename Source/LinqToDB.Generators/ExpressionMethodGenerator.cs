e659bf3308c5f02f6edb6737ee366bbe2e8f9154
Stuart Turner <stuart@turner-isageek.com>
2020-12-14 19:39:18 -0600

    Add Generators foundation

---
+++ Source/LinqToDB.Generators/ExpressionMethodGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace LinqToDB
+{
+	[Generator]
+	public class ExpressionMethodGenerator : ISourceGenerator
+	{
+		public void Initialize(GeneratorInitializationContext context)
+		{
+			// do nothing for now.
+			// TODO: Add SyntaxReceiver support
+		}
+
+		public void Execute(GeneratorExecutionContext context)
+		{
+			// r/restofthef***ingowl
+			context.AddSource("myGeneratedFile.cs", SourceText.From(@"
+namespace GeneratedNamespace
+{
+    public class GeneratedClass
+    {
+        public static void GeneratedMethod()
+        {
+            // generated code
+        }
+    }
+}", Encoding.UTF8));
+		}
+	}
+}
