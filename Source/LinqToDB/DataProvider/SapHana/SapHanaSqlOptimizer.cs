32b172ce50989cc57b784b86e5c5c833d5483d90
MaceWindu <MaceWindu@users.noreply.github.com>
2020-12-15 16:51:31 +0100

    fix like template escaping for SAP HANA

--- Source/LinqToDB/DataProvider/SapHana/SapHanaSqlOptimizer.cs
+++ Source/LinqToDB/DataProvider/SapHana/SapHanaSqlOptimizer.cs
@@ -114,5 +114,8 @@ namespace LinqToDB.DataProvider.SapHana
 			return base.ConvertFunction(func);
 		}
 
+		// https://help.sap.com/viewer/4fe29514fd584807ac9f2a04f6754767/2.0.01/en-US/20fa17f375191014a4d8d8cbfddfe340.html
+		protected static   string[] HanaLikeCharactersToEscape = { "%", "_" };
+		public    override string[] LikeCharactersToEscape     => HanaLikeCharactersToEscape;
 	}
 }
