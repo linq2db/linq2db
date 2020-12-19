384e8226626e7fc4a43ba5e2d18b8955ed93db3a
MaceWindu <MaceWindu@users.noreply.github.com>
2020-12-19 17:50:56 +0100

    nuget build refactoring

--- NuGet/PackLocal.cmd
+++ NuGet/PackLocal.cmd
@@ -1,3 +1,11 @@
-powershell ..\Build\BuildNuspecs.ps1 -path *.nuspec -version 3.0.0-local1
-call Pack.bat
+SET VERSION=%1
+SET SNUPKG=%2
+IF [%1] EQU [] (SET VERSION=3.0.0-local1)
+IF [%2] EQU [] (SET SNUPKG=)
 
+cd ..
+call Build.cmd
+cd NuGet
+
+powershell ..\Build\BuildNuspecs.ps1 -path *.nuspec -buildPath ..\BuiltNuGet -version %VERSION%
+call Pack.bat %SNUPKG%
