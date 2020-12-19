345cc705e42acc78b84862994a8b93c4171804f7
MaceWindu <MaceWindu@users.noreply.github.com>
2020-12-19 20:57:27 +0100

    remove nuget.exe from repository

--- NuGet/PackLocal.cmd
+++ NuGet/PackLocal.cmd
@@ -1,7 +1,16 @@
+ECHO OFF
+
 SET VERSION=%1
 SET SNUPKG=%2
 IF [%1] EQU [] (SET VERSION=3.0.0-local1)
 IF [%2] EQU [] (SET SNUPKG=)
+
+WHERE nuget.exe >nul 2>&1
+IF %errorlevel% NEQ 0 (
+ECHO Cannot find nuget.exe. Add it to PATH or place it to current folder
+ECHO nuget.exe could be downloaded from https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+GOTO :EOF
+)
 
 cd ..
 call Build.cmd
