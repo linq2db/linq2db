8b5eeec19068cb170611f6d5ba44a4252eabc68c
ustinkinsv <ustinkinsv@psbank.ru>
2020-12-24 12:15:19 +0300

    Add separate test to check datetime2 precision.

---
+++ Tests/Linq/UserTests/MsSqlDatetime2PrecisionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.DataProvider.Firebird;
+using LinqToDB.SqlProvider;
+using LinqToDB.SqlQuery;
+using NUnit.Framework;
+using Tests.DataProvider;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class MsSqlDatetime2PrecisionTest : TestBase
+	{
+		[Test]
+		public void TestDateTime2([IncludeDataSources(TestProvName.AllSqlServer2008Plus)] string context)
+		{
+			using (var conn = new DataConnection(context))
+			{
+				var dateTime2 = new DateTime(2012, 12, 12, 12, 12, 12, 0).AddTicks(1);
+
+				Assert.That(conn.Execute<DateTime> ("SELECT Cast('2012-12-12 12:12:12.0000001' as datetime2)"), Is.EqualTo(dateTime2));
+				Assert.That(conn.Execute<DateTime?>("SELECT Cast('2012-12-12 12:12:12.0000001' as datetime2)"), Is.EqualTo(dateTime2));
+
+				Assert.That(conn.Execute<DateTime> ("SELECT @p", DataParameter.DateTime2("p", dateTime2)),               Is.EqualTo(dateTime2));
+				Assert.That(conn.Execute<DateTime> ("SELECT @p", DataParameter.Create   ("p", dateTime2)),               Is.EqualTo(dateTime2));
+				Assert.That(conn.Execute<DateTime?>("SELECT @p", new DataParameter("p", dateTime2, DataType.DateTime2)), Is.EqualTo(dateTime2));
+			}
+		}
+	}
+}
