e52454ee91eff3e15cd470b0ccf56b19bf949cfb
MaceWindu <MaceWindu@users.noreply.github.com>
2020-12-28 13:36:42 +0100

    1

--- Build/Azure/scripts/informix12.sh
+++ Build/Azure/scripts/informix12.sh
@@ -8,7 +8,7 @@ cp -f ./IBM.Data.DB2.Core-lnx/lib/netstandard2.0/IBM.Data.DB2.Core.dll ./IBM.Dat
 echo "##vso[task.setvariable variable=PATH]$PATH:$PWD/clidriver/bin:$PWD/clidriver/lib"
 echo "##vso[task.setvariable variable=LD_LIBRARY_PATH]$PWD/clidriver/lib/"
 
-docker run -d --name informix -e LICENSE=ACCEPT -p 9087:9087 ibmcom/informix-developer-database:12.10.FC12W1DE -e SIZE=custom
+docker run -d --name informix -e LICENSE=ACCEPT -p 9089:9089 ibmcom/informix-developer-database:12.10.FC12W1DE -e SIZE=custom
 echo Generate CREATE DATABASE script
 cat <<-EOSQL > informix_init.sql
 CREATE DATABASE testdb WITH BUFFERED LOG
@@ -16,7 +16,7 @@ EOSQL
 
 cat informix_init.sql
 docker cp informix_init.sql informix:/linq2db.sql
-docker exec informix cat /linq2db.sql >> /opt/ibm/sql/sch_init_informix.custom.sql
+docker exec informix cat /linq2db.sql >> /opt/ibm/data/sch_init_informix.custom.sql
 
 docker ps -a
 
