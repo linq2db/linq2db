efe06df9b0be7c9f3fdcecdf6fec1c8e391d88ae
MaceWindu <MaceWindu@users.noreply.github.com>
2020-12-28 16:19:53 +0100

    1

--- Build/Azure/scripts/informix12.sh
+++ Build/Azure/scripts/informix12.sh
@@ -13,7 +13,7 @@ cat <<-EOSQL > linq2db.sql
 CREATE DATABASE testdb WITH BUFFERED LOG
 EOSQL
 
-docker run -d --name informix -e LICENSE=ACCEPT -p 9089:9089 ibmcom/informix-developer-database:12.10.FC12W1DE
+docker run -d --name informix -e LICENSE=ACCEPT --privileged -it -p 9089:9089 ibmcom/informix-developer-database:12.10.FC12W1DE
 
 # docker cp linq2db.sql informix:/opt/ibm/data/sch_init_informix.small.sql
 
@@ -33,9 +33,3 @@ done
 
 docker logs informix
 
-echo "1:"
-docker exec informix cat /opt/ibm/informix/etc/sqlhosts
-echo "2:"
-docker exec informix ls /opt/ibm/data
-echo "3:"
-docker exec informix cat /opt/ibm/data/sch_init_informix.small.sql
