612e9ee193e445c02fd18a6a8244b5c04ad1b114
MaceWindu <MaceWindu@users.noreply.github.com>
2020-12-29 15:39:48 +0100

    1

--- Build/Azure/scripts/informix12.sh
+++ Build/Azure/scripts/informix12.sh
@@ -8,8 +8,7 @@ cp -f ./IBM.Data.DB2.Core-lnx/lib/netstandard2.0/IBM.Data.DB2.Core.dll ./IBM.Dat
 echo "##vso[task.setvariable variable=PATH]$PATH:$PWD/clidriver/bin:$PWD/clidriver/lib"
 echo "##vso[task.setvariable variable=LD_LIBRARY_PATH]$PWD/clidriver/lib/"
 
-docker run -d --name informix -e LICENSE=ACCEPT --privileged -it -p 9089:9089 ibmcom/informix-developer-database:12.10.FC12W1DE
-
+docker run -d --name informix -e LICENSE=ACCEPT -e INFORMIXDIR=/opt/ibm/informix -e INFORMIXSERVER=informix --privileged -it -p 9089:9088 ibmcom/informix-developer-database:12.10.FC12W1DE
 
 docker ps -a
 
@@ -27,16 +26,12 @@ done
 
 docker ps -a
 
-docker start informix
-
-docker ps -a
-
 echo Generate CREATE DATABASE script
 cat <<-EOSQL > linq2db.sql
 CREATE DATABASE testdb WITH BUFFERED LOG
 EOSQL
 docker cp linq2db.sql informix:/opt/ibm/data/linq2db.sql
-docker exec informix dbaccess sysadmin /opt/ibm/data/linq2db.sql
+docker exec informix /opt/ibm/informix/bin/dbaccess sysadmin /opt/ibm/data/linq2db.sql
 
 docker logs informix
 
