38345e69ffe75a7b22f1b744c884b6bc97bc4dae
jkuehner <jochen.kuehner@kardex.com>
2020-12-30 00:14:32 +0100

    test delete

--- Tests/Linq/UserTests/Issue2743Tests.cs
+++ Tests/Linq/UserTests/Issue2743Tests.cs
@@ -44,5 +44,27 @@ namespace Tests.UserTests
 				}
 			}
 		}
+
+		[Test]
+		public void IssueTestDeleteViaSelect([DataSources] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				using (db.CreateLocalTable<MessageEventDTO>())
+				using (db.CreateLocalTable<MessageEventDTO>(tableName: "MessageEventDTOTb2"))
+				{
+					var evtQry = db.GetTable<MessageEventDTO>();
+					var q = from evt in evtQry
+							select new MessageEventCombinedWithoutTranslationDTO
+							{
+								MessageEventDTO = evt,
+							};
+
+					var queryDelete = q.Select(x => x.MessageEventDTO);
+
+					var rows = db.Delete(queryDelete);
+				}
+			}
+		}
 	}
 }
