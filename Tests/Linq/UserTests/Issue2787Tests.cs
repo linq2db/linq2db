1899d3c8fd44cd1468c515c85a38a72150c0e957
jkuehner <jochen.kuehner@kardex.com>
2021-01-20 21:56:04 +0100

    test for issue #2787

---
+++ Tests/Linq/UserTests/Issue2787Tests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.Mapping;
+using NUnit.Framework;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue2787Tests : TestBase
+	{
+		[Table]
+		class Car
+		{
+			[PrimaryKey]
+			public int Id { get; set; }
+
+			[Column(CanBeNull = false)]
+			public string? Name { get; set; }
+		}
+
+		[Table]
+		class Tyre
+		{
+			[PrimaryKey]
+			public int CarId { get; set; }
+		}
+
+		class MyProjection
+		{
+			public Car? Car { get; set; }
+			public Tyre? Tyre { get; set; }
+		}
+
+		[Test]
+		public void Issue2787Test([IncludeDataSources(TestProvName.AllSQLite)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				db.DropTable<Car>(throwExceptionIfNotExists: false);
+				db.DropTable<Tyre>(throwExceptionIfNotExists: false);
+				using (var carTable = db.CreateLocalTable<Car>())
+				using (var tyreTable = db.CreateLocalTable<Tyre>())
+				{
+					var query  = (from car in carTable
+								  from tyre in tyreTable.Where(x => x.CarId == car.Id)
+								  select new MyProjection
+								  {
+									  Car = car,
+									  Tyre = tyre
+								  });
+
+
+					var res = query.Select(x => new {a = x.Car.Id == 3 ? x.Car.Name : string.Empty}).ToList();
+
+					var lastQuery = ((DataConnection) db).LastQuery;
+
+					Assert.IsTrue(lastQuery?.ToLower().Contains("case"));
+				}
+			}
+		}
+	}
+}
