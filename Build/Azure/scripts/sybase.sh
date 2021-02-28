4352b3065424226b33c32dff3a6d19b46c54b2aa
MaceWindu <MaceWindu@users.noreply.github.com>
2021-02-28 16:39:58 +0100

    revert sybase changes

--- Build/Azure/scripts/sybase.sh
+++ Build/Azure/scripts/sybase.sh
@@ -16,28 +16,4 @@ until docker logs sybase | grep -q 'SYBASE INITIALIZED'; do
     echo 'Waiting for sybase'
 done
 
-# temporary fix for https://github.com/linq2db/linq2db/issues/2858
-docker exec sybase pkill dataserver
-
-cat <<-EOL > start_fixed.sh
-#!/bin/sh
-/opt/sybase/ASE-16_0/bin/dataserver \
--d/opt/sybase/data/master.dat \
--e/opt/sybase/ASE-16_0/install/MYSYBASE.log \
--c/opt/sybase/ASE-16_0/MYSYBASE.cfg \
--M/opt/sybase/ASE-16_0 \
--N/opt/sybase/ASE-16_0/sysam/MYSYBASE.properties \
--i/opt/sybase \
--sMYSYBASE \
--T11889
-EOL
-
-docker cp start_fixed.sh sybase:/opt/sybase/ASE-16_0/install/start_fixed.sh
-
-docker exec sybase chmod +x /opt/sybase/ASE-16_0/install/start_fixed.sh
-
-docker exec -d sybase bash -c 'export SYBASE=/opt/sybase && source /opt/sybase/SYBASE.sh && sh /opt/sybase/SYBASE.sh && sh /opt/sybase/ASE-16_0/install/start_fixed.sh'
-
-sleep 45
-
 docker logs sybase
