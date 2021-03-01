db33d16048148aac33b4e65a8250dc6c7e4d1dba
Jochen Kühner <jochen.kuehner@gmx.de>
2021-03-01 14:46:34 +0100

    Fix for #2856. Added unwrapping for column equality. (#2857)
    * test for issue #2856
    
    * Fix for #2856. Added unwrapping for column equality.
    
    Co-authored-by: Svyatoslav Danyliv <sdanyliv@gmail.com>

---
+++ Tests/Linq/UserTests/Issue2856Tests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using LinqToDB;
+using LinqToDB.Mapping;
+using NUnit.Framework;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue2856Tests : TestBase
+	{
+		public class GlobalTaskDTO
+		{
+			[PrimaryKey]
+			public Guid Id { get; set; }
+
+			public Guid? RPSourceID { get; set; }
+
+			public Guid? RPDestinationID { get; set; }
+		}
+
+		[Test]
+		public void TestIssue2856([IncludeDataSources(TestProvName.AllSQLite)] string context)
+		{
+			using (var s = GetDataContext(context))
+			using (s.CreateLocalTable<GlobalTaskDTO>())
+			{
+				var allRpIds = new[] {Guid.NewGuid(), Guid.NewGuid()};
+
+				Assert.DoesNotThrow(() =>
+					_ = (
+							from gt1 in s.GetTable<GlobalTaskDTO>()
+							where allRpIds.Contains(gt1.RPSourceID!.Value)
+							select gt1.RPSourceID!.Value
+						)
+						.Union(
+							from gt2 in s.GetTable<GlobalTaskDTO>()
+							select gt2.RPDestinationID!.Value
+						)
+						.ToList());
+			}
+		}
+	}
+}
