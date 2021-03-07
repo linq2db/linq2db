429a6f0d7599ef3545d388d99eacdf6cca186ecb
MaceWindu <MaceWindu@users.noreply.github.com>
2021-03-07 15:37:28 +0100

    partial fix for auto-commit transaction isolation level for eager load queries for sql server

---
+++ Source/LinqToDB/DataProvider/RawTransaction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using LinqToDB.Data;
+
+namespace LinqToDB.DataProvider
+{
+	/// <summary>
+	/// This is internal API and is not intended for use by Linq To DB applications.
+	/// It may change or be removed without further notice.
+	/// </summary>
+	public abstract class RawTransaction : IDisposable
+#if !NETFRAMEWORK
+		, IAsyncDisposable
+#endif
+	{
+		protected readonly DataConnection DataConnection;
+
+		protected RawTransaction(DataConnection dataConnection)
+		{
+			DataConnection = dataConnection;
+		}
+
+		public abstract RawTransaction BeginTransaction();
+		public abstract Task<RawTransaction> BeginTransactionAsync(CancellationToken cancellationToken);
+
+		protected abstract void RollbackTransaction();
+
+		void IDisposable.Dispose() => RollbackTransaction();
+
+#if !NETFRAMEWORK
+		protected abstract ValueTask RollbackTransactionAsync();
+		ValueTask IAsyncDisposable.DisposeAsync() => RollbackTransactionAsync();
+#endif
+	}
+}
---
+++ Source/LinqToDB/DataProvider/SqlServer/SqlServerRawTransaction.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using LinqToDB.Data;
+
+namespace LinqToDB.DataProvider.SqlServer
+{
+	public sealed class SqlServerRawTransaction : RawTransaction
+	{
+		private DataConnectionTransaction? _marsTransaction;
+
+		public SqlServerRawTransaction(DataConnection dataConnection)
+			: base(dataConnection)
+		{
+		}
+
+		public override RawTransaction BeginTransaction()
+		{
+			// explicit transaction management requires single batch when used over MARS-enabled connection
+			// https://docs.microsoft.com/en-us/archive/blogs/cbiyikoglu/mars-transactions-and-sql-error-3997-3988-or-3983
+			// in theory we can refactor eager load in future to support MARS batching to unblock this scenario
+			if (DataConnection.IsMarsEnabled)
+				_marsTransaction = DataConnection.BeginTransaction();
+			else
+				DataConnection.Execute("BEGIN TRAN");
+			return this;
+		}
+
+		protected override void RollbackTransaction()
+		{
+			if (DataConnection.IsMarsEnabled)
+				_marsTransaction!.Rollback();
+			else
+				DataConnection.Execute("ROLLBACK TRAN");
+		}
+
+		public override async Task<RawTransaction> BeginTransactionAsync(CancellationToken cancellationToken)
+		{
+			if (DataConnection.IsMarsEnabled)
+				_marsTransaction = await DataConnection.BeginTransactionAsync(cancellationToken)
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+			else
+				await DataConnection.ExecuteAsync("BEGIN TRAN", cancellationToken)
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+
+			return this;
+		}
+
+#if !NETFRAMEWORK
+		protected override async ValueTask RollbackTransactionAsync()
+		{
+			if (DataConnection.IsMarsEnabled)
+				await _marsTransaction!.RollbackAsync()
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+			else
+				await DataConnection.ExecuteAsync("ROLLBACK TRAN")
+					.ConfigureAwait(Common.Configuration.ContinueOnCapturedContext);
+		}
+#endif
+	}
+}
