f553b660c98cba69edf89a180ef5743f393b9314
MaceWindu <MaceWindu@users.noreply.github.com>
2021-03-20 14:40:18 +0100

    fix operation type

--- Source/LinqToDB/Data/DataReaderAsync.cs
+++ Source/LinqToDB/Data/DataReaderAsync.cs
@@ -30,7 +30,7 @@ namespace LinqToDB.Data
 
 				if (CommandInfo?.DataConnection.TraceSwitchConnection.TraceInfo == true)
 				{
-					CommandInfo.DataConnection.OnTraceConnection(new TraceInfo(CommandInfo.DataConnection, TraceInfoStep.Completed, TraceOperation.ExecuteReader)
+					CommandInfo.DataConnection.OnTraceConnection(new TraceInfo(CommandInfo.DataConnection, TraceInfoStep.Completed, TraceOperation.ExecuteReaderAsync)
 					{
 						TraceLevel      = TraceLevel.Info,
 						Command         = CommandInfo.DataConnection.GetCurrentCommand(),
