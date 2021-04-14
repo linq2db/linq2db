d56b0a20851cb10408a4f008b8d3ba130e9d77c9
MaceWindu <MaceWindu@users.noreply.github.com>
2021-04-14 16:33:52 +0200

    fix sybase on CI

--- Build/Azure/scripts/sybase.sh
+++ Build/Azure/scripts/sybase.sh
@@ -2,6 +2,29 @@
 
 docker run -d --name sybase -e SYBASE_DB=TestDataCore -p 5000:5000 datagrip/sybase:16.0
 docker ps -a
+
+# add trace flag to server startup procedure
+cat <<-EOL > start_fixed.sh
+#!/bin/sh
+/opt/sybase/ASE-16_0/bin/dataserver -T11889 \
+-d/opt/sybase/data/master.dat \
+-e/opt/sybase/ASE-16_0/install/MYSYBASE.log \
+-c/opt/sybase/ASE-16_0/MYSYBASE.cfg \
+-M/opt/sybase/ASE-16_0 \
+-N/opt/sybase/ASE-16_0/sysam/MYSYBASE.properties \
+-i/opt/sybase \
+-sMYSYBASE
+EOL
+
+docker cp start_fixed.sh sybase:/opt/sybase/ASE-16_0/install/RUN_MYSYBASE
+docker exec sybase chmod +x /opt/sybase/ASE-16_0/install/RUN_MYSYBASE
+
+# restart container to take effect
+docker stop sybase
+docker start sybase
+
+docker ps -a
+
 sleep 45
 
 retries=0
