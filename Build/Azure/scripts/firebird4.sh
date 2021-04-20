d59559ca95fc2002e8780c4fc1a0b2c8cff98c0c
MaceWindu <MaceWindu@users.noreply.github.com>
2021-04-20 11:16:22 +0200

    fix fb4 docker tag

--- Build/Azure/scripts/firebird4.sh
+++ Build/Azure/scripts/firebird4.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d --name firebird  -e ISC_PASSWORD=masterkey -e FIREBIRD_DATABASE=testdb.fdb -e EnableLegacyClientAuth=true -p 3050:3050 jacobalberty/firebird:4.0
+docker run -d --name firebird  -e ISC_PASSWORD=masterkey -e FIREBIRD_DATABASE=testdb.fdb -e EnableLegacyClientAuth=true -p 3050:3050 jacobalberty/firebird:v4.0.0rc1
 docker ps -a
 sleep 15
 
