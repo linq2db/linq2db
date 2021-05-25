a68a1b8dda8f82d5050f4e81eb5adcd664876de1
Svyatoslav Danyliv <sdanyliv@gmail.com>
2021-05-25 15:43:40 +0300

    Fixed table names collision.

--- Tests/Linq/UserTests/Issue3001Tests.cs
+++ Tests/Linq/UserTests/Issue3001Tests.cs
@@ -11,7 +11,7 @@ namespace Tests.UserTests
 	[TestFixture]
 	public class Issue3001Tests : TestBase
 	{
-		public class House
+		public class House3001
 		{
 			[Column] [PrimaryKey] public int Id;
 
@@ -19,7 +19,7 @@ namespace Tests.UserTests
 			public int Levels { get; set; }
 		}
 
-		public class Person
+		public class Person3001
 		{
 			[Column] public int HouseId;
 
@@ -27,10 +27,10 @@ namespace Tests.UserTests
 
 			[Association(ThisKey = nameof(HouseId), OtherKey = "Id", CanBeNull = true,
 				Relationship     = Relationship.ManyToOne)]
-			public House House { get; set; } = null!;
+			public House3001 House { get; set; } = null!;
 		}
 
-		public class Pet
+		public class Pet3001
 		{
 			[Column] [PrimaryKey] public int Id;
 
@@ -38,20 +38,20 @@ namespace Tests.UserTests
 
 			[Association(ThisKey = nameof(PersonId), OtherKey = "Id", CanBeNull = false,
 				Relationship     = Relationship.ManyToOne)]
-			public Person Person { get; set; } = null!;
+			public Person3001 Person { get; set; } = null!;
 
 			[ExpressionMethod(nameof(HouseExpression))]
-			public House House { get; set; } = null!;
+			public House3001 House { get; set; } = null!;
 
 			[ExpressionMethod(nameof(IsHouseMultiLevelExpression), IsColumn = true)]
 			public bool IsHouseMultiLevel { get; set; }
 
-			private static Expression<Func<Pet, House>> HouseExpression()
+			private static Expression<Func<Pet3001, House3001>> HouseExpression()
 			{
 				return entity => entity.Person.House;
 			}
 
-			private static Expression<Func<Pet, bool>> IsHouseMultiLevelExpression()
+			private static Expression<Func<Pet3001, bool>> IsHouseMultiLevelExpression()
 			{
 				// Replacing code below to entity.Parent.House.Levels > 1 will make column work
 				return entity => entity.House.Levels > 1;
@@ -63,9 +63,9 @@ namespace Tests.UserTests
 			string context)
 		{
 			using var db          = GetDataContext(context);
-			using var houseTable  = db.CreateLocalTable(new[] {new House  {Id = 1, Levels   = 2}});
-			using var personTable = db.CreateLocalTable(new[] {new Person {Id = 1, HouseId  = 1}});
-			using var petTable    = db.CreateLocalTable(new[] {new Pet    {Id = 1, PersonId = 1}});
+			using var houseTable  = db.CreateLocalTable(new[] {new House3001  {Id = 1, Levels   = 2}});
+			using var personTable = db.CreateLocalTable(new[] {new Person3001 {Id = 1, HouseId  = 1}});
+			using var petTable    = db.CreateLocalTable(new[] {new Pet3001    {Id = 1, PersonId = 1}});
 
 			var data = petTable
 				.Select(x => new {x.Id, x.IsHouseMultiLevel})
