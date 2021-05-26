0b79dba443bd8ddab279133ca575f9bbb49b3eb3
Stuart Turner <stuart@turner-isageek.com>
2021-05-26 17:01:07 -0500

    4/32 done

--- Tests/Linq/Update/UpdateWithOutputTests.cs
+++ Tests/Linq/Update/UpdateWithOutputTests.cs
@@ -1,6 +1,9 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using LinqToDB;
 using LinqToDB.Mapping;
+using LinqToDB.Tools.Comparers;
 using NUnit.Framework;
 
 namespace Tests.Playground
@@ -8,6 +11,23 @@ namespace Tests.Playground
 	[TestFixture]
 	public class UpdateWithOutputTests : TestBase
 	{
+		private class UpdateOutputComparer<T> : IEqualityComparer<UpdateOutput<T>>
+			where T : notnull
+		{
+			private readonly IEqualityComparer<T> tComparer;
+
+			public UpdateOutputComparer()
+			{
+				tComparer = ComparerBuilder.GetEqualityComparer<T>();
+			}
+
+			public bool Equals(UpdateOutput<T>? x, UpdateOutput<T>? y)
+				=> tComparer.Equals(x!.Deleted, y!.Deleted) && tComparer.Equals(x!.Inserted, y!.Inserted);
+
+			public int GetHashCode(UpdateOutput<T> obj)
+				=> tComparer.GetHashCode(obj.Deleted) * -1521134295 + tComparer.GetHashCode(obj.Inserted);
+		}
+
 		[Table]
 		class TableWithData
 		{
@@ -39,8 +59,10 @@ namespace Tests.Playground
 				.ToArray();
 		}
 
+		#region Update against ITable<T> target
+
 		[Test]
-		public void UpdateWithOutputProjectionFromQueryTest([IncludeDataSources(true, TestProvName.AllSqlServer2008Plus)] string context, [Values(100, 200)] int param)
+		public void UpdateITableWithDefaultOutputTest([IncludeDataSources(true, TestProvName.AllSqlServer2008Plus)] string context)
 		{
 			var sourceData    = GetSourceData();
 			using (var db     = GetDataContext(context))
@@ -48,17 +70,81 @@ namespace Tests.Playground
 			using (var target = db.CreateLocalTable(sourceData
 				.Select(s => new DestinationTable { Id = s.Id, Value = s.Value + 1, ValueStr = (s.Value + 1).ToString() + "Dst", })))
 			{
+				var expected = sourceData
+					.Select(s => new UpdateOutput<DestinationTable>()
+					{
+						Inserted = new DestinationTable { Id = s.Id, Value = s.Value,     ValueStr = s.ValueStr, },
+						Deleted  = new DestinationTable { Id = s.Id, Value = s.Value + 1, ValueStr = (s.Value + 1).ToString() + "Dst", },
+					})
+					.ToArray();
+
 				var output = source
-					.Where(s => s.Id > 3)
-					.SelectMany(_ => target, (s, t) => new { s, t, })
+					.SelectMany(s => target.Where(t => t.Id == s.Id), (s, t) => new { s, t, })
 					.UpdateWithOutput(
-						x => x.t,
-						x => new DestinationTable
-						{
-							Id = x.s.Id,
-							Value = x.s.Value + 2,
-							ValueStr = x.t.ValueStr,
-						},
+						target,
+						s => new DestinationTable { Id = s.s.Id, Value = s.s.Value, ValueStr = s.s.ValueStr, })
+					.ToArray();
+
+				AreEqual(
+					expected,
+					output,
+					new UpdateOutputComparer<DestinationTable>());
+			}
+		}
+
+		[Test]
+		public async Task UpdateITableWithDefaultOutputTestAsync([IncludeDataSources(true, TestProvName.AllSqlServer2008Plus)] string context)
+		{
+			var sourceData    = GetSourceData();
+			using (var db     = GetDataContext(context))
+			using (var source = db.CreateLocalTable(sourceData))
+			using (var target = db.CreateLocalTable(sourceData
+				.Select(s => new DestinationTable { Id = s.Id, Value = s.Value + 1, ValueStr = (s.Value + 1).ToString() + "Dst", })))
+			{
+				var expected = sourceData
+					.Select(s => new UpdateOutput<DestinationTable>()
+					{
+						Inserted = new DestinationTable { Id = s.Id, Value = s.Value,     ValueStr = s.ValueStr, },
+						Deleted  = new DestinationTable { Id = s.Id, Value = s.Value + 1, ValueStr = (s.Value + 1).ToString() + "Dst", },
+					})
+					.ToArray();
+
+				var output = await source
+					.SelectMany(s => target.Where(t => t.Id == s.Id), (s, t) => new { s, t, })
+					.UpdateWithOutputAsync(
+						target,
+						s => new DestinationTable { Id = s.s.Id, Value = s.s.Value, ValueStr = s.s.ValueStr, });
+
+				AreEqual(
+					expected,
+					output,
+					new UpdateOutputComparer<DestinationTable>());
+			}
+		}
+
+		[Test]
+		public void UpdateITableWithProjectionOutputTest([IncludeDataSources(true, TestProvName.AllSqlServer2008Plus)] string context)
+		{
+			var sourceData    = GetSourceData();
+			using (var db     = GetDataContext(context))
+			using (var source = db.CreateLocalTable(sourceData))
+			using (var target = db.CreateLocalTable(sourceData
+				.Select(s => new DestinationTable { Id = s.Id, Value = s.Value + 1, ValueStr = (s.Value + 1).ToString() + "Dst", })))
+			{
+				var expected = sourceData
+					.Select(s => new
+					{
+						SourceStr = s.ValueStr,
+						DeletedValue = s.Value + 1,
+						InsertedValue = s.Value,
+					})
+					.ToArray();
+
+				var output = source
+					.SelectMany(s => target.Where(t => t.Id == s.Id), (s, t) => new { s, t, })
+					.UpdateWithOutput(
+						target,
+						s => new DestinationTable { Id = s.s.Id, Value = s.s.Value, ValueStr = s.s.ValueStr, },
 						(source, deleted, inserted) => new
 						{
 							SourceStr = source.s.ValueStr,
@@ -66,7 +152,49 @@ namespace Tests.Playground
 							InsertedValue = inserted.Value,
 						})
 					.ToArray();
+
+				AreEqual(
+					expected,
+					output);
 			}
 		}
+
+		[Test]
+		public async Task UpdateITableWithProjectionOutputTestAsync([IncludeDataSources(true, TestProvName.AllSqlServer2008Plus)] string context)
+		{
+			var sourceData    = GetSourceData();
+			using (var db     = GetDataContext(context))
+			using (var source = db.CreateLocalTable(sourceData))
+			using (var target = db.CreateLocalTable(sourceData
+				.Select(s => new DestinationTable { Id = s.Id, Value = s.Value + 1, ValueStr = (s.Value + 1).ToString() + "Dst", })))
+			{
+				var expected = sourceData
+					.Select(s => new
+					{
+						SourceStr = s.ValueStr,
+						DeletedValue = s.Value + 1,
+						InsertedValue = s.Value,
+					})
+					.ToArray();
+
+				var output = await source
+					.SelectMany(s => target.Where(t => t.Id == s.Id), (s, t) => new { s, t, })
+					.UpdateWithOutputAsync(
+						target,
+						s => new DestinationTable { Id = s.s.Id, Value = s.s.Value, ValueStr = s.s.ValueStr, },
+						(source, deleted, inserted) => new
+						{
+							SourceStr = source.s.ValueStr,
+							DeletedValue = deleted.Value,
+							InsertedValue = inserted.Value,
+						});
+
+				AreEqual(
+					expected,
+					output);
+			}
+		}
+
+		#endregion
 	}
 }
