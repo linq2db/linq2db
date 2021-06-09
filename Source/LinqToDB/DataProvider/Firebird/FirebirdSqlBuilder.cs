7419f88629d53ac5c611a2f14dd3cf39936392cb
Eugine Savin <jack128a@gmail.com>
2021-06-09 10:47:18 +0300

    Fix issue 2823. Guid data type suppport for Firebird data provider. (#2826)
    * Fix issue 2823 (guid data type suppport for Firebird data provider
    
    * Fix test (after MaceWindu review)
    
    * Update Tests/Linq/Linq/IssueTests.cs
    
    Co-authored-by: MaceWindu <MaceWindu@users.noreply.github.com>
    
    * Update Tests/Linq/Linq/IssueTests.cs
    
    Co-authored-by: MaceWindu <MaceWindu@users.noreply.github.com>
    
    * Firebird guid support: ValueToSqlConverter for Guid + Sql.ConvertTo
    
    * ProviderAdapter should select version specific mapping
    
    * post-merge fixes
    
    * remove unused code
    
    * add `DataType.NChar` to list of Guid->string mappings for backward compatibility (as we use NChar in schema/T4)
    
    * add type note
    
    Co-authored-by: MaceWindu <MaceWindu@users.noreply.github.com>

--- Source/LinqToDB/DataProvider/Firebird/FirebirdSqlBuilder.cs
+++ Source/LinqToDB/DataProvider/Firebird/FirebirdSqlBuilder.cs
@@ -131,8 +131,19 @@ namespace LinqToDB.DataProvider.Firebird
 					else
 						StringBuilder.Append($"({type.Type.Length})");
 
+					// type for UUID, e.g. see https://firebirdsql.org/refdocs/langrefupd25-intfunc-gen_uuid.html
 					StringBuilder.Append(" CHARACTER SET UNICODE_FSS");
 					                                                                                      break;
+
+				case DataType.Guid          : StringBuilder.Append("CHAR(16) CHARACTER SET OCTETS");      break;
+				case DataType.NChar         :
+				case DataType.Char          :
+					if (type.Type.SystemType == typeof(Guid) || type.Type.SystemType == typeof(Guid?))
+						StringBuilder.Append("CHAR(38)");
+					else
+						base.BuildDataTypeFromDataType(type, forCreateTable);
+					break;
+
 				case DataType.VarBinary     : StringBuilder.Append("BLOB");                               break;
 				// BOOLEAN type available since FB 3.0, but FirebirdDataProvider.SetParameter converts boolean to '1'/'0'
 				// so for now we will use type, compatible with SetParameter by default
