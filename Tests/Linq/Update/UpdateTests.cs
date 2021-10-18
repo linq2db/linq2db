69e5788bf8375d18c896cfb8890e9b937b60d820
Andryushin Andrey Aleksandrovich <andryushinaa@psbank.ru>
2021-10-18 15:45:56 +0300

    Repro for https://github.com/linq2db/linq2db/issues/3274

--- Tests/Linq/Update/UpdateTests.cs
+++ Tests/Linq/Update/UpdateTests.cs
@@ -683,6 +683,50 @@ namespace Tests.xUpdate
 			}
 		}
 
+private Gender? nullableGender;
+
+[Test]
+public void SetWithTernaryOperatorIssue([DataSources] string context)
+{
+	ResetPersonIdentity(context);
+
+	using (var db = GetDataContext(context))
+	{
+		db.Person.Where(_ => _.FirstName.StartsWith("UpdateComplex")).Delete();
+		try
+		{
+
+			var id = Convert.ToInt32(db.InsertWithIdentity(
+				new ComplexPerson2
+				{
+					Name = new FullName
+					{
+						FirstName = "UpdateComplex",
+						LastName  = "Empty",
+					},
+					Gender = Gender.Male
+				}));
+
+			var cnt = db.GetTable<ComplexPerson2>()
+				.Where(_ => _.Name.FirstName.StartsWith("UpdateComplex"))
+				.Set(_ => _.Gender, _ => nullableGender.HasValue ? nullableGender.Value : _.Gender)
+				.Update();
+
+			Assert.AreEqual(1, cnt);
+
+			var obj = db.GetTable<ComplexPerson2>()
+				.First(_ => _.ID == id);
+
+			Assert.AreEqual(Gender.Other, obj.Gender);
+		}
+		finally
+		{
+			db.Person.Where(_ => _.FirstName.StartsWith("UpdateComplex")).Delete();
+		}
+
+	}
+}
+		
 		[Test]
 		public void UpdateComplex2([DataSources] string context)
 		{
