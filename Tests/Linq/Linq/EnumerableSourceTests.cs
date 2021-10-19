202a6ab7665174885644d2f47379efcef5906b31
MaceWindu <MaceWindu@users.noreply.github.com>
2021-10-20 00:35:04 +0200

    add test

--- Tests/Linq/Linq/EnumerableSourceTests.cs
+++ Tests/Linq/Linq/EnumerableSourceTests.cs
@@ -844,7 +844,8 @@ namespace Tests.Linq
 						{
 							Id   = p.ID,
 							Name = p.Name,
-							SomeList = null						}
+							SomeList = null
+						}
 					).ToList();
 
 				personWithList.Should().HaveCountGreaterThan(0);
@@ -867,11 +868,12 @@ namespace Tests.Linq
 						{
 							Id       = p.ID,
 							Name     = p.Name,
-							SomeList = new List<int>()						}
+							SomeList = new List<int>()
+						}
 					).ToList();
 
 				personWithList.Should().HaveCountGreaterThan(0);
-				personWithList.All(p => p.SomeList.Count == 0).Should().BeTrue();
+				personWithList.All(p => p.SomeList!.Count == 0).Should().BeTrue();
 			}
 
 		}
