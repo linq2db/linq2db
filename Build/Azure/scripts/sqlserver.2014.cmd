cef198beba1077ee08055f482903cd3a2c083883
MaceWindu <MaceWindu@users.noreply.github.com>
2021-10-21 14:36:04 +0200

    Abort run when docker failed to download/start for sql server windows pipelines

--- Build/Azure/scripts/sqlserver.2014.cmd
+++ Build/Azure/scripts/sqlserver.2014.cmd
@@ -4,8 +4,12 @@ docker run -d -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password12!" -p 1433:1433 -h ms
 docker ps -a
 
 echo "Waiting for SQL Server to accept connections"
+set max = 100
 :repeat
+set /a max=max-1
+if %max% EQU 0 goto fail
 echo pinging sql server
+sleep 1
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "SELECT 1"
 if %errorlevel% NEQ 0 goto repeat
 echo "SQL Server is operational"
@@ -15,3 +19,8 @@ echo "create TestData"
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE TestData;"
 echo "create TestData2014"
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE TestData2014;"
+goto:eof
+
+:fail
+echo "Fail"
+docker logs mssql
--- Build/Azure/scripts/sqlserver.2019.cmd
+++ Build/Azure/scripts/sqlserver.2019.cmd
@@ -2,8 +2,12 @@ docker run -d -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password12!" -p 1433:1433 -h ms
 docker ps -a
 
 echo "Waiting for SQL Server to accept connections"
+set max = 100
 :repeat
+set /a max=max-1
+if %max% EQU 0 goto fail
 echo pinging sql server
+sleep 1
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "SELECT 1"
 if %errorlevel% NEQ 0 goto repeat
 echo "SQL Server is operational"
@@ -26,3 +30,8 @@ echo "copy Northwind"
 docker cp scripts/northwind.sql mssql:northwind.sql
 echo "create Northwind"
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -i northwind.sql
+goto:eof
+
+:fail
+echo "Fail"
+docker logs mssql
