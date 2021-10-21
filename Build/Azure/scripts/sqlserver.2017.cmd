c7ea664482cbdeb6ece5f75a111119a7a2999153
MaceWindu <MaceWindu@users.noreply.github.com>
2021-10-21 16:16:51 +0200

    Abort run when docker failed to download/start for sql server windows pipelines (#3287)
    * Abort run when docker failed to download/start for sql server windows pipelines
    
    * whitespaces are evil
    
    * don't run tests on setup failure on windows

--- Build/Azure/scripts/sqlserver.2017.cmd
+++ Build/Azure/scripts/sqlserver.2017.cmd
@@ -3,8 +3,12 @@ docker run -d -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password12!" -p 1433:1433 -h ms
 docker ps -a
 
 echo "Waiting for SQL Server to accept connections"
+set max=100
 :repeat
+set /a max=max-1
+if %max% EQU 0 goto fail
 echo pinging sql server
+sleep 1
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "SELECT 1"
 if %errorlevel% NEQ 0 goto repeat
 echo "SQL Server is operational"
@@ -14,3 +18,8 @@ echo "create TestData"
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE TestData;"
 echo "create TestData2017"
 docker exec mssql sqlcmd -S localhost -U sa -P Password12! -Q "CREATE DATABASE TestData2017;"
+goto:eof
+
+:fail
+echo "Fail"
+docker logs mssql
