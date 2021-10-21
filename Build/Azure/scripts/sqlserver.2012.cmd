f063882c275c6a966d44b763ad8f200c2561c8b4
MaceWindu <MaceWindu@users.noreply.github.com>
2021-10-21 15:41:03 +0200

    whitespaces are evil

--- Build/Azure/scripts/sqlserver.2012.cmd
+++ Build/Azure/scripts/sqlserver.2012.cmd
@@ -4,7 +4,7 @@ docker run -d -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=Password12!" -p 1433:1433 -h ms
 docker ps -a
 
 echo "Waiting for SQL Server to accept connections"
-set max = 100
+set max=100
 :repeat
 set /a max=max-1
 if %max% EQU 0 goto fail
