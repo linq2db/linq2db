45f79f86454776357b432dc89d5a44ae8a63ac7a
jods4 <joel.dumas@elca.ch>
2022-02-09 00:19:48 +0100

    Add tests: between, overlaps

--- Tests/Linq/Linq/SqlRowTests.cs
+++ Tests/Linq/Linq/SqlRowTests.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using FluentAssertions;
 
 using static LinqToDB.Sql;
+using DT = System.DateTime;
+using DTO = System.DateTimeOffset;
 
 namespace Tests.Linq
 {
@@ -240,6 +242,148 @@ namespace Tests.Linq
 					Row((int?)0, 7, 9),
 					Row((int?)null, 2, i.Four)))
 				.Should().Be(0);
+		}
+
+		[Test]
+		public void Between([DataSources] string context)
+		{
+			using var db   = GetDataContext(context);
+			using var ints = SetupIntsTable(db);
+
+			ints.Count(i => Row(i.One, i.Two).Between(
+					Row(i.One, i.One * 2),
+					Row(i.One, i.One + i.One)))
+				.Should().Be(1);
+
+			ints.Count(i => Row(i.One, i.Three).Between(
+					Row(i.One, i.One),
+					Row(i.One, i.Four)))
+				.Should().Be(1);
+
+			ints.Count(i => Row(i.One, i.Two).Between(
+					Row(i.One, i.Three),
+					Row(i.One, i.Two)))
+				.Should().Be(0);
+
+			ints.Count(i => Row(i.Two, i.Five).Between(
+					Row(i.One, i.One),
+					Row(i.Three, i.Two)))
+				.Should().Be(1);
+
+			ints.Count(i => Row(i.Two, i.Five).Between(
+					Row(i.One, i.One),
+					Row(i.Two, i.Two)))
+				.Should().Be(0);
+
+			ints.Count(i => Row(i.Two, i.Nil).Between(
+					Row<int, int?>(i.One, i.One),
+					Row<int, int?>(i.Three, i.One)))
+				.Should().Be(1);
+
+			ints.Count(i => Row(i.Two, i.Nil).Between(
+					Row<int, int?>(i.Two, i.One),
+					Row<int, int?>(i.Two, i.Three)))
+				.Should().Be(0);
+
+			ints.Count(i => Row<int, int?>(i.Two, i.Five).Between(
+					Row(i.One, i.Nil),
+					Row(i.Three, i.Nil)))
+				.Should().Be(1);
+
+			ints.Count(i => Row(i.Two, i.Nil).Between(
+					Row(i.One, i.Nil),
+					Row(i.Three, i.Nil)))
+				.Should().Be(1);
+
+			ints.Count(i => Row<int?, int>(i.Two, i.Two).Between(
+					Row(i.Nil, i.One),
+					Row<int?, int>(i.Three, i.Five)))
+				.Should().Be(0);
+		}
+
+		[Test]
+		public void NotBetween([DataSources] string context)
+		{
+			using var db   = GetDataContext(context);
+			using var ints = SetupIntsTable(db);
+
+			ints.Count(i => Row(i.One, i.Two).NotBetween(
+					Row(i.One, i.One * 2),
+					Row(i.One, i.One + i.One)))
+				.Should().Be(0);
+
+			ints.Count(i => Row(i.One, i.Three).NotBetween(
+					Row(i.One, i.One),
+					Row(i.One, i.Four)))
+				.Should().Be(0);
+
+			ints.Count(i => Row(i.One, i.Two).NotBetween(
+					Row(i.One, i.Three),
+					Row(i.One, i.Two)))
+				.Should().Be(1);
+
+			ints.Count(i => Row(i.Two, i.Five).NotBetween(
+					Row(i.One, i.One),
+					Row(i.Three, i.Two)))
+				.Should().Be(0);
+
+			ints.Count(i => Row(i.Two, i.Five).NotBetween(
+					Row(i.One, i.One),
+					Row(i.Two, i.Two)))
+				.Should().Be(1);
+
+			ints.Count(i => Row(i.Two, i.Nil).NotBetween(
+					Row<int, int?>(i.One, i.One),
+					Row<int, int?>(i.Three, i.One)))
+				.Should().Be(0);
+
+			ints.Count(i => Row(i.Two, i.Nil).NotBetween(
+					Row<int, int?>(i.Two, i.One),
+					Row<int, int?>(i.Two, i.Three)))
+				.Should().Be(0);
+
+			ints.Count(i => Row<int, int?>(i.Two, i.Five).NotBetween(
+					Row(i.One, i.Nil),
+					Row(i.Three, i.Nil)))
+				.Should().Be(0);
+
+			ints.Count(i => Row(i.Two, i.Nil).NotBetween(
+					Row(i.One, i.Nil),
+					Row(i.Three, i.Nil)))
+				.Should().Be(0);
+
+			ints.Count(i => Row<int?, int>(i.Two, i.Two).NotBetween(
+					Row(i.Nil, i.One),
+					Row<int?, int>(i.Three, i.Five)))
+				.Should().Be(0);
+		}
+
+		[Test]
+		public void Overlaps(
+			[DataSources(TestProvName.AllOracle, TestProvName.AllPostgreSQL, ProviderName.InformixDB2)] string context)
+		{
+			using var db   = GetDataContext(context);
+			using var ints = SetupIntsTable(db);
+
+			// OVERLAPS is neither client-evaluated, nor converted in providers without native support.
+			// So tests are short because we don't want to test all edge cases of provider implementation.
+			// We simply want to check if valid SQL is generated for all basic support types
+
+			ints.Count(i => Row(DT.Parse("2020-10-01"), DT.Parse("2020-10-05"))
+				  .Overlaps(Row(DT.Parse("2020-10-03"), DT.Parse("2020-11-09"))))
+				.Should().Be(1);
+
+			ints.Count(i => Row(DTO.Parse("2020-10-05"), DTO.Parse("2020-10-01"))
+				  .Overlaps(Row(DTO.Parse("2020-10-03"), DTO.Parse("2020-11-09"))))
+				.Should().Be(1);
+
+			ints.Count(i => Row(DT.Parse("2020-10-05"), TimeSpan.Parse("6"))
+				  .Overlaps(Row(DT.Parse("2020-10-03"), TimeSpan.Parse("1"))))
+				.Should().Be(1);
+
+			ints.Count(i => Row(DT.Parse("2020-10-05"), TimeSpan.Parse("6"))
+				  .Overlaps(Row(DT.Parse("2020-10-03"), (TimeSpan?)null)))
+				.Should().Be(1);
 		}
 
 		class Ints
