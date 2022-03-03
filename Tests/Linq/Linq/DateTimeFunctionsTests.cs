f3e170979540399cd19d3abd4face9ac8f6896b3
Igor Tkachev <igor.tkachev@gmail.com>
2022-03-03 00:12:34 -0500

    #3328 Fixed tests.

--- Tests/Linq/Linq/DateTimeFunctionsTests.cs
+++ Tests/Linq/Linq/DateTimeFunctionsTests.cs
@@ -102,7 +102,7 @@ namespace Tests.Linq
 		[Test]
 		public void CurrentTimestampUtc(
 			[DataSources(ProviderName.Access, TestProvName.AllFirebird, ProviderName.SqlCe,
-				ProviderName.SqlServer2000, ProviderName.SqlServer2005)]
+				ProviderName.SqlServer2000, ProviderName.SqlServer2005, TestProvName.AllMySql)]
 			string context)
 		{
 			using (new DisableBaseline("Server-side date generation test"))
