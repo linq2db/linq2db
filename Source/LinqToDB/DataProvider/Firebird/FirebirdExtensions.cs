1fe3137dbf2545866fc231eca1a74ad2ad65e0e7
MaceWindu <MaceWindu@users.noreply.github.com>
2022-03-20 15:40:02 +0100

    add missing guid to string conversion for firebird

---
+++ Source/LinqToDB/DataProvider/Firebird/FirebirdExtensions.cs
@@ -0,0 +1,22 @@
+﻿using LinqToDB.Data;
+using LinqToDB.Linq;
+using LinqToDB.SqlProvider;
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LinqToDB.DataProvider.Firebird
+{
+	public interface IFirebirdExtensions
+	{
+	}
+
+	public static class FirebirdExtensions
+	{
+		public static IFirebirdExtensions? Firebird(this Sql.ISqlExtension? ext) => null;
+
+		[Sql.Extension("UUID_TO_CHAR({guid})", PreferServerSide = true, IsNullable = Sql.IsNullableType.SameAsFirstParameter)]
+		public static string? UuidToChar(this IFirebirdExtensions? ext, [ExprParameter] Guid? guid) => guid?.ToString("D").ToUpperInvariant();
+	}
+}
