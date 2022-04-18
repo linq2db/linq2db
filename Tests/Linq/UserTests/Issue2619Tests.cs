e2f042545a43e0511448f1009bcda76d117e7465
Svyatoslav Danyliv <sdanyliv@gmail.com>
2022-04-18 15:38:45 +0300

    Proper fix for Ordering with Set Operators.

--- Tests/Linq/UserTests/Issue2619Tests.cs
+++ Tests/Linq/UserTests/Issue2619Tests.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Linq;
+using FluentAssertions;
+using LinqToDB;
 using NUnit.Framework;
 using Tests.Model;
 
@@ -9,35 +11,138 @@ namespace Tests.UserTests
 	public class Issue2619Tests : TestBase
 	{
 		[Test]
-		public void OrderByUnionTest ([NorthwindDataContext] string context)
+		public void OrderByUnion ([DataSources] string context)
 		{
-			using (var db = new NorthwindDB (context))
+			using (var db = GetDataContext(context))
 			{
-				var products = db.Product
-						.OrderBy (c => c.ProductName)
-					;
+				var persons = db.Person
+						.OrderBy (c => c.LastName);
 
-				var orders = db.Order;
+				var union = persons
+						.Union (persons);
 
-				var orderDetails =
-					db.OrderDetail
-						.Join (products,
-							orderDetail => orderDetail.ProductID, product => product.ProductID,
-							(orderDetail, product) => orderDetail);
+				var sql = union.ToString();
 
-				var productOrders = orderDetails
-					.Join (db.Order, orderDetail => orderDetail.OrderID, order => order.OrderID,
-						(orderDetail, order) => order);
-
-				var union = orders
-						.Union (productOrders)
-					;
+				sql.Should().NotContain("ORDER");
 
 				Assert.DoesNotThrow (() =>
 				{
-					var list = union.ToList ();
+					union.ToList();
 				});
 			}
 		}
+
+		[Test]
+		public void OrderByUnionModifier([DataSources(TestProvName.AllSybase)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var persons = db.Person
+					.OrderBy (c => c.LastName).Take(1);
+
+				var union = persons
+					.Union (persons);
+
+				var sql = union.ToString();
+
+				sql.Should().Contain("ORDER", Exactly.Twice());
+
+				Assert.DoesNotThrow (() =>
+				{
+					union.ToList();
+				});
+			}
+		}
+
+		[Test]
+		public void OrderByConcat([DataSources(ProviderName.SqlCe, TestProvName.AllSqlServer, TestProvName.AllSybase)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var persons = db.Person
+					.OrderBy (c => c.LastName);
+
+				var concat = persons
+					.Concat(persons);
+
+				var sql = concat.ToString();
+
+				sql.Should().Contain("ORDER", Exactly.Twice());
+
+				Assert.DoesNotThrow (() =>
+				{
+					concat.ToList();
+				});
+			}
+		}
+
+		[Test]
+		public void OrderByConcatModifier([DataSources(TestProvName.AllSybase)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var persons = db.Person
+					.OrderBy (c => c.LastName).Take(1);
+
+				var concat = persons
+					.Concat(persons);
+
+				var sql = concat.ToString();
+
+				sql.Should().Contain("ORDER", Exactly.Twice());
+
+				Assert.DoesNotThrow (() =>
+				{
+					concat.ToList();
+				});
+			}
+		}
+
+
+		[Test]
+		public void OrderByExcept([DataSources(TestProvName.AllSybase, TestProvName.AllSqlServer)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var persons = db.Person
+					.OrderBy (c => c.LastName);
+
+				var concat = persons
+					.Except(persons);
+
+				var sql = concat.ToString();
+
+				sql.Should().Contain("ORDER", AtLeast.Once());
+
+				Assert.DoesNotThrow (() =>
+				{
+					concat.ToList();
+				});
+			}
+		}
+
+		[Test]
+		public void OrderByExceptModifier([DataSources(TestProvName.AllSybase)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var persons = db.Person
+					.OrderBy (c => c.LastName)
+					.Take(1);
+
+				var except = persons
+					.Except(persons);
+
+				var sql = except.ToString();
+
+				sql.Should().Contain("ORDER", AtLeast.Once());
+
+				Assert.DoesNotThrow (() =>
+				{
+					except.ToList();
+				});
+			}
+		}
+
 	}
 }
