937e6d073eedbd051b84becc6bceee60c1d0fac9
Svyatoslav Danyliv <sdanyliv@gmail.com>
2022-04-18 16:08:31 +0300

    Working on IExpressionInterceptor.

---
+++ Source/LinqToDB/Interceptors/IExpressionInterceptor.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace LinqToDB.Interceptors
+{
+	public interface IExpressionInterceptor : IInterceptor
+	{
+		public Expression ProcessExpression(Expression expression);
+	}
+}
