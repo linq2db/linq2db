35d0b715f45cdcd7c3df6f42d1ab36e71652e6ec
MaceWindu <MaceWindu@users.noreply.github.com>
2022-08-10 10:41:37 +0200

    refactor LIKE handling in BasicSqlOptimizer, move Access-specific logic to Access sql optimizer (#3689)

--- Source/LinqToDB/DataProvider/Access/AccessSqlOptimizer.cs
+++ Source/LinqToDB/DataProvider/Access/AccessSqlOptimizer.cs
@@ -31,6 +31,18 @@ namespace LinqToDB.DataProvider.Access
 			return base.ConvertLikePredicate(mappingSchema, predicate, context);
 		}
 
+		protected override string EscapeLikePattern(string str)
+		{
+			var newStr = DataTools.EscapeUnterminatedBracket(str);
+			if (newStr == str)
+				newStr = newStr.Replace("[", "[[]");
+
+			foreach (var s in LikeCharactersToEscape)
+				newStr = newStr.Replace(s, "[" + s + "]");
+
+			return newStr;
+		}
+
 		public override ISqlExpression EscapeLikeCharacters(ISqlExpression expression, ref ISqlExpression? escape)
 		{
 			throw new LinqException("Access does not support `Replace` function which is required for such query.");
@@ -144,7 +156,7 @@ namespace LinqToDB.DataProvider.Access
 			{
 				case "$ToLower$" : return new SqlFunction(func.SystemType, "LCase", func.IsAggregate, func.IsPure, func.Precedence, func.Parameters);
 				case "$ToUpper$" : return new SqlFunction(func.SystemType, "UCase", func.IsAggregate, func.IsPure, func.Precedence, func.Parameters);
-				case "Length"    : return new SqlFunction(func.SystemType, "LEN",   func.IsAggregate, func.IsPure, func.Precedence, func.Parameters);
+				case "Length"                : return new SqlFunction(func.SystemType, "LEN",   func.IsAggregate, func.IsPure, func.Precedence, func.Parameters);
 			}
 			return base.ConvertFunction(func);
 		}
