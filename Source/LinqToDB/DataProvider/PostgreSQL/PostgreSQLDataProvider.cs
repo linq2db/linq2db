6c56f98a3aa3738a3d671179b05752fbc86f089c
MaceWindu <MaceWindu@users.noreply.github.com>
2022-08-14 20:17:08 +0200

    - npgsql 7 support
    - npgsqlinterval support

--- Source/LinqToDB/DataProvider/PostgreSQL/PostgreSQLDataProvider.cs
+++ Source/LinqToDB/DataProvider/PostgreSQL/PostgreSQLDataProvider.cs
@@ -140,10 +140,14 @@ namespace LinqToDB.DataProvider.PostgreSQL
 			mapType("datemultirange"          , NpgsqlProviderAdapter.NpgsqlDbType.DateMultirange);
 
 
-			SetProviderField(Adapter.NpgsqlTimeSpanType, Adapter.NpgsqlTimeSpanType, Adapter.GetIntervalReaderMethod     , dataReaderType: Adapter.DataReaderType);
-			SetProviderField(Adapter.NpgsqlDateTimeType, Adapter.NpgsqlDateTimeType, Adapter.GetTimeStampReaderMethod    , dataReaderType: Adapter.DataReaderType);
-			SetProviderField(Adapter.NpgsqlInetType    , Adapter.NpgsqlInetType    , GetProviderSpecificValueReaderMethod, dataReaderType: Adapter.DataReaderType);
-			SetProviderField(Adapter.NpgsqlDateType    , Adapter.NpgsqlDateType    , Adapter.GetDateReaderMethod         , dataReaderType: Adapter.DataReaderType);
+			if (Adapter.NpgsqlTimeSpanType != null) SetProviderField(Adapter.NpgsqlTimeSpanType, Adapter.NpgsqlTimeSpanType, Adapter.GetIntervalReaderMethod! , dataReaderType: Adapter.DataReaderType);
+			if (Adapter.NpgsqlDateTimeType != null) SetProviderField(Adapter.NpgsqlDateTimeType, Adapter.NpgsqlDateTimeType, Adapter.GetTimeStampReaderMethod!, dataReaderType: Adapter.DataReaderType);
+			if (Adapter.NpgsqlDateType     != null) SetProviderField(Adapter.NpgsqlDateType    , Adapter.NpgsqlDateType    , Adapter.GetDateReaderMethod!     , dataReaderType: Adapter.DataReaderType);
+
+			if (Adapter.NpgsqlIntervalType != null)
+				ReaderExpressions[new ReaderInfo { ToType = Adapter.NpgsqlIntervalType }] = Adapter.NpgsqlIntervalReader!;
+
+			SetProviderField(Adapter.NpgsqlInetType, Adapter.NpgsqlInetType, GetProviderSpecificValueReaderMethod, dataReaderType: Adapter.DataReaderType);
 
 			bool mapType(string dbType, NpgsqlProviderAdapter.NpgsqlDbType type)
 			{
