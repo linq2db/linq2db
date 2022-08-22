228cbc04f42d59bf25df33be2eade7ac64e3b997
MaceWindu <MaceWindu@users.noreply.github.com>
2022-08-22 13:18:22 +0200

    prevent incorrect closures optimization (#3717)

--- Source/LinqToDB/Common/Internal/TypeExtensions.cs
+++ Source/LinqToDB/Common/Internal/TypeExtensions.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Text;
 using LinqToDB.Extensions;
 
 namespace LinqToDB.Common.Internal
