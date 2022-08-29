5e204aefd083bde9ca9ee690c2d2668687aa2339
MaceWindu <MaceWindu@users.noreply.github.com>
2022-08-29 16:56:45 +0200

    add sproc api test

--- Source/LinqToDB/DataProvider/Oracle/OracleDataProvider.cs
+++ Source/LinqToDB/DataProvider/Oracle/OracleDataProvider.cs
@@ -184,9 +184,9 @@
 					break;
 
 				case DataType.Boolean  :
-					dataType = dataType.WithDataType(DataType.Byte);
-					if (value is bool boolValue)
-						value = boolValue ? (byte)1 : (byte)0;
+					//dataType = dataType.WithDataType(DataType.Byte);
+					//if (value is bool boolValue)
+					//	value = boolValue ? (byte)1 : (byte)0;
 					break;
 
 				case DataType.Guid     :
@@ -282,6 +282,7 @@
 				case DataType.LongRaw  : type = OracleProviderAdapter.OracleDbType.LongRaw     ; break;
 				case DataType.Json     : type = OracleProviderAdapter.OracleDbType.Json        ; break;
 				case DataType.Guid     : type = OracleProviderAdapter.OracleDbType.Raw         ; break;
+				case DataType.Boolean  : type = OracleProviderAdapter.OracleDbType.Boolean     ; break;
 			}
 
 			if (type != null)
