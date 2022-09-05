a306e96c1533ba876bff8546aec22523255f7864
MaceWindu <MaceWindu@users.noreply.github.com>
2022-09-05 15:49:58 +0200

    SQL Server 2022 RC.0 support (#3759)
    * sql server 2022 RC.0 support/fix trim chars mapping
    
    * remove unnecessary limitation for extensions
    
    * fix FB4 compatibility for L/R trim
    
    * trim with empty chars should use default behavior

--- Source/LinqToDB/Sql/Sql.ExtensionAttribute.cs
+++ Source/LinqToDB/Sql/Sql.ExtensionAttribute.cs
@@ -501,14 +501,6 @@ namespace LinqToDB
 						mapping.GetAttributes<ExtensionAttribute>(memberInfo.ReflectedType!, memberInfo,
 							static a => a.Configuration, inherit: true, exactForConfiguration: true);
 
-				if (attributes.Length == 0)
-				{
-					// notify if there is method that has no defined attribute for specific configuration
-					attributes = mapping.GetAttributes<ExtensionAttribute>(memberInfo.ReflectedType!, memberInfo);
-					if (attributes.Length > 0)
-						ThrowHelper.ThrowLinqToDBException($"Member {memberInfo.Name}, unsupported for configuration(s) '{string.Join(", ", mapping.ConfigurationList)}'.");
-				}
-
 				return attributes;
 			}
 
