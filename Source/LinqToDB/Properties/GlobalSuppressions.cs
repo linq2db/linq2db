099a7189b72678ae497491b0dd98528afb5930c5
jods4 <joel.dumas@elca.ch>
2022-09-18 17:59:12 +0200

    Fix warning in Release build

--- Source/LinqToDB/Properties/GlobalSuppressions.cs
+++ Source/LinqToDB/Properties/GlobalSuppressions.cs
@@ -1,7 +1,11 @@
 using System.Diagnostics.CodeAnalysis;
 
+#if NETSTANDARD2_0_OR_GREATER || NETCOREAPP2_0_OR_GREATER || NET5_0_OR_GREATER
+
 [assembly: SuppressMessage(
   "Usage", "CA2201:Exception type System.Exception is not sufficiently specific",
   Justification = "Source code imported from 3rd party package Nullability.Source",
   Scope = "type", 
   Target = "T:System.Reflection.NullabilityInfoExtensions")]
+
+#endif
