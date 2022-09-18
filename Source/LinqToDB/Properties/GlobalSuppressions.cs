e1870b985f2e557fca8e857afd9886df058aa254
jods4 <joel.dumas@elca.ch>
2022-09-18 14:28:58 +0200

    Attempt to ignore warnings from imported c# code.

---
+++ Source/LinqToDB/Properties/GlobalSuppressions.cs
@@ -0,0 +1,7 @@
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+  "Usage", "CA2201:Exception type System.Exception is not sufficiently specific",
+  Justification = "Source code imported from 3rd party package Nullability.Source",
+  Scope = "type", 
+  Target = "T:System.Reflection.NullabilityInfoExtensions")]
