cbb36c8afacb245a2dfc53d5033dc9fc668bbb41
MaceWindu <MaceWindu@users.noreply.github.com>
2022-10-09 18:06:17 +0200

    revert style changes

--- Source/LinqToDB.Tools/CodeModel/Utils/AstExtensions.cs
+++ Source/LinqToDB.Tools/CodeModel/Utils/AstExtensions.cs
@@ -1,35 +1,36 @@
 ﻿using System.Collections.Generic;
 
-namespace LinqToDB.CodeModel;
-
-public static class AstExtensions
+namespace LinqToDB.CodeModel
 {
-	public static IEnumerable<TGroup> EnumerateMemberGroups<TGroup>(this IEnumerable<IMemberGroup> groups)
-		where TGroup : IMemberGroup
+	public static class AstExtensions
 	{
-		foreach (var group in groups)
+		public static IEnumerable<TGroup> EnumerateMemberGroups<TGroup>(this IEnumerable<IMemberGroup> groups)
+			where TGroup : IMemberGroup
 		{
-			if (group is RegionGroup regionGroup)
+			foreach (var group in groups)
 			{
-				foreach (var region in regionGroup.Members)
+				if (group is RegionGroup regionGroup)
 				{
-					foreach (var memberGroup in region.Members.EnumerateMemberGroups<TGroup>())
+					foreach (var region in regionGroup.Members)
 					{
-						yield return memberGroup;
+						foreach (var memberGroup in region.Members.EnumerateMemberGroups<TGroup>())
+						{
+							yield return memberGroup;
+						}
 					}
 				}
+				else if (group is TGroup memberGroup)
+					yield return memberGroup;
 			}
-			else if (group is TGroup memberGroup)
-				yield return memberGroup;
 		}
-	}
 
-	public static IEnumerable<TElement> EnumerateMembers<TGroup, TElement>(this IEnumerable<IMemberGroup> groups)
-		where TGroup : MemberGroup<TElement>
-		where TElement : IGroupElement
-	{
-		foreach (var group in groups.EnumerateMemberGroups<TGroup>())
-			foreach (var element in group.Members)
-				yield return element;
+		public static IEnumerable<TElement> EnumerateMembers<TGroup, TElement>(this IEnumerable<IMemberGroup> groups)
+			where TGroup : MemberGroup<TElement>
+			where TElement : IGroupElement
+		{
+			foreach (var group in groups.EnumerateMemberGroups<TGroup>())
+				foreach (var element in group.Members)
+					yield return element;
+		}
 	}
 }
