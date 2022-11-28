0db28bcc3e45d3085bc3873d1d1ba4391ae0206d
jkuehner <jochen.kuehner@kardex.com>
2022-11-28 17:54:20 +0100

    test for #3840

---
+++ Tests/Linq/UserTests/Issue3840Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.Serialization;
+using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.Mapping;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace Tests.UserTests.Test3840
+{
+	public class Test
+	{
+		public virtual DateTime? StartDateTime { get; set; }
+
+		public virtual TimeSpan? PreNotification { get; set; }
+	}
+
+	[TestFixture]
+	public class Test3840Tests : TestBase
+	{
+		[Test]
+		public void Test3840([IncludeDataSources(TestProvName.AllSqlServer)] string configuration)
+		{
+			var ms = new MappingSchema();
+			var mb = ms.GetFluentMappingBuilder();
+			mb.Entity<Test>()
+			   .HasTableName("Common_Topology_Locations")
+			   .Property(e => e.StartDateTime)
+			   .Property(e => e.PreNotification);
+
+			using (var db = GetDataContext(configuration, ms))
+			{
+				using (db.CreateLocalTable<Test>())
+				{
+					var qry = from t in db.GetTable<Test>()
+							  select new
+							  {
+								  StartDateTime = t.StartDateTime,
+								  PreNotification = t.PreNotification,
+								  NotificationDateTime = Sql.DateAdd(Sql.DateParts.Millisecond, -1 * t.PreNotification!.Value.Milliseconds, t.StartDateTime)
+							  };
+					var lst = qry.ToList();
+					var sql = ((DataConnection)db).LastQuery;
+				}				
+			}
+		}
+	}
+}
