676368ef75b4c41bdd86d015d4e98c022c4dc600
MaceWindu <MaceWindu@users.noreply.github.com>
2022-12-06 10:36:14 +0100

    Fix Nullable<T>.GetDefaultValue mapping (#3837)
    * fix constant parameters deduplication
    
    * restore formatting
    
    * revert incorrect fix, update GetValueOrDefault mapper to be optimization-friendly
    
    * fix Informix NVL translation

--- Source/LinqToDB/SqlProvider/BasicSqlOptimizer.cs
+++ Source/LinqToDB/SqlProvider/BasicSqlOptimizer.cs
@@ -2515,12 +2515,19 @@ namespace LinqToDB.SqlProvider
 			return p;
 		}
 
-		protected ISqlExpression ConvertCoalesceToBinaryFunc(SqlFunction func, string funcName)
+		protected ISqlExpression ConvertCoalesceToBinaryFunc(SqlFunction func, string funcName, bool supportsParameters = true)
 		{
 			var last = func.Parameters[func.Parameters.Length - 1];
+			if (!supportsParameters && last is SqlParameter p1)
+				p1.IsQueryParameter = false;
+
 			for (int i = func.Parameters.Length - 2; i >= 0; i--)
 			{
-				last = new SqlFunction(func.SystemType, funcName, func.Parameters[i], last);
+				var param = func.Parameters[i];
+				if (!supportsParameters && param is SqlParameter p2)
+					p2.IsQueryParameter = false;
+
+				last = new SqlFunction(func.SystemType, funcName, param, last);
 			}
 			return last;
 		}
