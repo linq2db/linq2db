ce7251f32b35d60361dc82fef01f9e077ecb6e3b
Svyatoslav Danyliv <sdanyliv@gmail.com>
2022-12-29 18:42:08 +0200

    Introduced BuildContextBase.

---
+++ Source/LinqToDB/Linq/Builder/BuildContextBase.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace LinqToDB.Linq.Builder
+{
+	using SqlQuery;
+
+	[DebuggerDisplay("{BuildContextDebuggingHelper.GetContextInfo(this)}")]
+	abstract class BuildContextBase : IBuildContext
+	{
+
+#if DEBUG
+		public string SqlQueryText => SelectQuery?.SqlText ?? "";
+		public string Path         => this.GetPath();
+		public int    ContextId    { get; }
+#endif
+
+		protected BuildContextBase(ExpressionBuilder builder, SelectQuery selectQuery)
+		{
+			Builder     = builder;
+			SelectQuery = selectQuery;
+#if DEBUG
+			ContextId = builder.GenerateContextId();
+#endif
+		}
+
+		public         ExpressionBuilder Builder     { get; }
+		public virtual Expression?       Expression  => null;
+		public         SelectQuery       SelectQuery { get; set; }
+		public virtual SqlStatement?     Statement   { get; set; }
+		public         IBuildContext?    Parent      { get; set; }
+
+		public abstract Expression MakeExpression(Expression path, ProjectFlags flags);
+
+		public abstract void SetRunQuery<T>(Query<T> query, Expression expr);
+
+		public abstract IBuildContext Clone(CloningContext context);
+
+		public abstract SqlStatement GetResultStatement();
+
+		public virtual void SetAlias(string? alias)
+		{
+		}
+
+		public virtual ISqlExpression? GetSubQuery(IBuildContext context)
+		{
+			return null;
+		}
+		
+		#region Obsolete
+
+		public virtual void BuildQuery<T>(Query<T> query, ParameterExpression queryParameter)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual Expression BuildExpression(Expression? expression, int level, bool enforceServerSide)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual SqlInfo[] ConvertToSql(Expression? expression, int level, ConvertFlags flags)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual SqlInfo[] ConvertToIndex(Expression? expression, int level, ConvertFlags flags)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual IsExpressionResult IsExpression(Expression? expression, int level, RequestFor requestFlag)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual IBuildContext? GetContext(Expression? expression, int level, BuildInfo buildInfo)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual int ConvertToParentIndex(int index, IBuildContext context)
+		{
+			throw new NotImplementedException();
+		}
+
+		public virtual void CompleteColumns()
+		{
+		}
+
+		#endregion Obsolete
+
+	}
+}
--- Source/LinqToDB/Linq/Builder/SequenceContextBase.cs
+++ Source/LinqToDB/Linq/Builder/SequenceContextBase.cs
@@ -6,22 +6,15 @@ namespace LinqToDB.Linq.Builder
 	using SqlQuery;
 
 	[DebuggerDisplay("{BuildContextDebuggingHelper.GetContextInfo(this)}")]
-	abstract class SequenceContextBase : IBuildContext
+	abstract class SequenceContextBase : BuildContextBase
 	{
 		protected SequenceContextBase(IBuildContext? parent, IBuildContext[] sequences, LambdaExpression? lambda)
+			: base(sequences[0].Builder, sequences[0].SelectQuery)
 		{
 			Parent          = parent;
 			Sequences       = sequences;
-			Builder         = sequences[0].Builder;
-			Lambda          = lambda;
 			Body            = lambda == null ? null : SequenceHelper.PrepareBody(lambda, sequences);
-			SelectQuery     = sequences[0].SelectQuery;
 			Sequence.Parent = this;
-
-			Builder.Contexts.Add(this);
-#if DEBUG
-			ContextId = Builder.GenerateContextId();
-#endif
 		}
 
 		protected SequenceContextBase(IBuildContext? parent, IBuildContext sequence, LambdaExpression? lambda)
@@ -29,63 +22,31 @@ namespace LinqToDB.Linq.Builder
 		{
 		}
 
-#if DEBUG
-		public string SqlQueryText  => SelectQuery?.SqlText ?? "";
-		public string Path          => this.GetPath();
-		public int    ContextId     { get; }
-#endif
-
-		public IBuildContext?    Parent      { get; set; }
 		public IBuildContext[]   Sequences   { get; set; }
-		public ExpressionBuilder Builder     { get; set; }
-		public LambdaExpression? Lambda      { get; set; }
 		public Expression?       Body        { get; set; }
-		public SelectQuery       SelectQuery { get; set; }
-		public SqlStatement?     Statement   { get; set; }
 		public IBuildContext     Sequence    => Sequences[0];
 
-		Expression? IBuildContext.Expression => Lambda;
+		public override Expression? Expression => Body;
 
-		public virtual void BuildQuery<T>(Query<T> query, ParameterExpression queryParameter)
-		{
-			throw new NotImplementedException();
-
-			/*var expr = Builder.FinalizeProjection(this,
-				Builder.MakeExpression(new ContextRefExpression(typeof(T), this), ProjectFlags.Expression));
-
-			var mapper = Builder.BuildMapper<T>(expr);
-
-			QueryRunner.SetRunQuery(query, mapper);*/
-		}
-
-		public abstract Expression         BuildExpression(Expression? expression, int level, bool enforceServerSide);
-		public abstract SqlInfo[]          ConvertToSql   (Expression? expression, int level, ConvertFlags flags);
-		public abstract SqlInfo[]          ConvertToIndex (Expression? expression, int level, ConvertFlags flags);
-
-		public virtual Expression MakeExpression(Expression path, ProjectFlags flags)
+		public override Expression MakeExpression(Expression path, ProjectFlags flags)
 		{
 			path = SequenceHelper.CorrectExpression(path, this, Sequence);
 			return Builder.MakeExpression(Sequence, path, flags);
 		}
 
-		public abstract IBuildContext Clone(CloningContext    context);
-
-		public virtual void SetRunQuery<T>(Query<T> query, Expression expr)
+		public override void SetRunQuery<T>(Query<T> query, Expression expr)
 		{
 			var mapper = Builder.BuildMapper<T>(expr);
 
 			QueryRunner.SetRunQuery(query, mapper);
 		}
 
-		public abstract IsExpressionResult IsExpression   (Expression? expression, int level, RequestFor requestFlag);
-		public abstract IBuildContext?     GetContext     (Expression? expression, int level, BuildInfo buildInfo);
-
-		public virtual SqlStatement GetResultStatement()
+		public override SqlStatement GetResultStatement()
 		{
 			return Sequence.GetResultStatement();
 		}
 
-		public void CompleteColumns()
+		public override void CompleteColumns()
 		{
 			foreach (var sequence in Sequences)
 			{
@@ -93,12 +54,7 @@ namespace LinqToDB.Linq.Builder
 			}
 		}
 
-		public virtual int ConvertToParentIndex(int index, IBuildContext context)
-		{
-			return Parent?.ConvertToParentIndex(index, context) ?? index;
-		}
-
-		public virtual void SetAlias(string? alias)
+		public override void SetAlias(string? alias)
 		{
 			if (SelectQuery.Select.Columns.Count == 1)
 			{
@@ -108,11 +64,5 @@ namespace LinqToDB.Linq.Builder
 			if (SelectQuery.From.Tables.Count > 0)
 				SelectQuery.From.Tables[SelectQuery.From.Tables.Count - 1].Alias = alias;
 		}
-
-		public virtual ISqlExpression? GetSubQuery(IBuildContext context)
-		{
-			return null;
-		}
-
 	}
 }
