fad6d7dbce5bc0be3be6cac555ac96458d1c7ca8
MaceWindu <MaceWindu@users.noreply.github.com>
2023-01-24 17:57:51 +0100

    fix merge

--- Tests/Linq/UserTests/Issue3840Tests.cs
+++ Tests/Linq/UserTests/Issue3840Tests.cs
@@ -1,4 +1,4 @@
-﻿ï»¿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -28,25 +28,25 @@ namespace Tests.UserTests.Test3840
 			var ms = new MappingSchema();
 			var mb = ms.GetFluentMappingBuilder();
 			mb.Entity<Test>()
-			   .HasTableName("Common_Topology_Locations")
-			   .Property(e => e.StartDateTime)
-			   .Property(e => e.PreNotification).HasDataType(LinqToDB.DataType.Int64);
+				.HasTableName("Common_Topology_Locations")
+				.Property(e => e.StartDateTime)
+				.Property(e => e.PreNotification).HasDataType(LinqToDB.DataType.Int64);
+			mb.Build();
 
-			using (var db = GetDataContext(configuration, ms))
-			{
-				using (db.CreateLocalTable<Test>())
-				{
-					db.Insert(new Test() { StartDateTime = DateTime.UtcNow, PreNotification = TimeSpan.FromSeconds(2000) });
-					var qry = from t in db.GetTable<Test>()
-							  select new
-							  {
-								  StartDateTime = t.StartDateTime,
-								  PreNotification = t.PreNotification,
-								  NotificationDateTime = Sql.DateAdd(Sql.DateParts.Millisecond, -1 * t.PreNotification!.Value.Milliseconds, t.StartDateTime)
-							  };
-					var lst = qry.ToList();
-				}
-			}
+			using var db = GetDataContext(configuration, ms);
+			using var _ = db.CreateLocalTable<Test>();
+
+			db.Insert(new Test() { StartDateTime = DateTime.UtcNow, PreNotification = TimeSpan.FromSeconds(2000) });
+
+			var qry = from t in db.GetTable<Test>()
+					  select new
+					  {
+						  StartDateTime = t.StartDateTime,
+						  PreNotification = t.PreNotification,
+						  NotificationDateTime = Sql.DateAdd(Sql.DateParts.Millisecond, -1 * t.PreNotification!.Value.Milliseconds, t.StartDateTime)
+					  };
+			
+			var lst = qry.ToList();
 		}
 	}
 }
