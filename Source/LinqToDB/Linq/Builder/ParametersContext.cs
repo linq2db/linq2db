986af38f29c50ff884ce19ef5431cbd8a527b472
Igor Tkachev <igor.tkachev@gmail.com>
2023-04-11 00:21:20 -0400

    Fixed #4046.

--- Source/LinqToDB/Linq/Builder/ParametersContext.cs
+++ Source/LinqToDB/Linq/Builder/ParametersContext.cs
@@ -281,11 +281,13 @@ namespace LinqToDB.Linq.Builder
 					}
 					else
 					{
+						// Try GetConvertExpression<.., DataParameter>() first.
+						//
 						if (newExpr.ValueExpression.Type != typeof(DataParameter))
 						{
 							var expr = MappingSchema.GetConvertExpression(newExpr.ValueExpression.Type, typeof(DataParameter), false, false);
 
-							newExpr.ValueExpression = expr != null ?
+							newExpr.ValueExpression = expr is not null ?
 								InternalExtensions.ApplyLambdaToExpression(expr, newExpr.ValueExpression) :
 								ColumnDescriptor.ApplyConversions(MappingSchema, newExpr.ValueExpression, newExpr.DataType, null, true);
 						}
