50dc2a977f537215529aa2deeb6680ca8550c0fd
MaceWindu <MaceWindu@users.noreply.github.com>
2023-05-02 11:15:29 +0200

    add test and fix  #3034 (#4093)

--- Tests/Linq/Linq/InterfaceTests.cs
+++ Tests/Linq/Linq/InterfaceTests.cs
@@ -25,6 +25,42 @@ namespace Tests.Linq
 			}
 		}
 
+		#region Issue 3034
+		interface IA
+		{
+			int Id { get; set; }
+		}
+
+		interface IB : IA
+		{
+			string Name { get; set; }
+		}
+
+		sealed class MyTable
+		{
+			public int     Id   { get; set; }
+			public string? Name { get; set; }
+		}
+
+		[Test]
+		public void Issue3034([IncludeDataSources(ProviderName.SQLiteClassic)] string context)
+		{
+			using var db = GetDataContext(context);
+			using var t = db.CreateLocalTable<MyTable>(new[]{ new MyTable() { Id = 1, Name = "old_name" }, new MyTable() { Id = 2, Name = "old_name" } });
+
+			db.GetTable<IB>().TableName("MyTable")
+				.Where(x => x.Id == 1)
+				.Set(x => x.Name, x => "new_name")
+				.Update();
+
+			var results = t.OrderBy(r => r.Id).ToArray();
+
+			Assert.AreEqual(2, results.Length);
+			Assert.AreEqual(1, results[0].Id);
+			Assert.AreEqual("new_name", results[0].Name);
+			Assert.AreEqual(2, results[1].Id);
+			Assert.AreEqual("old_name", results[1].Name);
+
 		#region Issue 4082
 		public interface IIdentifiable
 		{
