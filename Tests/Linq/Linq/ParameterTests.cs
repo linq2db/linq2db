a9d62d89f993f8c0667657f7115d669a1a0a2336
MaceWindu <MaceWindu@users.noreply.github.com>
2023-05-14 18:36:49 +0200

    add #4052 regression test

--- Tests/Linq/Linq/ParameterTests.cs
+++ Tests/Linq/Linq/ParameterTests.cs
@@ -1554,5 +1554,23 @@ namespace Tests.Linq
 			_cnt++;
 			return new[] { 1, 2, 3, 4 }.Where(_ => _ != _param);
 		}
+
+		[Test]
+		public void Issue4052([DataSources] string context)
+		{
+			using var db = GetDataContext(context);
+
+			var data = new Person()
+			{
+				ID = 1,
+			};
+
+#pragma warning disable CA1311 // Specify a culture or use an invariant version
+			var query1 = (from c in db.Person
+						  where c.ID == data.ID
+						  && (c.MiddleName != null ? c.MiddleName.Trim().ToLower() : string.Empty) == (data.MiddleName != null ? data.MiddleName.Trim().ToLower() : string.Empty)
+						  select c).ToList();
+#pragma warning restore CA1311 // Specify a culture or use an invariant version
+		}
 	}
 }
