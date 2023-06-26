db38e20712ebb8d429e41b06c114e94b13c71afe
Igor Tkachev <igor.tkachev@gmail.com>
2023-06-26 18:02:34 -0400

    Rework T4 templates.

--- Source/LinqToDB/Configuration/LinqToDBSettings.cs
+++ Source/LinqToDB/Configuration/LinqToDBSettings.cs
@@ -18,17 +18,10 @@ namespace LinqToDB.Configuration
 			_connectionStringSettings = new ConnectionStringSettings(connectionName, connectionString, providerName);
 		}
 
-		public IEnumerable<IDataProviderSettings> DataProviders        => Enumerable.Empty<IDataProviderSettings>();
-		public string                             DefaultConfiguration => _connectionStringSettings.Name;
-		public string                             DefaultDataProvider  => ProviderName.SqlServer;
-
-		public IEnumerable<IConnectionStringSettings> ConnectionStrings
-		{
-			get
-			{
-				yield return _connectionStringSettings;
-			}
-		}
+		public IEnumerable<IDataProviderSettings>     DataProviders        => Enumerable.Empty<IDataProviderSettings>();
+		public string                                 DefaultConfiguration => _connectionStringSettings.Name;
+		public string                                 DefaultDataProvider  => ProviderName.SqlServer;
+		public IEnumerable<IConnectionStringSettings> ConnectionStrings    => new [] { _connectionStringSettings };
 	}
 
 }
