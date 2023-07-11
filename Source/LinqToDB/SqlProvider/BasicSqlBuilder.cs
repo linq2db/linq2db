58504f51656a3aa8d003c842ca6d6c972a23a02b
Igor Tkachev <igor.tkachev@gmail.com>
2023-07-11 19:36:43 -0400

    Update Source/LinqToDB/SqlProvider/BasicSqlBuilder.cs
    Co-authored-by: Stuart Turner <stuart@turner-isageek.com>

--- Source/LinqToDB/SqlProvider/BasicSqlBuilder.cs
+++ Source/LinqToDB/SqlProvider/BasicSqlBuilder.cs
@@ -456,7 +456,7 @@ namespace LinqToDB.SqlProvider
 				BuildStep = Step.HavingClause;    BuildHavingClause   (statement.SelectQuery);
 				BuildStep = Step.OrderByClause;   BuildOrderByClause  (statement.SelectQuery);
 				BuildStep = Step.OffsetLimit;     BuildOffsetLimit    (statement.SelectQuery);
-				BuildStep = Step.QueryExtensions; BuildQueryExtensions(statement, false, true);
+				BuildStep = Step.QueryExtensions; BuildQueryExtensions(statement, buildQueryHints: false, buildSubQueryHints: true);
 			}
 
 			if (insertClause.WithIdentity)
