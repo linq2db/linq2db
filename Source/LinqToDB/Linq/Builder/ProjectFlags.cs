5616046e023225242a04d43852ec0b70115dbcb1
MaceWindu <MaceWindu@users.noreply.github.com>
2023-11-01 12:35:37 +0100

    fix context leak regression

--- Source/LinqToDB/Linq/Builder/ProjectFlags.cs
+++ Source/LinqToDB/Linq/Builder/ProjectFlags.cs
@@ -11,9 +11,9 @@ namespace LinqToDB.Linq.Builder
 		/// <summary>
 		/// Forces expanding associations and GroupJoin into query expression
 		/// </summary>
-		ExtractProjection = 0x08,	
+		ExtractProjection = 0x08,
 
-		AggregationRoot = 0x10,		
+		AggregationRoot = 0x10,
 		/// <summary>
 		/// Specify that from whole context we need just key fields.
 		/// </summary>
@@ -22,25 +22,25 @@ namespace LinqToDB.Linq.Builder
 		/// Validates that expression can be converted to the SQL. Returned value cannot be used.
 		/// </summary>
 		Test            = 0x40,
-		AssociationRoot = 0x80,	
+		AssociationRoot = 0x80,
 		/// <summary>
 		/// Specify that we are looking for a table
 		/// </summary>
-		Table = 0x100,	
+		Table = 0x100,
 		/// <summary>
 		/// Specify that we associations should not filter out recordset
 		/// </summary>
-		ForceOuterAssociation = 0x200,	
+		ForceOuterAssociation = 0x200,
 		/// <summary>
 		/// Specify that we expect real expression under hidden by Selects chain 
 		/// </summary>
-		Traverse = 0x800,	
+		Traverse = 0x800,
 
-		Subquery = 0x1000,	
+		Subquery = 0x1000,
 
 		/// <summary>
 		/// Indicates that generated SQL is for extension method.
 		/// </summary>
-		ForExtension = 0x2000,	
+		ForExtension = 0x2000,
 	}
 }
