bc3dd736c9a47126258df8802d2a3c0de1dbc2ce
MaceWindu <MaceWindu@users.noreply.github.com>
2023-11-29 13:16:17 +0100

    switch to our images

--- Build/Azure/scripts/sqlserver.2016.cmd
+++ Build/Azure/scripts/sqlserver.2016.cmd
@@ -1,4 +1,4 @@
-docker run -d -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Password12!" -p 1433:1433 -h mssql --name=mssql cagrin/mssql-server-ltsc2022:2016-latest
+docker run -d -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=Password12!" -p 1433:1433 -h mssql --name=mssql linq2db/linq2db:win-mssql-2016
 docker ps -a
 
 echo "Waiting for SQL Server to accept connections"
