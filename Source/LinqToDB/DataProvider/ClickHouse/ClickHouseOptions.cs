19cafcd118ef3162642f27f8a4e5731e909a84ef
Igor Tkachev <igor.tkachev@gmail.com>
2024-01-27 07:40:52 -0500

    Options small fixes and comments. (#4378)

--- Source/LinqToDB/DataProvider/ClickHouse/ClickHouseOptions.cs
+++ Source/LinqToDB/DataProvider/ClickHouse/ClickHouseOptions.cs
@@ -18,6 +18,8 @@ namespace LinqToDB.DataProvider.ClickHouse
 	(
 		BulkCopyType BulkCopyType                    = BulkCopyType.ProviderSpecific,
 		bool         UseStandardCompatibleAggregates = default
+		// If you add another parameter here, don't forget to update
+		// ClickHouseOptions copy constructor and CreateID method.
 	)
 		: DataProviderOptions<ClickHouseOptions>(BulkCopyType)
 	{
