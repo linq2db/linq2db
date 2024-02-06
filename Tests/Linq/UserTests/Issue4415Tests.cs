9079779ecd784b8221deebfb64e2006ce33dcd45
jkuehner <jochen.kuehner@kardex.com>
2024-02-06 13:18:42 +0100

    test for issue #4415

---
+++ Tests/Linq/UserTests/Issue4415Tests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.Serialization;
+
+using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.Mapping;
+
+using Newtonsoft.Json;
+
+using NUnit.Framework;
+
+using DataType = LinqToDB.DataType;
+
+namespace Tests.UserTests.Test4415
+{
+	[TestFixture]
+	public class Test4415Tests : TestBase
+	{
+		public class LanguageDTO
+		{
+			public string? LanguageID { get; set; }
+
+			public string? Name { get; set; }
+
+			public string? AlternativeLanguageID { get; set; }
+
+			public int Order { get; set; }
+
+			public string? IsoCode { get; set; }
+		}
+
+		[Test]
+		public void TestIssue4415_Test1([DataSources] string configuration)
+		{
+			MappingSchema ms;
+			Model.ITestDataContext? db = null;
+			try
+			{
+				ms = new FluentMappingBuilder()
+					.Entity<LanguageDTO>()
+						.HasTableName("Common_Language")
+						.Property(e => e.LanguageID).IsPrimaryKey()
+						.Property(e => e.AlternativeLanguageID).IsNullable()
+					.Build()
+					.MappingSchema;
+
+				db = GetDataContext(configuration, ms);
+
+				using var tbl = db.CreateLocalTable(new[]
+				{
+					new LanguageDTO
+					{
+						LanguageID    = "de",
+						Name = "deutsch"
+					}
+				});
+
+				var p = db.GetTable<LanguageDTO>();
+				var qry = p.GroupBy(x => x.Name).Select(x => x.Max(y => y.LanguageID));
+				var qry2 = p.Where(x => qry.Contains(x.LanguageID));
+				var lst = qry2.ToList();
+			}
+			finally
+			{
+				db?.Dispose();
+			}
+		}
+	}
+}
