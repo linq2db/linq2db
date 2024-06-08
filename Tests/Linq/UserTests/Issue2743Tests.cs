5988dec1783f56790422870c1f2f472f90aa5559
Jochen Kühner <jochen.kuehner@kardex.com>
2024-06-08 12:31:14 +0200

    Unprojected Insert/Delete from Select (#2744)
    * test for issue #2743
    
    * test delete
    
    * fix insert
    
    * update test
    
    ---------
    
    Co-authored-by: MaceWindu <MaceWindu@users.noreply.github.com>

---
+++ Tests/Linq/UserTests/Issue2743Tests.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using LinqToDB;
+using LinqToDB.Mapping;
+using NUnit.Framework;
+
+namespace Tests.UserTests
+{
+	[TestFixture]
+	public class Issue2743Tests : TestBase
+	{
+		[Table]
+		class MessageEventDTO
+		{
+			[PrimaryKey]
+			public int Id { get; set; }
+		}
+
+		class MessageEventCombinedWithoutTranslationDTO
+		{
+			public virtual MessageEventDTO? MessageEventDTO { get; set; }
+
+		}
+
+		[Test]
+		public void IssueTestInsertViaSelect([DataSources] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				using (db.CreateLocalTable<MessageEventDTO>())
+				using (db.CreateLocalTable<MessageEventDTO>(tableName: "MessageEventDTOTb2"))
+				{
+					var evtQry = db.GetTable<MessageEventDTO>();
+					var q = from evt in evtQry
+							select new MessageEventCombinedWithoutTranslationDTO
+							{
+								MessageEventDTO = evt,
+							};
+
+					var queryDelete = q.Select(x => x.MessageEventDTO!);
+
+					var destination = db.GetTable<MessageEventDTO>().TableName("MessageEventDTOTb2");
+
+					queryDelete.Insert(destination, x => x);
+				}
+			}
+		}
+
+		[Test]
+		public void IssueTestDeleteViaSelect([DataSources] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				using (db.CreateLocalTable<MessageEventDTO>())
+				using (db.CreateLocalTable<MessageEventDTO>(tableName: "MessageEventDTOTb2"))
+				{
+					var evtQry = db.GetTable<MessageEventDTO>();
+					var q = from evt in evtQry
+							select new MessageEventCombinedWithoutTranslationDTO
+							{
+								MessageEventDTO = evt,
+							};
+
+					var queryDelete = q.Select(x => x.MessageEventDTO!);
+
+					var rows = db.Delete(queryDelete);
+				}
+			}
+		}
+	}
+}
