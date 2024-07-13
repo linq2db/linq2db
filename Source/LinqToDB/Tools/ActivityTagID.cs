fb104f7f6f80c278c2455f6692fb45ec773bcd37
MaceWindu <MaceWindu@users.noreply.github.com>
2024-07-13 14:19:21 +0200

    address CR

--- Source/LinqToDB/Tools/ActivityTagID.cs
+++ Source/LinqToDB/Tools/ActivityTagID.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.Common;
 
 namespace LinqToDB.Tools
 {
@@ -13,8 +14,10 @@ namespace LinqToDB.Tools
 		/// <summary>Data Provider name.</summary>
 		DataProviderName,
 		/// <summary>The name of the database server to which to connect.</summary>
+		/// <remarks>Tag value taken from <see cref="DbConnection.DataSource"/> property and could contain incorrect data if provider doesn't fill it properly.</remarks>
 		DataSourceName,
 		/// <summary>The name of the current database.</summary>
+		/// <remarks>Tag value taken from <see cref="DbConnection.Database"/> property and could contain incorrect data if provider doesn't fill it properly.</remarks>
 		DatabaseName,
 		/// <summary>The text command to run against the data source.</summary>
 		CommandText
