968f115fde340681f900295bcb02fc87ce4453c8
MaceWindu <MaceWindu@users.noreply.github.com>
2024-08-31 14:52:36 +0200

    CI integration

--- Build/SetVersion.ps1
+++ Build/SetVersion.ps1
@@ -1,6 +1,7 @@
 Param(
 	[Parameter(Mandatory=$true)][string]$path,
-	[Parameter(Mandatory=$true)][string]$version
+	[Parameter(Mandatory=$true)][string]$version,
+	[Parameter(Mandatory=$true)][string]$prop
 )
 
 $ErrorActionPreference = "Stop"
@@ -14,7 +15,7 @@ if ($version) {
 	$xml.PreserveWhitespace = $true
 	$save = $false
 
-	$xPath = "//PropertyGroup/Version"
+	$xPath = "//PropertyGroup/" + $prop
 	$nodes = $xml.SelectNodes($xPath)
 	foreach($node in $nodes) {
 		$node.InnerXml = $version
