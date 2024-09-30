71072f1bd5ef3a370ec99601478491f64292f861
MaceWindu <MaceWindu@users.noreply.github.com>
2024-09-30 12:23:41 +0200

    add tests for https://github.com/linq2db/linq2db/issues/4646

--- Tests/EntityFrameworkCore/Tests/FSharpTests.cs
+++ Tests/EntityFrameworkCore/Tests/FSharpTests.cs
@@ -5,6 +5,9 @@ using EntityFrameworkCore.FSharp;
 
 using NUnit.Framework;
 using System;
+using System.Linq;
+using LinqToDB.Mapping;
+using Tests;
 
 namespace LinqToDB.EntityFrameworkCore.Tests
 {
@@ -31,6 +34,58 @@ namespace LinqToDB.EntityFrameworkCore.Tests
 
 			FSharpTestMethods.TestLeftJoin(ctx);
 		}
+
+		#region Issue 260
+
+		[Table]
+		public class Issue4646Table
+		{
+			[Identity]
+			public int Id { get; set; }
+			[Column]
+			public int? Value { get; set; }
+			[Column]
+			public int? ValueN { get; set; }
+		}
+
+		[ActiveIssue]
+		[Test(Description = "https://github.com/linq2db/linq2db.EntityFrameworkCore/issues/260")]
+		public void Issue4646TestLinqToDB([EFDataSources] string provider)
+		{
+			using var ctx = CreateContext(provider);
+			using var db = ctx.CreateLinqToDBConnection();
+
+			FSharpTestMethods.Issue4646TestLinqToDB(ctx);
+
+			var result = db.GetTable<Issue4646Table>().Single();
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(result.Value, Is.Null);
+				Assert.That(result.ValueN, Is.Null);
+			});
+		}
+
+		[Test(Description = "https://github.com/linq2db/linq2db.EntityFrameworkCore/issues/260")]
+		public void Issue4646TestEF([EFDataSources] string provider)
+		{
+			using var ctx = CreateContext(provider);
+			using var db = ctx.CreateLinqToDBConnection();
+
+			FSharpTestMethods.Issue4646TestEF(ctx);
+			ctx.SaveChanges();
+
+			var result = db.GetTable<Issue4646Table>().Single();
+
+			Assert.Multiple(() =>
+			{
+				Assert.That(result.Value, Is.Null);
+				Assert.That(result.ValueN, Is.Null);
+			});
+		}
+
+		#endregion
 	}
 }
+
 #endif
