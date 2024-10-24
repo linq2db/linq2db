2795eacabaae7057ba0927abe947d45c3c8ff86b
MaceWindu <MaceWindu@users.noreply.github.com>
2024-10-24 19:38:21 +0200

    fix script

--- Build/Azure/scripts/db2.provider.sh
+++ Build/Azure/scripts/db2.provider.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+#!/bin/bash -v
 
 rm -rf ./clidriver/*
-if [ $? -eq 0 ]; then exit 1; fi
+if [ $? != 0 ]; then exit 1; fi
 
 nuget install Net.IBM.Data.Db2-lnx -Version 7.0.0.400 -ExcludeVersion
-if [ $? -eq 0 ]; then exit 1; fi
+if [ $? != 0 ]; then exit 1; fi
 rm ./IBM.Data.Db2.dll
-if [ $? -eq 0 ]; then exit 1; fi
+if [ $? != 0 ]; then exit 1; fi
 ls
 cp -a ./Net.IBM.Data.Db2-lnx/buildTransitive/clidriver/. ./clidriver/
-if [ $? -eq 0 ]; then exit 1; fi
+if [ $? != 0 ]; then exit 1; fi
 cp -f ./Net.IBM.Data.Db2-lnx/lib/net6.0/IBM.Data.Db2.dll ./IBM.Data.Db2.dll
-if [ $? -eq 0 ]; then exit 1; fi
+if [ $? != 0 ]; then exit 1; fi
 
 echo "##vso[task.setvariable variable=PATH]$PATH:$PWD/clidriver/bin:$PWD/clidriver/lib"
 echo "##vso[task.setvariable variable=LD_LIBRARY_PATH]$PWD/clidriver/lib/"
