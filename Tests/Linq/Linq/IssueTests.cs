cf2195a57591c59089184f160a5b99b1c967a9d6
Svyatoslav Danyliv <sdanyliv@gmail.com>
2024-12-01 16:45:14 +0200

    Disabled test.

--- Tests/Linq/Linq/IssueTests.cs
+++ Tests/Linq/Linq/IssueTests.cs
@@ -397,6 +397,7 @@ namespace Tests.Linq
 			Client
 		}
 
+		[ActiveIssue("https://github.com/linq2db/linq2db/issues/754", Configuration = TestProvName.AllOracle)]
 		[Test]
 		public void Issue535Test2([DataSources(TestProvName.AllSybase)] string context)
 		{
