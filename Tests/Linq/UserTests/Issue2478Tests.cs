2887b5c47e8f36569178b70bd68b2f71762f2c30
Svyatoslav Danyliv <sdanyliv@gmail.com>
2024-12-06 13:02:38 +0200

    Fixed EXISTS optimization.

--- Tests/Linq/UserTests/Issue2478Tests.cs
+++ Tests/Linq/UserTests/Issue2478Tests.cs
@@ -116,7 +116,6 @@ namespace Tests.UserTests
 			}
 		}
 
-		[ActiveIssue(Details = "Converted FuncLikePredicate expression is not a Predicate expression.")]
 		[Test]
 		public void ExistsRemoval([IncludeDataSources(TestProvName.AllSqlServer, TestProvName.AllClickHouse)] string context, [Values] bool shouldFilter, [Values] bool isPositive)
 		{
