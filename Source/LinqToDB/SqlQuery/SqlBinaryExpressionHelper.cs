a12eb033b728c49195c8023fc18363e192d82e35
MaceWindu <MaceWindu@users.noreply.github.com>
2024-12-22 14:28:21 +0100

    fix build

--- Source/LinqToDB/SqlQuery/SqlBinaryExpressionHelper.cs
+++ Source/LinqToDB/SqlQuery/SqlBinaryExpressionHelper.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Frozen;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq.Expressions;
 
 namespace LinqToDB.SqlQuery
 {
