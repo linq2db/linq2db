abe05424eadf3a4091e4135710a8869dc1dda86f
MaceWindu <MaceWindu@users.noreply.github.com>
2025-02-08 16:13:14 +0100

    add test for https://github.com/linq2db/linq2db/issues/4816

--- Tests/EntityFrameworkCore/Models/IssueModel/SQLServer/IssueEntities.cs
+++ Tests/EntityFrameworkCore/Models/IssueModel/SQLServer/IssueEntities.cs
@@ -9,4 +9,11 @@ namespace LinqToDB.EntityFrameworkCore.Tests.SqlServer.Models.IssueModel
 		public int Id { get; private set; }
 		public int Key { get; private set; }
 	}
+
+	public class Issue4816Table
+	{
+		public int Id { get; private set; }
+		public string? ValueVarChar { get; set; }
+		public string? ValueNVarChar { get; set; }
+	}
 }
