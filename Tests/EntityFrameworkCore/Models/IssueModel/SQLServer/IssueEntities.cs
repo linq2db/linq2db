8453d08dbe6fbc5123ff249a054d89cbee37f876
MaceWindu <MaceWindu@users.noreply.github.com>
2025-02-16 12:25:15 +0100

    Bunch of small repo tasks (#4807)
    * migrate linq2db.ef readme
    
    * fix nuspec for linq2db.scaffold
    
    * revert internal access modifier for some constants, used by linqpad
    
    * add test for https://github.com/linq2db/linq2db/issues/4816
    
    * add tests for https://github.com/linq2db/linq2db/issues/4811
    
    * add test for https://github.com/linq2db/linq2db/issues/4808
    
    * test for discovered issue
    
    * add tests for https://github.com/linq2db/linq2db/issues/4799
    
    * https://github.com/linq2db/linq2db/issues/4798
    
    * add test for https://github.com/linq2db/linq2db/issues/4797
    
    * fix https://github.com/linq2db/linq2db/issues/4796
    
    * merge fix
    
    * Update Pack.cmd
    
    fix cmd.exe comprehension issues
    
    * Update Pack.cmd
    
    fix more cmd issues
    
    * Update TestToolLocal.cmd
    
    fix cmd
    
    * Update MappingTests.cs
    
    fix baselines

--- Tests/EntityFrameworkCore/Models/IssueModel/SQLServer/IssueEntities.cs
+++ Tests/EntityFrameworkCore/Models/IssueModel/SQLServer/IssueEntities.cs
@@ -5,4 +5,11 @@
 		public int Id { get; private set; }
 		public int Key { get; private set; }
 	}
+
+	public class Issue4816Table
+	{
+		public int Id { get; private set; }
+		public string? ValueVarChar { get; set; }
+		public string? ValueNVarChar { get; set; }
+	}
 }
