76bffba559fb216c444d53be5db4c67a003dc7bc
Stuart Turner <stuart@turner-isageek.com>
2025-02-19 10:51:11 -0600

    One more

--- Tests/Linq/Microsoft/MicrosoftODataTests.cs
+++ Tests/Linq/Microsoft/MicrosoftODataTests.cs
@@ -5,12 +5,10 @@ using System.Linq;
 using System.Reflection;
 
 using LinqToDB;
+using LinqToDB.Internal.Expressions;
 using LinqToDB.Mapping;
 
 using NUnit.Framework;
-
-using LinqToDB.Internal.Expressions;
-
 
 #if NETFRAMEWORK
 using System.Net.Http;
