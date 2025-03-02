438eca1396d3927a47a7285534bdf58ad545b927
MaceWindu <MaceWindu@users.noreply.github.com>
2025-03-02 16:06:18 +0100

    Update CachingTests.cs

--- Tests/Linq/Linq/CachingTests.cs
+++ Tests/Linq/Linq/CachingTests.cs
@@ -377,7 +377,7 @@ namespace Tests.Linq
 		{
 			var ms = new MappingSchema();
 			ms.SetConverter<IntArrayClass, DataParameter>(v => v.CreateParameter());
-			ms.AddScalarType(typeof(IntArrayClass), new SqlDataType(IntArrayClass.Type));
+			ms.AddScalarType(typeof(IntArrayClass), new DbDataType(IntArrayClass.Type));
 
 			using var db = GetDataConnection(context, ms);
 
@@ -415,7 +415,7 @@ namespace Tests.Linq
 		{
 			var ms = new MappingSchema();
 			ms.SetConverter<IntArrayStruct, DataParameter>(v => v.CreateParameter());
-			ms.SetDataType(typeof(IntArrayStruct), new SqlDataType(IntArrayStruct.Type));
+			ms.SetDataType(typeof(IntArrayStruct), new DbDataType(IntArrayStruct.Type));
 
 			using var db = GetDataConnection(context, ms);
 
