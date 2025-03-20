6327d5cc0b146fff6f6821c2fce8d57bc48227ba
Sergey Kovalenko <sergey.kovalenko@dotdigital.com>
2025-03-20 17:28:42 +0100

    Don't open the connection when obtaining ConfigurationID

---
+++ Tests/Linq/UserTests/Issue4883Tests.cs
@@ -0,0 +1,59 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using LinqToDB;
+
+using NUnit.Framework;
+
+namespace Tests.UserTests;
+
+[TestFixture]
+public class Issue4883Tests : TestBase
+{
+	[Test]
+	public async Task TestDataConnection()
+	{
+		var dbConn = new MockDbConnection();
+		var db     = GetDataConnection(new DataOptions().UseConnection(
+			new TestNoopProvider(), dbConn));
+
+		var _ = await db.GetTable<TestEntity>().SingleOrDefaultAsync();
+		
+		Assert.That(dbConn.OpenedAsync, Is.True);
+	}
+	
+	[Test]
+	public async Task TestDataContext()
+	{
+		var dbConn = new MockDbConnection();
+		var db     = GetDataContext("", dbOptions => dbOptions.UseConnection(
+			new TestNoopProvider(), dbConn));
+
+		var _ = await db.GetTable<TestEntity>().SingleOrDefaultAsync();
+		
+		Assert.That(dbConn.OpenedAsync, Is.True);
+	}
+
+	private class TestEntity
+	{
+		
+	}
+
+	private class MockDbConnection() : TestNoopConnection(string.Empty)
+	{
+		public bool OpenedAsync { get; private set; }
+
+		public override    void          Open()
+		{
+			OpenedAsync = false;
+			base.Open();
+		}
+
+		public override Task OpenAsync(CancellationToken cancellationToken)
+		{
+			OpenedAsync = true;
+			base.Open();
+			return Task.CompletedTask;
+		}
+	}
+}
