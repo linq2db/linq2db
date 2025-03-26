817417059862e8ed1e77123ffdedb2a7547ab42b
Svyatoslav Danyliv <sdanyliv@gmail.com>
2025-03-26 12:30:10 +0200

    Fixed member comparison. (#4892)

--- Source/LinqToDB/Linq/Builder/ExpressionBuilder.SqlBuilder.cs
+++ Source/LinqToDB/Linq/Builder/ExpressionBuilder.SqlBuilder.cs
@@ -1331,7 +1331,7 @@ namespace LinqToDB.Linq.Builder
 							for (int i = 0; i < genericConstructor.Assignments.Count; i++)
 							{
 								var assignment = genericConstructor.Assignments[i];
-								if (MemberInfoEqualityComparer.Default.Equals(assignment.MemberInfo, member))
+								if (IsEqualMembers(assignment.MemberInfo, member))
 								{
 									bodyExpresion = assignment.Expression;
 									break;
@@ -1343,7 +1343,7 @@ namespace LinqToDB.Linq.Builder
 								for (int i = 0; i < genericConstructor.Parameters.Count; i++)
 								{
 									var parameter = genericConstructor.Parameters[i];
-									if (MemberInfoEqualityComparer.Default.Equals(parameter.MemberInfo, member))
+									if (parameter.MemberInfo != null && IsEqualMembers(parameter.MemberInfo, member))
 									{
 										bodyExpresion = parameter.Expression;
 										break;
@@ -1360,7 +1360,7 @@ namespace LinqToDB.Linq.Builder
 									if (assignment.MemberInfo.ReflectedType != member.ReflectedType && assignment.MemberInfo.Name == member.Name)
 									{
 										var mi = assignment.MemberInfo.ReflectedType!.GetMemberEx(member);
-										if (mi != null && mi.GetMemberType() == member.GetMemberType() && MemberInfoEqualityComparer.Default.Equals(assignment.MemberInfo, mi))
+										if (mi != null && IsEqualMembers(assignment.MemberInfo, mi))
 										{
 											if (member.ReflectedType?.IsInterface == true && assignment.MemberInfo.ReflectedType?.IsClass == true && member is PropertyInfo propInfo && assignment.MemberInfo is PropertyInfo classPropinfo)
 											{
@@ -1455,7 +1455,7 @@ namespace LinqToDB.Linq.Builder
 						{
 							var memberLocal = ne.Members[i];
 
-							if (MemberInfoEqualityComparer.Default.Equals(memberLocal, member))
+							if (IsEqualMembers(memberLocal, member))
 							{
 								var projected = Project(context, path, nextPath, nextIndex - 1, flags, ne.Arguments[i], strict);
 
@@ -1478,8 +1478,7 @@ namespace LinqToDB.Linq.Builder
 							var parameter     = parameters[i];
 							var memberByParam = SqlGenericConstructorExpression.FindMember(ne.Constructor.DeclaringType!, parameter);
 
-							if (memberByParam != null &&
-								MemberInfoEqualityComparer.Default.Equals(memberByParam, member))
+							if (memberByParam != null && member != null && IsEqualMembers(memberByParam, member))
 							{
 								return Project(context, path, nextPath, nextIndex - 1, flags, ne.Arguments[i], strict);
 							}
@@ -1520,7 +1519,7 @@ namespace LinqToDB.Linq.Builder
 						{
 							var memberLocal = ne.Members[i];
 
-							if (MemberInfoEqualityComparer.Default.Equals(memberLocal, member))
+							if (IsEqualMembers(memberLocal, member))
 							{
 								return Project(context, path, nextPath, nextIndex - 1, flags, ne.Arguments[i], strict);
 							}
@@ -1544,7 +1543,7 @@ namespace LinqToDB.Linq.Builder
 								case MemberBindingType.Assignment:
 								{
 									var assignment = (MemberAssignment)binding;
-									if (MemberInfoEqualityComparer.Default.Equals(assignment.Member, memberInType))
+									if (IsEqualMembers(assignment.Member, memberInType))
 									{
 										return Project(context, path, nextPath, nextIndex - 1, flags,
 											assignment.Expression, strict);
@@ -1555,7 +1554,7 @@ namespace LinqToDB.Linq.Builder
 								case MemberBindingType.MemberBinding:
 								{
 									var memberMemberBinding = (MemberMemberBinding)binding;
-									if (MemberInfoEqualityComparer.Default.Equals(memberMemberBinding.Member, memberInType))
+									if (IsEqualMembers(memberMemberBinding.Member, memberInType))
 									{
 										return Project(context, path, nextPath, nextIndex - 1, flags,
 											new SqlGenericConstructorExpression(
@@ -1580,8 +1579,7 @@ namespace LinqToDB.Linq.Builder
 								var parameter     = parameters[i];
 								var memberByParam = SqlGenericConstructorExpression.FindMember(ne.Constructor.DeclaringType!, parameter);
 
-								if (memberByParam != null &&
-									MemberInfoEqualityComparer.Default.Equals(memberByParam, member))
+								if (memberByParam != null && IsEqualMembers(memberByParam, member))
 								{
 									return Project(context, path, nextPath, nextIndex - 1, flags, ne.Arguments[i], strict);
 								}
@@ -1704,8 +1702,7 @@ namespace LinqToDB.Linq.Builder
 							var parameter     = parameters[i];
 							var memberByParam = SqlGenericConstructorExpression.FindMember(mc.Method.ReturnType, parameter);
 
-							if (memberByParam != null &&
-								MemberInfoEqualityComparer.Default.Equals(memberByParam, member))
+							if (memberByParam != null && member != null && IsEqualMembers(memberByParam, member))
 							{
 								return Project(context, path, nextPath, nextIndex - 1, flags, mc.Arguments[i], strict);
 							}
@@ -1759,6 +1756,23 @@ namespace LinqToDB.Linq.Builder
 			}
 
 			return CreateSqlError(next ?? path!);
+		}
+
+		static bool IsEqualMembers(MemberInfo member1, MemberInfo member2)
+		{
+			if (MemberInfoEqualityComparer.Default.Equals(member1, member2))
+				return true;
+
+			if (member1.GetMemberType() != member2.GetMemberType())
+				return false;
+
+			if (member1.DeclaringType == null || member2.DeclaringType == null)
+				return false;
+
+			if (member1.Name != member2.Name)
+				return false;
+
+			return member1.EqualsTo(member2);
 		}
 
 		public Expression ParseGenericConstructor(Expression createExpression, ProjectFlags flags, ColumnDescriptor? columnDescriptor, bool force = false)
