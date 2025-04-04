585f5aad2c63752cadde7d59590e49deea30c8ab
Svyatoslav Danyliv <sdanyliv@gmail.com>
2025-04-04 16:56:56 +0300

    Added paramterization for FromSql. Added Sql.Constant and Sql.Parameter for focing compiler to generate appropriate SQL.

---
+++ Tests/Linq/Linq/SpecialFunctionsTest.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using LinqToDB;
+using LinqToDB.SqlQuery;
+
+using NUnit.Framework;
+
+namespace Tests.Linq
+{
+	public class SpecialFunctionsTest : TestBase
+	{
+		[Test]
+		public void Ordinal([IncludeDataSources(TestProvName.AllSQLite)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var query = from q in db.Parent.AsQueryable()
+							orderby Sql.Ordinal(q.ParentID), q.Value1
+							select q;
+
+				var result = query.ToList();
+
+				var orderBy = query.GetSelectQuery().OrderBy;
+
+				Assert.That(orderBy.Items, Has.Count.EqualTo(2));
+				Assert.That(orderBy.Items[0].IsPositioned, Is.True);
+			}
+		}
+
+		[Test]
+		public void Parameter([IncludeDataSources(TestProvName.AllSQLite)] string context)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var query = from q in db.Parent.AsQueryable()
+							where q.ParentID == Sql.Parameter(1)
+							select q;
+
+				var result = query.ToList();
+
+				var cacheMissCount = query.GetCacheMissCount();
+
+				var query2 = from q in db.Parent.AsQueryable()
+					where q.ParentID == Sql.Parameter(2)
+					select q;
+
+				var result2 = query2.ToList();
+
+				Assert.That(query2.GetCacheMissCount(), Is.EqualTo(cacheMissCount));
+
+				var parameters = new List<SqlParameter>();
+				QueryHelper.CollectParameters(query.GetSelectQuery(),  parameters);
+
+				Assert.That(parameters, Has.Count.EqualTo(1));
+				Assert.That(parameters[0].IsQueryParameter, Is.True);
+			}
+		}
+
+		[Test]
+		public void Constant([IncludeDataSources(TestProvName.AllSQLite)] string context, [Values(1, 2)] int iteration)
+		{
+			using (var db = GetDataContext(context))
+			{
+				var parentId = iteration;
+
+				var query = from q in db.Parent.AsQueryable()
+							where q.ParentID == Sql.Constant(parentId)
+							select q;
+				var result = query.ToList();
+
+				var parameters = new List<SqlParameter>();
+				QueryHelper.CollectParameters(query.GetSelectQuery(), parameters);
+
+				Assert.That(parameters, Has.Count.EqualTo(1));
+				Assert.That(parameters[0].IsQueryParameter, Is.False);
+			}
+		}
+	}
+	
+}
