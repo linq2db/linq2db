99b538900896f75281a312f6f05e167f9de95627
poma12390 <kochnevr21@gmail.com>
2025-04-28 04:37:42 +0300

    create YdbProvider

---
+++ Source/LinqToDB/DataProvider/Ydb/YdbProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace LinqToDB.DataProvider.Ydb
+{
+	/// <summary>
+	/// Defines supported YDB ADO.NET provider implementation libraries.
+	/// </summary>
+	public enum YdbProvider
+	{
+		/// <summary>
+		/// Detect provider automatically.
+		/// </summary>
+		AutoDetect,
+
+		/// <summary>
+		/// Official YDB .NET SDK provider
+		/// (<c>Ydb.Sdk</c>nbsp;package, see https://github.com/ydb-platform/ydb-dotnet-sdk).
+		/// </summary>
+		YdbSdk
+	}
+}
