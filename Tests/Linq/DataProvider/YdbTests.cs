52f2f8156cf02e2e7a408029c4ae8490b191cc8b
poma12390 <kochnevr21@gmail.com>
2025-04-30 21:36:07 +0300

    create mapping schema tests

--- Tests/Linq/DataProvider/YdbTests.cs
+++ Tests/Linq/DataProvider/YdbTests.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Text;
 
 using LinqToDB;
 using LinqToDB.Data;
 using LinqToDB.DataProvider.Ydb;
+using LinqToDB.Mapping;
+using LinqToDB.SqlQuery;
 
 using NUnit.Framework;
 
 namespace Tests.DataProvider
 {
 	[TestFixture]
-	public class YdbTests: DataProviderTestBase
+	public class YdbTests : DataProviderTestBase
+
 	{
+		#region MappingSchemaTests
+		//------------------------------------------------------------------
+		//  ADAPTER
+		//------------------------------------------------------------------
 		[Test]
 		public void GetInstance_ShouldCreateAdapterSuccessfully()
 		{
@@ -28,7 +38,12 @@ namespace Tests.DataProvider
 				Assert.That(adapter.MappingSchema, Is.Not.Null);
 			});
 		}
+		#endregion
 
+		#region MappingSchemaTests
+		//------------------------------------------------------------------
+		//  SCALAR-TYPES
+		//------------------------------------------------------------------
 		[Test]
 		public void MappingSchema_ShouldMapBasicDotNetTypes()
 		{
@@ -41,7 +56,31 @@ namespace Tests.DataProvider
 				Assert.That(schema.GetDataType(typeof(bool)).Type.DataType, Is.EqualTo(DataType.Boolean));
 				Assert.That(schema.GetDataType(typeof(Guid)).Type.DataType, Is.EqualTo(DataType.Guid));
 				Assert.That(schema.GetDataType(typeof(byte[])).Type.DataType, Is.EqualTo(DataType.VarBinary));
+				Assert.That(schema.GetDataType(typeof(TimeSpan)).Type.DataType, Is.EqualTo(DataType.Interval));
 			});
 		}
+
+		//------------------------------------------------------------------
+		//  DECIMAL-LITERAL
+		//------------------------------------------------------------------
+		[Test]
+		public void DecimalLiteralBuilder_ShouldRespectSqlDataTypeDefaults()
+		{
+			var mi = typeof(YdbMappingSchema)
+					.GetMethod("BuildDecimalLiteral", BindingFlags.Static | BindingFlags.NonPublic)!;
+
+			var sb  = new StringBuilder();
+			var val = 123.45m;
+
+			// SqlDataType by default Precision=29, Scale=10.
+			var sqlType = new SqlDataType(DataType.Decimal, typeof(decimal));
+
+			mi.Invoke(null, new object[] { sb, val, sqlType });
+
+			var expected = $"Decimal(\"{val.ToString(CultureInfo.InvariantCulture)}\", {sqlType.Type.Precision}, {sqlType.Type.Scale})";
+
+			Assert.That(sb.ToString(), Is.EqualTo(expected));
+		}
+		#endregion
 	}
 }
