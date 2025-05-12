c28e167c89696705d39b86385022424c82b1a85e
Igor Tkachev <igor.tkachev@gmail.com>
2025-05-12 04:38:04 -0400

    LinqToDB.Remote.Http.Client / Server (#4775)
    * Added HttpClient support.
    
    * Working on HttpClient.
    
    * Added Remote Http example.
    
    * Working on.
    
    * Fix compile errors.
    
    * Working on Remote Http client.
    
    * Working on Remote Signal/R client.
    
    * Working on Remote Signal/R client.
    
    * Working on Remote HttpClient.
    
    * Working on Remote HttpClient, server side.
    
    * Working on Remote HttpClient, server side.
    
    * Working on Remote HttpClient, client side.
    
    * Working on Remote HttpClient, client side.
    
    * fix merge
    
    * merge fix
    
    * Working on Remote HttpClient example.
    
    * Working on Remote HttpClient example.
    
    * Working on Remote SignalR example.
    
    * Working on Remote SignalR example.
    
    * Working on Remote SignalR example.
    
    * Working on Remote SignalR example.
    
    * Working on Remote SignalR example.
    
    * Working on Remote SignalR example.
    
    * Added nuget packages.
    
    * Added artifacts.
    
    * Test update.
    
    * Updated services.
    
    * Merge remote-tracking branch 'origin/master' into http-client
    
    * Review changes.
    
    * Review changes.
    
    * Review changes.
    
    * Update Source/LinqToDB.Remote.SignalR/SignalRLinqServiceClient.cs
    
    Co-authored-by: MaceWindu <MaceWindu@users.noreply.github.com>
    
    * Review changes.
    
    * Review changes.
    
    * Review changes.
    
    * Review changes.
    
    * Review changes.
    
    * Review changes.
    
    * restore TLS options after tests
    
    * Fixed tests.
    
    * Fixed tests.
    
    * update deps, remove net6
    
    * restore examples deps
    
    * add all remote transports testing
    
    * restore blazor compatibility
    
    * split signalr client and server, downgrade netfx signalr version, enable binding redirects generation
    
    * fix tests
    
    * disable https in tests
    
    * test context behavior without default configuration set
    
    * don't reset DefaultSettings for netfx tests due to implementation-specific side-effects
    
    * fix tests
    
    ---------
    
    Co-authored-by: MaceWindu <MaceWindu@users.noreply.github.com>

---
+++ Tests/Linq/Linq/RemoteContextTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using LinqToDB;
+using LinqToDB.Remote;
+
+using NUnit.Framework;
+
+namespace Tests.Linq
+{
+	[TestFixture]
+	public sealed class RemoteContextTests : TestBase
+	{
+		[Test]
+		public void TestILinqService_GetInfo([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			_ = db.MappingSchema;
+		}
+
+		[Test]
+		public void TestILinqService_ExecuteNonQuery([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			_ = db.Person.Where(r => r.ID == -1).Delete();
+		}
+
+		[Test]
+		public void TestILinqService_ExecuteScalar([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			_ = db.Person.Count();
+		}
+
+		[Test]
+		public void TestILinqService_ExecuteReader([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			_ = db.Person.ToArray();
+		}
+
+		[Test]
+		public async Task TestILinqService_ExecuteBatch([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			var dbRemote = (RemoteDataContextBase)db;
+
+			dbRemote.BeginBatch();
+
+			_ = db.Person.Where(r => r.ID == -1).Delete();
+			_ = await db.Person.Where(r => r.ID == -2).DeleteAsync();
+
+			dbRemote.CommitBatch();
+		}
+
+		[Test]
+		public async Task TestILinqService_GetInfoAsync([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			await ((RemoteDataContextBase)db).ConfigureAsync(default);
+		}
+
+		[Test]
+		public async Task TestILinqService_ExecuteNonQueryAsync([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			_ = await db.Person.Where(r => r.ID == -1).DeleteAsync();
+		}
+
+		[Test]
+		public async Task TestILinqService_ExecuteScalarAsync([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			_ = await db.Person.CountAsync();
+		}
+
+		[Test]
+		public async Task TestILinqService_ExecuteReaderAsync([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			_ = await db.Person.ToArrayAsync();
+		}
+
+		[Test]
+		public async Task TestILinqService_ExecuteBatchAsync([IncludeDataSources(true, TestProvName.AllSQLite)] string context, [Values] RemoteTransport transport)
+		{
+			if (!context.IsRemote()) Assert.Ignore("Skip non-remote context");
+
+			using var db = GetDataContext(context, transport: transport);
+
+			var dbRemote = (RemoteDataContextBase)db;
+
+			dbRemote.BeginBatch();
+
+			_ = db.Person.Where(r => r.ID == -1).Delete();
+			_ = await db.Person.Where(r => r.ID == -2).DeleteAsync();
+
+			await dbRemote.CommitBatchAsync();
+		}
+	}
+}
