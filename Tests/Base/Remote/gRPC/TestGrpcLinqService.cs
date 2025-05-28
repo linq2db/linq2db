3f716ab71f19dd4615c50f35ff28803f42bc2c12
poma12390 <kochnevr21@gmail.com>
2025-05-28 21:56:19 +0300

    Reset к upstream/master

---
+++ Tests/Base/Remote/gRPC/TestGrpcLinqService.cs
@@ -0,0 +1,38 @@
+﻿#if !NETFRAMEWORK
+using System;
+
+using LinqToDB;
+using LinqToDB.Data;
+using LinqToDB.Interceptors;
+using LinqToDB.Mapping;
+using LinqToDB.Remote;
+
+using LinqToDB.Remote.Grpc;
+
+namespace Tests.Remote
+{
+	internal sealed class TestGrpcLinqService : GrpcLinqService, ITestLinqService
+	{
+		private readonly LinqService    _linqService;
+
+		public bool AllowUpdates
+		{
+			get => _linqService.AllowUpdates;
+			set => _linqService.AllowUpdates = value;
+		}
+
+		MappingSchema? ITestLinqService.MappingSchema
+		{
+			get => _linqService.MappingSchema;
+			set => _linqService.MappingSchema = value;
+		}
+
+		public TestGrpcLinqService(LinqService linqService)
+			: base(linqService, true)
+		{
+			_linqService = linqService;
+
+		}
+	}
+}
+#endif
