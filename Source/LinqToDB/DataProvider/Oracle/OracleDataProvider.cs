bd5b072121759450713522e456e7cbfb76ec5d3a
Svyatoslav Danyliv <sdanyliv@gmail.com>
2025-06-19 13:39:33 +0300

    Revert flag.

--- Source/LinqToDB/DataProvider/Oracle/OracleDataProvider.cs
+++ Source/LinqToDB/DataProvider/Oracle/OracleDataProvider.cs
@@ -40,7 +40,7 @@ namespace LinqToDB.DataProvider.Oracle
 			SqlProviderFlags.IsRowNumberWithoutOrderBySupported                    = false;
 			SqlProviderFlags.IsSubqueryWithParentReferenceInJoinConditionSupported = false;
 			SqlProviderFlags.SupportedCorrelatedSubqueriesLevel                    = 1;
-			SqlProviderFlags.IsColumnSubqueryShouldNotContainParentIsNotNull       = false;
+			SqlProviderFlags.IsColumnSubqueryShouldNotContainParentIsNotNull       = true;
 			SqlProviderFlags.IsColumnSubqueryWithParentReferenceAndTakeSupported   = version >= OracleVersion.v12;
 			SqlProviderFlags.IsDistinctFromSupported                               = true;
 			SqlProviderFlags.DoesProviderTreatsEmptyStringAsNull                   = true;
