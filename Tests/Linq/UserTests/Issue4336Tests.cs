9ff558bb4c9e1bb578844c573c1a15508f98af3e
MaceWindu <MaceWindu@users.noreply.github.com>
2025-06-24 12:18:00 +0200

    disable ifx due to database bug

--- Tests/Linq/UserTests/Issue4336Tests.cs
+++ Tests/Linq/UserTests/Issue4336Tests.cs
@@ -4,6 +4,8 @@ using System.Linq;
 
 using FluentAssertions;
 
+using IBM.Data.Db2;
+
 using LinqToDB;
 using LinqToDB.Mapping;
 using LinqToDB.SqlQuery;
@@ -15,6 +17,8 @@ namespace Tests.UserTests
 	[TestFixture]
 	public class Issue4336Tests : TestBase
 	{
+		// bug in informix
+		[ThrowsForProvider(typeof(DB2Exception), TestProvName.AllInformix, ErrorMessage = "ERROR [IX000] [IBM][IDS/UNIX64] Internal error in routine opjoin().")]
 		[Test]
 		public void Issue4336Test([DataSources(TestProvName.AllAccess)] string context)
 		{
