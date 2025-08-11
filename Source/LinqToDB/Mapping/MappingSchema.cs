5143e916b6f1dabfe94d3ce4b709fed28513c0f7
MaceWindu <MaceWindu@users.noreply.github.com>
2025-08-11 13:20:15 +0200

    fix build

--- Source/LinqToDB/Mapping/MappingSchema.cs
+++ Source/LinqToDB/Mapping/MappingSchema.cs
@@ -7,6 +7,7 @@ using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Numerics;
 using System.Reflection;
 using System.Text;
 using System.Threading;
