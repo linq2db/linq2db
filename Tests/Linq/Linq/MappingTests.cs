2db6ea74b40c19bc1c8e6de0d7340b1634b738fd
MaceWindu <MaceWindu@users.noreply.github.com>
2025-08-13 13:36:20 +0200

    fix merge

--- Tests/Linq/Linq/MappingTests.cs
+++ Tests/Linq/Linq/MappingTests.cs
@@ -1188,6 +1188,7 @@ namespace Tests.Linq
 
 #pragma warning disable CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
 #pragma warning disable CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
+			[ScalarType]
 			public struct TenderId
 #pragma warning restore CS0661 // Type defines operator == or operator != but does not override Object.GetHashCode()
 #pragma warning restore CS0660 // Type defines operator == or operator != but does not override Object.Equals(object o)
