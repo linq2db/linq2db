2503d4547d8948e19a78d9efaa6ca7bdc7a91689
MaceWindu <MaceWindu@users.noreply.github.com>
2025-09-09 14:56:25 +0200

    and first one too

--- Tests/Linq/Linq/EnumerableInQuery.cs
+++ Tests/Linq/Linq/EnumerableInQuery.cs
@@ -340,16 +340,16 @@ namespace Tests.Linq
 				{
 					new
 					{
+						ColorId     = (item.Color == null ? null : item.ColorId) ?? 0,
 						ColorName   = (string?)item.Color!.Name,
 						StyleName   = item.Style!.Name,
-						ColorId     = (item.Color == null ? null : item.ColorId) ?? 0,
 						Conditional = item.Color!.Name == "Red" ? itemsTable.Count() : 0,
 					},
 					new
 					{
+						ColorId     = (item.Color == null ? null : item.ColorId) ?? 0,
 						ColorName   = (string?)null,
 						StyleName   = item.Style!.Name,
-						ColorId     = (item.Color == null ? null : item.ColorId) ?? 0,
 						Conditional = 0,
 					},
 				}.DefaultIfEmpty()
