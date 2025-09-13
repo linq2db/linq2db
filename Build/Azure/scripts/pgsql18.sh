65e4d7e9bf1f24c701b2de0e360c0e296f1e7e80
MaceWindu <MaceWindu@users.noreply.github.com>
2025-09-13 18:21:56 +0200

    move to pgsql 18 rc1

--- Build/Azure/scripts/pgsql18.sh
+++ Build/Azure/scripts/pgsql18.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -d --name pgsql -e POSTGRES_PASSWORD=Password12! -p 5432:5432 -v /var/run/postgresql:/var/run/postgresql postgres:18beta2
+docker run -d --name pgsql -e POSTGRES_PASSWORD=Password12! -p 5432:5432 -v /var/run/postgresql:/var/run/postgresql postgres:18rc1
 
 retries=0
 until docker exec pgsql psql -U postgres -c '\l' | grep -q 'testdata'; do
