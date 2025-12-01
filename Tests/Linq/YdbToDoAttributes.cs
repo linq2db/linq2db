e949d92f00a520861b9c16a5f466773766155759
MaceWindu <MaceWindu@users.noreply.github.com>
2025-12-01 10:12:04 +0100

    fix build

--- Tests/Linq/YdbToDoAttributes.cs
+++ Tests/Linq/YdbToDoAttributes.cs
@@ -1,9 +1,10 @@
-﻿#if NET8_0_OR_GREATER
-using System;
+﻿using System;
 
 using LinqToDB;
 
+#if NET8_0_OR_GREATER
 using Ydb.Sdk.Ado;
+#endif
 
 namespace Tests
 {
@@ -30,7 +31,11 @@ namespace Tests
 	public sealed class YdbMemberNotFoundAttribute : ThrowsForProviderAttribute
 	{
 		public YdbMemberNotFoundAttribute()
+#if NET8_0_OR_GREATER
 			: base(typeof(YdbException),
+#else
+			: base(typeof(InvalidOperationException),
+#endif
 			ProviderName.Ydb)
 		{
 			ErrorMessage = "Error: Member not found";
@@ -40,7 +45,11 @@ namespace Tests
 	public sealed class YdbIntoValuesNotImplementedAttribute : ThrowsForProviderAttribute
 	{
 		public YdbIntoValuesNotImplementedAttribute()
+#if NET8_0_OR_GREATER
 			: base(typeof(YdbException),
+#else
+			: base(typeof(InvalidOperationException),
+#endif
 			ProviderName.Ydb)
 		{
 			ErrorMessage = "into_values_source: alternative is not implemented yet";
@@ -57,4 +66,3 @@ namespace Tests
 		}
 	}
 }
-#endif
