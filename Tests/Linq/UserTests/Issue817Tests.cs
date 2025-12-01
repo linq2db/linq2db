9bffc1acf13db3e2e4331fda69401b881fdfe7d6
MaceWindu <MaceWindu@users.noreply.github.com>
2025-12-01 18:35:04 +0100

    reenable test

--- Tests/Linq/UserTests/Issue817Tests.cs
+++ Tests/Linq/UserTests/Issue817Tests.cs
@@ -35,7 +35,6 @@ namespace Tests.UserTests
 			}
 		}
 
-		[ActiveIssue("https://github.com/ydb-platform/ydb/issues/22493", Configuration = ProviderName.Ydb)]
 		[Test]
 		public void TestUnorderedTakeSkip([DataSources] string context)
 		{
