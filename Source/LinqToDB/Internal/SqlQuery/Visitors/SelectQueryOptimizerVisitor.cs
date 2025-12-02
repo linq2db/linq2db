25742c70ef1ceef2e0d68f4012c003aa4f21df0a
MaceWindu <MaceWindu@users.noreply.github.com>
2025-12-02 22:23:00 +0100

    fix netfx build

--- Source/LinqToDB/Internal/SqlQuery/Visitors/SelectQueryOptimizerVisitor.cs
+++ Source/LinqToDB/Internal/SqlQuery/Visitors/SelectQueryOptimizerVisitor.cs
@@ -2259,7 +2259,7 @@ namespace LinqToDB.Internal.SqlQuery.Visitors
 									nestedWhereCond = subQuery.Where.EnsureConjunction();
 									subQuery.From.Table(t);
 
-									var tableSources = new HashSet<ISqlTableSource>(1) { t };
+									var tableSources = new HashSet<ISqlTableSource>() { t };
 									var foundFields  = new HashSet<ISqlExpression>();
 
 									QueryHelper.CollectDependencies(_rootElement, tableSources, foundFields, ignore: join.Table.Joins);
