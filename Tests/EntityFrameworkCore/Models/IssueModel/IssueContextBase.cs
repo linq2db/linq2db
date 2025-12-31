a13869c0f75b584f0ba5d4ccbad5092f3638af80
MaceWindu <MaceWindu@users.noreply.github.com>
2026-01-01 00:31:59 +0100

    update code

--- Tests/EntityFrameworkCore/Models/IssueModel/IssueContextBase.cs
+++ Tests/EntityFrameworkCore/Models/IssueModel/IssueContextBase.cs
@@ -371,10 +371,42 @@ namespace LinqToDB.EntityFrameworkCore.Tests.Models.IssueModel
 			Expression<Func<TEntity, bool>> CreateFilterExpression<TEntity>()
 					where TEntity : class
 			{
-				return e => !EF.Property<bool>(e, "IsDeleted") && EF.Property<Guid>(e, "TenantId") == Issue5267CurrentTenantId;
+				Expression<Func<TEntity, bool>> filter1 = e => !EF.Property<bool>(e, "IsDeleted");
+				Expression<Func<TEntity, bool>> filter2 = e => EF.Property<Guid>(e, "TenantId") == Issue5267CurrentTenantId;
+				return CombineExpressions(filter1, filter2);
+			}
+
+			static Expression<Func<T, bool>> CombineExpressions<T>(Expression<Func<T, bool>> expression1, Expression<Func<T, bool>> expression2)
+			{
+				var parameter = Expression.Parameter(typeof(T));
+
+				var leftVisitor = new ReplaceExpressionVisitor(expression1.Parameters[0], parameter);
+				var left = leftVisitor.Visit(expression1.Body);
+
+				var rightVisitor = new ReplaceExpressionVisitor(expression2.Parameters[0], parameter);
+				var right = rightVisitor.Visit(expression2.Body);
+
+				return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(left!, right!), parameter);
 			}
 		}
 
 		public Guid? Issue5267CurrentTenantId { get; set; }
+
+		private class ReplaceExpressionVisitor : ExpressionVisitor
+		{
+			private readonly Expression _oldValue;
+			private readonly Expression _newValue;
+
+			public ReplaceExpressionVisitor(Expression oldValue, Expression newValue)
+			{
+				_oldValue = oldValue;
+				_newValue = newValue;
+			}
+
+			public override Expression? Visit(Expression? node)
+			{
+				return node == _oldValue ? _newValue : base.Visit(node);
+			}
+		}
 	}
 }
