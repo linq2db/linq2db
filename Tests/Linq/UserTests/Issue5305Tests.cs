ba1a468853afa22c372eac38326e3b9acb445d82
Svyatoslav Danyliv <sdanyliv@gmail.com>
2026-01-19 22:24:24 +0200

    Fixed column usage collector.

---
+++ Tests/Linq/UserTests/Issue5305Tests.cs
@@ -0,0 +1,75 @@
+﻿using System.Linq;
+
+using LinqToDB;
+using LinqToDB.Mapping;
+
+using NUnit.Framework;
+
+namespace Tests.UserTests
+{
+	public class Issue5305Tests : TestBase
+	{
+		[Table("Lines")]
+		public class Line
+		{
+			[PrimaryKey, Identity] public int     Id     { get; set; }
+			[Column]               public string  Order  { get; set; } = "";
+			[Column]               public decimal Amount { get; set; }
+		}
+
+		[Table("InvoiceLines")]
+		public class InvoiceLine
+		{
+			[PrimaryKey, Identity] public int     Id     { get; set; }
+			[Column]               public string  Order  { get; set; } = "";
+			[Column]               public decimal Amount { get; set; }
+
+			// If you comment this out so they have the same amount of columns it works again
+			[Column] public string ExtraColumnSoTheyDontAlign { get; set; } = "";
+		}
+
+		// The DTO both sides of UNION project to
+		public class Row
+		{
+			public string  Order  { get; set; } = "";
+			public decimal Amount { get; set; }
+		}
+
+		[Test]
+		public void CountInUnionAll([IncludeDataSources(TestProvName.AllSQLite)] string context)
+		{
+			using var db = GetDataContext(context);
+			using var lineTable        = db.CreateLocalTable<Line>();
+			using var invoiceLineTable = db.CreateLocalTable<InvoiceLine>();
+
+			db.Insert(new Line { Amount        = 10m });
+			db.Insert(new InvoiceLine { Amount = 5m });
+
+			var queryable =
+				db.GetTable<Line>()
+					.Select(o => new Row
+					{
+						Order  = o.Id.ToString(),
+						Amount = o.Amount
+					})
+					.UnionAll(
+						db.GetTable<InvoiceLine>()
+							.Select(x => new Row
+							{
+								Order  = x.Order,
+								Amount = x.Amount,
+							})
+					);
+
+			var pagedQuery =
+				queryable.Select(x => new
+					{
+						Value      = x,
+						TotalCount = queryable.Count(), // <-- triggers the malformed COUNT-subquery
+					})
+					.Skip(0).Take(10);
+
+			var result = pagedQuery.ToList();
+		}
+	}
+}
