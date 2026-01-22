eb632cb9a3ba75a598436a07f828dd48eea202d3
MaceWindu <MaceWindu@users.noreply.github.com>
2026-01-22 16:46:39 +0100

    add mapping tests

--- Tests/Linq/Linq/OperatorsTests.cs
+++ Tests/Linq/Linq/OperatorsTests.cs
@@ -3,10 +3,15 @@
 #pragma warning disable CA1046 // Do not overload equality operator on reference types
 using System;
 using System.Linq;
+using System.Linq.Expressions;
 
 using LinqToDB;
 using LinqToDB.Data;
+using LinqToDB.Internal.DataProvider.Translation;
+using LinqToDB.Linq;
+using LinqToDB.Linq.Translation;
 using LinqToDB.Mapping;
+using LinqToDB.SqlQuery;
 
 using NUnit.Framework;
 
@@ -17,6 +22,7 @@ namespace Tests.Linq
 	{
 		// 1. remote disabled as we don't have serialization configured for custom types
 		// 2. tests don't put operator mappings on custom types directly to ensure we can map 3rd-party code
+		// 3. test different mapping approaches except Expressions.Map* APIs as they are obsoleted
 		const bool WITH_REMOTE = false;
 
 		struct CustomInt
@@ -33,7 +39,11 @@ namespace Tests.Linq
 			public static bool operator ==(CustomInt? left, int right) => left?.Value == right;
 			public static bool operator !=(CustomInt? left, int right) => left?.Value != right;
 
+			public static CustomInt operator +(CustomInt left, int right) => new CustomInt() { Value = left.Value + right };
+			public static CustomInt operator *(CustomInt left, int right) => new CustomInt() { Value = left.Value * right };
+
 			public static CustomInt operator -(CustomInt value) => new CustomInt() { Value = -value.Value };
+			public static CustomInt? operator -(CustomInt? value) => value == null ? null : -value.Value;
 		}
 
 		sealed class CustomIntClass
@@ -46,6 +56,9 @@ namespace Tests.Linq
 
 			public static bool operator ==(CustomIntClass? left, int right) => left?.Value == right;
 			public static bool operator !=(CustomIntClass? left, int right) => left?.Value != right;
+
+			public static CustomIntClass operator +(CustomIntClass left, int right) => new CustomIntClass() { Value = left.Value + right };
+			public static CustomIntClass operator *(CustomIntClass left, int right) => new CustomIntClass() { Value = left.Value * right };
 
 			public static CustomIntClass? operator -(CustomIntClass? value) => value == null ? null : new CustomIntClass() { Value = -value.Value };
 		}
@@ -98,6 +111,163 @@ namespace Tests.Linq
 			using var tb = db.CreateLocalTable(OperatorTable.Data);
 
 			var value = -2;
+			var res = tb.Where(r => -r.Field == value && -r.FieldN == value && -r.FieldClass == value).ToList();
+
+			Assert.That(res, Has.Count.EqualTo(1));
+			Assert.That(res[0].Id, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void BinaryOperator_Mapped_SqlExpression([DataSources(WITH_REMOTE)] string context)
+		{
+			using var db = GetDataContext(context, SetupMapping(fb =>
+			{
+				// operator== : (L + 3) == R
+				fb.Entity<CustomInt>().Member(v => v == 3).HasAttribute(new Sql.ExpressionAttribute("({0} + 3) = {1}") { IsPredicate = true });
+				fb.Entity<CustomInt?>().Member(v => v == 3).HasAttribute(new Sql.ExpressionAttribute("({0} + 3) = {1}") { IsPredicate = true });
+				fb.Entity<CustomIntClass>().Member(v => v == 3).HasAttribute(new Sql.ExpressionAttribute("({0} + 3) = {1}") { IsPredicate = true });
+			}));
+			using var tb = db.CreateLocalTable(OperatorTable.Data);
+
+			var value = 5;
+			var res = tb.Where(r => r.Field == value && r.FieldN == value && r.FieldClass == value).ToList();
+
+			Assert.That(res, Has.Count.EqualTo(1));
+			Assert.That(res[0].Id, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void UnaryOperator_Mapped_SqlExpression([DataSources(WITH_REMOTE)] string context)
+		{
+			using var db = GetDataContext(context, SetupMapping(fb =>
+			{
+				// operator- : v*3
+				fb.Entity<CustomInt>().Member(v => -v).HasAttribute(new Sql.ExpressionAttribute("3 * {0}") { Precedence = Precedence.Multiplicative });
+				fb.Entity<CustomInt?>().Member(v => -v).HasAttribute(new Sql.ExpressionAttribute("3 * {0}") { Precedence = Precedence.Multiplicative });
+				fb.Entity<CustomIntClass>().Member(v => -v).HasAttribute(new Sql.ExpressionAttribute("3 * {0}") { Precedence = Precedence.Multiplicative });
+			}));
+			using var tb = db.CreateLocalTable(OperatorTable.Data);
+
+			var value = 6;
+			var res = tb.Where(r => -r.Field == value && -r.FieldN == value && -r.FieldClass == value).ToList();
+
+			Assert.That(res, Has.Count.EqualTo(1));
+			Assert.That(res[0].Id, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void BinaryOperator_Mapped_MemberExpression([DataSources(WITH_REMOTE)] string context)
+		{
+			using var db = GetDataContext(context, SetupMapping(fb =>
+			{
+				// operator== : (L + 3) == R
+				// Equals used intead of == to avoid infinite recursion
+				fb.Entity<CustomInt>().Member(v => v == 3).HasAttribute(new ExpressionMethodAttribute((Expression<Func<CustomInt, int, bool>>)((l, r) => (l + 3).Equals(r))));
+				fb.Entity<CustomInt?>().Member(v => v == 3).HasAttribute(new ExpressionMethodAttribute((Expression<Func<CustomInt?, int, bool>>)((l, r) => l != null && ((l + 3).Equals(r)))));
+				fb.Entity<CustomIntClass>().Member(v => v == 3).HasAttribute(new ExpressionMethodAttribute((Expression<Func<CustomIntClass, int, bool>>)((l, r) => (l + 3).Equals(r))));
+			}));
+			using var tb = db.CreateLocalTable(OperatorTable.Data);
+
+			var value = 5;
+			var res = tb.Where(r => r.Field == value && r.FieldN == value && r.FieldClass == value).ToList();
+
+			Assert.That(res, Has.Count.EqualTo(1));
+			Assert.That(res[0].Id, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void UnaryOperator_Mapped_MemberExpression([DataSources(WITH_REMOTE)] string context)
+		{
+			using var db = GetDataContext(context, SetupMapping(fb =>
+			{
+				// operator- : v*3
+				fb.Entity<CustomInt>().Member(v => -v).HasAttribute(new ExpressionMethodAttribute((Expression<Func<CustomInt, CustomInt>>)(v => v * 3)));
+				fb.Entity<CustomInt?>().Member(v => -v).HasAttribute(new ExpressionMethodAttribute((Expression<Func<CustomInt?, CustomInt?>>)(v => v * 3)));
+				fb.Entity<CustomIntClass>().Member(v => -v).HasAttribute(new ExpressionMethodAttribute((Expression<Func<CustomIntClass, CustomIntClass>>)(v => v * 3)));
+			}));
+			using var tb = db.CreateLocalTable(OperatorTable.Data);
+
+			var value = 6;
+			var res = tb.Where(r => -r.Field == value && -r.FieldN == value && -r.FieldClass == value).ToList();
+
+			Assert.That(res, Has.Count.EqualTo(1));
+			Assert.That(res[0].Id, Is.EqualTo(2));
+		}
+
+		sealed class BinaryOperatorsMemberTranslator : MemberTranslatorBase
+		{
+			public static readonly IMemberTranslator Instance = new BinaryOperatorsMemberTranslator();
+
+			public BinaryOperatorsMemberTranslator()
+			{
+				Registration.RegisterBinaryOperator((CustomInt left, int right) => left == right, TranslateEquals);
+				Registration.RegisterBinaryOperator((CustomInt? left, int right) => left == right, TranslateEquals);
+				Registration.RegisterBinaryOperator((CustomIntClass left, int right) => left == right, TranslateEquals);
+			}
+
+			private Expression? TranslateEquals(ITranslationContext translationContext, BinaryExpression binaryExpression, TranslationFlags translationFlags)
+			{
+				if (translationContext.CanBeEvaluatedOnClient(binaryExpression.Left) && translationContext.CanBeEvaluatedOnClient(binaryExpression.Right))
+					return null;
+
+				if (!translationContext.TranslateToSqlExpression(binaryExpression.Left, out var leftExpr))
+					return translationContext.CreateErrorExpression(binaryExpression.Left, type: binaryExpression.Method!.DeclaringType);
+				if (!translationContext.TranslateToSqlExpression(binaryExpression.Right, out var rightExpr))
+					return translationContext.CreateErrorExpression(binaryExpression.Right, type: binaryExpression.Method!.DeclaringType);
+
+				var factory = translationContext.ExpressionFactory;
+				var dbType  = factory.GetDbDataType(typeof(bool));
+
+				return translationContext.CreatePlaceholder(translationContext.CurrentSelectQuery, factory.SearchCondition().Add(factory.Equal(factory.Add(factory.GetDbDataType(leftExpr), leftExpr, factory.Value(3)), rightExpr)), binaryExpression);
+			}
+		}
+
+		sealed class UnaryOperatorsMemberTranslator : MemberTranslatorBase
+		{
+			public static readonly IMemberTranslator Instance = new UnaryOperatorsMemberTranslator();
+
+			public UnaryOperatorsMemberTranslator()
+			{
+				Registration.RegisterUnaryOperator((CustomInt value) => -value, TranslateNegate);
+				Registration.RegisterUnaryOperator((CustomInt? value) => -value, TranslateNegate);
+				Registration.RegisterUnaryOperator((CustomIntClass value) => -value, TranslateNegate);
+			}
+
+			private Expression? TranslateNegate(ITranslationContext translationContext, UnaryExpression unaryExpression, TranslationFlags translationFlags)
+			{
+				if (translationContext.CanBeEvaluatedOnClient(unaryExpression.Operand))
+					return null;
+
+				if (!translationContext.TranslateToSqlExpression(unaryExpression.Operand, out var operandExpr))
+					return translationContext.CreateErrorExpression(unaryExpression.Operand, type: unaryExpression.Method!.DeclaringType);
+
+				var factory = translationContext.ExpressionFactory;
+				var dbType  = factory.GetDbDataType(operandExpr);
+
+				return translationContext.CreatePlaceholder(translationContext.CurrentSelectQuery, factory.Multiply(dbType, operandExpr, factory.Value(3)), unaryExpression);
+			}
+		}
+
+		[Test]
+		public void BinaryOperator_Mapped_MemberTranslator([DataSources(WITH_REMOTE)] string context)
+		{
+			using var db = GetDataContext(context, o => o.UseMappingSchema(SetupMapping()).UseMemberTranslator(BinaryOperatorsMemberTranslator.Instance));
+			using var tb = db.CreateLocalTable(OperatorTable.Data);
+
+			var value = 5;
+			var res = tb.Where(r => r.Field == value && r.FieldN == value && r.FieldClass == value).ToList();
+
+			Assert.That(res, Has.Count.EqualTo(1));
+			Assert.That(res[0].Id, Is.EqualTo(2));
+		}
+
+		[Test]
+		public void UnaryOperator_Mapped_MemberTranslator([DataSources(WITH_REMOTE)] string context)
+		{
+			using var db = GetDataContext(context, o => o.UseMappingSchema(SetupMapping()).UseMemberTranslator(UnaryOperatorsMemberTranslator.Instance));
+			using var tb = db.CreateLocalTable(OperatorTable.Data);
+
+			var value = 6;
 			var res = tb.Where(r => -r.Field == value && -r.FieldN == value && -r.FieldClass == value).ToList();
 
 			Assert.That(res, Has.Count.EqualTo(1));
