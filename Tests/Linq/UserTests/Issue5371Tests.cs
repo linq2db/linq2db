2166943fa5c59d0f8cfad4fe646356d3bb87f717
Svyatoslav Danyliv <sdanyliv@gmail.com>
2026-02-26 06:50:02 +0200

    Corner cases

--- Tests/Linq/UserTests/Issue5371Tests.cs
+++ Tests/Linq/UserTests/Issue5371Tests.cs
@@ -77,12 +77,76 @@ namespace Tests.UserTests
 			using var table = db.CreateLocalTable(testData);
 
 			var query = table
-				.Where(x => Sql.Expr<bool>($"LOWER({Sql.FieldExpr(x.Value)}) = LOWER({new DataParameter("@p1", "ONE", DataType.NVarChar)})"));
+				.Where(x => Sql.Expr<bool>($"UPPER({Sql.FieldExpr(x.Value)}) = UPPER({new DataParameter("@p1", "ONE", DataType.NVarChar)})"));
 
 			var parameters = query.GetStatement().CollectParameters();
 
 			parameters.Length.ShouldBe(1);
 			parameters[0].Name.ShouldBe("@p1");
+		}
+
+		[Test]
+		public void DataParameterNullName([IncludeDataSources(TestProvName.AllPostgreSQL)] string context)
+		{
+			var testData = new[]
+			{
+				new Item { Id = 1, Value = "ONE" },
+				new Item { Id = 2, Value = "two" },
+			};
+
+			using var db    = GetDataContext(context);
+			using var table = db.CreateLocalTable(testData);
+
+			var result = table
+				.Where(x => Sql.Expr<bool>($"LOWER({Sql.FieldExpr(x.Value)}) = LOWER({new DataParameter(null, "ONE", DataType.NVarChar)})"))
+				.ToArray();
+
+			result.Length.ShouldBe(1);
+			result[0].Id.ShouldBe(1);
+		}
+
+		[Test]
+		public void DataParameterEmptyName([IncludeDataSources(TestProvName.AllPostgreSQL)] string context)
+		{
+			var testData = new[]
+			{
+				new Item { Id = 1, Value = "ONE" },
+				new Item { Id = 2, Value = "two" },
+			};
+
+			using var db    = GetDataContext(context);
+			using var table = db.CreateLocalTable(testData);
+
+			var result = table
+				.Where(x => Sql.Expr<bool>($"LOWER({Sql.FieldExpr(x.Value)}) = LOWER({new DataParameter("", "ONE", DataType.NVarChar)})"))
+				.ToArray();
+
+			result.Length.ShouldBe(1);
+			result[0].Id.ShouldBe(1);
+		}
+
+		[Test]
+		public void DataParameterNullNameAndExplicitNameMixed([IncludeDataSources(TestProvName.AllPostgreSQL)] string context)
+		{
+			var testData = new[]
+			{
+				new Item { Id = 1, Value = "ONE" },
+				new Item { Id = 2, Value = "two" },
+				new Item { Id = 3, Value = "three" },
+			};
+
+			using var db    = GetDataContext(context);
+			using var table = db.CreateLocalTable(testData);
+
+			var result = table
+				.Where(x => Sql.Expr<bool>($"LOWER({Sql.FieldExpr(x.Value)}) = LOWER({new DataParameter("@p1", "ONE", DataType.NVarChar)})") ||
+				             Sql.Expr<bool>($"LOWER({Sql.FieldExpr(x.Value)}) = LOWER({new DataParameter(null, "two", DataType.NVarChar)})"))
+				.OrderBy(x => x.Id)
+				.ToArray();
+
+			result.Length.ShouldBe(2);
+			result[0].Id.ShouldBe(1);
+			result[1].Id.ShouldBe(2);
 		}
 	}
 }
