fb8c3441947becd39c70d70265d8d1d2cb000aa8
jkuehner <jochen.kuehner@kardex.com>
2026-02-28 11:44:25 +0100

    implement workin unionBy, exceptBy and intersectBy

--- Source/LinqToDB/Internal/Linq/Builder/SetOperationByBuilder.cs
+++ Source/LinqToDB/Internal/Linq/Builder/SetOperationByBuilder.cs
@@ -6,6 +6,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 
 using LinqToDB.Expressions;
+using LinqToDB.Internal.Common;
 using LinqToDB.Internal.Expressions;
 using LinqToDB.Internal.Reflection;
 
@@ -14,6 +15,14 @@ namespace LinqToDB.Internal.Linq.Builder
 	[BuildsMethodCall(nameof(Queryable.ExceptBy), nameof(Queryable.UnionBy), nameof(Queryable.IntersectBy))]
 	sealed class SetOperationByBuilder : MethodCallBuilder
 	{
+		private class UnionByTuple<T>
+		{
+#pragma warning disable CS8618
+			public T Data { get; set; }
+			public long RowNumber { get; set; }
+#pragma warning restore CS8618
+		}
+
 		public static bool CanBuildMethod(MethodCallExpression call)
 			=> call.IsSameGenericMethod(Methods.Enumerable.ExceptBy, Methods.Queryable.ExceptBy)
 			|| call.IsSameGenericMethod(Methods.Enumerable.UnionBy, Methods.Queryable.UnionBy)
@@ -21,107 +30,206 @@ namespace LinqToDB.Internal.Linq.Builder
 
 		protected override BuildSequenceResult BuildMethodCall(ExpressionBuilder builder, MethodCallExpression methodCall, BuildInfo buildInfo)
 		{
-			// Get the generic arguments
 			var sourceType = methodCall.Method.GetGenericArguments()[0];
-			var keyType = methodCall.Method.GetGenericArguments()[1];
+			var keyType    = methodCall.Method.GetGenericArguments()[1];
 
-			// Arguments: source, second, keySelector
 			var sourceExpression = methodCall.Arguments[0];
 			var secondExpression = methodCall.Arguments[1];
-			var keySelector = methodCall.Arguments[2].UnwrapLambda();
-
-			var methodName = methodCall.Method.Name;
+			var keySelector      = methodCall.Arguments[2].UnwrapLambda();
 
 			Expression transformedExpression;
 
-			if (methodName == "ExceptBy")
-			{
-				// source.ExceptBy(second, keySelector)
-				// → source.Where(x => !second.Distinct().Contains(keySelector(x)))
+			if (methodCall.Method.Name == "ExceptBy")
 				transformedExpression = BuildExceptBy(sourceExpression, secondExpression, keySelector, sourceType, keyType);
-			}
-			else if (methodName == "IntersectBy")
-			{
-				// source.IntersectBy(second, keySelector)
-				// → source.Where(x => second.Distinct().Contains(keySelector(x)))
+			else if (methodCall.Method.Name == "IntersectBy")
 				transformedExpression = BuildIntersectBy(sourceExpression, secondExpression, keySelector, sourceType, keyType);
-			}
-			else // UnionBy
-			{
-				// source.UnionBy(second, keySelector)
-				// → source.Concat(second).GroupBy(keySelector).Select(g => g.First())
-				transformedExpression = BuildUnionBy(sourceExpression, secondExpression, keySelector, sourceType, keyType);
-			}
+			else
+				transformedExpression = BuildUnionBy(sourceExpression, secondExpression, keySelector, sourceType);
 
 			return builder.TryBuildSequence(new BuildInfo(buildInfo, transformedExpression));
 		}
 
 		static Expression BuildExceptBy(Expression source, Expression second, LambdaExpression keySelector, Type sourceType, Type keyType)
 		{
-			// second is IEnumerable<TKey>, convert to IQueryable and apply Distinct
 			var asQueryableMethod = Methods.Queryable.AsQueryable.MakeGenericMethod(keyType);
-			var secondQueryable = Expression.Call(null, asQueryableMethod, second);
+			var secondAsQueryable = Expression.Call(null, asQueryableMethod, second);
 
 			var distinctMethod = Methods.Queryable.Distinct.MakeGenericMethod(keyType);
-			var distinctKeys = Expression.Call(null, distinctMethod, secondQueryable);
+			var distinctKeys   = Expression.Call(null, distinctMethod, secondAsQueryable);
 
-			// Create parameter for Where clause: x => !distinctKeys.Contains(keySelector(x))
-			var parameter = Expression.Parameter(sourceType, "x");
+			var parameter       = Expression.Parameter(sourceType, "x");
 			var keySelectorBody = keySelector.GetBody(parameter);
 
 			var containsMethod = Methods.Queryable.Contains.MakeGenericMethod(keyType);
-			var containsCall = Expression.Call(null, containsMethod, distinctKeys, keySelectorBody);
-			var notContains = Expression.Not(containsCall);
+			var containsCall   = Expression.Call(null, containsMethod, distinctKeys, keySelectorBody);
+			var notContains    = Expression.Not(containsCall);
 
 			var whereLambda = Expression.Lambda(notContains, parameter);
 			var whereMethod = Methods.Queryable.Where.MakeGenericMethod(sourceType);
+			var filtered = Expression.Call(null, whereMethod, source, Expression.Quote(whereLambda));
 
-			return Expression.Call(null, whereMethod, source, Expression.Quote(whereLambda));
+			var itemType = typeof(UnionByTuple<>).MakeGenericType(sourceType);
+
+			var partitionPart = ExpressionHelpers.CollectMembers(keySelectorBody).ToArray();
+			if (partitionPart.Length == 0)
+				partitionPart = [Expression.Constant(1)];
+
+			var orderByPart = partitionPart
+				.Select(p => (p, false))
+				.ToArray();
+
+			var rnCall = WindowFunctionHelpers.BuildRowNumber(partitionPart, orderByPart);
+
+			var selectBody = Expression.MemberInit(
+				Expression.New(itemType),
+				Expression.Bind(itemType.GetProperty(nameof(UnionByTuple<>.Data))!, parameter),
+				Expression.Bind(itemType.GetProperty(nameof(UnionByTuple<>.RowNumber))!, rnCall));
+
+			var selectLambda = Expression.Lambda(selectBody, parameter);
+			var selectCall   = Expression.Call(
+				null,
+				Methods.Queryable.Select.MakeGenericMethod(sourceType, itemType),
+				filtered,
+				Expression.Quote(selectLambda));
+
+			var tupleParam = Expression.Parameter(itemType, "x");
+			var rnFilter   = Expression.Equal(
+				Expression.PropertyOrField(tupleParam, nameof(UnionByTuple<>.RowNumber)),
+				Expression.Constant(1L));
+
+			var whereRnCall = Expression.Call(
+				null,
+				Methods.Queryable.Where.MakeGenericMethod(itemType),
+				selectCall,
+				Expression.Quote(Expression.Lambda(rnFilter, tupleParam)));
+
+			var resultParam = Expression.Parameter(itemType, "x");
+			var resultBody  = Expression.PropertyOrField(resultParam, nameof(UnionByTuple<>.Data));
+
+			return Expression.Call(
+				null,
+				Methods.Queryable.Select.MakeGenericMethod(itemType, sourceType),
+				whereRnCall,
+				Expression.Quote(Expression.Lambda(resultBody, resultParam)));
 		}
 
 		static Expression BuildIntersectBy(Expression source, Expression second, LambdaExpression keySelector, Type sourceType, Type keyType)
 		{
-			// second is IEnumerable<TKey>, convert to IQueryable and apply Distinct
 			var asQueryableMethod = Methods.Queryable.AsQueryable.MakeGenericMethod(keyType);
-			var secondQueryable = Expression.Call(null, asQueryableMethod, second);
+			var secondAsQueryable = Expression.Call(null, asQueryableMethod, second);
 
 			var distinctMethod = Methods.Queryable.Distinct.MakeGenericMethod(keyType);
-			var distinctKeys = Expression.Call(null, distinctMethod, secondQueryable);
+			var distinctKeys   = Expression.Call(null, distinctMethod, secondAsQueryable);
 
-			// Create parameter for Where clause: x => distinctKeys.Contains(keySelector(x))
-			var parameter = Expression.Parameter(sourceType, "x");
+			var parameter       = Expression.Parameter(sourceType, "x");
 			var keySelectorBody = keySelector.GetBody(parameter);
 
 			var containsMethod = Methods.Queryable.Contains.MakeGenericMethod(keyType);
-			var containsCall = Expression.Call(null, containsMethod, distinctKeys, keySelectorBody);
+			var containsCall   = Expression.Call(null, containsMethod, distinctKeys, keySelectorBody);
 
 			var whereLambda = Expression.Lambda(containsCall, parameter);
 			var whereMethod = Methods.Queryable.Where.MakeGenericMethod(sourceType);
+			var filtered = Expression.Call(null, whereMethod, source, Expression.Quote(whereLambda));
 
-			return Expression.Call(null, whereMethod, source, Expression.Quote(whereLambda));
+			var itemType = typeof(UnionByTuple<>).MakeGenericType(sourceType);
+
+			var partitionPart = ExpressionHelpers.CollectMembers(keySelectorBody).ToArray();
+			if (partitionPart.Length == 0)
+				partitionPart = [Expression.Constant(1)];
+
+			var orderByPart = partitionPart
+				.Select(p => (p, false))
+				.ToArray();
+
+			var rnCall = WindowFunctionHelpers.BuildRowNumber(partitionPart, orderByPart);
+
+			var selectBody = Expression.MemberInit(
+				Expression.New(itemType),
+				Expression.Bind(itemType.GetProperty(nameof(UnionByTuple<>.Data))!, parameter),
+				Expression.Bind(itemType.GetProperty(nameof(UnionByTuple<>.RowNumber))!, rnCall));
+
+			var selectLambda = Expression.Lambda(selectBody, parameter);
+			var selectCall   = Expression.Call(
+				null,
+				Methods.Queryable.Select.MakeGenericMethod(sourceType, itemType),
+				filtered,
+				Expression.Quote(selectLambda));
+
+			var tupleParam = Expression.Parameter(itemType, "x");
+			var rnFilter   = Expression.Equal(
+				Expression.PropertyOrField(tupleParam, nameof(UnionByTuple<>.RowNumber)),
+				Expression.Constant(1L));
+
+			var whereRnCall = Expression.Call(
+				null,
+				Methods.Queryable.Where.MakeGenericMethod(itemType),
+				selectCall,
+				Expression.Quote(Expression.Lambda(rnFilter, tupleParam)));
+
+			var resultParam = Expression.Parameter(itemType, "x");
+			var resultBody  = Expression.PropertyOrField(resultParam, nameof(UnionByTuple<>.Data));
+
+			return Expression.Call(
+				null,
+				Methods.Queryable.Select.MakeGenericMethod(itemType, sourceType),
+				whereRnCall,
+				Expression.Quote(Expression.Lambda(resultBody, resultParam)));
 		}
 
-		static Expression BuildUnionBy(Expression source, Expression second, LambdaExpression keySelector, Type sourceType, Type keyType)
+		static Expression BuildUnionBy(Expression source, Expression second, LambdaExpression keySelector, Type sourceType)
 		{
-			// UnionBy needs distinct elements by key from both sources
-			// Transform to: source.Concat(second).GroupBy(keySelector).Select(g => g.First())
+			var secondAsQueryable = Expression.Call(
+				Methods.Queryable.AsQueryable.MakeGenericMethod(sourceType),
+				second);
 
 			var concatMethod = Methods.Queryable.Concat.MakeGenericMethod(sourceType);
-			var concatenated = Expression.Call(concatMethod, source, second);
+			var concatenated = Expression.Call(concatMethod, source, secondAsQueryable);
 
-			// GroupBy(keySelector)
-			var groupByMethod = Methods.Queryable.GroupBy.MakeGenericMethod(sourceType, keyType);
-			var grouped = Expression.Call(groupByMethod, concatenated, Expression.Quote(keySelector));
+			var itemType   = typeof(UnionByTuple<>).MakeGenericType(sourceType);
+			var sourceItem = Expression.Parameter(sourceType, "x");
+			var keyBody    = keySelector.GetBody(sourceItem);
 
-			// Select(g => g.First())
-			var groupingType = typeof(IGrouping<,>).MakeGenericType(keyType, sourceType);
-			var groupParam = Expression.Parameter(groupingType, "g");
-			var firstMethod = Methods.Enumerable.First.MakeGenericMethod(sourceType);
-			var firstCall = Expression.Call(firstMethod, groupParam);
-			var selectLambda = Expression.Lambda(firstCall, groupParam);
+			var partitionPart = ExpressionHelpers.CollectMembers(keyBody).ToArray();
+			if (partitionPart.Length == 0)
+				partitionPart = [Expression.Constant(1)];
 
-			var selectMethod = Methods.Queryable.Select.MakeGenericMethod(groupingType, sourceType);
-			return Expression.Call(selectMethod, grouped, Expression.Quote(selectLambda));
+			var orderByPart = partitionPart
+				.Select(p => (p, false))
+				.ToArray();
+
+			var rnCall = WindowFunctionHelpers.BuildRowNumber(partitionPart, orderByPart);
+
+			var selectBody = Expression.MemberInit(
+				Expression.New(itemType),
+				Expression.Bind(itemType.GetProperty(nameof(UnionByTuple<>.Data))!, sourceItem),
+				Expression.Bind(itemType.GetProperty(nameof(UnionByTuple<>.RowNumber))!, rnCall));
+
+			var selectLambda = Expression.Lambda(selectBody, sourceItem);
+			var selectCall   = Expression.Call(
+				null,
+				Methods.Queryable.Select.MakeGenericMethod(sourceType, itemType),
+				concatenated,
+				Expression.Quote(selectLambda));
+
+			var tupleParam = Expression.Parameter(itemType, "x");
+			var whereBody  = Expression.Equal(
+				Expression.PropertyOrField(tupleParam, nameof(UnionByTuple<>.RowNumber)),
+				Expression.Constant(1L));
+
+			var whereCall = Expression.Call(
+				null,
+				Methods.Queryable.Where.MakeGenericMethod(itemType),
+				selectCall,
+				Expression.Quote(Expression.Lambda(whereBody, tupleParam)));
+
+			var resultParam = Expression.Parameter(itemType, "x");
+			var resultBody  = Expression.PropertyOrField(resultParam, nameof(UnionByTuple<>.Data));
+
+			return Expression.Call(
+				null,
+				Methods.Queryable.Select.MakeGenericMethod(itemType, sourceType),
+				whereCall,
+				Expression.Quote(Expression.Lambda(resultBody, resultParam)));
 		}
 	}
 }
