6376fc251dc1a8a4979fb7ce373a608d5d71b9e6
jkuehner <jochen.kuehner@kardex.com>
2026-02-28 12:27:17 +0100

    test cleanup

--- Tests/Linq/UserTests/Issue4412Tests.cs
+++ Tests/Linq/UserTests/Issue4412Tests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 using LinqToDB;
 using LinqToDB.Mapping;
@@ -38,7 +36,7 @@ namespace Tests.UserTests
 		{
 			using var db = GetDataContext(context);
 
-			using var testeeCoverageTable = db.CreateLocalTable(CreateTestTableData());
+			using var _ = db.CreateLocalTable(CreateTestTableData());
 			var result = db.GetTable<TestTable>().OrderBy(x => x.Id).DistinctBy(x => x.TestId).ToList();
 			var compareData = CreateTestTableData().OrderBy(x => x.Id).DistinctBy(x => x.TestId).ToList();
 			result.Count.ShouldBe(compareData.Count);
@@ -49,7 +47,7 @@ namespace Tests.UserTests
 		{
 			using var db = GetDataContext(context);
 
-			using var testeeCoverageTable = db.CreateLocalTable(CreateTestTableData());
+			using var _ = db.CreateLocalTable(CreateTestTableData());
 			var result = db.GetTable<TestTable>().MinBy(x => x.Id);
 			var compareData = CreateTestTableData().MinBy(x => x.Id);
 			result!.Id.ShouldBe(compareData!.Id);
@@ -60,7 +58,7 @@ namespace Tests.UserTests
 		{
 			using var db = GetDataContext(context);
 
-			using var testeeCoverageTable = db.CreateLocalTable(CreateTestTableData());
+			using var _ = db.CreateLocalTable(CreateTestTableData());
 			var result = db.GetTable<TestTable>().MaxBy(x => x.Id);
 			var compareData = CreateTestTableData().MaxBy(x => x.Id);
 			result!.Id.ShouldBe(compareData!.Id);
@@ -71,7 +69,7 @@ namespace Tests.UserTests
 		{
 			using var db = GetDataContext(context);
 
-			using var testeeCoverageTable = db.CreateLocalTable(CreateTestTableData());
+			using var _ = db.CreateLocalTable(CreateTestTableData());
 			var result = db.GetTable<TestTable>().ExceptBy(new[] { 20 }, x => x.TestId).ToList();
 			var compareData = CreateTestTableData().ExceptBy(new[] { 20 }, x => x.TestId).ToList();
 			result.Count.ShouldBe(compareData.Count);
@@ -82,7 +80,7 @@ namespace Tests.UserTests
 		{
 			using var db = GetDataContext(context);
 
-			using var testeeCoverageTable = db.CreateLocalTable(CreateTestTableData());
+			using var _ = db.CreateLocalTable(CreateTestTableData());
 			var result = db.GetTable<TestTable>().IntersectBy(new[] { 20, 30 }, x => x.TestId).ToList();
 			var compareData = CreateTestTableData().IntersectBy(new[] { 20, 30 }, x => x.TestId).ToList();
 			result.Count.ShouldBe(compareData.Count);
@@ -93,7 +91,7 @@ namespace Tests.UserTests
 		{
 			using var db = GetDataContext(context);
 
-			using var testeeCoverageTable = db.CreateLocalTable(CreateTestTableData());
+			using var _ = db.CreateLocalTable(CreateTestTableData());
 			var result = db.GetTable<TestTable>().UnionBy(db.GetTable<TestTable>(), x => x.TestId).ToList();
 			var compareData = CreateTestTableData().UnionBy(CreateTestTableData(), x => x.TestId).ToList();
 			result.Count.ShouldBe(compareData.Count);
@@ -104,7 +102,7 @@ namespace Tests.UserTests
 		{
 			using var db = GetDataContext(context);
 
-			using var testeeCoverageTable = db.CreateLocalTable(CreateTestTableData());
+			using var _ = db.CreateLocalTable(CreateTestTableData());
 			var result = db.GetTable<TestTable>().OrderBy(x => x.Id).Index().ToList();
 			var compareData = CreateTestTableData().OrderBy(x => x.Id).Index().ToList();
 			result.Count.ShouldBe(compareData.Count);
@@ -120,7 +118,7 @@ namespace Tests.UserTests
 		{
 			using var db = GetDataContext(context);
 
-			using var testeeCoverageTable = db.CreateLocalTable(CreateTestTableData());
+			using var _ = db.CreateLocalTable(CreateTestTableData());
 			var result = db.GetTable<TestTable>().CountBy(x => x.TestId).OrderBy(x => x.Key).ToList();
 			var compareData = CreateTestTableData().CountBy(x => x.TestId).OrderBy(x => x.Key).ToList();
 			result.Count.ShouldBe(compareData.Count);
@@ -137,7 +135,7 @@ namespace Tests.UserTests
 		//{
 		//	using var db = GetDataContext(context);
 
-		//	using var testeeCoverageTable = db.CreateLocalTable(CreateTestTableData());
+		//	using var _ = db.CreateLocalTable(CreateTestTableData());
 		//	var result = db.GetTable<TestTable>().AggregateBy(x => x.TestId, new List<string>(), );
 		//	var compareData = CreateTestTableData().AggregateBy(x => x.TestId);
 		//	result.ShouldBe(compareData);
