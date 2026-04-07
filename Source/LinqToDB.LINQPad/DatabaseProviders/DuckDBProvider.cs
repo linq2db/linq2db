9ca45b3617b608239276eb7e569891f446b994eb
MaceWindu <MaceWindu@users.noreply.github.com>
2026-04-08 00:50:26 +0200

    add LINQPad support

---
+++ Source/LinqToDB.LINQPad/DatabaseProviders/DuckDBProvider.cs
@@ -0,0 +1,29 @@
+﻿#if !NETFRAMEWORK
+using DuckDB.NET.Data;
+
+namespace LinqToDB.LINQPad;
+
+internal sealed class DuckDBProvider : DatabaseProviderBase
+{
+	private static readonly IReadOnlyList<ProviderInfo> _providers =
+	[
+		new(ProviderName.DuckDB, "DuckDB", true),
+	];
+
+	public DuckDBProvider()
+		: base(ProviderName.DuckDB, "DuckDB", _providers)
+	{
+	}
+
+	public override void ClearAllPools(string providerName)
+	{
+	}
+
+	public override DateTime? GetLastSchemaUpdate(ConnectionSettings settings) => null;
+
+	public override DbProviderFactory? GetProviderFactory(string providerName)
+	{
+		return DuckDBClientFactory.Instance;
+	}
+}
+#endif
