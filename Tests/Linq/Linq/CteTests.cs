e59031253e8539f93dba0e72603dbe5dae4bc2b0
Svyatoslav Danyliv <sdanyliv@gmail.com>
2026-04-12 14:21:26 +0300

    Disable Oracle tests.

--- Tests/Linq/Linq/CteTests.cs
+++ Tests/Linq/Linq/CteTests.cs
@@ -2842,7 +2842,7 @@ namespace Tests.Linq
 
 		// https://github.com/linq2db/linq2db/issues/5457
 		[Test]
-		public void RecursiveHierarchyWithJoinOnCteColumn([RecursiveCteContextSource(TestProvName.AllClickHouse)] string context)
+		public void RecursiveHierarchyWithJoinOnCteColumn([RecursiveCteContextSource(TestProvName.AllClickHouse, TestProvName.AllOracle)] string context)
 		{
 			using var db = GetDataContext(context);
 
