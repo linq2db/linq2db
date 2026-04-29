b3e15c0309cc90a4e921a7edd60571bea987c54c
Igor Tkachev <igor.tkachev@gmail.com>
2026-04-28 22:51:10 -0400

    tests: split OptimizeDuplicateParameters tests into dedicated file
    - move OptimizeDuplicateParameters_* tests out of ParameterTests.cs
    
    - add dedicated partial file ParameterTests.OptimizeDuplicateParameters.cs
    
    - keep SQL Server-specific coverage via IncludeDataSources
    
    - use DataSources with ClickHouse exclusion for cross-provider cases
    
    - update string/array scenarios to parameter-count tests

---
+++ Tests/Linq/Linq/ParameterTests.OptimizeDuplicateParameters.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+
+using LinqToDB;
+
+using NUnit.Framework;
+
+using Shouldly;
+
+namespace Tests.Linq
+{
+	public partial class ParameterTests
+	{
+		[Test]
+		public void OptimizeDuplicateParameters_ReusesSameLinqParameter([DataSources(TestProvName.AllClickHouse)] string context)
+		{
+			using var db = GetDataContext(context, o => o.UseOptimizeDuplicateParameters(true));
+
+			var value = 1;
+
+			var query = db.GetTable<ParameterDeduplication>()
+				.Where(t => t.Int1 == value || t.Int2 == value);
+
+			query.ToSqlQuery().Parameters.Count.ShouldBe(1);
+		}
+
+		[Test]
+		public void OptimizeDuplicateParameters_DefaultKeepsCurrentBehavior([DataSources(TestProvName.AllClickHouse)] string context)
+		{
+			using var db = GetDataContext(context, o => o.UseOptimizeDuplicateParameters(false));
+
+			var value = "str";
+
+			var query = db.GetTable<ParameterDeduplication>()
+				.Where(t => t.String1 == value || t.String2 == value);
+
+			query.ToSqlQuery().Parameters.Count.ShouldBe(2);
+		}
+
+		[Test]
+		public void OptimizeDuplicateParameters_CompareSqlWithAndWithoutOption([IncludeDataSources(TestProvName.AllSqlServer)] string context)
+		{
+			Issue404? usage = Issue404.Value1;
+			var allUsages = !usage.HasValue;
+
+			QuerySql sqlWithoutOptimization;
+			using (var db = GetDataContext(context, o => o.UseOptimizeDuplicateParameters(false)))
+			{
+				var query = db.GetTable<Table404Two>()
+					.Where(v => allUsages || v.Usage == usage.GetValueOrDefault());
+
+				sqlWithoutOptimization = query.ToSqlQuery();
+			}
+
+			QuerySql sqlWithOptimization;
+			using (var db = GetDataContext(context, o => o.UseOptimizeDuplicateParameters(true)))
+			{
+				var query = db.GetTable<Table404Two>()
+					.Where(v => allUsages || v.Usage == usage.GetValueOrDefault());
+
+				sqlWithOptimization = query.ToSqlQuery();
+			}
+
+			// This test validates parameter name consistency between optimization modes.
+			// It is not about duplicate count differences for this query shape.
+			//
+			sqlWithoutOptimization.Parameters.Count.ShouldBe(1);
+			sqlWithOptimization.   Parameters.Count.ShouldBe(1);
+			sqlWithoutOptimization.Sql.ShouldContain("@usage");
+			sqlWithOptimization.   Sql.ShouldContain("@usage");
+			sqlWithOptimization.   Sql.ShouldNotContain("@p");
+		}
+
+		[Test]
+		public void OptimizeDuplicateParameters_ReusesSameLinqParameter_String([DataSources(TestProvName.AllClickHouse)] string context)
+		{
+			using var db = GetDataContext(context, o => o.UseOptimizeDuplicateParameters(true));
+
+			var value = "str";
+
+			var query = db.GetTable<ParameterDeduplication>()
+				.Where(t => t.String2 == value || t.String3 == value);
+
+			query.ToSqlQuery().Parameters.Count.ShouldBe(1);
+		}
+
+		[Test]
+		public void OptimizeDuplicateParameters_UsesMultipleParameters_FromArrayElements([DataSources(TestProvName.AllClickHouse)] string context)
+		{
+			using var db = GetDataContext(context, o => o.UseOptimizeDuplicateParameters(true));
+
+			var values = new[] { "str", "str1" };
+
+			var query = db.GetTable<ParameterDeduplication>()
+				.Where(t => t.String2 == values[0] || t.String3 == values[1]);
+
+			query.ToSqlQuery().Parameters.Count.ShouldBe(2);
+		}
+
+		[Test]
+		public void OptimizeDuplicateParameters_DoesNotReuseDifferentColumnDataTypes([IncludeDataSources(TestProvName.AllSqlServer)] string context)
+		{
+			using var db = GetDataContext(context, o => o.UseOptimizeDuplicateParameters(true));
+
+			var value = "str";
+
+			var query = db.GetTable<ParameterDeduplication>()
+				.Where(t => t.String1 == value || t.String2 == value);
+
+			query.ToSqlQuery().Parameters.Count.ShouldBe(2);
+		}
+
+		[Test]
+		public void OptimizeDuplicateParameters_DoesNotReuseDifferentLinqParameters([DataSources(TestProvName.AllClickHouse)] string context)
+		{
+			using var db = GetDataContext(context, o => o.UseOptimizeDuplicateParameters(true));
+
+			var value1 = "str";
+			var value2 = "str";
+
+			var query = db.GetTable<ParameterDeduplication>()
+				.Where(t => t.String1 == value1 || t.String2 == value2);
+
+			query.ToSqlQuery().Parameters.Count.ShouldBe(2);
+		}
+	}
+}
